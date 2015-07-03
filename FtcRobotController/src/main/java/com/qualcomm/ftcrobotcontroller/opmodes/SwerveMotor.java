2d5f5048b5dfd290b759336f16d6b64f70e4c577
Steve Geffner <steve.geffner.phd@gmail.com>
2015-07-03 10:48:35 -0700

    Refactor SwerveMotor so loop() isn't talking directly to the SwerveMotor member variables.
    Former-commit-id: 348c5217c9c84b4f26f7096ec4410f93d6d14c1a
    Former-commit-id: 3b8b6db95ae7e7c5c96be875fdaa331e7eac6155

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SwerveMotor.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SwerveMotor.java
@@ -1,6 +1,7 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.util.LinkedList;
 
@@ -11,7 +12,7 @@ public class SwerveMotor {
 
     String name;
     private DcMotor motor;
-    public LinkedList<SwerveAction> queue = new LinkedList<SwerveAction>();
+    private LinkedList<SwerveAction> queue = new LinkedList<SwerveAction>();
 
     int actioncounter = 0;//for generating unique debugging names for actions
 
@@ -35,5 +36,60 @@ public class SwerveMotor {
     {
         queue.add( new SwerveActionDelay(Integer.toString(actioncounter++), targetDurationSeconds));
     }
+
+    public String ToString()
+    {
+        String message = "";
+
+        for (SwerveAction d : queue)
+        {
+            message += d.ToString() +"\n";
+        }
+        return message;
+    }
+
+    public int GetQueueLength()
+    {
+        return queue.size();
+    }
+
+    public void ClearQueue()
+    {
+        queue.clear();
+    }
+
+    public void Update(ElapsedTime currentTime)
+    {
+        if (queue.size() > 0)
+        {
+            //get the first item
+            SwerveAction a = queue.getFirst();
+            if (a!=null)  //don't really need this check since we looked at the queue size already
+            {
+                if (!a.IsStarted()) {
+                    a.Start(currentTime);
+                }
+                a.Update(currentTime);
+            }
+
+            //remove any done actions from the queue
+            //in c# you can't remove an item during an iterator because that messes up the iterator
+            //I'm not sure if java handles that better, but to be safer I'll remove after iterating
+
+            //To Do...since we only activate the top item in the queue,
+            //...perhaps we only need to check the first item in the list?
+            LinkedList<SwerveAction> removelist = new LinkedList<SwerveAction>();
+            for (SwerveAction d : queue)
+            {
+                if (d.IsDone()) removelist.add(d);
+            }
+            for (SwerveAction r : removelist)
+            {
+                queue.remove(r);
+            }
+
+        }
+    }
+
 
 }
