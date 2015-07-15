e16ca0831e23e275d32fcc57f34db9af81ab735d
RobertAtkinson <bob@theatkinsons.org>
2015-07-15 00:06:00 -0700

    update some comments

--- FtcRobotController/src/main/java/org/swerverobotics/library/examples/TeleOp.java
+++ FtcRobotController/src/main/java/org/swerverobotics/library/examples/TeleOp.java
@@ -21,6 +21,13 @@ public class TeleOp extends SynchronousOpMode
         this.motorLeft = this.hardwareMap.dcMotor.get("motorLeft");
         this.motorRight = this.hardwareMap.dcMotor.get("motorRight");
 
+        // Configure the knobs of the hardware according to how you've wired your
+        // robot. Here, we assume that there are no encoders connected to the motors,
+        // so we inform the motor objects of that fact.
+        this.motorLeft.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+        this.motorRight.setChannelMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+
+        // Enter an infinite loop processing all the input we receive
         for (;;)
             {
             if (this.gamePadInputAvailable())
@@ -47,7 +54,7 @@ public class TeleOp extends SynchronousOpMode
         {
         // Remember that the gamepad sticks range from -1 to +1, and that the motor
         // power levels range over the same amount
-        float ctlPower    = pad.left_stick_y();
+        float ctlPower    =  pad.left_stick_y();
         float ctlSteering = -pad.right_stick_x();
 
         // We're going to assume that the deadzone processing has been taken care of for us
@@ -56,8 +63,8 @@ public class TeleOp extends SynchronousOpMode
         // within the deadzone.
 
         // Map the power and steering to have more oomph at low values (optional)
-        ctlPower = this.mapDrivingPowerLevels(ctlPower);
-        ctlSteering = this.mapDrivingPowerLevels(ctlSteering);
+        ctlPower = this.xformDrivingPowerLevels(ctlPower);
+        ctlSteering = this.xformDrivingPowerLevels(ctlSteering);
 
         // Dampen power to avoid clipping so we can still effectively steer even
         // under heavy throttle.
@@ -81,17 +88,17 @@ public class TeleOp extends SynchronousOpMode
         this.motorRight.setPower(powerRight);
         }
 
-    float mapDrivingPowerLevels(float level)
+    float xformDrivingPowerLevels(float level)
+    // A useful thing to do in some robots is to map the power levels so that
+    // low power levels have more power than they otherwise would. This sometimes
+    // help give better driveability.
         {
-        // A useful thing to do in some robots is to map the power levels so that
-        // low power levels have more power than they otherwise would. This sometimes
-        // help give better driveability.
-
-        if (level == 0) return 0;
-
+        // We use a log function here as a simple way to transform the levels.
+        // You might want to try something different: perhaps construct a
+        // manually specified function using a table of values over which
+        // you interpolate.
         float zeroToOne = Math.abs(level);
         float oneToTen  = zeroToOne * 9 + 1;
-
         return (float)(Math.log10(oneToTen) * Math.signum(level));
         }
 
