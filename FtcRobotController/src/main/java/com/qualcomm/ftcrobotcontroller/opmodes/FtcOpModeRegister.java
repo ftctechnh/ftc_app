ca27cb90530b52612fcceb60feeb90826c26a233
RobertAtkinson <bob@theatkinsons.org>
2015-07-15 21:28:27 -0700

    revert all FTC-provided code back to original state

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -56,12 +56,11 @@ public class FtcOpModeRegister implements OpModeRegister {
      */
 
     manager.register("NullOp", NullOp.class);
-    //manager.register("K9TeleOp", K9TeleOp.class);
-    //manager.register("K9TankDrive", K9TankDrive.class);
-    //manager.register("K9Line", K9Line.class);
-    //manager.register("K9IrSeeker", K9IrSeeker.class);
-    //manager.register("K9AutoTime", K9AutoTime.class);
-    manager.register("ControllerTest",ControllerTest.class);
+    manager.register("K9TeleOp", K9TeleOp.class);
+    manager.register("K9TankDrive", K9TankDrive.class);
+    manager.register("K9Line", K9Line.class);
+    manager.register("K9IrSeeker", K9IrSeeker.class);
+    manager.register("K9AutoTime", K9AutoTime.class);
     /*
     manager.register("IrSeekerOp", IrSeekerOp.class);
     manager.register("CompassCalibration", CompassCalibration.class);
