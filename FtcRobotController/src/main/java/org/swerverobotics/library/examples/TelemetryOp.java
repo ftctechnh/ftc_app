b1fc962fb1808d9200986914da84be7b6604a136
RobertAtkinson <bob@theatkinsons.org>
2015-07-21 17:06:57 -0700

    comments

--- FtcRobotController/src/main/java/org/swerverobotics/library/examples/TelemetryOp.java
+++ FtcRobotController/src/main/java/org/swerverobotics/library/examples/TelemetryOp.java
@@ -12,9 +12,13 @@ public class TelemetryOp extends SynchronousOpMode
     {
     @Override protected void main() throws InterruptedException
         {
+        // Declare variables that will be used as our dashboard is updated. The variables
+        // are declared 'final' so that they will be accessible inside the item expressions
+        // of the dashboard.
         final ElapsedTime elapsed = new ElapsedTime();
         final int loopCountStart = loopCount.get();
 
+        // Configure the dashboard. Here, it will have one line, which will contain three items
         this.telemetry.dashboard.line
             (
             this.telemetry.dashboard.item("time: ",  new IFunc<Object>() { @Override public Object value()
@@ -31,25 +35,36 @@ public class TelemetryOp extends SynchronousOpMode
                 }})
             );
 
+        // Go go gadget robot!
         while (!this.stopRequested())
             {
             if (this.newGamePadInputAvailable())
                 {
+                // There is (likely) new gamepad input available.
+                
+                // Put out a log message if the left bumper is pressed
                 if (this.gamepad1.left_bumper())
                     {
                     this.telemetry.log.add(format(elapsed) + ": left bumper pressed");
                     }
 
+                // Emit telemetry with the freshest possible values
                 this.telemetry.dashboard.update();
                 }
             else
                 {
+                // There's no new gamepad input available.
+
+                // Emit any telemetry that hasn't been sent in a while
                 this.telemetry.dashboard.update();
+
+                // Let the rest of the system run until there's a stimulus from the robot controller runtime.
                 this.idle();
                 }
             }
         }
 
+    // A couple of handy functions for formatting data for the dashboard
     String format(ElapsedTime elapsed)
         {
         return String.format("%.1f", elapsed.time());
