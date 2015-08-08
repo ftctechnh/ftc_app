1dc651893d33df7c7a25f0b5e6333e9c604ab486
RobertAtkinson <bob@theatkinsons.org>
2015-08-08 12:42:22 -0700

    tidy action key management
    Former-commit-id: 33e8d225324153617c3fbc54db724c1e1b80713c
    Former-commit-id: 38a56b9579b75014bdd323cd170091138f2fb1ac

--- SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/internal/IActionKeyed.java
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/internal/IActionKeyed.java
@@ -1,11 +1,11 @@
 package org.swerverobotics.library.internal;
 
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * An interface for accessing administrative information associated with actions. 
  */
 public interface IActionKeyed
     {
-    ArrayList<Integer> getActionKeys();
+    List<Integer> getActionKeys();
     }
