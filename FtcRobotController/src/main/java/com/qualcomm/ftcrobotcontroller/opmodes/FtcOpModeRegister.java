1fac309c1779e05f890edf07ae89b751a95f2061
samohtj <thomas.jonathan.c@gmail.com>
2015-08-13 19:06:58 -0400

    Need this

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -82,6 +82,8 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("K9TeleOp", K9TeleOp.class);
     manager.register("K9IrSeeker", K9IrSeeker.class);
     manager.register("K9Line", K9Line.class);
+    manager.register("K9AutoTime", K9AutoTime.class);
+    manager.register("IrSeekerOp", IrSeekerOp.class);
 
     /*
      * The following example op modes are designed to work with a pushbot-style robot.
