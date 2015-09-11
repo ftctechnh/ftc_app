dd1799776c1f22060d78a4a3e89ae8990a67a6e2
Zach Ohara <zjohara@gmail.com>
2015-09-10 17:42:44 -0700

    Add documentation to DalvikCompiler

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DalvikCompiler.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DalvikCompiler.java
@@ -9,15 +9,36 @@ import java.util.List;
 import com.android.dx.cf.iface.ParseException;
 import com.android.dx.command.Main;
 
+/**
+ * The {@code DalvikCompiler} class contains static methods for compiling standard java
+ * jar files to Dalvik-compatible jar files. This allows the VM to search and instantiate
+ * classes in the jar later.
+ *
+ * @author Zach Ohara
+ */
 public class DalvikCompiler {
 
+    /**
+     * The sub-directory of the application's private data folder that is used to cache
+     * the Dalvik-compiled jar files. The actual directory doesn't matter at all, as long
+     * as the directory is not used for anything else.
+     */
     private static final String OUTPUT_TO = "/thunderbots/compiled";
 
+    /**
+     * Compiles the given list of jar files to Dalvik-compatible jar files, and stores the compiled
+     * jar files in a cache folder. As every jar file is compiled, its {@code File} in the list is
+     * changed to reflect its new location. Files that cannot be opened are logged to Logcat.
+     * <br>
+     * The files in {@code jarList} must all be .jar files as a prerequisite.
+     *
+     * @param jarList the list of jar files to compile.
+     */
     public static void convertJars(List<File> jarList) {
         for (int i = 0; i < jarList.size(); i++) {
             File f = jarList.get(i);
-            System.out.println("in: " + f);
-            System.out.println("out: " + outputFile(f));
+            //System.out.println("in: " + f);
+            //System.out.println("out: " + outputFile(f));
             List<String> args = new ArrayList<String>();
             args.add("--dex");
             args.add("--output=" + outputFile(f).getAbsolutePath() + "");
@@ -32,6 +53,12 @@ public class DalvikCompiler {
         }
     }
 
+    /**
+     * Generates a file path for the compiled and cached jar from the given jar file.
+     *
+     * @param inputFile the jar file to generate a cache file path for.
+     * @return the cache file path for the given jar.
+     */
     private static File outputFile(File inputFile) {
         String inputName = inputFile.getName();
         inputName = inputName.replace(" ", "-");
@@ -43,6 +70,12 @@ public class DalvikCompiler {
         return new File(output, "compiled-" + inputFile.getName());
     }
 
+    /**
+     * Removes all the non-jar files from the given list of {@code File} objects.
+     *
+     * @param fileList a list of file objects.
+     * @return the list of file objects, but with all non-jar files removed.
+     */
     public static List<File> getJarList(List<File> fileList) {
         for (int i = fileList.size() - 1; i >= 0; i--) {
             if (!fileList.get(i).getName().endsWith(".jar")) {
