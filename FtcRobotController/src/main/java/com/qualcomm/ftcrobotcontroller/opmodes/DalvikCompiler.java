401f62781b5b0de4fc6a05c46806b7e77c11f13b
Zach Ohara <zjohara@gmail.com>
2015-09-11 22:41:05 -0700

    Modify copyright headers

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DalvikCompiler.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DalvikCompiler.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2015 Thunderbots-5604
+/* Copyright (C) 2015 Thunderbots Robotics and Zach Ohara
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
