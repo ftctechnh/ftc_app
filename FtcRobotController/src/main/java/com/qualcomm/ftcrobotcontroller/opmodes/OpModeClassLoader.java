d5d7fc7f19e61f5711a857230e22cbf51dd04573
Zach Ohara <zjohara@gmail.com>
2015-09-11 21:13:47 -0700

    Add documentation for OpModeClassLoader

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OpModeClassLoader.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OpModeClassLoader.java
@@ -17,6 +17,12 @@ import java.util.List;
 import dalvik.system.DexClassLoader;
 import dalvik.system.DexFile;
 
+/**
+ * The {@code OpModeClassLoader} class is responsible for loading class files into instantiated
+ * objects.
+ *
+ * @author Zach Ohara
+ */
 public class OpModeClassLoader {
 
     private static ClassLoader classLoader;
@@ -28,7 +34,6 @@ public class OpModeClassLoader {
 
     public static List<Class<? extends OpMode>> loadJars(List<File> filelist) {
         URL[] jarurls = getJarURLs(filelist);
-        //classLoader = new URLClassLoader(jarurls, OpModeClassLoader.class.getClassLoader());
         classLoader = getClassLoader(jarurls);
         Thread.currentThread().setContextClassLoader(classLoader);
         opModeList = new ArrayList<Class<? extends OpMode>>();
@@ -42,7 +47,6 @@ public class OpModeClassLoader {
         return opModeList;
     }
 
-    // experimental
     private static ClassLoader getClassLoader(URL[] jarurls) {
         String pathString = getDelimitedPathString(jarurls);
         File cacheFile = new File(FtcRobotControllerActivity.getPrivateFilesDir(), "/thunderbots/");
@@ -50,9 +54,16 @@ public class OpModeClassLoader {
         String cacheDir = cacheFile.toString();
         ClassLoader parentLoader = OpModeClassLoader.class.getClassLoader();
         return new DexClassLoader(pathString, cacheDir, null, parentLoader);
-        //return new PathClassLoader(pathString, parentLoader);
     }
 
+    /**
+     * Returns a single string that contains the value of toString() for every object given
+     * in the array, delimited by {@code java.io.File#pathSeperator}
+     *
+     * @param arr the array of objects.
+     * @param <T> the type of the array. This doesn't really matter at all.
+     * @return the concatenation of o.toString() for every object o in arr.
+     */
     private static <T> String getDelimitedPathString(T[] arr) {
         String result = "";
         for (T obj : arr){
@@ -62,41 +73,35 @@ public class OpModeClassLoader {
         return result.substring(1);
     }
 
-    /*
+    /**
+     * Loads the classes contained in a given jar file. The jar file must have already been converted
+     * to a dalvik-compatible form.
+     *
+     * @param jarfile the file to load classes from.
+     * @throws IOException if an IOException is thrown by the underlying class loading system.
+     */
     private static void loadJarFile(File jarfile) throws IOException {
-        RobotLog.i("[Thunderbots] Now opening " + jarfile);
-        JarFile jarobj = new JarFile(jarfile);
-        Enumeration<JarEntry> jarentries = jarobj.entries();
-        while (jarentries.hasMoreElements()) {
-            JarEntry entry = jarentries.nextElement();
-            if (entry.getName().endsWith(".class")) {
-                attemptLoadClass(entry.getName());
-            }
-        }
-        jarobj.close();
-    }
-    */
-
-    private static void loadJarFile(File jarfile) throws IOException {
-        //Log.d(LOG_TAG, "Now opening " + jarfile);
-        //DexFile jarobj = new DexFile(jarfile);
         File cache = new File(FtcRobotControllerActivity.getPrivateFilesDir(), "/thunderbots/temp");
         DexFile jarobj = DexFile.loadDex(jarfile.getAbsolutePath(), cache.getAbsolutePath(), 0);
         Enumeration<String> jarentries = jarobj.entries();
         while (jarentries.hasMoreElements()) {
             String entry = jarentries.nextElement();
-            //Log.d(LOG_TAG, "Examining entry: " + entry);
-            //if (entry.endsWith(".class")) {
             attemptLoadClass(entry);
-            //}
         }
         jarobj.close();
     }
 
+    /**
+     * Attempts to load a class with the given fully-qualified name. Any exception thrown from
+     * within this method will be caught and handled (either ignored or logged).
+     * <br>
+     * The class will be loaded by the {@code classLoader} class variable. If the loaded class is
+     * a valid, instantiable {@code OpMode}, it will be added to the {@code opModeList} class
+     * variable.
+     *
+     * @param classname the fully-qualified name of the class to be resolved.
+     */
     private static void attemptLoadClass(String classname) {
-        //String classname = entryname;//.replace('/', '.');
-        //classname = classname.substring(0, classname.length() - 6);
-        //Log.d(LOG_TAG, "Checking class " + classname);
         try {
             Class<?> c = classLoader.loadClass(classname);
             Object instance = c.newInstance();
@@ -124,23 +129,40 @@ public class OpModeClassLoader {
         return jarList.toArray(new URL[jarList.size()]);
     }
 
+    /**
+     * Gets a list of all the files contained within the app's directory, usually "sdcard/FIRST/"
+     *
+     * @return a list of all files in the apps directory.
+     */
     public static List<File> getFileSet() {
         List<File> fileList = new ArrayList<File>();
         getFilesInDirectory(getTargetDirectory(), fileList);
         return fileList;
     }
 
+    /**
+     * Recursively browses the given directory and builds a list that contains every file
+     * in the given directory.
+     *
+     * @param current the directory to look for files in.
+     * @param fileList the list of all files in the given directory. This list should be
+     * empty when 
+     */
     private static void getFilesInDirectory(File current, List<File> fileList) {
         for (File f : current.listFiles()) {
             if (f.isFile()) {
                 fileList.add(f);
-                //RobotLog.d("[Thunderbots] Found file: " + f);
             } else if (f.isDirectory()) {
                 getFilesInDirectory(f, fileList);
             }
         }
     }
 
+    /**
+     * Gets the {@code File} object for the directory that should be searched for jar files.
+     *
+     * @return the {@code File} to search.
+     */
     private static File getTargetDirectory() {
         File sdcard = Environment.getExternalStorageDirectory();
         return new File(sdcard, FILE_LOCATION);
