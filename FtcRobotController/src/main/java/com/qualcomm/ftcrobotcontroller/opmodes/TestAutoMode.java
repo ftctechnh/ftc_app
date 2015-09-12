6746d875a3ff92ab2143c1b64d5099493d4536aa
JackVenberg <jvenberg@gmail.com>
2015-09-12 13:31:56 -0700

    Added TestOpMode and TestAutoMode

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestAutoMode.java
@@ -0,0 +1,13 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by JackV on 9/12/15.
+ */
+public class TestAutoMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestOpMode.java
@@ -0,0 +1,18 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by JackV on 9/12/15.
+ */
+public class TestOpMode extends OpMode{
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
