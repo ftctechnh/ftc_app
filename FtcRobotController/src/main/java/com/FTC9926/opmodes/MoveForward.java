dbcd3a49e6ec94500e8ef087e9ffa40992ebbb14
Ignacio Bravo <ibravo@hotmail.com>
2015-09-19 08:47:00 -0400

    Added 2 programs based on another teams samples from github

---
+++ FtcRobotController/src/main/java/com/FTC9926/opmodes/MoveForward.java
@@ -0,0 +1,38 @@
+package com.FTC9926.opmodes;
+
+  import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+  import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by ibravo on 9/19/15.
+ * based on team FTC 7123
+ */
+public class MoveForward extends OpMode{
+
+    DcMotor rightMotor;
+    DcMotor leftMotor;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start() {
+        hardwareMap.dcMotor.get("RightDriveMotor");
+        hardwareMap.dcMotor.get("LeftDriveMotor");
+
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        rightMotor.setPower(gamepad1.right_stick_y);
+        leftMotor.setPower(gamepad1.left_stick_y);
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
