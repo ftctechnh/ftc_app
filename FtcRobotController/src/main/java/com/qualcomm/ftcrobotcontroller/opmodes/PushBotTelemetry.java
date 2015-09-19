43f018c0bdc78ec1d9b25fb7f155b7423afe10a5
Jeffrey Lavezzo <jlavezzo@blackhawkenterprise.com>
2015-09-19 17:44:58 -0400

    fixed untracked files

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotTelemetry.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotTelemetry.java
@@ -1,9 +1,5 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-//------------------------------------------------------------------------------
-//
-// PushBotTelemetry
-//
 /**
  * Extends the PushBotHardware class to provide basic telemetry for the Push
  * Bot.
@@ -11,44 +7,19 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  * @author SSI Robotics
  * @version 2015-08-02-13-57
  */
-public class PushBotTelemetry extends PushBotHardware
-
-{
-    //--------------------------------------------------------------------------
-    //
-    // update_telemetry
-    //
+public class PushBotTelemetry extends PushBotHardware {
     /**
      * Update the telemetry with current values from the base class.
      */
-    public void update_telemetry ()
-
-    {
-        //
+    public void update_telemetry() {
         // Send telemetry data to the driver station.
-        //
-        telemetry.addData
-            ( "01"
-            , "Left Drive: "
-                + a_left_drive_power ()
-                + ", "
-                + a_left_encoder_count ()
-            );
-        telemetry.addData
-            ( "02"
-            , "Right Drive: "
-                + a_right_drive_power ()
-                + ", "
-                + a_right_encoder_count ()
-            );
-        telemetry.addData
-            ( "03"
-            , "Left Arm: " + a_left_arm_power ()
-            );
-        telemetry.addData
-            ( "04"
-            , "Hand Position: " + a_hand_position ()
-            );
-    } // PushBotTelemetry::loop
-
-} // PushBotTelemetry
+        telemetry.addData ("01", "Left Drive: "
+                + a_left_drive_power() + ", "
+                + a_left_encoder_count());
+        telemetry.addData("02", "Right Drive: "
+                + a_right_drive_power() + ", "
+                + a_right_encoder_count());
+        telemetry.addData("03", "Left Arm: " + a_left_arm_power());
+        telemetry.addData("04", "Hand Position: " + a_hand_position());
+    }
+}
