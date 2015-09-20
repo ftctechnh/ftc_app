d47d23690e96bb296971c48e80d59b17ecbf8d38
JackVenberg <jvenberg@gmail.com>
2015-09-20 00:47:16 -0700

    Added ArcadeDrive class for modularity

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/codelib/ArcadeDrive.java
@@ -0,0 +1,40 @@
+package com.qualcomm.ftcrobotcontroller.codelib;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+/**
+ * Created by JackV on 9/20/15.
+ */
+public class ArcadeDrive {
+    DcMotor motorRight, motorLeft;
+    public ArcadeDrive(DcMotor motorRight, DcMotor motorLeft) {
+        this.motorRight = motorRight;
+        this.motorLeft = motorLeft;
+    }
+
+    public void arcadeDrive(float y, float x) {
+        float max = Math.abs(x);
+        if (Math.abs(y) > max)
+            max = Math.abs(y);
+        float sum = y + x;
+        float dif = y - x;
+        if(y <= 0) {
+            if(x >= 0) {
+                motorLeft.setPower(max);
+                motorRight.setPower(-sum);
+            } else {
+                motorLeft.setPower(dif);
+                motorRight.setPower(max);
+            }
+        } else {
+            if(y >= 0) {
+                motorLeft.setPower(dif);
+                motorRight.setPower(-max);
+            } else {
+                motorLeft.setPower(-max);
+                motorRight.setPower(-sum);
+            }
+        }
+    }
+}
