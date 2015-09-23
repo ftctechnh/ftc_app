bf440b697a47641abc10cd2addfaae4fd80021bb
RobertAtkinson <bob@theatkinsons.org>
2015-09-23 14:51:06 -0700

    implement OpModeRegistrar methods

--- SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/interfaces/IOpModeManager.java
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/interfaces/IOpModeManager.java
@@ -12,25 +12,6 @@ public interface IOpModeManager
     {
     /**
      * Register a class for display on the driver station and availability for game play.
-     * New instances of this class will be created as needed.
-     *
-     * @param name          the name to show on the driver station menu
-     * @param opModeClass   the class of the OpMode to create
-     * @see #register(Class)
-     */
-    void register(String name, Class opModeClass);
-
-    /**
-     * Register an *instance* of a class for display on the driver station and availability
-     * for game play.
-     *
-     * @param name              the name to show on the driver station menu
-     * @param opModeInstance    the object instance to use for that menu item
-     */
-    void register(String name, OpMode opModeInstance);
-
-    /**
-     * Register a class for display on the driver station and availability for game play.
      * New instances of this class will be created as needed. The name used on the driver
      * station menu is taken from a {@link TeleOp} or {@link Autonomous} annotation on the
      * class itself (any {@link Disabled} attribute which may also be present is ignored).
@@ -41,4 +22,23 @@ public interface IOpModeManager
      * @see #register(String, Class)
      */
     void register(Class opModeClass);
+
+    /**
+     * Register a class for display on the driver station and availability for game play.
+     * New instances of this class will be created as needed.
+     *
+     * @param name          the name to show on the driver station menu
+     * @param opModeClass   the class of the OpMode to create
+     * @see #register(Class)
+     */
+    void register(String name, Class opModeClass);
+
+    /**
+     * Register an *instance* of a class for display on the driver station and availability
+     * for game play. You won't likely use this method very often.
+     *
+     * @param name              the name to show on the driver station menu
+     * @param opModeInstance    the object instance to use for that menu item
+     */
+    void register(String name, OpMode opModeInstance);
     }
