89ea4331b46a73a6e04d6edd4a716d6215b267a9
RobertAtkinson <bob@theatkinsons.org>
2015-09-23 18:34:05 -0700

    clean up code in light of ftc release
    Former-commit-id: 6db119f9cc04c045220e18c92533a0c6cf802682
    Former-commit-id: 0b2671848d191094cf2a5f7bc7e2f1da8ad22f63

--- SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/internal/I2cDeviceOnI2cDeviceController.java
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/internal/I2cDeviceOnI2cDeviceController.java
@@ -137,9 +137,9 @@ public final class I2cDeviceOnI2cDeviceController implements II2cDevice
         return this.controller.isI2cPortReady(port);
         }
 
-    @Override public void readI2cCacheFromModule()
+    @Override public void readI2cCacheFromController()
         {
-        this.controller.readI2cCacheFromModule(port);
+        this.controller.readI2cCacheFromController(port);
         }
 
     @Override public void setI2cPortActionFlag()
@@ -147,14 +147,14 @@ public final class I2cDeviceOnI2cDeviceController implements II2cDevice
         this.controller.setI2cPortActionFlag(port);
         }
 
-    @Override public void writeI2cCacheToModule()
+    @Override public void writeI2cCacheToController()
         {
-        this.controller.writeI2cCacheToModule(port);
+        this.controller.writeI2cCacheToController(port);
         }
 
-    @Override public void writeI2cPortFlagOnlyToModule()
+    @Override public void writeI2cPortFlagOnlyToController()
         {
-        this.controller.writeI2cPortFlagOnlyToModule(port);
+        this.controller.writeI2cPortFlagOnlyToController(port);
         }
 
     @Override public void deregisterForPortReadyCallback()
