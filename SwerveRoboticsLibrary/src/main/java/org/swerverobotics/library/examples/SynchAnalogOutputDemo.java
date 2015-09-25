fe8657a5b2ef921f4a0cc1754435b3a87e012a05
Steve Geffner <steve.geffner.phd@gmail.com>
2015-09-25 14:52:55 -0700

    Adding examples for servos, digital pins (led), and analog output. Analog output doesn't work yet (but it seems like it should...); I'm hoping the latest ftc update fixes something in the framework such that analog out will work now.

---
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/examples/SynchAnalogOutputDemo.java
@@ -0,0 +1,102 @@
+package org.swerverobotics.library.examples;
+
+import com.qualcomm.robotcore.hardware.AnalogOutput;
+
+import org.swerverobotics.library.SynchronousOpMode;
+import org.swerverobotics.library.TelemetryDashboardAndLog;
+import org.swerverobotics.library.interfaces.Disabled;
+import org.swerverobotics.library.interfaces.IFunc;
+import org.swerverobotics.library.interfaces.TeleOp;
+
+/**
+ * SynchAnalogOutputDemo is a short demo of an AnalogOutput port.
+ * We will look at the output signal in an oscilloscope, but we could also use a piezo buzzer to hear the output
+ *
+ * THIS CODE IS NOT WORKING YET...
+ */
+@TeleOp(name="AnalogOut")
+@Disabled
+public class SynchAnalogOutputDemo extends SynchronousOpMode {
+
+    AnalogOutput analog;
+    int frequency = 440; //this is A440.
+    //the difference between notes on an even-tempered scale changes as you move up/down the scale,
+    //but I'm keeping this simple (i.e. wrong) for now, at least until I can see some output on the Analog port
+    int notediff = 26;
+    byte mode = 0; //what are the valid values for mode???
+
+    @Override public void main() throws InterruptedException {
+        // We are expecting the analog output to be attached to a core device interface
+        // module and named "analogout".
+        analog = hardwareMap.analogOutput.get("analogout");
+        analog.setAnalogOutputVoltage(4);
+        analog.setAnalogOutputFrequency(frequency);
+        //analog.setAnalogOutputMode(byte);  //what are the values for mode???
+
+        // Set up our dashboard computations
+        composeDashboard();
+
+        // Wait until we're told to go
+        waitForStart();
+
+        // Loop and update the dashboard
+        while (this.opModeIsActive()) {
+            if (this.updateGamepads()) {
+                if (this.gamepad1.a) {
+                    frequency += notediff;
+                    analog.setAnalogOutputFrequency(frequency);
+                } else if (this.gamepad1.b && (frequency > notediff) ) { //don't let frequency get below the lowest notediff above zero
+                    frequency -= notediff;
+                    analog.setAnalogOutputFrequency(frequency);
+                } else if (this.gamepad1.x) {//we don't know what the valid mode values are so we have to explore
+                    mode++;
+                    analog.setAnalogOutputMode(mode);
+                } else if (this.gamepad1.y) { //we don't care if mode goes "negative", we can use the full range of the byte
+                    mode--;
+                    analog.setAnalogOutputMode(mode);
+                } else if (this.gamepad1.left_bumper) { //a 'reset' to see if this will get the port working
+                    analog.setAnalogOutputVoltage(4);
+                    analog.setAnalogOutputFrequency(frequency);
+                    analog.setAnalogOutputMode(mode);
+                }
+
+
+            }
+
+            telemetry.update();
+            idle();
+        }
+    }
+    
+    void composeDashboard() {
+        telemetry.addLine(
+                telemetry.item("loop count: ", new IFunc<Object>() {
+                    @Override
+                    public Object value() {
+                        return getLoopCount();
+                    }
+                }));
+        telemetry.addLine(
+                telemetry.item("help: ", new IFunc<Object>() {
+                    @Override
+                    public Object value() {
+                        return "freq:a+ b-, mode:x+ y-";
+                    }
+                }));
+        telemetry.addLine(
+                telemetry.item("frequency: ", new IFunc<Object>() {
+                    @Override
+                    public Object value() {
+                        return frequency;
+                    }
+                }));
+        telemetry.addLine(
+                telemetry.item("mode: ", new IFunc<Object>() {
+                    @Override
+                    public Object value() {
+                        return mode;
+                    }
+                }));
+    }
+    
+}
