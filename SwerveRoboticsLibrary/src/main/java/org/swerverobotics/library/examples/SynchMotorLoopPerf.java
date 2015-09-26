7b214c8d264e714b4d7281377c9b5146e6f91215
RobertAtkinson <bob@theatkinsons.org>
2015-09-25 21:05:32 -0700

    simplify motor loop perf logic
    Former-commit-id: 9c4dc34feb65971ffad50efc594dcf3c4640042f
    Former-commit-id: d5de8e72cb02348ecbd7a5a8cf2d67ab7420d4d1

--- SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/examples/SynchMotorLoopPerf.java
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/examples/SynchMotorLoopPerf.java
@@ -38,17 +38,27 @@ public class SynchMotorLoopPerf extends SynchronousOpMode
         while (this.opModeIsActive())
             {
             position = motor.getCurrentPosition();
-            
+            long loopCount = getLoopCount() - loopCountStart;
+            double ms = elapsed.time() * 1000;
+
+            // Need to call this or we won't see any gamepad changes
+            updateGamepads();
+
+            // Note: normally you'd only bother putting code that looked at the game pads
+            // inside of the if (updateGamepads()) clause. But here we're trying to do some
+            // performance analysis, and we don't want that to be subject to human issues.
+            // Instead, we always set the power exactly once each spin around the loop.
             if (gamepad1.left_bumper)
                 motor.setPower(0.5);
             else
                 motor.setPower(0);
 
             telemetry.addData("position",      position);
-            telemetry.addData("#loop()",       getLoopCount() - loopCountStart);
+            telemetry.addData("#loop()",       loopCount);
             telemetry.addData("#spin",         spinCount);
-            telemetry.addData("#loop()/#spin", (getLoopCount() - loopCountStart) / (double)spinCount);
-            telemetry.addData("ms/spin",       String.format("%.1f ms", (elapsed.time()*1000) / (double)spinCount));
+            telemetry.addData("#loop()/#spin", String.format("%.1f", loopCount / (double)spinCount));
+            telemetry.addData("ms/spin",       String.format("%.1f ms", ms / spinCount));
+            telemetry.addData("ms/loop",       String.format("%.1f ms", ms / loopCount));
             telemetry.update();
             idle();
             
