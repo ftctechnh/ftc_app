e4efcf64224b4db96905ac6495f8af0618646a56
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2015-09-26 09:36:08 -0400

    New package
    Created a separate package just for us.

---
+++ FtcRobotController/src/main/java/com/powerstackers/resq/opmodes/WorkshopTeleop.java
@@ -0,0 +1,25 @@
+package com.powerstackers.resq.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Teleop program made at the workshop.
+ */
+public class WorkshopTeleop extends OpMode{
+
+    /**
+     * Get the robot ready to run.
+     */
+    @Override
+    public void init() {
+
+    }
+
+    /**
+     * Run the robot.
+     */
+    @Override
+    public void loop() {
+
+    }
+}
