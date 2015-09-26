f137805c4c999cef6208338bf36c62a64196021a
derekred6 <derekred9@gmail.com>
2015-09-26 10:51:28 -0400

    Workshop experimenting

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EveryThingTeleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EveryThingTeleop.java
@@ -1,7 +1,19 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 /**
  * Created by Derek on 9/26/2015.
  */
-public class EveryThingTeleop {
+public class EveryThingTeleop extends OpMode{
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
