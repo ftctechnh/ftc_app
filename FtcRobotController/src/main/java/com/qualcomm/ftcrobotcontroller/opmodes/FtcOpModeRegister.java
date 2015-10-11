e6f1fe7ac837a5a84bd39b50125ed32700e3527b
brandontechnicbots <brandontechnicbots@users.noreply.github.com>
2015-10-11 10:18:30 -0500

    Started Autonomous

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -58,8 +58,8 @@ public class FtcOpModeRegister implements OpModeRegister {
 
     manager.register("NullOp", NullOp.class);
 
-    manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-    manager.register("K9TeleOp", K9TeleOp.class);
+    //manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
+    //manager.register("K9TeleOp", K9TeleOp.class);
     manager.register ("PushBotAuto", PushBotAuto.class);
     manager.register ("PushBotManual", PushBotManual.class);
 
@@ -99,7 +99,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     //manager.register("PushBotIrSeek", PushBotIrSeek.java);
     //manager.register("PushBotSquare", PushBotSquare.java);
 
-    
-    
+
+    manager.register("ResQAuto", ResQAuto.class);
   }
 }
