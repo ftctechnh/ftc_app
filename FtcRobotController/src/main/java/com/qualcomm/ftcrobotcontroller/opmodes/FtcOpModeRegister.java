8b916d2cc299b5d71b4deb62dadd1b29e2517c5c
team_7974 <7974robotics@gmail.com>
2015-10-14 18:29:15 -0500

    lol

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -56,12 +56,7 @@ public class FtcOpModeRegister implements OpModeRegister {
      */
 
     manager.register("NullOp", NullOp.class);
-
-    manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-    manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
-
+    manager.register("testr", testr.class);
     /*
      * Uncomment any of the following lines if you want to register an op mode.
      */
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/testr.java
@@ -0,0 +1,28 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by no on 10/7/15.
+ */
+public class testr extends OpMode {
+    DcMotor leftmotor;
+    DcMotor rightmotor;
+
+    @Override
+    public void init() {
+        leftmotor = hardwareMap.dcMotor.get("left_drive");
+        rightmotor = hardwareMap.dcMotor.get("right_drive");
+        //leftmotor.setDirection(DcMotor.Direction.REVERSE);
+    }
+    @Override
+    public void loop() {
+        float leftY = -gamepad1.left_stick_y;
+        float rightY = gamepad1.right_stick_y;
+
+        leftmotor.setPower(leftY);
+        rightmotor.setPower(rightY);
+    }
+
+}
