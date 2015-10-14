b86ad2427e77b2be492ca1b3703392bdf871efed
RobertAtkinson <bob@theatkinsons.org>
2015-10-13 21:27:35 -0700

    fix timing math
    Former-commit-id: ed3abcc391d930bfe4662a2b8aa57e7d24fa751a
    Former-commit-id: d0f625b326edaf3ad3443ae7781da66d3ec1f2c9

--- SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/internal/tests/DaveGoldyAutonomous.java
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/internal/tests/DaveGoldyAutonomous.java
@@ -1,10 +1,9 @@
 package org.swerverobotics.library.internal.tests;
 
 import android.util.Log;
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.GyroSensor;
+import org.swerverobotics.library.interfaces.Autonomous;
 
 /**
  * Created by davegoldy on 9/1/15.
@@ -12,7 +11,10 @@ import com.qualcomm.robotcore.hardware.GyroSensor;
  * turn left 90 degrees and drive forward for 1 second. It will repeat these steps until the
  * robot has completed a square.
  */
+@Autonomous(name = "Dave Goldy")
 public class DaveGoldyAutonomous extends OpMode {
+
+    public static String LOGGING_TAG = "Swerve: DaveGoldy";
 
     // TETRIX VALUES.
     protected DcMotor motorRight;
@@ -41,9 +43,12 @@ public class DaveGoldyAutonomous extends OpMode {
     @Override
     public void init() {
 
-        motorRight = hardwareMap.dcMotor.get("rightMotor");
-        motorLeft = hardwareMap.dcMotor.get("leftMotor");
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+        motorLeft = hardwareMap.dcMotor.get("motorLeft");
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override public void start() {
         startTime = System.currentTimeMillis();
     }
 
@@ -57,40 +62,50 @@ public class DaveGoldyAutonomous extends OpMode {
 
         double left = 0.0;
         double right = 0.0;
-        runningTime = this.getRuntime();
-        if (this.getRuntime() <= driveTime) {
+        runningTime = System.currentTimeMillis() - startTime;
+
+        if (runningTime <= driveTime) {
+            log("forward 1");
             //Drive straight forward
             left = drivePower;
             right = drivePower;
         } else if (runningTime > (driveTime + delta) && runningTime <= (driveTime + turnTime)) {
             //Turn left
+            log("left 1");
             left = -drivePower;
             right = drivePower;
         } else if (runningTime > (driveTime + turnTime + delta) && runningTime <= (2 * driveTime + turnTime)) {
             //Drive straight forward
+            log("forward 2");
             left = drivePower;
             right = drivePower;
         } else if (runningTime > (2 * driveTime + turnTime + delta) && runningTime <= (2 * driveTime + 2 * turnTime)) {
             //Turn Left
+            log("left 2");
             left = -drivePower;
             right = drivePower;
         } else if (runningTime > (2 * driveTime + 2 * turnTime + delta) && runningTime <= (3 * driveTime + 2 * turnTime)) {
             //Drive straight forward
+            log("forward 3");
             left = drivePower;
             right = drivePower;
         } else if (runningTime > (3 * driveTime + 2 * turnTime + delta) && runningTime <= (3 * driveTime + 3 * turnTime)) {
             //Turn Left
+            log("left 3");
             left = -drivePower;
             right = drivePower;
         } else if (runningTime > (3 * driveTime + 3 * turnTime + delta) && runningTime <= (4 * driveTime + 3 * turnTime)) {
             //Drive straight forward
+            log("forward 4");
             left = drivePower;
             right = drivePower;
         } else if (runningTime > (4 * driveTime + 3 * turnTime + delta) && runningTime <= (4 * driveTime + 4 * turnTime)) {
             //Turn Left
+            log("left 4");
             left = -drivePower;
             right = drivePower;
         } else {
+            log("stop");
             //Stop
             left = 0.0;
             right = 0.0;
@@ -104,7 +119,11 @@ public class DaveGoldyAutonomous extends OpMode {
     //telemetry.addData("Run Time","run time: "+String.format("%4d",runningTime));
     //telemetry.addData("left tgt pwr","left  pwr: "+String.format("%.2f",left));
     //telemetry.addData("right tgt pwr","right pwr: "+String.format("%.2f",right));
-}
+    }
+
+    void log(String message) {
+        Log.d(LOGGING_TAG, String.format("%f: %s", this.runningTime, message));
+    }
 
     /*
      * Code to run when the op mode is first disabled goes here
