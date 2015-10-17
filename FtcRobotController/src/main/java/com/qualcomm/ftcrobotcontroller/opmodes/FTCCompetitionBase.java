aa26f095d6ca969bbe4ee011746600bcf304823a
Eric <eric.weber.r@gmail.com>
2015-10-17 13:38:37 -0400

    Added Base Code

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FTCCompetitionBase.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FTCCompetitionBase.java
@@ -3,6 +3,8 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
 
 /**
  * Created by Eric on 10/3/2015.
@@ -14,6 +16,7 @@ public class FTCCompetitionBase extends OpMode {
     private DcMotorController RightDriveController;
     private DcMotorController Arm1Controller;
     private DcMotorController Arm2Controller;
+    private ServoController ServoController;
 
     // Drive Motors
     private DcMotor LeftDrive1;
@@ -23,16 +26,31 @@ public class FTCCompetitionBase extends OpMode {
     private DcMotor ArmLift;
     private DcMotor ArmTilt;
     private DcMotor BlockPickup;
-    private DcMotor PullupHook;
+    private DcMotor PullUpHook;
+
+    // Servos
+    private Servo DumpServo1;
+    private Servo DumpServo2;
 
     @Override
     public void init() {
+        // Controllers
         LeftDriveController = hardwareMap.dcMotorController.get("LeftDriveController");
         RightDriveController = hardwareMap.dcMotorController.get("RightDriveController");
+        ServoController = hardwareMap.servoController.get("ServoController");
+
+        // DC Motors
         LeftDrive1 = hardwareMap.dcMotor.get("LeftDrive1");
         LeftDrive2 = hardwareMap.dcMotor.get("LeftDrive2");
         RightDrive1 = hardwareMap.dcMotor.get("RightDrive1");
         RightDrive2 = hardwareMap.dcMotor.get("RightDrive2");
+        ArmLift = hardwareMap.dcMotor.get("ArmLift");
+        ArmTilt = hardwareMap.dcMotor.get("ArmTilt");
+        BlockPickup = hardwareMap.dcMotor.get("BlockPickup");
+
+        // Servos
+        DumpServo1 = hardwareMap.servo.get("DumpServo1");
+        DumpServo2 = hardwareMap.servo.get("DumpServo2");
     }
 
     @Override
@@ -52,6 +70,7 @@ public class FTCCompetitionBase extends OpMode {
                 ForwardPower + TurnPower); //Right Side
     }
 
+    // Encoder Functions
     public int getLeftEncoder(){ return LeftDrive1.getCurrentPosition(); }
     public void resetLeftEncoder(){ LeftDrive1.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);}
     public int getRightEncoder(){ return RightDrive1.getCurrentPosition(); }
