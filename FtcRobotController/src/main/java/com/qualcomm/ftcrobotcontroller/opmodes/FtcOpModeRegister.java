f0d318060463b8a61afcc40c8b726607a4e32839
FirePieman <mitchellsch@live.com>
2015-10-26 20:25:27 -0700

    test opmodes for movement, arm and color sensor

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -56,12 +56,8 @@ public class FtcOpModeRegister implements OpModeRegister {
      */
 
     manager.register("NullOp", NullOp.class);
-
-    manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-    manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
-
+    manager.register ("TestProgram", TestProgram.class);
+    manager.register("ArmMotor", ArmMotor.class);
     /*
      * Uncomment any of the following lines if you want to register an op mode.
      */
