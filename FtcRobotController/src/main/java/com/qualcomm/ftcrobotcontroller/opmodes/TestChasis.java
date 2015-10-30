e173fd84883e9b10e37bde0bae5d29982bf66ff2
tonyTesoriero <tony.tesoriero>
2015-10-29 20:15:12 -0400

    Adds reduction factor

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestChasis.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestChasis.java
@@ -2,7 +2,6 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
 /**
  * Created by Tony_Air on 10/1/15.
  */
@@ -11,25 +10,42 @@ public class TestChasis extends OpMode {
     DcMotor LEFT;
     DcMotor RIGHT;
 
+    float leftX;
+    float leftY;
 
+    float reductionMin;
+
+    float reduction = 1;
 
     public void init(){
 
         LEFT =  hardwareMap.dcMotor.get("motor1");
         RIGHT = hardwareMap.dcMotor.get("motor2");
 
-
     }
+
 
     public void loop(){
 
-        float speed = -gamepad1.left_stick_y * .75f;
-        float rotation = -gamepad1.left_stick_x * .25f;
+        float speed = gamepad1.left_stick_y * leftY;
+        float rotation = gamepad1.left_stick_x * leftX;
 
-        float leftSpeed = speed - rotation;
-        float rightSpeed = speed + rotation;
 
-        LEFT.setPower(leftSpeed);
+        if(gamepad1.left_stick_button) {
+
+            if (reduction == 1) {
+                reduction = reductionMin;
+
+            } else if(reduction == reductionMin){
+                reduction = 1;
+
+            }
+        }
+
+        float leftSpeed = (speed + rotation) * reduction;
+        float rightSpeed = (speed - rotation) * reduction;
+
+        LEFT.setPower(-leftSpeed);
         RIGHT.setPower(rightSpeed);
 
         telemetry.addData("01:", "Left Motor: " + leftSpeed);
