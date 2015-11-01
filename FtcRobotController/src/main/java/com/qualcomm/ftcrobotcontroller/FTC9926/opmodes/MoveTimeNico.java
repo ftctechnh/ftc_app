a61b7d7695ff53e8f62a2dd5b7b9dab0b256119f
Ignacio Bravo <ibravo@hotmail.com>
2015-11-01 09:30:46 -0500

    Servos working on AutoMode but needs to be pushed to HardwareMap environment still

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/MoveTimeNico.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/FTC9926/opmodes/MoveTimeNico.java
@@ -2,16 +2,22 @@ package com.qualcomm.ftcrobotcontroller.FTC9926.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Nicolas Bravo on 10/30/15.
  */
-public class MoveTimeNico extends HardwareMap9926{
+public class MoveTimeNico extends Telemetry9926{
 
     int move_state = 0;
+    Servo Servo1;
+    double SM1_Position;
+
 
     @Override
     public void init() {
+        Servo1 = hardwareMap.servo.get("SM1");
+
     }
 
     @Override
@@ -21,6 +27,8 @@ public class MoveTimeNico extends HardwareMap9926{
 
     @Override public void loop()
     {
+
+
         switch (move_state)
         {
             case 0:
@@ -29,36 +37,41 @@ public class MoveTimeNico extends HardwareMap9926{
                 break;
 
             case 1:
-                m_hand_position(0.2);
+                //m_hand_position(0.2);
 
-//                if (getRuntime() > 5){
+                if (getRuntime() > 5){
 //                    Motor1.setPower(0);
 //                    Motor2.setPower(0);
                     move_state++;
-//                }
+                }
                 break;
 
             case 2:
-                m_hand_position(0.4);
-  //              Motor1.setPower(1);
+                //a_hand_position();
+                SM1_Position = 0.2;
+                Servo1.setPosition(SM1_Position);
+
+                //              Motor1.setPower(1);
   //              Motor2.setPower(0);
- //               if (getRuntime() > 1.5)
- //               {
+                if (getRuntime() > 15)
+                {
   //                  Motor1.setPower(0);
   //                  Motor2.setPower(0);
                     move_state++;
- //               }
+                }
                 break;
 
             case 3:
-                m_hand_position(.8);
+                SM1_Position = 0.3;
+                Servo1.setPosition(SM1_Position);
+ //               m_hand_position(.8);
   //              Motor1.setPower(1);
   //              Motor2.setPower(1);
-  //              if (getRuntime() > 5){
+                if (getRuntime() > 20){
   //                  Motor1.setPower(0);
   //                  Motor2.setPower(0);
                     move_state++;
-  //              }
+                }
                 break;
             default:
                 break;
@@ -97,6 +110,10 @@ public class MoveTimeNico extends HardwareMap9926{
                 //break;
         }
 
+        UpdateTelemetry();
+        telemetry.addData("11", "State: " + move_state);
+        telemetry.addData("12", "Time: " + getRuntime());
+        telemetry.addData("13","Servo: " + SM1_Position);
     }
 
     @Override
