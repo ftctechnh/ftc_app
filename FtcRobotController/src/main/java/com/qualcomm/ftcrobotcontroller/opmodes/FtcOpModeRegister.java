1366253e3c561c44df43f47c4f7ba41dd7175ceb
Sean Burt <mrcreeper@cox.net>
2015-11-02 15:52:57 -0500

    wrote testing opmode
    Created k9teleopTest, from standard k9teleop.
    Tested using 1 motor controller, and 2 andymark motors,
    Funcitonal.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -61,6 +61,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("K9TeleOp", K9TeleOp.class);
     manager.register ("PushBotAuto", PushBotAuto.class);
     manager.register ("PushBotManual", PushBotManual.class);
+    manager.register("K9TeleOpTest", K9TeleOpTest.class);
 
     /*
      * Uncomment any of the following lines if you want to register an op mode.
