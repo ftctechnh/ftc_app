ef14244b16f73bd94be6cdd151d03a633d72b78c
pearc1 <pearc1@isd191.org>
2015-11-02 19:53:00 -0600

    Revert "Set SDK"
    This reverts commit c062e1eebc0ad4b0425d2ea7da83d002b458c6d3.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotManual.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotManual.java
@@ -74,8 +74,8 @@ public class PushBotManual extends PushBotTelemetry
         //
         // Manage the drive wheel motors.
         //
-        float l_left_drive_power = scale_motor_power (gamepad1.left_stick_y);
-        float l_right_drive_power = scale_motor_power (gamepad1.right_stick_y);
+        float l_left_drive_power = scale_motor_power (-gamepad1.left_stick_y);
+        float l_right_drive_power = scale_motor_power (-gamepad1.right_stick_y);
 
         set_drive_power (l_left_drive_power, l_right_drive_power);
 
