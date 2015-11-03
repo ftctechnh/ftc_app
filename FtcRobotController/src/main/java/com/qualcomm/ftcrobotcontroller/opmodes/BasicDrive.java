bd0910c8ac5cf4630d02e265448455006befc6b1
pearc1 <pearc1@isd191.org>
2015-11-02 19:44:38 -0600

    Implementing Methods
    Implemented methods

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BasicDrive.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BasicDrive.java
@@ -4,4 +4,13 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  * Created by Cormac on 11/2/2015.
  */
 public class BasicDrive extends OpMode {
+    @Override
+    public void init(){
+
+    }
+    
+    @Override
+    public void loop(){
+
+    }
 }
