55a7fc2a3a99351dbd135016e0c3f788458832a2
Eden Grown-Haeberli <edengh@gmail.com>
2015-11-05 15:50:17 -0800

    Test

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EETestAuton.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EETestAuton.java
@@ -34,7 +34,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.LightSensor;
-
+//eden 
 /**
  * Example autonomous program.
  * <p>
