595962610411dd23bf075bb375ff24d6ac11cda0
pearc1 <pearc1@isd191.org>
2015-11-05 19:12:15 -0600

    Adding motors, IML Ignore
    Added in motor variables as "leftMotor" and "rightMotor"
    Added in calls to motors from the hardware map
    IML files are now ignored (Should have been done before...?)

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BasicDrive.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BasicDrive.java
@@ -4,9 +4,15 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  * Created by Cormac on 11/2/2015.
  */
 public class BasicDrive extends OpMode {
+
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+
     @Override
     public void init(){
-
+        //get references to the motors from the hardware map
+        leftMotor = hardwareMap.dcmotor.get("left_drive");
+        rightMotor = hardwareMap.dcmotor.get("right_drive");
     }
     
     @Override
