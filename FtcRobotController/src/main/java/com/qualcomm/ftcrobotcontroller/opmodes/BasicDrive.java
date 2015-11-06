6fc312e77d0ea6dbc02ceb0d87acd7c2a2f9e5e0
pearc1 <pearc1@isd191.org>
2015-11-05 19:29:27 -0600

    Full Arcade
    - Reversed right motor to drive straight
    - added x and y val floats
    - added power values
    - added max/min range
    - set motor power

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BasicDrive.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BasicDrive.java
@@ -13,10 +13,27 @@ public class BasicDrive extends OpMode {
         //get references to the motors from the hardware map
         leftMotor = hardwareMap.dcmotor.get("left_drive");
         rightMotor = hardwareMap.dcmotor.get("right_drive");
+
+        //reverse right motor so forward is forward
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
     }
     
     @Override
     public void loop(){
+        float xValue = gamepad.left_stick.y;
+        float yValue = -gamepad.left_stick.y;
+
+        //calc value for each motor
+        float leftPower = yValue + xValue;
+        float rightPower = yValue - xValue;
+
+        //clip range aka set max and min
+        leftPower = Range.clip(leftPower, -1, 1);
+        rightPower = Range.clip(rightPower, -1, 1);
+
+        //set power
+        leftMotor.setPower(leftPower);
+        rightMotor.setPower(rightPower);
 
     }
 }
