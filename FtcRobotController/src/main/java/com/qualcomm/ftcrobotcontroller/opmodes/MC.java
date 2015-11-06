c624a719bef43b175aaa9c19919624e9e5658aad
Patrick Brady <pj.brady@comcast.net>
2015-11-05 19:28:50 -0800

    Added the MC class and a few method headers to start us off.

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MC.java
@@ -0,0 +1,26 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * Created by Patrick.Brady on 11/5/2015.
+ */
+public class MC {
+    final int DEFAULT_SPEED = 50;
+
+    public void moveForward(int distance){
+        moveForward(distance, DEFAULT_SPEED);
+    }
+
+    public void moveForward(int distance, int speed){
+
+    }
+
+    public void moveBarackward(int distance, int speed){
+
+    }
+
+    public void setSpeed(){
+
+    }
+
+
+}
