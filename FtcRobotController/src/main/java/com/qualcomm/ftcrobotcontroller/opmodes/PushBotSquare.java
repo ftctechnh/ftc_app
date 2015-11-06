c9a59e92acf72e65b5dd2a50fef4195d3fd68c1e
Darkonis <flameshadow98@yahoo.com>
2015-11-05 17:07:07 -0800

    code almost finalized

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotSquare.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotSquare.java
@@ -6,37 +6,34 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 
 /*
  * An example linear op mode where the pushbot
- * will drive in a square pattern using sleep()
+ * will drive in a square pattern using sleep() 
  * and a for loop.
  */
 public class PushBotSquare extends LinearOpMode {
-    DcMotor leftMotor;
-    DcMotor rightMotor;
+    DcMotor leftmotor;
+    DcMotor rightmotor;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        leftMotor = hardwareMap.dcMotor.get("left_drive");
-        rightMotor = hardwareMap.dcMotor.get("right_drive");
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);
-        leftMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        rightMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-
+        leftmotor = hardwareMap.dcMotor.get("leftmotor");
+        rightmotor = hardwareMap.dcMotor.get("rightmotor");
+        rightmotor.setDirection(DcMotor.Direction.REVERSE);
+        rightmotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        leftmotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         waitForStart();
+        rightmotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        leftmotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
 
-        for(int i=0; i<4; i++) {
-            leftMotor.setPower(1.0);
-            rightMotor.setPower(1.0);
+       while (rightmotor.getCurrentPosition()< 1000)
+       {
+           rightmotor.setPower(.5);
+           leftmotor.setPower(.5);
 
-            sleep(1000);
 
-            leftMotor.setPower(0.5);
-            rightMotor.setPower(-0.5);
-
-            sleep(500);
         }
 
-        leftMotor.setPowerFloat();
-        rightMotor.setPowerFloat();
+        leftmotor.setPower(0);
+        rightmotor.setPower(0);
 
     }
 }
