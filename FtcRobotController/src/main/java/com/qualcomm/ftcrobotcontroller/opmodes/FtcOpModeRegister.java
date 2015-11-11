39a44a0a468b9e544677438f10b55a2662d994be
kerrj <kerrj@guilford.edu>
2015-11-11 16:22:13 -0500

    Update FtcOpModeRegister.java

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -57,10 +57,10 @@ public class FtcOpModeRegister implements OpModeRegister {
 
     manager.register("NullOp", NullOp.class);
 
-    manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
-    manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
+   // manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
+   // manager.register("K9TeleOp", K9TeleOp.class);
+   // manager.register ("PushBotAuto", PushBotAuto.class);
+   // manager.register ("PushBotManual", PushBotManual.class);
     manager.register("MotorMove",MotorMove.class);
     manager.register("MotorMoveAutonomouse", MotorMoveAutonomouse.class);
     manager.register("DucksTeleOp",DucksTeleOp.class);
