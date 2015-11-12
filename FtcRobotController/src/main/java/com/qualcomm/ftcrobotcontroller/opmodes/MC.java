f2a64b1414e42b34fd1ef61bf5231efc279cd31b
Patrick Brady <pj.brady@comcast.net>
2015-11-12 14:57:36 -0800

    I added blank methods to the MC class that we can fill during robotics today.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MC.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MC.java
@@ -6,21 +6,32 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 public class MC {
     final int DEFAULT_SPEED = 50;
 
+    public void moveForward(int distance, int speed){
+        //move for distance at speed
+    }
+
+    public void moveBackward(int distance, int speed){
+        //move backward for distance at speed
+    }
+
+    public void turnRight(int distance, int speed){
+        //turn right for distance at speed
+    }
+
+    public void turnLeft(int distance, int speed){
+        //turn left for distance at speed
+    }
+
     public void moveForward(int distance){
         moveForward(distance, DEFAULT_SPEED);
     }
-
-    public void moveForward(int distance, int speed){
-
+    public void moveBackward(int distance){
+        moveBackward(distance, DEFAULT_SPEED);
     }
-
-    public void moveBarackward(int distance, int speed){
-
+    public void turnRight(int distance){
+        turnRight(distance, DEFAULT_SPEED);
     }
-
-    public void setSpeed(){
-
+    public void turnLeft(int distance){
+        turnLeft(distance, DEFAULT_SPEED);
     }
-
-
 }
