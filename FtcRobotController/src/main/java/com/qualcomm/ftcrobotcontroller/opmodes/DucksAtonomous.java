7c69c5572a2b7a43c1b2d67bff47c3724f7c739f
kerrj <kerrj@guilford.edu>
2015-11-12 08:49:50 -0500

    ade atonomous and minor tweaks to teleop

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DucksAtonomous.java
@@ -0,0 +1,14 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by Dan on 11/12/2015.
+ */
+
+public class DucksAtonomous extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DucksTeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DucksTeleOp.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.hardware.Gamepad;
  */
 public class DucksTeleOp extends OpMode {
     @Override
-//test comment plz ignore
+
     public void init() {
 
     }
@@ -21,23 +21,23 @@ public class DucksTeleOp extends OpMode {
         DcMotor winch=hardwareMap.dcMotor.get("winch");
         DcMotor winchpivot=hardwareMap.dcMotor.get("winchpivot");
         DcMotor winchwheel=hardwareMap.dcMotor.get("winchwheel");
-
-        if(gamepad1.left_stick_y>.03){
-            left.setPower(Math.pow(gamepad1.left_stick_y, 2));
-        }else if(gamepad1.left_stick_y<.03) {
-            left.setPower(-Math.pow(gamepad1.left_stick_y, 2));
+//left wheel
+        if(gamepad1.left_stick_y>.05){
+            left.setPower(Math.pow(gamepad1.left_stick_y, 3/2));
+        }else if(gamepad1.left_stick_y<.05) {
+            left.setPower(-Math.pow(gamepad1.left_stick_y, 3/2));
         } else{
             left.setPower(0);
         }
-
-        if(gamepad1.right_stick_y>.03){
-            right.setPower(-Math.pow(gamepad1.right_stick_y,2));
-        } else if(gamepad1.right_stick_y<.03){
-            right.setPower(Math.pow(gamepad1.right_stick_y,2));
+//right wheel
+        if(gamepad1.right_stick_y>.05){
+            right.setPower(-Math.pow(gamepad1.right_stick_y,3/2));
+        } else if(gamepad1.right_stick_y<.05){
+            right.setPower(Math.pow(gamepad1.right_stick_y,3/2));
         }else{
             right.setPower(0);
         }
-
+//winch in and out
         if(gamepad1.a){
             winch.setPower(1);
             winchwheel.setPower(1);
@@ -48,11 +48,11 @@ public class DucksTeleOp extends OpMode {
             winch.setPower(0);
             winchwheel.setPower(0);
         }
-
+//winch up and down
         if(gamepad1.y){
-            winchpivot.setPower(.3);
+            winchpivot.setPower(.2);
         } else if(gamepad1.x){
-            winchpivot.setPower(-.3);
+            winchpivot.setPower(-.2);
         } else {
             winchpivot.setPower(0);
         }
