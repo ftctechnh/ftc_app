2d3e7d0d55a05e26bfa6b0e1084c151e95adae95
PeterG <peterohio1@gmail.com>
2015-11-12 20:46:51 -0500

    using the new interface model

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushButtonAuto.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushButtonAuto.java
@@ -1,22 +1,33 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import junit.framework.Test;
 
 /**
  * Created by Peter on 10/8/2015.
  */
- abstract public class PushButtonAuto extends LinearOpMode
+ abstract public class PushButtonAuto extends OpMode
 {
-    public void runOpMode()
+
+    TestBot testBot;
+
+    public void init()
     {
-        moveStraightLineDist(48.0f, .75f,  true)
-        spinOnCenter(90.0f, .5f,  true)
-        moveStraightLineDist(48.0f, .75f,   true)
-        spinOnCenter(90f, .5f,  false)
-        moveStraightLineDist(24.0f, .75f,true)
-        spinOnCenter(90.0f, .5f,  true)
-        moveStraightLineDist(24.0f, .75f, true)
-        senseColor()
+        testBot = new TestBot(hardwareMap);
+    }
+
+    public void start()
+    {
+       /* testBot.moveStraightEncoders(48.0f, .75f);
+        testBot.spinOnCenter(90.0f, .5f, true);
+        testBot.moveStraightEncoders(48.0f, .75f);
+        testBot.spinOnCenter(90f, .5f, false);
+        testBot.moveStraightEncoders(24.0f, .75f);
+        testBot.spinOnCenter(90.0f, .5f, true);
+        testBot.moveStraightEncoders(24.0f, .75f);
+        senseColor();
         if (//parameter for red)
             {
                    pushBotton(true)
@@ -24,8 +35,18 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
         if (//parameter for blue)
                 {
                   pushButton(false)
-                }
-        spinOnCenter(48.0f, .75f, true)
-        moveStraightLineDist(24.0f, .75f, true)
+                };
+        spinOnCenter(48.0f, .75f, true);
+        testBot.moveStraightEncoders(24.0f, .75f);*/
+    }
+
+    public void loop()
+    {
+
+    }
+
+    public void stop()
+    {
+        testBot.stop();
     }
 }
