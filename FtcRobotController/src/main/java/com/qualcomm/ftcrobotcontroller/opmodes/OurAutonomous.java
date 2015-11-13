ceed02318a320bb18e11738cafb06634edb02ba9
Caroline.Sonnen <carolinesonnen@hotmail.com>
2015-11-12 16:28:43 -0800

    added some more psuedocode

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurAutonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurAutonomous.java
@@ -4,5 +4,33 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  * Created by patrick.brady on 11/12/2015.
  */
 public class OurAutonomous {
-    
+
+    //public static light(){
+
+    // go straight some amount
+
+    //open claw...
+    //**claw is to remain open for autonomous
+
+    //turn left/right goes to button (depending on position)
+
+    //extend arm and press light button
+
+    //mission partially accomplished
+
+//}
+    //public static climb(){
+
+    //back up/ turn right/ left(depending on position)
+
+    //go forward to press button to release climbers
+
+    //extend arm to press button to release climbers
+
+    //back up and turn (to reposition) to go straight onto mountain
+
+    //with all force climb to highest point on mountain
+
+    //}
+
 }
