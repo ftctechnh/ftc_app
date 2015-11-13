76fef507fdda8acc3a864fa1841af5a96e46f7f4
Caroline.Sonnen <carolinesonnen@hotmail.com>
2015-11-12 16:07:04 -0800

    added some psuedocode

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Mitchellcanyouseethis.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Mitchellcanyouseethis.java
@@ -4,7 +4,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  * Created by Patrick.Brady on 11/3/2015.
  */
 public class Mitchellcanyouseethis {
-    //hello my name is caroline, i go to eastisde catholic high school. i am a junior. i play softball and i like long walks on the beach and ice cream. 
+    //hello my name is caroline, i go to eastisde catholic high school. i am a junior. i play softball and i like long walks on the beach and ice cream.
     //edit this if you GIT it. lol. did you like that pun?
     //I did GIT that pun m8. I kinda wish there was a Git Rekt refernce some how
 }
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurAutonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurAutonomous.java
@@ -4,5 +4,26 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  * Created by patrick.brady on 11/12/2015.
  */
 public class OurAutonomous {
+    public static light() {
+
+        // go straight
+
+        //turn left/right (depending on current position)
+
+        //** robotic will be going towards the sacred button
+
+        // robot arm extends to press light button using light sensor in order to know what button to press
+    }
+
+    public static climb(){
+
+      // back up/ turn right/left (depending on position)
+
+        //forward movment to press button to release climbers
+
+        // back up and turn (reposition) to go on to mountain
+
+        // with all force, climb to higest level on mountain
+    }
     
 }
