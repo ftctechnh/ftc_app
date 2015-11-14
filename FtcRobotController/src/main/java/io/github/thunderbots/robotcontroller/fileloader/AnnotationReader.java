b55c5dc6f00621129646902c8f4619132b60186d
Pranav Mathur <pranavmathur99@gmail.com>
2015-11-14 14:10:16 -0800

    Implement @Active functionality
    This replaces the old @OpMode active field

--- FtcRobotController/src/main/java/io/github/thunderbots/robotcontroller/fileloader/AnnotationReader.java
+++ FtcRobotController/src/main/java/io/github/thunderbots/robotcontroller/fileloader/AnnotationReader.java
@@ -2,18 +2,13 @@ package io.github.thunderbots.robotcontroller.fileloader;
 
 import java.lang.annotation.Annotation;
 
+import io.github.thunderbots.lightning.annotation.Active;
 import io.github.thunderbots.lightning.annotation.OpMode;
 
 public class AnnotationReader {
 
     public static boolean isActive(Class<? extends com.qualcomm.robotcore.eventloop.opmode.OpMode> c) {
-        Annotation[] annotations = c.getAnnotations();
-        for (Annotation annotation : annotations) {
-            if (annotation instanceof OpMode) {
-                return ((OpMode) annotation).active();
-            }
-        }
-        return false;
+        return c.isAnnotationPresent(Active.class);
     }
 
     public static String getOpModeName(Class<? extends com.qualcomm.robotcore.eventloop.opmode.OpMode> c) {
