14b5dbdf7a7ddede35f6656602efbb137a8faa26
Jack Shew <jack@shew.net>
2015-11-13 16:06:28 -0800

    Organization branch

---
+++ Overlake/src/main/java/org/Overlake/ftc/Team_7330/Autonomous/AutonomousOpMode.java
@@ -0,0 +1,10 @@
+package org.Overlake.ftc.Team_7330.Autonomous;
+
+import org.swerverobotics.library.SynchronousOpMode;
+
+/**
+ * Created by jacks on 11/13/2015.
+ */
+public abstract class AutonomousOpMode extends SynchronousOpMode {
+
+}
