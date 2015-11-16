501801c572ac44c681d65a4a3a5a5f1bd5ec13c4
Caroline.Sonnen <carolinesonnen@hotmail.com>
2015-11-15 16:34:08 -0800

    ourAutonomous updates

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurAutonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurAutonomous.java
@@ -5,6 +5,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
  */
 
 
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
