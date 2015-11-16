4ae8d70c7b0c8ce2fde356485cd4441a438a0c3b
Patrick Brady <pj.brady@comcast.net>
2015-11-15 16:56:17 -0800

    Almost all the errors in our autonomous are fixed.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurAutonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurAutonomous.java
@@ -8,21 +8,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 
-/**
- * Created by patrick.brady on 11/12/2015.
- */
-
-
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.ftcrobotcontroller.opmodes.MC;
-
-
-public class OurAutonomous {
+public class OurAutonomous extends OpMode{
 
 
     public OurAutonomous(){
@@ -35,14 +21,42 @@ public class OurAutonomous {
     DcMotor leftMotor;
 
     public void init(){
-        DcMotor armLowerMotor;
-        DcMotor armUpperMotor;
-        DcMotor rightMotor = hardwareMap.dcMotor.get("motor_right");
-        DcMotor leftMotor = hardwareMap.dcMotor.get("motor_left");
+
+        armUpperMotor = hardwareMap.dcMotor.get("armUpperMotor");
+        armLowerMotor = hardwareMap.dcMotor.get("armLowerMotor");
+        rightMotor = hardwareMap.dcMotor.get("motor_right");
+        leftMotor = hardwareMap.dcMotor.get("motor_left");
     }
 
     public void start(){
+//public static light(){
 
+        // go straight some amount
+        MC.moveForward(100, 34, rightMotor, leftMotor);
+        //open claw...
+
+        //**claw is to remain open for autonomous
+
+        //turn left/right goes to button (depending on position)
+        MC.turnRight(100, 34, rightMotor, leftMotor);
+        //extend arm and press light button
+
+//}
+        //public static climb(){
+
+        //back up/ turn right/ left(depending on position)
+        turnandbackup.BackTurn(100,34, rightMotor, leftMotor);
+        //go forward to press button to release climbers
+        MC.moveForward(100, 34, rightMotor, leftMotor);
+        //extend arm to press button to release climbers
+
+        //back up and turn (to reposition) to go straight onto mountain
+        turnandbackup.BackTurn(100,34, rightMotor, leftMotor);
+        //with all force climb to highest point on mountain
+        MC.moveForward(100, 34, rightMotor, leftMotor);
+        //}
+
+        MC.moveForward(100, 34, rightMotor, leftMotor);
     }
 
     public void loop(){
@@ -52,32 +66,5 @@ public class OurAutonomous {
     public void stop(){
 
     }
-    //public static light(){
 
-    // go straight some amount
-    MC.moveForward(100, 34);
-    //open claw...
-
-    //**claw is to remain open for autonomous
-
-    //turn left/right goes to button (depending on position)
-    MC.turnRight(100, 34);
-    //extend arm and press light button
-
-//}
-    //public static climb(){
-
-    //back up/ turn right/ left(depending on position)
-   turnandbackup.BackTurn(100,34);
-    //go forward to press button to release climbers
-    MC.moveForward(100, 34);
-    //extend arm to press button to release climbers
-
-    //back up and turn (to reposition) to go straight onto mountain
-    turnandbackup.BackTurn(100,34);
-    //with all force climb to highest point on mountain
-    MC.moveForward(100, 34);
-    //}
-
-    MC.moveForward(100, 34, rightMotor, leftMotor);
 }
