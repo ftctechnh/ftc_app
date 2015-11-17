fbdca3ce952707bc5960683e0fa9cffea1b7459f
Patrick Brady <pj.brady@comcast.net>
2015-11-17 09:47:02 -0800

    Improved the Autonomous class by adding more structure

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurAutonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurAutonomous.java
@@ -19,6 +19,8 @@ public class OurAutonomous extends OpMode{
     DcMotor armUpperMotor;
     DcMotor rightMotor;
     DcMotor leftMotor;
+    boolean peopleAndLight = true;
+    boolean climbing = true;
 
     public void init(){
         armUpperMotor = hardwareMap.dcMotor.get("armUpperMotor");
@@ -28,34 +30,61 @@ public class OurAutonomous extends OpMode{
     }
 
     public void start(){
-    //public static light(){
+        if(peopleAndLight) {
+            //dumpAndLight
+            MC.moveForward(3000, rightMotor, leftMotor); //move in front of the light
+            MC.turnRight(200, rightMotor, leftMotor); //face the light approximately
+            MC.moveForward(400, rightMotor, leftMotor); //CAN USE LINE FOLLOWING TO CENTER ROBOT AND TOUCH TO DETECT WALL.
+            dumpClimbers(); //method to use the arm to dump the climbers into the bin that should be in front of the robot
+            detectAndPushLight();//method to use the light sensor to see which button to push
+            //make sure to have the methods return the robot to this position
+        }
 
-            // go straight some amount
-            MC.moveForward(100, 34, rightMotor, leftMotor);
-            //open claw...
+        if(climbing && peopleAndLight){
+            climbFromBasket();
+        }
+        else if(climbing){
+            climbFromStart();
+        }
 
-            //**claw is to remain open for autonomous
 
-            //turn left/right goes to button (depending on position)
-            MC.turnRight(100, 34, rightMotor, leftMotor);
-            //extend arm and press light button
+        //turn left/right goes to button (depending on position)
+        MC.turnRight(100, rightMotor, leftMotor);
+        //extend arm and press light button
 
-    //}
-            //public static climb(){
+//}
+        //public static climb(){
 
-            //back up/ turn right/ left(depending on position)
-            //turnandbackup.BackTurn(100,34, rightMotor, leftMotor);
-            //go forward to press button to release climbers
-            MC.moveForward(100, 34, rightMotor, leftMotor);
-            //extend arm to press button to release climbers
+        //back up/ turn right/ left(depending on position)
+        //turnandbackup.BackTurn(100,34, rightMotor, leftMotor);
+        //go forward to press button to release climbers
+        MC.moveForward(100, rightMotor, leftMotor);
+        //extend arm to press button to release climbers
 
-            //back up and turn (to reposition) to go straight onto mountain
-            //turnandbackup.BackTurn(100,34, rightMotor, leftMotor);
-            //with all force climb to highest point on mountain
-            MC.moveForward(100, 34, rightMotor, leftMotor);
-            //}
+        //back up and turn (to reposition) to go straight onto mountain
+        //turnandbackup.BackTurn(100,34, rightMotor, leftMotor);
+        //with all force climb to highest point on mountain
+        MC.moveForward(100, rightMotor, leftMotor);
+        //}
 
-            MC.moveForward(100, 34, rightMotor, leftMotor);
+        MC.moveForward(100, rightMotor, leftMotor);
+    }
+
+    public void dumpClimbers(){
+        //starting from the point where line following stopped, the robot will use its arm to reach up and dump the people
+
+    }
+
+    public void detectAndPushLight(){
+        //after dumping (or trying to dump) the people, this is the method for using the light sensor to pursh the right button
+    }
+
+    public void climbFromBasket(){
+        //the robot starts from the location that is at the end of the light and basket methods
+    }
+
+    public void climbFromStart(){
+        //the robot starts from the position on the wall
     }
 
     public void loop(){
