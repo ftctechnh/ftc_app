ecb5815624c875fc0b35c7e0611ccc8b427f58c5
Ryan Petschek <petschekr@gmail.com>
2015-11-17 21:56:28 -0500

    Add DragonoidsTest opmode
    (Currently) for testing the climbing motors

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DragonoidsOpMode.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DragonoidsOpMode.java
@@ -7,12 +7,17 @@ import java.util.HashMap;
 
 public class DragonoidsOpMode extends OpMode {
     protected HashMap<String, DcMotor> driveMotors = new HashMap<String, DcMotor>();
+    protected HashMap<String, DcMotor> climbMotors = new HashMap<String, DcMotor>();
 
     @Override
     public void init() {
         driveMotors.put("right", hardwareMap.dcMotor.get("rightDrive"));
         driveMotors.put("left", hardwareMap.dcMotor.get("leftDrive"));
         driveMotors.get("left").setDirection(DcMotor.Direction.REVERSE);
+
+        climbMotors.put("right", hardwareMap.dcMotor.get("rightClimb"));
+        climbMotors.put("left", hardwareMap.dcMotor.get("leftClimb"));
+        climbMotors.get("left").setDirection(DcMotor.Direction.REVERSE);
     }
     @Override
     public void loop() {
@@ -23,10 +28,14 @@ public class DragonoidsOpMode extends OpMode {
         // Stop all motors
         driveMotors.get("right").setPower(0);
         driveMotors.get("left").setPower(0);
+        climbMotors.get("right").setPower(0);
+        climbMotors.get("left").setPower(0);
     }
 
     private void outputTelemetry() {
-        telemetry.addData("Right motor power", driveMotors.get("right").getPower());
-        telemetry.addData("Left motor power", driveMotors.get("left").getPower());
+        telemetry.addData("Right drive motor power", driveMotors.get("right").getPower());
+        telemetry.addData("Left drive motor power", driveMotors.get("left").getPower());
+        telemetry.addData("Right climb motor power", climbMotors.get("right").getPower());
+        telemetry.addData("Left climb motor power", climbMotors.get("left").getPower());
     }
 }
