369de19336ed029d111fd511197284ffeec7e899
Darkonis <flameshadow98@yahoo.com>
2015-11-17 17:33:09 -0800

    Working!!!!! motor encoder

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotSquare.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotSquare.java
@@ -1,43 +1,59 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
-
+import com.qualcomm.ftcrobotcontroller.opmodes.PushBotTelemetry;
+import com.qualcomm.hardware.HiTechnicNxtDcMotorController;
 /*
  * An example linear op mode where the pushbot
  * will drive forward
  */
-public class PushBotSquare extends LinearOpMode {
+public class PushBotSquare extends OpMode {
     DcMotor leftmotor;
     DcMotor rightmotor;
 
+
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void init()
+    {
         leftmotor = hardwareMap.dcMotor.get("leftmotor");
         rightmotor = hardwareMap.dcMotor.get("rightmotor");
-        rightmotor.setDirection(DcMotor.Direction.REVERSE);
+        leftmotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        rightmotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+
         leftmotor.setDirection(DcMotor.Direction.REVERSE);
 
-        waitForStart();
+    }
+    @Override
+    public void start() {
+        leftmotor.setTargetPosition(2000);
+        rightmotor.setTargetPosition(2000);
 
-        move(500);
+        leftmotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        rightmotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
 
+        leftmotor.setPower(0.5);
+        rightmotor.setPower(0.5);
 
     }
+
+    public void loop() {
+
+    }
+
     public void move (int count)
     {
-        leftmotor.setTargetPosition(count);
-        rightmotor.setTargetPosition(count);
+
+
+        leftmotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        rightmotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         rightmotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
         leftmotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        leftmotor.setTargetPosition(200);
+        rightmotor.setTargetPosition(200);
         rightmotor.setPower(.5);
         leftmotor.setPower(.5);
-        data();
+    }
 
-    }
-    public void data()
-    {
-        telemetry.addData("rightmotor",rightmotor.getCurrentPosition());
-    }
 }
