7cd6e32c397ae7f72697ded7bdc9a597e40cc8b4
Robert Atkinson <bob@theatkinsons.org>
2015-11-18 20:37:45 -0800

    disable opmodes by default

--- SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/examples/SynchMotorLoopPerf.java
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/examples/SynchMotorLoopPerf.java
@@ -14,7 +14,7 @@ import org.swerverobotics.library.interfaces.*;
  * flag, set in the constructor, controls whether just one or both of the motors are used.
  */
 @TeleOp(name="Motor Perf (sync)", group="Swerve Examples")
-// @Disabled
+@Disabled
 public class SynchMotorLoopPerf extends SynchronousOpMode
     {
     final boolean useBothMotors;
