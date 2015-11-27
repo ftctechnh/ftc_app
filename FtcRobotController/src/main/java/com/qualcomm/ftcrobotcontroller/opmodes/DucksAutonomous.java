6c81ec200a6c36cff86b5635ccbaf2f154c198c0
kerrj <kerrj@guilford.edu>
2015-11-27 16:03:48 -0500

    updated autonomous to use motor controllers, added "goforward" method

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DucksAutonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DucksAutonomous.java
@@ -3,6 +3,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.hardware.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 
 /**
@@ -10,59 +11,71 @@ import com.qualcomm.robotcore.hardware.GyroSensor;
  */
 
 public class DucksAutonomous extends LinearOpMode{
-    DcMotor left;
-    DcMotor right;
-    DcMotor winch;
-    DcMotor winchpivot;
-    DcMotor winchwheel;
+    static int WINCH=1;
+    static int WINCHPIVOT=2;
+    static int WINCHWHEEL=1;
+    static int RIGHT=2;
+    static int LEFT=1;
     GyroSensor gyro;
+    DcMotorController winchMC;
+    DcMotorController driveMC;
+    DcMotorController wheelMC;
     public DucksAutonomous(){
 
     }
 
+    public void goForward(double speed, long MS) throws InterruptedException{
+        driveMC.setMotorPower(RIGHT,speed);
+        driveMC.setMotorPower(LEFT,-speed);
+        Thread.sleep(MS);
+        driveMC.setMotorPower(LEFT, 0);
+        driveMC.setMotorPower(RIGHT,0);
+        Thread.sleep(500);
+    }
 
-    public void turnLeft (int degrees, double speed){
+    public void turnLeft (int degrees, double speed) throws InterruptedException{
         int startHeading=gyro.getHeading();
         int targetHeading=startHeading-degrees;
         if(targetHeading<0){
             targetHeading=targetHeading+360;
         }
         while(gyro.getHeading()<targetHeading-3 || gyro.getHeading()>targetHeading+3){
-            left.setPower(-speed);
-            right.setPower(-speed);
+            driveMC.setMotorPower(LEFT, speed);
+            driveMC.setMotorPower(RIGHT,speed);
+            telemetry.addData("currentheading", gyro.getHeading());
+            telemetry.addData("targetHeading",targetHeading);
         }
-        left.setPower(0);
-        right.setPower(0);
+        driveMC.setMotorPower(LEFT, 0);
+        driveMC.setMotorPower(RIGHT, 0);
+        Thread.sleep(500);
     }
 
-    public void turnRight (int degrees,double speed){
+    public void turnRight (int degrees,double speed) throws InterruptedException{
         int startHeading=gyro.getHeading();
         int targetHeading=startHeading+degrees;
-        telemetry.addData("targetHeading",targetHeading);
         if(targetHeading>360){
             targetHeading=targetHeading-360;
         }
         while(gyro.getHeading()<targetHeading-5 || gyro.getHeading()>targetHeading+5){
             printHeading();
-            telemetry.addData("targetHeading", targetHeading);
-            left.setPower(speed);
-            right.setPower(speed);
+            telemetry.addData("currentheading", gyro.getHeading());
+            telemetry.addData("targetHeading",targetHeading);
+            driveMC.setMotorPower(LEFT, -speed);
+            driveMC.setMotorPower(RIGHT, -speed);
         }
-        left.setPower(0);
-        right.setPower(0);
+        driveMC.setMotorPower(LEFT,0);
+        driveMC.setMotorPower(RIGHT,0);
+        Thread.sleep(500);
     }
     public void printHeading(){
         telemetry.addData("currentHeading",gyro.getHeading());
     }
     @Override
     public void runOpMode() throws InterruptedException{
-        left= hardwareMap.dcMotor.get("left");
-        right= hardwareMap.dcMotor.get("right");
-        winch=hardwareMap.dcMotor.get("winch");
-        winchpivot=hardwareMap.dcMotor.get("winchpivot");
-        winchwheel=hardwareMap.dcMotor.get("winchwheel");
+        driveMC=hardwareMap.dcMotorController.get("driveMC");
+        winchMC=hardwareMap.dcMotorController.get("winchMC");
+        wheelMC=hardwareMap.dcMotorController.get("wheelMC");
         gyro=hardwareMap.gyroSensor.get("gyro");
-
         gyro.calibrate();
         while(gyro.isCalibrating()){
             telemetry.addData("lol","lol");
@@ -71,13 +84,8 @@ public class DucksAutonomous extends LinearOpMode{
         waitForStart();
 
         //go forward from starting position
-        left.setPower(-.5);
-        right.setPower(  .5);
-        Thread.sleep(4000);
-        left.setPower(0);
-        right.setPower(0);
-
-        turnLeft(45, .3);
+        goForward(1, 1000);
+        turnLeft(90, 1);
 
 //        //go forward after turning
 //        left.setPower(.5);
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DucksTeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DucksTeleOp.java
@@ -26,6 +26,9 @@ public class DucksTeleOp extends OpMode {
     static int WINCHWHEEL=1;
     static int RIGHT=2;
     static int LEFT=1;
+    int UPDATES=0;
+    int toggleLeft=0;
+    int toggleRight=0;
     @Override
     public void init() {
 
@@ -43,66 +46,73 @@ public class DucksTeleOp extends OpMode {
 //left wheel
         if(gamepad1.left_stick_y>.05 && driveMC.getMotorPower(LEFT)!=gamepad1.left_stick_y){
             driveMC.setMotorPower(LEFT,Math.pow(gamepad1.left_stick_y, 2));
-            telemetry.addData("updating",1);
+            UPDATES+=1;
         }else if(gamepad1.left_stick_y<.05 && driveMC.getMotorPower(LEFT)!=gamepad1.left_stick_y) {
             driveMC.setMotorPower(LEFT,-Math.pow(gamepad1.left_stick_y, 2));
-            telemetry.addData("updating", 1);
+            UPDATES+=1;
         } else if(Math.abs(gamepad1.left_stick_y)<.05 && driveMC.getMotorPower(LEFT)!=0){
             driveMC.setMotorPower(LEFT,0);
-            telemetry.addData("updating", 1);
+            UPDATES+=1;
         }
 //right wheel
         if(gamepad1.right_stick_y>.05 && driveMC.getMotorPower(RIGHT)!=gamepad1.right_stick_y){
             driveMC.setMotorPower(RIGHT,-Math.pow(gamepad1.right_stick_y,2));
-            telemetry.addData("updating", 1);
+            UPDATES+=1;
         } else if(gamepad1.right_stick_y<.05 && driveMC.getMotorPower(RIGHT)!=gamepad1.right_stick_y){
             driveMC.setMotorPower(RIGHT,Math.pow(gamepad1.right_stick_y,2));
-            telemetry.addData("updating", 1);
+            UPDATES+=1;
         }else if(Math.abs(gamepad1.right_stick_y)<.05 && driveMC.getMotorPower(RIGHT)!=0){
             driveMC.setMotorPower(RIGHT,0);
-            telemetry.addData("updating", 1);
+            UPDATES+=1;
         }
 //winch in and out
         if(gamepad1.a && winchMC.getMotorPower(WINCH)!=1){
             winchMC.setMotorPower(WINCH,1);
             wheelMC.setMotorPower(WINCHWHEEL, .2);
-            telemetry.addData("updating", 1);
+            UPDATES+=1;
         } else if(gamepad1.b && winchMC.getMotorPower(WINCH)!=-1){
             winchMC.setMotorPower(WINCH, -1);
             wheelMC.setMotorPower(WINCHWHEEL,-1);
-            telemetry.addData("updating", 1);
+            UPDATES+=1;
         } else if(!gamepad1.a && !gamepad1.b && winchMC.getMotorPower(WINCH)!=0){
             winchMC.setMotorPower(WINCH, 0);
             wheelMC.setMotorPower(WINCHWHEEL,0);
-            telemetry.addData("updating", 1);
+            UPDATES+=1;
         }
 //winch up and down
         if(gamepad1.y && winchMC.getMotorPower(WINCHPIVOT)!=.2){
             winchMC.setMotorPower(WINCHPIVOT,.2);
-            telemetry.addData("updating", 1);
+            UPDATES+=1;
         } else if(gamepad1.x && winchMC.getMotorPower(WINCHPIVOT)!=-.1){
             winchMC.setMotorPower(WINCHPIVOT,-.1);
-            telemetry.addData("updating", 1);
+            UPDATES+=1;
         } else if(!gamepad1.y && !gamepad1.x && winchMC.getMotorPower(WINCHPIVOT)!=0) {
             winchMC.setMotorPower(WINCHPIVOT,0);
-            telemetry.addData("updating", 1);
+            UPDATES+=1;
         }
         //right climbers up
-        if(gamepad1.dpad_up){
+        if(gamepad1.dpad_down && Math.abs(climbersRight.getPosition()-1)>.1){
             climbersRight.setPosition(1);
+            UPDATES+=1;
         }
         //right climbers down
-        if(gamepad1.dpad_right){
+        if(gamepad1.dpad_right && Math.abs(climbersRight.getPosition() - .25)>.1){
             climbersRight.setPosition(.25);
+            UPDATES+=1;
         }
         //left climbers up
-        if(gamepad1.dpad_down){
+        if(gamepad1.dpad_up && Math.abs(climbersLeft.getPosition() - .3)>.1){
             climbersLeft.setPosition(.3);
+            UPDATES+=1;
         }
         //left climbers down
-        if(gamepad1.dpad_left){
+        if(gamepad1.dpad_left && Math.abs(climbersLeft.getPosition()-1)>.1){
             climbersLeft.setPosition(1);
+            UPDATES+=1;
         }
+        telemetry.addData("UPDATES",UPDATES);
+        telemetry.addData("climbersLeft",climbersLeft.getPosition());
+        telemetry.addData("climbersRight", climbersRight.getPosition());
     }
     @Override
     public void stop(){
