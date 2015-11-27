189f4fc12fa26551d2fa5252415a4de61bd28325
subash <subashch6@gmail.com>
2015-11-27 18:58:31 -0500

    changed newbase to have grabber and diverter as well as A button on gampad1

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/newBase.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/newBase.java
@@ -2,19 +2,23 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by subash on 11/17/2015.
  */
 public class newBase extends OpMode
 {
-    float speed = 0.75f;
+    float speed = 1.0f;
     DcMotor frontRight;
     DcMotor frontLeft;
     DcMotor backRight;
     DcMotor backLeft;
     DcMotor right;
     DcMotor left;
+    DcMotor grabber;
+    Servo diverter;
+
 
     @Override
     public void stop()
@@ -37,48 +41,78 @@ public class newBase extends OpMode
         frontLeft = hardwareMap.dcMotor.get("frontLeft");
         backRight = hardwareMap.dcMotor.get("backRight");
         backLeft = hardwareMap.dcMotor.get("backLeft");
+        grabber = hardwareMap.dcMotor.get("grabber");
+        diverter = hardwareMap.servo.get("diverter");
+        grabber.setDirection(DcMotor.Direction.REVERSE);
         right.setDirection(DcMotor.Direction.REVERSE);
         left.setDirection(DcMotor.Direction.FORWARD);
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
-        frontLeft.setDirection(DcMotor.Direction.FORWARD);
-        backRight.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setDirection(DcMotor.Direction.FORWARD);
-
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        diverter.setDirection(Servo.Direction.FORWARD);
+        gamepad1.setJoystickDeadzone(0.01f);
+        gamepad2.setJoystickDeadzone(0.01f);
     }
 
     @Override
     public void loop()
     {
         setDrive(gamepad1.right_stick_y, gamepad1.left_stick_y);
-        if(gamepad1.right_bumper)
+        grabber.setPower(gamepad2.left_stick_y);
+        if(gamepad2.right_stick_button)
         {
-            frontRight.setPower(speed);
-            frontLeft.setPower(speed);
+                diverter.setPosition(1.0f);
         }
-        else if(gamepad1.right_trigger > 0.05f)
+        else if(gamepad2.left_stick_button)
         {
-            frontRight.setPower(-speed);
-            frontLeft.setPower(-speed);
-        }
-        else
-        {
-            frontRight.setPower(0.0f);
-            frontLeft.setPower(0.0f);
-        }
-        if(gamepad1.left_bumper)
-        {
-            backRight.setPower(speed);
-            backLeft.setPower(speed);
-        }
-        else if(gamepad1.left_trigger > 0.05f)
-        {
-            backRight.setPower(-speed);
-            backLeft.setPower(-speed);
+                diverter.setPosition(0.0f);
         }
         else
         {
-            backRight.setPower(0.0f);
-            backLeft.setPower(0.0f);
+                diverter.setPosition(0.5f);
+        }
+
+        if(gamepad1.a)
+        {
+            frontRight.setPower(-0.2f);
+            frontLeft.setPower(-0.2f);
+            backRight.setPower(0.2f);
+            backLeft.setPower(0.2f);
+        }
+        else
+        {
+
+            if(gamepad1.right_bumper)
+            {
+                frontRight.setPower(speed);
+                frontLeft.setPower(speed);
+            }
+            else if(gamepad1.right_trigger > 0.05f)
+            {
+                frontRight.setPower(-speed);
+                frontLeft.setPower(-speed);
+            }
+            else
+            {
+                frontRight.setPower(0.0f);
+                frontLeft.setPower(0.0f);
+            }
+            if(gamepad1.left_bumper)
+            {
+                backRight.setPower(speed);
+                backLeft.setPower(speed);
+            }
+            else if(gamepad1.left_trigger > 0.05f)
+            {
+                backRight.setPower(-speed);
+                backLeft.setPower(-speed);
+            }
+            else
+            {
+                backRight.setPower(0.0f);
+                backLeft.setPower(0.0f);
+            }
         }
     }
 
