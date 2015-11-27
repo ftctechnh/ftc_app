34686c73f65f04a34c7078e437177bfdb958b148
Jeremy Yao <jeremy.yao.info@gmail.com>
2015-11-27 17:08:57 -0500

    made ThreeLightColorFile and test

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PracticeAttachments.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PracticeAttachments.java
@@ -3,8 +3,46 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 /**
  * Created by grempa on 11/27/15.
  */
-public class PracticeAttachments
+public class PracticeAttachments implements AttachmentInterface
 {
 
+    @Override
+    public void moveGrabberUp() {
 
+    }
+
+    @Override
+    public void moveGrabberDown() {
+
+    }
+
+    @Override
+    public void releaseClimbers() {
+
+    }
+
+    @Override
+    public void grabClimbers() {
+
+    }
+
+    @Override
+    public void primeObjectsInRobot() {
+
+    }
+
+    @Override
+    public void loadGrabber() {
+
+    }
+
+    @Override
+    public void collectObjects() {
+
+    }
+
+    @Override
+    public void pushButton(boolean isButtonLeft) {
+
+    }
 }
