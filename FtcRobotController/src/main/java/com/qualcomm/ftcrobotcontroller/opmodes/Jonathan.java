c6f1e70f380678f87d8db1f1fe982202f1964e1d
zrjve0815 <aiyoumaya@gmail.com>
2015-11-30 10:50:35 -0600

    change the a y to bumper, and add a new opcode for another robot

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Jonathan.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Jonathan.java
@@ -12,28 +12,32 @@ public class Jonathan extends OpMode {
     DcMotor upperMotor;
     DcMotor bottomMotor;
     @Override
-    public void init(){
+    public void init()
+    {
         leftMotor = hardwareMap.dcMotor.get("left_drive");
         rightMotor = hardwareMap.dcMotor.get("right_drive");
         upperMotor= hardwareMap.dcMotor.get("small_arm");
         bottomMotor= hardwareMap.dcMotor.get("big_arm");
     }
     @Override
-    public void loop(){
+    public void loop()
+    {
         float leftY = -gamepad1.left_stick_y;
         float rightY= gamepad1.right_stick_y;
         float rightTrigger=gamepad1.right_trigger;
         float leftTrigger=-gamepad1.left_trigger;
+        boolean rightBumper=gamepad1.right_bumper;
+        boolean leftBumper=gamepad1.left_bumper;
         boolean a=gamepad1.a;
         boolean b=gamepad1.b;
         boolean x=gamepad1.x;
         boolean y=gamepad1.y;
-        if (y)
+        if (rightBumper)
         {
             upperMotor.setDirection(DcMotor.Direction.FORWARD);
             upperMotor.setPower(1);
         }
-        if (a)
+        if (leftBumper)
         {
             upperMotor.setDirection(DcMotor.Direction.REVERSE);
             upperMotor.setPower(1);
@@ -42,18 +46,18 @@ public class Jonathan extends OpMode {
         {
             if (rightTrigger>0)
             {
-                bottomMotor.setPower(0.6*rightTrigger);
+                bottomMotor.setPower(0.25*rightTrigger);
             }
             if (leftTrigger<0)
             {
-                bottomMotor.setPower(0.6*leftTrigger);
+                bottomMotor.setPower(0.25*leftTrigger);
             }
         }
         if ((leftTrigger==0)&&(rightTrigger==0))
         {
             bottomMotor.setPower(0);
         }
-        if ((a==false)&&(y==false))
+        if ((leftBumper==false)&&(rightBumper==false))
         {
             upperMotor.setPower(0);
         }
