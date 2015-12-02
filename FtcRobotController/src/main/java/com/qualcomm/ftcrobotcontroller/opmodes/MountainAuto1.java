7a9c5ecb837d9ff80f2f8ca9fd734af0c10afe19
Kaitlin <k.berrygirl.10@gmail.com>
2015-12-01 19:37:53 -0500

    added first edition of mountain

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MountainAuto1.java
@@ -0,0 +1,21 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Kaitlin on 12/1/15.
+ */
+public class MountainAuto1 extends LinearOpMode
+{
+    DcMotor right;
+    DcMotor left;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+
+    }
+}
