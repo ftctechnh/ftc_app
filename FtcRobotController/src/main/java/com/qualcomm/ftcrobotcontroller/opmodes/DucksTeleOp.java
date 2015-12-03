c0fd0a19295f8b02b7b5276daa05a0da27613d50
Justin <kerrj@guilford.edu>
2015-12-02 20:50:13 -0500

    moved all basic teleop functions to "TeleOpCommands", creating setLeftPower, setRightPower, setClimbersPosition, and setPivotPower. added a time check to not update drive speed faster than 100 times per second

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DucksTeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DucksTeleOp.java
@@ -21,11 +21,6 @@ import java.util.logging.Handler;
  * Created by Dan on 11/9/2015.
  */
 public class DucksTeleOp extends TeleOpCommands {
-    int UPDATES=0;
-    long leftDown;
-    long leftUp;
-    long rightDown;
-    long rightUp;
     @Override
     public void init() {
         driveMC=hardwareMap.dcMotorController.get("driveMC");
@@ -35,71 +30,16 @@ public class DucksTeleOp extends TeleOpCommands {
 
     @Override
     public void loop() {
-//left wheel
-        if(gamepad1.left_stick_y>0 && driveMC.getMotorPower(LEFT)!=scaleInput(gamepad1.left_stick_y)){
-//            driveMC.setMotorPower(LEFT,Math.pow(gamepad1.left_stick_y, 2));
-            driveMC.setMotorPower(LEFT,scaleInput(gamepad1.left_stick_y));
-            UPDATES+=1;
-        }else if(gamepad1.left_stick_y<0 && driveMC.getMotorPower(LEFT)!=-scaleInput(gamepad1.left_stick_y)) {
-//            driveMC.setMotorPower(LEFT,-Math.pow(gamepad1.left_stick_y, 2));
-            driveMC.setMotorPower(LEFT,-scaleInput(gamepad1.left_stick_y));
-            UPDATES+=1;
+        if(System.currentTimeMillis()-LEFTUPDATE>10){
+            setLeftPower();
         }
-//right wheel
-        if(gamepad1.right_stick_y>0 && driveMC.getMotorPower(RIGHT)!=-scaleInput(gamepad1.right_stick_y)){
-//            driveMC.setMotorPower(RIGHT,-Math.pow(gamepad1.right_stick_y,2));
-            driveMC.setMotorPower(RIGHT,-scaleInput(gamepad1.right_stick_y));
-            UPDATES+=1;
-        } else if(gamepad1.right_stick_y<-.05 && driveMC.getMotorPower(RIGHT)!=scaleInput(gamepad1.right_stick_y)){
-//            driveMC.setMotorPower(RIGHT,Math.pow(gamepad1.right_stick_y,2));
-            driveMC.setMotorPower(RIGHT,scaleInput(gamepad1.right_stick_y));
-            UPDATES+=1;
+        if(System.currentTimeMillis()-RIGHTUPDATE>10){
+            setRightPower();
         }
-//winch in and out
-        if(gamepad1.a && winchMC.getMotorPower(WINCH)!=1){
-            winchMC.setMotorPower(WINCH,1);
-            wheelMC.setMotorPower(WINCHWHEEL, .2);
-            UPDATES+=1;
-        } else if(gamepad1.b && winchMC.getMotorPower(WINCH)!=-1){
-            winchMC.setMotorPower(WINCH, -1);
-            wheelMC.setMotorPower(WINCHWHEEL,-1);
-            UPDATES+=1;
-        } else if(!gamepad1.a && !gamepad1.b && winchMC.getMotorPower(WINCH)!=0){
-            winchMC.setMotorPower(WINCH, 0);
-            wheelMC.setMotorPower(WINCHWHEEL,0);
-            UPDATES+=1;
-        }
-//winch up and down
-        if(gamepad1.y && winchMC.getMotorPower(WINCHPIVOT)!=.2){
-            winchMC.setMotorPower(WINCHPIVOT,.2);
-            UPDATES+=1;
-        } else if(gamepad1.x && winchMC.getMotorPower(WINCHPIVOT)!=-.1){
-            winchMC.setMotorPower(WINCHPIVOT,-.1);
-            UPDATES+=1;
-        } else if(!gamepad1.y && !gamepad1.x && winchMC.getMotorPower(WINCHPIVOT)!=0) {
-            winchMC.setMotorPower(WINCHPIVOT,0);
-            UPDATES+=1;
-        }
-        //right climbers up
-        if(gamepad1.dpad_down && Math.abs(climbersRight.getPosition()-1)>.1){
-            climbersRight.setPosition(1);
-            UPDATES+=1;
-        }
-        //right climbers down
-        if(gamepad1.dpad_right && Math.abs(climbersRight.getPosition() - .25)>.1){
-            climbersRight.setPosition(.25);
-            UPDATES+=1;
-        }
-        //left climbers up
-        if(gamepad1.dpad_up && Math.abs(climbersLeft.getPosition() - .3)>.1){
-            climbersLeft.setPosition(.3);
-            UPDATES+=1;
-        }
-        //left climbers down
-        if(gamepad1.dpad_left && Math.abs(climbersLeft.getPosition()-1)>.1){
-            climbersLeft.setPosition(1);
-            UPDATES+=1;
-        }
+        setWinchPower();
+        setPivotPower();
+        setClimbersPosition();
+
         telemetry.addData("UPDATES",UPDATES);
         telemetry.addData("climbersLeft",climbersLeft.getPosition());
         telemetry.addData("climbersRight", climbersRight.getPosition());
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpCommands.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpCommands.java
@@ -19,6 +19,7 @@ public class TeleOpCommands extends OpMode {
     public static int WINCHWHEEL=1;
     public static int RIGHT=2;
     public static int LEFT=1;
+    public int UPDATES=0;
     public GyroSensor gyro;
     public DcMotorController winchMC;
     public DcMotorController driveMC;
@@ -26,7 +27,8 @@ public class TeleOpCommands extends OpMode {
     public Servo climbersLeft;
     public Servo climbersRight;
     public ColorSensor color;
-
+    public long LEFTUPDATE=0;
+    public long RIGHTUPDATE=0;
 
     double scaleInput(double dVal)  {
         double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
@@ -55,6 +57,82 @@ public class TeleOpCommands extends OpMode {
 
         // return scaled value.
         return dScale;
+    }
+    public void setLeftPower(){
+        if(gamepad1.left_stick_y>0 && driveMC.getMotorPower(LEFT)!=scaleInput(gamepad1.left_stick_y)){
+//            driveMC.setMotorPower(LEFT,Math.pow(gamepad1.left_stick_y, 2));
+            driveMC.setMotorPower(LEFT,scaleInput(gamepad1.left_stick_y));
+            UPDATES+=1;
+            LEFTUPDATE=System.currentTimeMillis();
+        }else if(gamepad1.left_stick_y<0 && driveMC.getMotorPower(LEFT)!=-scaleInput(gamepad1.left_stick_y)) {
+//            driveMC.setMotorPower(LEFT,-Math.pow(gamepad1.left_stick_y, 2));
+            driveMC.setMotorPower(LEFT,-scaleInput(gamepad1.left_stick_y));
+            UPDATES+=1;
+            LEFTUPDATE=System.currentTimeMillis();
+        }
+    }
+
+    public void setRightPower(){
+        if(gamepad1.right_stick_y>0 && driveMC.getMotorPower(RIGHT)!=-scaleInput(gamepad1.right_stick_y)){
+//            driveMC.setMotorPower(RIGHT,-Math.pow(gamepad1.right_stick_y,2));
+            driveMC.setMotorPower(RIGHT,-scaleInput(gamepad1.right_stick_y));
+            UPDATES+=1;
+            RIGHTUPDATE=System.currentTimeMillis();
+        } else if(gamepad1.right_stick_y<-.05 && driveMC.getMotorPower(RIGHT)!=scaleInput(gamepad1.right_stick_y)){
+//            driveMC.setMotorPower(RIGHT,Math.pow(gamepad1.right_stick_y,2));
+            driveMC.setMotorPower(RIGHT,scaleInput(gamepad1.right_stick_y));
+            UPDATES+=1;
+            RIGHTUPDATE=System.currentTimeMillis();
+        }
+    }
+    public void setWinchPower(){
+        if(gamepad1.a && winchMC.getMotorPower(WINCH)!=1){
+            winchMC.setMotorPower(WINCH,1);
+            wheelMC.setMotorPower(WINCHWHEEL, .2);
+            UPDATES+=1;
+        } else if(gamepad1.b && winchMC.getMotorPower(WINCH)!=-1){
+            winchMC.setMotorPower(WINCH, -1);
+            wheelMC.setMotorPower(WINCHWHEEL,-1);
+            UPDATES+=1;
+        } else if(!gamepad1.a && !gamepad1.b && winchMC.getMotorPower(WINCH)!=0){
+            winchMC.setMotorPower(WINCH, 0);
+            wheelMC.setMotorPower(WINCHWHEEL,0);
+            UPDATES+=1;
+        }
+    }
+    public void setPivotPower(){
+        if(gamepad1.y && winchMC.getMotorPower(WINCHPIVOT)!=.2){
+            winchMC.setMotorPower(WINCHPIVOT,.2);
+            UPDATES+=1;
+        } else if(gamepad1.x && winchMC.getMotorPower(WINCHPIVOT)!=-.1){
+            winchMC.setMotorPower(WINCHPIVOT,-.1);
+            UPDATES+=1;
+        } else if(!gamepad1.y && !gamepad1.x && winchMC.getMotorPower(WINCHPIVOT)!=0) {
+            winchMC.setMotorPower(WINCHPIVOT,0);
+            UPDATES+=1;
+        }
+    }
+    public void setClimbersPosition(){
+        //right climbers up
+        if(gamepad1.dpad_down && Math.abs(climbersRight.getPosition()-1)>.1){
+            climbersRight.setPosition(1);
+            UPDATES+=1;
+        }
+        //right climbers down
+        if(gamepad1.dpad_right && Math.abs(climbersRight.getPosition() - .25)>.1){
+            climbersRight.setPosition(.25);
+            UPDATES+=1;
+        }
+        //left climbers up
+        if(gamepad1.dpad_up && Math.abs(climbersLeft.getPosition() - .3)>.1){
+            climbersLeft.setPosition(.3);
+            UPDATES+=1;
+        }
+        //left climbers down
+        if(gamepad1.dpad_left && Math.abs(climbersLeft.getPosition()-1)>.1){
+            climbersLeft.setPosition(1);
+            UPDATES+=1;
+        }
     }
     @Override
     public void init() {
