ff84de0d580cbec544bd0e1b3ca9d2ccbfa1da98
Kaitlin <k.berrygirl.10@gmail.com>
2015-12-05 17:02:36 -0500

    Tried to average out the difference for the comparison, still needs work.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ThreeLightColorSensor.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ThreeLightColorSensor.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.hardware.LightSensor;
 import java.util.Scanner;
 
 /**
- * Created by grempa on 11/27/15.
+ * Created by grempa on 11/27/15
  */
 
 public class ThreeLightColorSensor
@@ -13,11 +13,12 @@ public class ThreeLightColorSensor
     public final int blueDetected = -1;
     public final int indeterminate = 0;
     public final int redDetected = 1;
-    public final int farFromBeacon = 2;
+   // public final int farFromBeacon = 2;
 
     private LightSensor red;
     private LightSensor blue;
     private LightSensor clear;
+
 
     public ThreeLightColorSensor(LightSensor redSensor, LightSensor blueSensor, LightSensor clearSensor)
     {
@@ -25,6 +26,8 @@ public class ThreeLightColorSensor
         blue = blueSensor;
         clear = clearSensor;
     }
+
+
 
     public int colorDetected()
     {
@@ -34,18 +37,37 @@ public class ThreeLightColorSensor
 
         int diffOfClearRed = Math.abs(clearLightDetected - redLightDetected);
         int diffOfClearBlue = Math.abs(clearLightDetected - blueLightDetected);
+        int totalDiffofClearRed = 0, totalDiffofClearBlue = 0;
 
 
-     //   if (blueLightDetected <= redLightDetected / 2 || redLightDetected <= blueLightDetected / 2) {
+        //   if (blueLightDetected <= redLightDetected / 2 || redLightDetected <= blueLightDetected / 2) {
      // may need beacon detection code as it requires the color sensors to be close to the beacon
 
             if (diffOfClearRed > diffOfClearBlue * 5)
             {
-                return blueDetected; //-1 means the light is blue
+                for (int i = 0; i < 5; i++)
+                {
+                    totalDiffofClearBlue += diffOfClearBlue;
+                    totalDiffofClearRed += diffOfClearRed;
+                }
+
+                if (totalDiffofClearBlue > totalDiffofClearRed * 5) // originally a algorithm used for finding the aveerage but if we divide, they cancel each other out
+                {
+                    return redDetected;//returns 1, meaning it's red
+                }
             }
             else if (diffOfClearBlue > diffOfClearRed * 5)
             {
-                return redDetected; // returns 1, meaning that the light is red
+                for (int i = 0; i < 5; i++)
+                {
+                    totalDiffofClearBlue += diffOfClearBlue;
+                    totalDiffofClearRed += diffOfClearRed;
+                }
+
+                if (totalDiffofClearRed > totalDiffofClearBlue * 5)
+                {
+                    return blueDetected;//returns -1, meaning blue is detected
+                }
             }
             else
             {
