d6fbb34ea0dd2e4bd96fb43f4b93192c7e610d27
PeterG <peterohio1@gmail.com>
2015-12-05 15:09:09 -0500

    Added a set of definitions for the driver interface on the competition bot and calibrated the values on the field mat

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CompBot.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CompBot.java
@@ -7,14 +7,14 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 /**
  * Created by Peter on 12/3/2015.
  */
-public class CombBot implements DriverInterface
+public class CompBot implements DriverInterface
 {
     static DcMotor leftMotor;
     static DcMotor rightMotor;
     private static final String leftMotorName = "left";
     private static final String rightMotorName = "right";
 
-    public CombBot(HardwareMap hardwareMap)
+    public CompBot(HardwareMap hardwareMap)
     {
         leftMotor = hardwareMap.dcMotor.get(leftMotorName);
         rightMotor = hardwareMap.dcMotor.get(rightMotorName);
@@ -42,7 +42,7 @@ public class CombBot implements DriverInterface
             speed*=-1.0;
             inches*=-1.0;
         }
-        int encoderTarget = ((int)(128.5713 * inches));
+        int encoderTarget = ((int)(123.42844 * inches));
         if(encoderTarget > 0)
         {
             leftMotor.setPower(speed);
@@ -75,7 +75,7 @@ public class CombBot implements DriverInterface
         rightMotor.setTargetPosition((int)(131.65432 * inches));*///previous code to run to set positions with setTargetPosition function
         leftMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         rightMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);//15.75 inc diameter
-        int encoderPivotTarget = ((int)(128.5713 * 98.960168 * degrees/360));
+        int encoderPivotTarget = ((int)(123.42844 * 148.44024 * degrees/360));//148.44024 is different than the calculation of the original turn diameter due to the robot jiggling and turning only half of its intended turn
         if (encoderPivotTarget > 0 && speed > 0)
         {
             rightMotor.setPower(speed);
@@ -114,7 +114,7 @@ public class CombBot implements DriverInterface
         while(rightMotor.getCurrentPosition() != 0){}
         leftMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         rightMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        int encoderSpinTarget = ((int)(128.5713 * 49.48 * degrees/360));
+        int encoderSpinTarget = ((int)(123.42844 * 98.96 * degrees/360));//98.96 is different than the calculation of the original turn diameter due to the robot jiggling and turning only half of its intended tur
         if(encoderSpinTarget > 0 && speed > 0)
         {
             leftMotor.setPower(speed);
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CompBotTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CompBotTest.java
@@ -1,8 +1,6 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
 
 /**
  * Created by Peter on 12/3/2015.
@@ -11,9 +9,9 @@ public class CompBotTest extends LinearOpMode
 {
     @Override public void runOpMode() throws InterruptedException
    {
-       DriverInterface robotDrive = new CombBot(hardwareMap);
+       DriverInterface robotDrive = new CompBot(hardwareMap);
        waitForStart();
-       robotDrive.moveStraightEncoders(24, 1.0f);//3ft-96.5% 2ft 95.3% need to recollect on a mat
-       stop();
+       robotDrive.spinOnCenter(-90, 1.0f);
+       robotDrive.stop();
    }
 }
