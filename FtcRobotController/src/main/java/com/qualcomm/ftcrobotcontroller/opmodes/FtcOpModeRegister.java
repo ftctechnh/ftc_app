29a6532424eaec6a5b16094c5cf589f8679dd510
PeterG <peterohio1@gmail.com>
2015-12-27 16:13:28 -0500

    Added a Class to test the robot's motors

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -75,6 +75,8 @@ public class FtcOpModeRegister implements OpModeRegister {
 
     manager.register("BlueMountainClimb", BlueMountClimb1.class);
 
+    manager.register("TestElectronics", TestElectronics.class);
+
     //manager.register("AdafruitRGBExample", AdafruitRGBExample.class);
     //manager.register("ColorSensorDriver", ColorSensorDriver.class);
 
