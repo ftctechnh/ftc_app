3ed312d6e78356f4408fec64f9f0f8b1dc60fd3c
yan vologzhanin <yanvolo@gmail.com>
2016-01-07 19:53:07 -0500

    Update LinearWalnutOpMode.java

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/LinearWalnutOpMode.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/LinearWalnutOpMode.java
@@ -1,92 +1,38 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-//FTC imports
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
-//Walnut Imports
-import com.qualcomm.ftcrobotcontroller.walnutLibrary.*;
+import com.qualcomm.ftcrobotcontroller.walnutLibrary.DistanceMotor;
+import com.qualcomm.ftcrobotcontroller.walnutLibrary.TimedMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import java.util.ArrayList;
 
 /**
  * Created by Yan Vologzhanin on 1/2/2016.
  */
-public class WalnutTeleOp extends OpMode{
-    //Initilze new thread
-    private WalnutMotor.GamepadUpdater updater;
-    private Thread processor;
-
-    //Physical Hardware
+public class LinearWalnutOpMode extends LinearOpMode {
+    //Initilize HardWare
     private DcMotor motorLeft;
     private DcMotor motorRight;
-    private DcMotor intakeSpinners;
-    private DcMotor spool;
 
-    private Servo belt;
-    private Servo doors;
-    //Button Assignments
-    private ArrayList<WalnutMotor> motors;
-    private IncSpinner leftDrive;
-    private IncSpinner rightDrive;
-    private DigSpinner intake;
-    private IncSpinner slides;
-    //No Button assignments available for servos :(
+    //Assign Hardware
+    private TimedMotor leftDrive;
+    private TimedMotor rightDrive;
 
-    //FTC Methods
-    public void init(){
-        //Initilize Hardware
+    public void runOpMode(){
+        //Initilize  Hardware
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
         motorRight = hardwareMap.dcMotor.get("motorRight");
-        intakeSpinners = hardwareMap.dcMotor.get("spinners");
-        spool = hardwareMap.dcMotor.get("slideLeft");
-        //Assign Buttons
-        leftDrive = new IncSpinner(motorLeft,"Left SimpleDrive",true,"LEFTY1", true, 0.05);
-        rightDrive = new IncSpinner(motorRight,"Right SimpleDrive",true,"RIGHTY1", false, 0.05);
-        slides = new IncSpinner(spool, "Slider", false, "LEFTY2",false,0.25);
-
-        intake = new DigSpinner(intakeSpinners,"intake",false,
-                "A2",-1,true);
-        intake.addButton("B2",0,true);
-        //Add Buttons to arrayList and initilize
-        motors = new ArrayList<WalnutMotor>();
-        motors.add(leftDrive);
-        motors.add(rightDrive);
-        motors.add(slides);
-        motors.add(intake);
-        //Can't do nutin about dem servos :(
-        belt = hardwareMap.servo.get("belt");
-        doors = hardwareMap.servo.get("doors");
-        belt.setPosition(0.5);
-        doors.setPosition(0.5);
-        //Initilize Stuff for Processing
-        updater = new WalnutMotor.GamepadUpdater();
-        processor = new Thread(updater);
-    }
-    public void start(){
-        //Start Gamepad Processing
-        updater.setGamepads(gamepad1, gamepad2);
-        processor.start();
-    }
-    public void loop(){
-        //Process motors
-        for(int i=0;i<motors.size();i++){
-            motors.get(i).operate();
+        //Assign Hardware
+        leftDrive = new TimedMotor(motorLeft,"Left Drive",false,false);
+        rightDrive = new TimedMotor(motorRight,"Right Drive",false,true);
+        //Wait for Start
+        try{
+            waitForStart();
         }
-        //Why Servos why???
-        belt.setPosition(1 - (gamepad2.right_stick_x + 1.0) / 2.0);
-        doors.setPosition((gamepad2.left_stick_x / 2.0) + 0.5);
-        //Used to compare actual and abstract values to see if value is updating fast enough
-        telemetry.addData("Direct Gamepad", String.format("%2f", gamepad1.left_stick_y));
-        telemetry.addData("Gamepad Updater",String.format("%2f",WalnutMotor.GamepadUpdater.doubleValues[1]));
-    }
-    public void stop(){
-       for(int i = 0;i<motors.size();i++){
-           motors.get(i).stopMotor();
-       }
-        belt.setPosition(0.5);
-        doors.setPosition(0.5);
-        WalnutMotor.GamepadUpdater.turnOffProcessing();
+        catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+        //SimpleDrive Forward
+        leftDrive.operate(3,1);
+        rightDrive.operate(3,1);
     }
 }
