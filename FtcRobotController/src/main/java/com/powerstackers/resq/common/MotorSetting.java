b74ed43c9c5e97f393ff6b53dd199c949071cf4d
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2016-01-07 20:16:38 -0500

    Adds license headers where needed
    Lots of files were missing the file headers, so I put them in.
    Also added back in the Swerve notations where they could be utilized.

--- FtcRobotController/src/main/java/com/powerstackers/resq/common/MotorSetting.java
+++ FtcRobotController/src/main/java/com/powerstackers/resq/common/MotorSetting.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2015 Powerstackers
  *
- * Different settings for servos.
+ * Different settings for motors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
