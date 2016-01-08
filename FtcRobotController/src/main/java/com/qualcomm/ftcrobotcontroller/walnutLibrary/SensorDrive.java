9d3d38f33ba7a8cfaa0c10fa7551f4be6fa017cc
yan vologzhanin <yanvolo@gmail.com>
2016-01-07 19:49:31 -0500

    Create SensorDrive.java

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/walnutLibrary/SensorDrive.java
@@ -0,0 +1,37 @@
+package com.qualcomm.ftcrobotcontroller.walnutLibrary;
+
+import com.qualcomm.robotcore.hardware.AccelerationSensor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+
+/**
+ * Created by Yan Vologzhanin on 1/4/2016.
+ */
+@Deprecated
+public class SensorDrive extends SimpleDrive {
+    private GyroSensor gyro;
+    private AccelerationSensor accel;
+    private boolean gyroOnly;
+
+    //Value used when motors need to be realigned
+    public static final double MOTORADJUSTMENTPOW = 0.8;
+    //Gyro only
+    public SensorDrive(IncSpinner myLeft, IncSpinner myRight, GyroSensor myGyro){
+        super(myLeft,myRight);
+        gyro = myGyro;
+        gyroOnly = true;
+    }
+
+    //Add an Accelerameter
+    public SensorDrive(IncSpinner myLeft, IncSpinner myRight,
+                       GyroSensor myGyro,AccelerationSensor myAccel){
+        this(myLeft, myRight, myGyro);
+        accel = myAccel;
+        //Will override the previous "true" value
+        gyroOnly = false;
+    }
+    //Stuff users can access
+    //public void linearDrive(int inches, ){
+    //
+    //}
+    //Stuff I worry about
+}
