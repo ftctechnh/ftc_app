4de0e7ae79fa66006dfb0b7726d24df754e03437
Jonathan Thomas <thomas.jonathan.c@gmail.com>
2016-01-07 21:23:36 -0500

    Adds a todo marker for hopper constants

--- FtcRobotController/src/main/java/com/powerstackers/resq/common/RobotConstants.java
+++ FtcRobotController/src/main/java/com/powerstackers/resq/common/RobotConstants.java
@@ -8,11 +8,12 @@ public class RobotConstants {
     public static final double CRS_REVERSE = 0.0;
     public static final double CRS_STOP    = 0.5;
     public static final double CRS_FORWARD = 1.0;
-
+// TODO Hopper constants
     public static final double HOPPER_RIGHT_OPEN = 0.0;
     public static final double HOPPER_RIGHT_CLOSE = 0.0;
     public static final double HOPPER_LEFT_OPEN = 0.0;
     public static final double HOPPER_LEFT_CLOSE = 0.0;
+
     public static final double BEACON_TAP_LEFT = 0.8;
     public static final double BEACON_TAP_RIGHT = 0.2;
     public static final double BEACON_RESTING = 0.5;
