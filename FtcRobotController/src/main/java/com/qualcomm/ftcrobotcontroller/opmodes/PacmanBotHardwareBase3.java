38a507c79cdbcd3a25f45b9e226108a30ddb95df
ThomasDoylend <tmdoylend@gmail.com>
2016-01-09 09:53:42 -0800

    Big update; added most schtuff

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PacmanBotHardwareBase3.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PacmanBotHardwareBase3.java
@@ -1,5 +1,6 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import com.qualcomm.ftcrobotcontroller.opmodes.control.RunToPositionController;
 import com.qualcomm.ftcrobotcontroller.opmodes.control.TwoPositionServo;
 import com.qualcomm.ftcrobotcontroller.opmodes.drive.Tesla;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -38,10 +39,12 @@ public class PacmanBotHardwareBase3 extends OpMode {
     DcMotor brush;
 
     DcMotor spareTire;
+    RunToPositionController spareTireController;
 
     public TwoPositionServo climberTripper;
     public TwoPositionServo hookRelease;
     public TwoPositionServo basketDoor;
+    public TwoPositionServo climberBucket;
 
     public Tesla drive;
 
@@ -59,5 +62,38 @@ public class PacmanBotHardwareBase3 extends OpMode {
 
         if (!side) climberTripper = new TwoPositionServo(hardwareMap.servo.get("finger"),0.55,0);
         //else       climberTripper = new TwoPositionServo(hardwareMap.servo.get("finger"),0.6,1);
+
+        basket = hardwareMap.dcMotor.get("basket");
+        brush  = hardwareMap.dcMotor.get("collector");
+
+        climberBucket = new TwoPositionServo(hardwareMap.servo.get("thrower"),1,0);
+        basketDoor = new TwoPositionServo(hardwareMap.servo.get("door"),0.65,0.05);
+        hookRelease = new TwoPositionServo(hardwareMap.servo.get("release"),.1,1);
+
+        winch = hardwareMap.dcMotor.get("winch");
+
+        spareTire = hardwareMap.dcMotor.get("tire");
+        spareTireController = new RunToPositionController(spareTire,false);
+
+        spareTireController.goTo(0,.5);
+
+    }
+
+    public void setBasketPower(double power) {
+        basket.setPower(power * -0.2);
+    }
+
+    public void setBrushPower(double power) {
+        brush.setPower(-power);
+    }
+
+    public void setWinchPower(double power) {winch.setPower(power);}
+
+    public void setSpareTire(boolean pos) {
+        if (pos) {
+            spareTireController.goTo(240,1);
+        } else {
+            spareTireController.goTo(0,.25);
+        }
     }
 }
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/red/RedManualDrive.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/red/RedManualDrive.java
@@ -1,6 +1,8 @@
 package com.qualcomm.ftcrobotcontroller.opmodes.red;
 
+import com.qualcomm.ftcrobotcontroller.opmodes.DriveMath;
 import com.qualcomm.ftcrobotcontroller.opmodes.PacmanBotHardwareBase3;
+import com.qualcomm.ftcrobotcontroller.opmodes.drive.Drive;
 import com.qualcomm.ftcrobotcontroller.opmodes.ui.ToggleButton;
 
 /**
@@ -17,6 +19,9 @@ public class RedManualDrive extends PacmanBotHardwareBase3 {
     public String version = "1.0.0";
 
     public ToggleButton climberTripperToggle = new ToggleButton();
+    public ToggleButton mtnModeToggle = new ToggleButton();
+    public ToggleButton hookReleaseToggle = new ToggleButton();
+    public ToggleButton spareTireToggle = new ToggleButton();
 
     @Override
     public void init() {
@@ -39,11 +44,35 @@ public class RedManualDrive extends PacmanBotHardwareBase3 {
         double driveRate = -gamepad1.left_stick_y; //Negate the Y as mentioned above
         double turnRate  = gamepad1.right_stick_x;
 
-        drive.driveStd(driveRate,turnRate); //Drive as desired!
+        mtnModeToggle.update(gamepad1.left_stick_button);
+
+        if (mtnModeToggle.getState()) {
+            drive.driveMtn(driveRate,turnRate); //Drive as desired!
+        } else {
+            drive.driveStd(driveRate,turnRate);
+        }
 
         climberTripperToggle.update(gamepad1.x);
         if (climberTripperToggle.isEvent()) {
             climberTripper.set(climberTripperToggle.getState());
+        }
+
+        setBasketPower(DriveMath.threeWay(gamepad1.dpad_left,gamepad1.dpad_right));
+        setBrushPower(DriveMath.threeWay(gamepad1.left_trigger>.5,gamepad1.left_bumper));
+
+        climberBucket.set(gamepad1.a);
+        basketDoor.set(gamepad1.b || gamepad1.left_bumper);
+
+        hookReleaseToggle.update(gamepad1.start);
+        if (hookReleaseToggle.isEvent()) {
+            hookRelease.set(hookReleaseToggle.getState());
+        }
+
+        setWinchPower(DriveMath.threeWay(gamepad1.right_bumper,gamepad1.right_trigger>.5));
+
+        spareTireToggle.update(gamepad1.y);
+        if (spareTireToggle.isEvent()){
+            setSpareTire(spareTireToggle.getState());
         }
     }
 }
