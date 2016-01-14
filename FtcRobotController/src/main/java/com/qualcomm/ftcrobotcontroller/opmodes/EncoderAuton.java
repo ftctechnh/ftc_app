039abcb0a4707175e7f2c0048ad0ceeabd049827
Eden Grown-Haeberli <edengh@gmail.com>
2016-01-14 15:20:52 -0800

    Updates

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EncoderAuton.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EncoderAuton.java
@@ -58,27 +58,53 @@ public class EncoderAuton extends OpMode {
         telemetry.addData("sliderL", "sliderL: " + String.format("%.2f", tbc.sliderLPosition));
         telemetry.addData("sliderR", "sliderR:" + String.format("%.2f", tbc.sliderRPosition));
         telemetry.addData("mtape", "mtape: " + String.format("%.2f", tbc.mtapePosition));
+        telemetry.addData("motorFLeft", "motorFLeft" + String.format("%.2f", tbc.motorFLeft.getPower()));
+        telemetry.addData("motorRLeft", "motorRLeft" + String.format("%.2f", tbc.motorRLeft.getPower()));
+        telemetry.addData("motorFRight", "motorFRight" + String.format("%.2f", tbc.motorFRight.getPower()));
+        telemetry.addData("motorRRight", "motorRRight" + String.format("%.2f", tbc.motorRRight.getPower()));
+        telemetry.addData("lf encoder", "lf encoder:" + String.format("%d", tbc.motorFLeft.getCurrentPosition()));
+        telemetry.addData("rf encoder", "rf encoder:" + String.format("%d", tbc.motorFRight.getCurrentPosition()));
+        telemetry.addData("lr encoder", "lr encoder:" + String.format("%d", tbc.motorRLeft.getCurrentPosition()));
+        telemetry.addData("rr encoder", "rr encoder:" + String.format("%d", tbc.motorRRight.getCurrentPosition()));
+
+        tbc.motorFRight.setTargetPosition(-13500);
+        tbc.motorRLeft.setTargetPosition(-13500);
+        tbc.motorFLeft.setTargetPosition(-13500);
+        tbc.motorRRight.setTargetPosition(-13500);
+        tbc.setMotorRLeftPower(-1.0f);
+        tbc.setMotorFRightPower(-1.0f);
+        tbc.setMotorFLeftPower(-1.0f);
+        tbc.setMotorRRightPower(-1.0f);
+
+        /*
+        tbc.motorFRight.setTargetPosition(-15000);
+        tbc.motorRLeft.setTargetPosition(-12200);
+        tbc.motorFLeft.setTargetPosition(-12200);
+        tbc.motorRRight.setTargetPosition(-15000);
+        tbc.setMotorRLeftPower(0.5f);
+        tbc.setMotorFRightPower(-0.5f);
+        tbc.setMotorFLeftPower(0.5f);
+        tbc.setMotorRRightPower(-0.5f);
+
+        tbc.motorFRight.setTargetPosition(-16200);
+        tbc.motorRLeft.setTargetPosition(-13600);
+        tbc.motorFLeft.setTargetPosition(-13600);
+        tbc.motorRRight.setTargetPosition(-16200);
+        tbc.setMotorRLeftPower(-0.5f);
+        tbc.setMotorFRightPower(-0.5f);
+        tbc.setMotorFLeftPower(-0.5f);
+        tbc.setMotorRRightPower(-0.5f);
+
+        */
+
+            //mRuntime.reset();
+
+           // while (time <= 0.5) {
+               // tbc.setButtonServoSpeed(0.5);
+           // }
+
+            //tbc.setClimberPosition(0.0);
 
 
-            tbc.motorFRight.setTargetPosition(2000);
-            tbc.motorRLeft.setTargetPosition(2000);
-            tbc.motorFLeft.setTargetPosition(2000);
-            tbc.motorRRight.setTargetPosition(2000);
-            tbc.setMotorFLeftPower(-1.0f);
-            tbc.setMotorFRightPower(-1.0f);
-            tbc.setMotorRLeftPower(-1.0f);
-            tbc.setMotorRRightPower(-1.0f);
-
-
-
-            mRuntime.reset();
-
-            while (time <= 0.5) {
-                tbc.setButtonServoSpeed(0.5);
-            }
-
-            tbc.setClimberPosition(0.0);
-
-        
     }
 }
