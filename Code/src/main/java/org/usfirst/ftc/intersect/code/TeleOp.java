1951e4546b2af7cdc3c8419caebac8bf2817c559
Anton <antonjlin@gmail.co>
2016-01-15 21:57:28 -0800

    servo code

--- Code/src/main/java/org/usfirst/ftc/intersect/code/TeleOp.java
+++ Code/src/main/java/org/usfirst/ftc/intersect/code/TeleOp.java
@@ -20,7 +20,7 @@ public class TeleOp extends SynchronousOpMode {
 	DcMotor linearSlideL;
 
 	Servo containerTilt;
-
+	Servo tubeExtender;
 	DcMotor sweeper;
 
 	//Declare gamepad objects
@@ -36,6 +36,8 @@ public class TeleOp extends SynchronousOpMode {
 	boolean sweeperForward = false;
 	boolean sweeperBackward = false;
 
+	boolean tubeExtend = false;
+	boolean tubeRetract = false;
 	@Override public void main() throws InterruptedException {
 		//Initialize hardware
 		frontRightWheel = hardwareMap.dcMotor.get("frontRightWheel");
@@ -47,6 +49,7 @@ public class TeleOp extends SynchronousOpMode {
 		linearSlideL = hardwareMap.dcMotor.get("linearSlideL");
 
 		//containerTilt = hardwareMap.servo.get("containerTilt");
+        //tubeExtender = hardwareMap.servo.get("tubeExtender");
 
 		sweeper = hardwareMap.dcMotor.get("sweeper");
 
@@ -75,24 +78,27 @@ public class TeleOp extends SynchronousOpMode {
 				rightWheel = gamepad1.right_stick_y;
 				leftWheel = gamepad1.left_stick_y;
 
-				linearSlideForward = gamepad1.a || gamepad2.a;
-				linearSlideBackward = gamepad1.b || gamepad2.b;
+				linearSlideForward = gamepad1.right_bumper || gamepad2.right_bumper;
+				linearSlideBackward = gamepad1.left_bumper || gamepad2.left_bumper;
 
 				containerTiltRight = gamepad1.dpad_right || gamepad2.dpad_right;
 				containerTiltLeft = gamepad1.dpad_left || gamepad1.dpad_left;
 
-				if(gamepad1.right_bumper || gamepad2.right_bumper) {
+				tubeExtend = gamepad1.x || gamepad2.x;
+				tubeRetract = gamepad1.b || gamepad2.b;
+
+				if(gamepad1.a || gamepad2.a) {
 					sweeperForward = !sweeperForward;
 					sweeperBackward = false;
-				} else if(gamepad1.left_bumper || gamepad2.left_bumper) {
+				} else if(gamepad1.y || gamepad2.y) {
 					sweeperBackward  = !sweeperBackward;
 					sweeperForward = false;
 				}
 
 				//Use gamepad values to move robot
-				Functions.moveTwoMotors(frontRightWheel, backRightWheel,
+				Functions.moveTwoMotors(backRightWheel,frontRightWheel,
 						Functions.convertGamepad(rightWheel));
-				Functions.moveTwoMotors(frontLeftWheel, backLeftWheel,
+				Functions.moveTwoMotors( backLeftWheel,frontLeftWheel,
 						Functions.convertGamepad(leftWheel));
 
 				if(linearSlideForward) {
@@ -101,6 +107,14 @@ public class TeleOp extends SynchronousOpMode {
 					Functions.moveTwoMotors(linearSlideR, linearSlideL, -0.3);
 				} else {
 					Functions.moveTwoMotors(linearSlideR, linearSlideL, 0.0);
+				}
+
+				if(tubeExtend) {
+					tubeExtender.setPosition(0.75);
+				} else if(tubeRetract) {
+					tubeExtender.setPosition(-0.75);
+				} else {
+					tubeExtender.setPosition(0);
 				}
 
 				if(containerTiltRight) {
@@ -112,9 +126,9 @@ public class TeleOp extends SynchronousOpMode {
 				}
 
 				if(sweeperForward) {
-					sweeper.setPower(0.5);
+					sweeper.setPower(-1.0);
 				} else if(sweeperBackward){
-					sweeper.setPower(-0.5);
+					sweeper.setPower(1.0);
 				} else {
 					sweeper.setPower(0);
 				}
