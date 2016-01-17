a1040761d2c74943857de6464a4c939c5c5f206d
yan vologzhanin <yanvolo@gmail.com>
2016-01-17 13:39:15 -0500

    bv 1.0.1
    Uploaded changes from work on 1/16
    Fixed timers on timed motors
    Created test for distance motors that I will apply to actual object

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/LinearDebug.java
@@ -0,0 +1,21 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by wolfie on 1/16/16.
+ */
+public class LinearDebug extends OpMode {
+    public void init(){
+
+    }
+    public void start(){
+
+    }
+    public void loop(){
+
+    }
+    public void stop(){
+
+    }
+}
