1ac761457f72cb4896bcc5af466c6ba7ad0604b2
brandontechnicbots <brandonbigbrother@gmail.com>
2016-01-18 18:28:43 -0600

    Autonomous Debug

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/_ResQAuto.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/_ResQAuto.java
@@ -77,7 +77,7 @@ public abstract class _ResQAuto extends LinearOpMode {
         } else {
             debugValues.add(formatter.format(new Date()) + " Red ");
         }
-        double EOPDThreshold = 0.1 * BLACKVALUE + 0.9 * WHITEVALUE;
+        double EOPDThreshold = 0.2 * BLACKVALUE + 0.8 * WHITEVALUE;
         debugValues.add(formatter.format(new Date()) + " EOPDThreshold:" + EOPDThreshold);
 
         //map the code to hardware map
@@ -131,7 +131,7 @@ public abstract class _ResQAuto extends LinearOpMode {
         frontLeftWheel.setPower(0.2);
         backRightWheel.setPower(0.2);
         backLeftWheel.setPower(0.2);
-        sleep(3000);
+       // sleep(3000);
         sweeper.setPower(-1);
 
 
@@ -251,9 +251,9 @@ public abstract class _ResQAuto extends LinearOpMode {
             telemetry.addData("Reflectance Value", reflectance);
             telemetry.addData("Ultrasonic Value", distance);
 
-            //Wait until ultrasonic distance <24
+            //Wait until ultrasonic distance <=23
             //Ignore the if/else
-            if(distance < 24 && distance>1) {
+            if(distance <= 23 && distance>1) {
                 if (getRedAlliance() == 0) {
                     /*frontLeftWheel.setPower(0.3);
                     backLeftWheel.setPower(0.3);*/
