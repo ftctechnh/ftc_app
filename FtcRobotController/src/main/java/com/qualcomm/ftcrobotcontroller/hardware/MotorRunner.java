dbdc058c011cbb1e7148d37dd2cf4f5a75f9d78f
ectucker <ETundo15@gmail.com>
2016-01-21 19:32:06 -0600

    Added ability to run multiple motors with MotorRunner

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/hardware/MotorRunner.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/hardware/MotorRunner.java
@@ -19,6 +19,18 @@ public class MotorRunner {
     private static ArrayList<RunEvent> events = new ArrayList<RunEvent>();
 
     /**
+     * Sets an array of {@link DcMotor} to a certain power.
+     *
+     * @param motors The motors to run
+     * @param power  The power to run at
+     */
+    public static void setMotorPowers(DcMotor[] motors, double power) {
+        for (DcMotor motor : motors) {
+            motor.setPower(power);
+        }
+    }
+
+    /**
      * Runs a {@link DcMotor} with an {@link Unit}
      * This function will block, only use in a {@link LinearOpMode}
      *
@@ -40,6 +52,33 @@ public class MotorRunner {
                 motor.setPower(0);
                 motor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
                 motor.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+            }
+        }
+    }
+
+    /**
+     * Runs an array of {@link DcMotor} with an {@link Unit}
+     * The the first element in the array must be a motor with an encoder
+     * This function will block, only use in a {@link LinearOpMode}
+     *
+     * @param motors The motors to run
+     * @param power  The power to run at
+     * @param unit   The unit to run
+     */
+    public static void run(DcMotor[] motors, double power, Unit unit) throws InterruptedException {
+        if (motors[0] != null) {
+            if (unit instanceof TimeUnit) {
+                setMotorPowers(motors, power);
+                Thread.sleep(unit.getValue());
+                setMotorPowers(motors, 0);
+            } else if (unit instanceof EncoderUnit) {
+                motors[0].setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+                motors[0].setTargetPosition((int) unit.getValue());
+                setMotorPowers(motors, power);
+                while (motors[0].isBusy()) ;
+                setMotorPowers(motors, 0);
+                motors[0].setMode(DcMotorController.RunMode.RESET_ENCODERS);
+                motors[0].setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
             }
         }
     }
