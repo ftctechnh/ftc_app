1e22c23c4b5a780b983014cdf110a0fd008a4ba3
PeterG <peterohio1@gmail.com>
2016-02-04 19:36:38 -0500

    Added method to initialize the robot in autonomous

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OpModeAutoTrial.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OpModeAutoTrial.java
@@ -6,47 +6,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 /**
  * Created by Kaitlin on 1/15/16.
  */
-public class OpModeAutoTrial extends OpMode implements DriverInterface
-{
-    CompBotOpMode compBot;
-     int step;
+public class OpModeAutoTrial// implements DriverInterface
+ {
 
-    @Override
-    public void init()
-    {
-        compBot = new CompBotOpMode(hardwareMap);
-        compBot.stop();
-        step = 1;
-        telemetry.addData("init",0);
-    }
-
-    @Override
-    public void loop()
-    {
-                    telemetry.addData("move forward 2", 3);
-                    //compBot.moveStraightEncoders(5, (float) .9);
-                    //compBot.stop();
-                    break;
-                default:
-                    telemetry.addData("default", step);
-                    //compBot.stop();
-                    break;
-            }
-            step++;
-            telemetry.addData("stepped ", 6);
-        }
-        else
-        {
-            telemetry.addData("running step",step);
-            telemetry.addData("Current is", compBot.returnCurrent());
-            telemetry.addData("Target is", compBot.returnTarget());
-        }
-
-    }
-
-    @Override
-    public void moveStraightEncoders(float inches, float speed) { }
-
-
-    public void spinOnCenter(float degrees, float speed) { }
 }
