6457f56f3f4803f63a2458e01093e1648c30ae3a
Subash Chebolu <subashch6@gmail.com>
2016-02-04 19:46:42 -0500

    changes stuff

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OpModeAutoTrial.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OpModeAutoTrial.java
@@ -6,24 +6,22 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 /**
  * Created by Kaitlin on 1/15/16.
  */
-public class OpModeAutoTrial extends OpMode implements DriverInterface
+public class OpModeAutoTrial extends OpMode //implements DriverInterface
 {
     CompBotOpMode compBot;
-     int step;
+    int step;
 
     @Override
-    public void init()
-    {
+    public void init() {
         compBot = new CompBotOpMode(hardwareMap);
         compBot.stop();
         step = 1;
-        telemetry.addData("init",0);
+        telemetry.addData("init", 0);
     }
 
     @Override
-    public void loop()
-    {
-                    telemetry.addData("move forward 2", 3);
+    public void loop() {
+                    /*telemetry.addData("move forward 2", 3);
                     //compBot.moveStraightEncoders(5, (float) .9);
                     //compBot.stop();
                     break;
@@ -42,11 +40,6 @@ public class OpModeAutoTrial extends OpMode implements DriverInterface
             telemetry.addData("Target is", compBot.returnTarget());
         }
 
+    }*/
     }
-
-    @Override
-    public void moveStraightEncoders(float inches, float speed) { }
-
-
-    public void spinOnCenter(float degrees, float speed) { }
 }
