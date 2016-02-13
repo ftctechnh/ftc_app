a6d367e85cb2d2ed896a3112c4ed92e6415cf4cb
Robert Atkinson <bob@theatkinsons.org>
2016-02-13 12:50:46 -0800

    fix TelemetryDashboardAndLog ctor usage in SynchronousOpMode; improve motor servo tests
    Former-commit-id: 6d1d2f24b1fdfb5796a6c8a2e381fce2a5eb57bb
    Former-commit-id: 44f76c639a6295113c3730eebcc5fdec57283f3d

--- SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/internal/tests/LinearMotorServoTest.java
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/internal/tests/LinearMotorServoTest.java
@@ -5,8 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.robocol.Telemetry;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.swerverobotics.library.ClassFactory;
+import org.swerverobotics.library.TelemetryDashboardAndLog;
 import org.swerverobotics.library.interfaces.Disabled;
+import org.swerverobotics.library.interfaces.IOpModeLoopCounter;
 import org.swerverobotics.library.interfaces.TeleOp;
 
 /**
@@ -26,22 +31,16 @@ public class LinearMotorServoTest extends LinearOpMode
         motor = this.hardwareMap.dcMotor.get("motor");
         servo = this.hardwareMap.servo.get("servo");
 
-        motorController = motor.getController();
-        motorController.setMotorControllerDeviceMode(DcMotorController.DeviceMode.WRITE_ONLY);
-        for (;;)
-            {
-            DcMotorController.DeviceMode mode = motorController.getMotorControllerDeviceMode();
-            if (mode == DcMotorController.DeviceMode.READ_WRITE || mode== DcMotorController.DeviceMode.WRITE_ONLY)
-                break;
-            Thread.yield();
-            }
-
         waitForStart();
 
         double servoPosition = 0;
         servo.setPosition(servoPosition);
 
         motor.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
+
+        ElapsedTime elapsedTime = new ElapsedTime();
+        IOpModeLoopCounter loopCounter = ClassFactory.createLoopCounter(this);
+        int spinCount = 0;
 
         while (this.opModeIsActive())
             {
@@ -52,8 +51,25 @@ public class LinearMotorServoTest extends LinearOpMode
 
             motor.setPower(0.1);
 
-            telemetry.addData("position", servoPosition);
-            waitOneFullHardwareCycle();
+            int loopCount = loopCounter.getLoopCount();
+            spinCount++;
+            double ms = elapsedTime.milliseconds();
+            telemetry.addData("position", format(servoPosition));
+            telemetry.addData("#spin",    format(spinCount));
+            telemetry.addData("ms/spin",  format(ms / spinCount));
+            telemetry.addData("ms/loop",  format(ms / loopCount));
+            this.updateTelemetry(telemetry);
             }
+
+        loopCounter.close();
+        }
+
+    static String format(double d)
+        {
+        return String.format("%.3f", d);
+        }
+    static String format(int i)
+        {
+        return String.format("%d", i);
         }
     }
