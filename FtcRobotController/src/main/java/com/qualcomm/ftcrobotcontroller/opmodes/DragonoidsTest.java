463f3512e2d0ea0d7dd4783d09b31d471fbc8985
Ryan Petschek <petschekr@gmail.com>
2016-02-19 20:08:46 -0500

    Add color and optical distance sensor tests

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DragonoidsTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DragonoidsTest.java
@@ -1,40 +1,31 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 
 public class DragonoidsTest extends OpMode {
-    private DcMotor testMotor;
-    private final double maxPower = 0.65;
-    private final double midPower = 0.30;
+    private ColorSensor colorSensor;
+    private OpticalDistanceSensor opticalDistanceSensor;
     @Override
     public void init() {
         DragonoidsGlobal.init(hardwareMap);
-        testMotor = hardwareMap.dcMotor.get("test");
+        colorSensor = hardwareMap.colorSensor.get("color");
+        opticalDistanceSensor = hardwareMap.opticalDistanceSensor.get("distance");
+        // Change for testing
+        colorSensor.enableLed(true);
     }
     @Override
     public void loop() {
-        if (gamepad1.a){
-            testMotor.setPower(midPower);
-        }
-        else if (gamepad1.x) {
-            testMotor.setPower(maxPower);
-        }
-        else if (gamepad1.y) {
-            testMotor.setPower(-midPower);
-        }
-        else if (gamepad1.b) {
-            testMotor.setPower(-maxPower);
-        }
-        else {
-            testMotor.setPower(0);
-            DragonoidsGlobal.stopAll();
-        }
-
         this.outputTelemetry();
     }
     private void outputTelemetry() {
-        // No telemetry in this test program for the moment
+        telemetry.addData("Red", colorSensor.red());
+        telemetry.addData("Blue", colorSensor.blue());
+        telemetry.addData("Light", opticalDistanceSensor.getLightDetected());
+        telemetry.addData("Light raw", opticalDistanceSensor.getLightDetectedRaw());
+        // Stop when > 0.1 on ODS
     }
     @Override
     public void stop() {
