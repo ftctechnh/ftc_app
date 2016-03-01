c0507c2acb6a9c56e45d9870db34986271755059
d33ck0 <Lucasburkowski@gmail.com>
2016-02-29 19:31:43 -0500

    Revert "Swerve Beta Update"
    This reverts commit 69aae43fdcd0d5fdcd1687b81967fbe76460e8a3.

--- SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/interfaces/IInterruptableRunnable.java
+++
@@ -1,9 +0,0 @@
-package org.swerverobotics.library.interfaces;
-
-/**
- * A variation on Runnable that allows InterruptedExceptions to be thrown
- */
-public interface IInterruptableRunnable
-    {
-    void run() throws InterruptedException;
-    }
