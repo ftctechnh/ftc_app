3128304b6c342b3d30e3277f7813e5d74652ee46
Arka Purkayastha <arka.purkayastha10@gmail.com>
2016-03-12 16:19:45 +0000

    Arm control code 1
    Please work

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ArmControl.java
@@ -0,0 +1,36 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by arkapurkayastha on 12/03/2016.
+ */
+public class ArmControl extends OpMode {
+
+    DcMotor leftArm;
+    DcMotor rightArm
+
+    @Override
+    public void init() {
+        leftArm = hardwareMap.dcMotor.get("left_arm");
+        rightArm = hardwareMap.dcMotor.get("right_arm");
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad2.y) {
+            leftArm.setPower(.1);
+            rightArm.setPower(.1);
+        }
+        else if(gamepad2.b) {
+            leftArm.setPower(-.1);
+            rightArm.setPower(-.1);
+        }
+        else {
+            leftArm.setPower(0);
+            rightArm.setPower(0);
+        }
+    }
+}
+
