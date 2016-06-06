381db72d5ae71dfd7f35903dda9fda29015ae9fe
Michael Ray <mray190@gmail.com>
2016-06-06 18:34:18 -0500

    Created base code

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ARMAuto.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ARMAuto.java
@@ -7,26 +7,44 @@ public class ARMAuto extends LinearOpMode {
     DcMotor leftMotor;
     DcMotor rightMotor;
 
+    enum State { FWD, TURN, STOP}
+
     @Override
     public void runOpMode() throws InterruptedException {
+
+        boolean endCondition = false;
+        State current_state = State.STOP;
+
         leftMotor = hardwareMap.dcMotor.get("left_drive");
         rightMotor = hardwareMap.dcMotor.get("right_drive");
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
 
-        for(int i=0; i<4; i++) {
-            leftMotor.setPower(1.0);
-            rightMotor.setPower(1.0);
-            sleep(1000);
+        while (!endCondition) {
 
-            leftMotor.setPower(0.5);
-            rightMotor.setPower(-0.5);
-            sleep(1000);
+            //@TODO Update sensor model
+
+            //@TODO Update state conditions
+
+            switch (current_state) {
+                case FWD:
+                    //@TODO Use PD control to drive straight
+                    //@TODO update current state
+                    break;
+                case TURN:
+                    //@TODO Use P control to turn desired angle
+                    //@TODO update current state
+                    break;
+                case STOP:
+                    //@TODO stop wheels
+                    //@TODO update current state
+                    break;
+                default:
+                    current_state = State.STOP;
+                    break;
+            }
         }
-
-        leftMotor.setPowerFloat();
-        rightMotor.setPowerFloat();
     }
 }
 
