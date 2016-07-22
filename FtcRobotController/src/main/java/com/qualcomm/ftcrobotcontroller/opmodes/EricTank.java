87aa411da4aa796d0d2e10c87ad66d498a4e14bc
FTC9977 <eharwood6311@gmail.com>
2016-07-22 15:28:46 -0400

    Test Commit of Tank Code...  Kevin said so...

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/EricTank.java
@@ -0,0 +1,40 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by eharwood on 7/17/16.
+ */
+public class EricTank extends OpMode {
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+    DcMotor leftMotor2;
+
+    @Override
+    public void init() {
+        // get references to the motors from the hardware map
+        //  and apply to each motor identifier bkah bka
+        leftMotor = hardwareMap.dcMotor.get("Left_Drive");
+        rightMotor = hardwareMap.dcMotor.get("Right_Drive");
+
+        //Reverse the Right Motor Direction
+        // yet ahoaofaodsjajdsasdfasdf
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+    }
+
+    @Override
+    public void loop() {
+        // get the values from the gamepads
+        // note: pushing the joystick all the way up returns -1
+        // so we need to reverse the values
+
+        float leftY = -gamepad1.left_stick_y;
+        float rightY = -gamepad1.right_stick_y;
+
+        // set the power of the motors with the gamepad values
+        leftMotor.setPower(leftY);
+        rightMotor.setPower(rightY);
+    }
+}
