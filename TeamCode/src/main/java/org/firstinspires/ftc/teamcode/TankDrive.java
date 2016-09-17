da92f233ef548c8126e2e180fcb76cabfba893fc
Team Shockwave 3848 <robotics@grauerschool.com>
2016-09-17 12:42:55 -0700

    Createed TankDrive class
    New sdk requires creation of HardwareMap

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -0,0 +1,92 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * This OpMode uses the common HardwareK9bot class to define the devices on the robot.
+ * All device access is managed through the HardwareK9bot class. (See this class for device names)
+ * The code is structured as a LinearOpMode
+ *
+ * This particular OpMode executes a basic Tank Drive Teleop for the K9 bot
+ * It raises and lowers the arm using the Gampad Y and A buttons respectively.
+ * It also opens and closes the claw slowly using the X and B buttons.
+ *
+ * Note: the configuration of the servos is such that
+ * as the arm servo approaches 0, the arm position moves up (away from the floor).
+ * Also, as the claw servo approaches 0, the claw opens up (drops the game element).
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Shockwave: TankDrive", group="Shockwave")
+@Disabled
+public class TankDrive extends LinearOpMode {
+
+    public DcMotor left = null;
+    public DcMotor right = null;
+
+    HardwareMap hwmap = null;
+
+
+    public void init(HardwareMap ahwmap){
+        hwmap = ahwmap;
+        left = hwmap.dcMotor.get("left");
+        right = hwmap.dcMotor.get("right");
+        right.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        telemetry.addData("Say", "Welcome to the exreme scrimmage bot");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            left.setPower(-gamepad1.left_stick_y);
+            right.setPower(-gamepad1.right_stick_y);
+
+            idle();
+        }
+    }
+}
