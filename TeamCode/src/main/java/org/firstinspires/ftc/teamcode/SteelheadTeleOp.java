783041d62200af1dda9e5e4db8c537f8298c2364
alec500oo <alec500oo@gmail.com>
2016-09-18 11:54:57 -0700

    sync

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SteelheadTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SteelheadTeleOp.java
@@ -100,9 +100,10 @@ public class SteelheadTeleOp extends OpMode{
 
         // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
         left = gamepad1.left_stick_y;
-        right = gamepad1.right_stick_y;
+        right = (-1d*gamepad1.right_stick_y);
         robot.leftMotor.setPower(left);
         robot.rightMotor.setPower(right);
+
 
         telemetry.addData("left",  "%.2f", left);
         telemetry.addData("right", "%.2f", right);
