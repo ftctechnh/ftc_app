dae84d4acd692376ab9872dcb6c350b8a2e8dbf3
DJ Figs <djfigs10@gmail.com>
2016-09-23 19:40:13 -0400

    Removed Pushbot Comments, added Sensors, and added method.
    Seriously, we don’t need those pushbot comments.
    They’re very large and take up so much space.
    And we don’t even have a pushbot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -1,88 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.ftccommon.DbgLog;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
-
-//------------------------------------------------------------------------------
-//
-// PushBotHardware
-//
-/**
- * Provides a single hardware access point between custom op-modes and the
- * OpMode class for the Push Bot.
- *
- * This class prevents the custom op-mode from throwing an exception at runtime.
- * If any hardware fails to map, a warning will be shown via telemetry data,
- * calls to methods will fail, but will not cause the application to crash.
- *
- * @author SSI Robotics
- * @version 2015-08-13-20-04
- */
 
 public class RobotHardware extends OpMode
 
 {
-    //--------------------------------------------------------------------------
-    //
-    // PushBotHardware
-    //
-    /**
-     * Construct the class.
-     *
-     * The system calls this member when the class is instantiated.
-     */
-    public RobotHardware()
-
-    {
-
-
-        //
-        // Initialize base classes.
-        //
-        // All via self-construction.
-
-        //
-        // Initialize class members.
-        //
-        // All via self-construction.
-
-    } // PushBotHardware
-
-    //--------------------------------------------------------------------------
-    //
-    // init
-    //
-    /**
-     * Perform any actions that are necessary when the OpMode is enabled.
-     *
-     * The system calls this member once when the OpMode is enabled.
-     */
     @Override public void init ()
 
     {
-        //
-        // Use the hardwareMap to associate class members to hardware ports.
-        //
-        // Note that the names of the devices (i.e. arguments to the get method)
-        // must match the names specified in the configuration file created by
-        // the FTC RobotTeleOp Controller (Settings-->Configure RobotTeleOp).
-        //
-        // The variable below is used to provide telemetry data to a class user.
-        //
+
         v_warning_generated = false;
         v_warning_message = "Can't map; ";
 
         //
-        // Connect the drive wheel motors.
-        //
-        // The direction of the right motor is reversed, so joystick inputs can
-        // be more generically applied.
-        //
-
         try {
             v_motor_left_drive_front = hardwareMap.dcMotor.get("left_drive_front");
             v_motor_left_drive_front.setDirection(DcMotor.Direction.REVERSE);
@@ -149,11 +87,46 @@ public class RobotHardware extends OpMode
         } catch (Exception p_exeception){
             m_warning_message("arm_3");
             DbgLog.msg(p_exeception.getLocalizedMessage());
-
             arm_servo_3 = null;
 
         }
 
+        //Sensorz
+        //Color Sensors
+        try {
+            leftColorSensor = hardwareMap.colorSensor.get("leftColorSensor");
+        } catch (Exception p_exeception) {
+            DbgLog.msg(p_exeception.getLocalizedMessage());
+            telemetry.addData("error: ", "left color");
+            leftColorSensor = null;
+        }
+
+        try {
+            //The Right Color Sensor's I2C Address has been modified to 62.
+            rightColorSensor = hardwareMap.colorSensor.get("rightColorSensor");
+            rightColorSensor.setI2cAddress(I2cAddr.create8bit(62));
+        } catch (Exception p_exeception) {
+            DbgLog.msg(p_exeception.getLocalizedMessage());
+            telemetry.addData("error: ", "right color");
+            rightColorSensor = null;
+        }
+
+        try {
+            rightColorSensor = hardwareMap.colorSensor.get("rightColorSensor");
+            rightColorSensor.setI2cAddress(I2cAddr.create8bit(62));
+        } catch (Exception p_exeception) {
+            DbgLog.msg(p_exeception.getLocalizedMessage());
+            telemetry.addData("error: ", "right color");
+            rightColorSensor = null;
+        }
+
+        try {
+            rangeSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "rangeSensor");
+        } catch (Exception p_exeception) {
+            DbgLog.msg(p_exeception.getLocalizedMessage());
+            telemetry.addData("error: ", "range sensor");
+            rangeSensor = null;
+        }
     }
 
 
@@ -494,6 +467,10 @@ public class RobotHardware extends OpMode
 
     }
 
+    void stopdrive() {
+        set_drive_power(0, 0);
+    }
+
     void set_rear_drive_power (double p_left_power, double p_right_power)
 
     {
@@ -510,519 +487,6 @@ public class RobotHardware extends OpMode
 
     }
 
-     // set_drive_power
-
-    //--------------------------------------------------------------------------
-    //
-    // run_using_left_drive_encoder
-    //
-    /**
-     * Set the left drive wheel encoder to run, if the mode is appropriate.
-     */
-    public void run_using_left_drive_encoder ()
-
-    {
-        if (v_motor_left_drive_front != null && v_motor_right_drive_back != null)
-        {
-            v_motor_left_drive_front.setMode
-                    ( DcMotorController.RunMode.RUN_USING_ENCODERS
-                    );
-            v_motor_left_drive_back.setMode
-                    (DcMotorController.RunMode.RUN_USING_ENCODERS
-                    );
-        }
-
-    } // run_using_left_drive_encoder
-
-    //--------------------------------------------------------------------------
-    //
-    // run_using_right_drive_encoder
-    //
-    /**
-     * Set the right drive wheel encoder to run, if the mode is appropriate.
-     */
-    public void run_using_right_drive_encoder ()
-
-    {
-        if (v_motor_right_drive_front != null && v_motor_right_drive_back != null)
-        {
-            v_motor_right_drive_front.setMode
-                    ( DcMotorController.RunMode.RUN_USING_ENCODERS
-                    );
-            v_motor_right_drive_back.setMode
-                    ( DcMotorController.RunMode.RUN_USING_ENCODERS
-                    );
-        }
-
-    } // run_using_right_drive_encoder
-
-    //--------------------------------------------------------------------------
-    //
-    // run_using_encoders
-    //
-    /**
-     * Set both drive wheel encoders to run, if the mode is appropriate.
-     */
-    public void run_using_encoders ()
-
-    {
-        //
-        // Call other members to perform the action on both motors.
-        //
-        run_using_left_drive_encoder ();
-        run_using_right_drive_encoder ();
-
-    } // run_using_encoders
-
-    //--------------------------------------------------------------------------
-    //
-    // run_without_left_drive_encoder
-    //
-    /**
-     * Set the left drive wheel encoder to run, if the mode is appropriate.
-     */
-    public void run_without_left_drive_encoder ()
-
-    {
-        if (v_motor_left_drive_front != null && v_motor_left_drive_back != null)
-        {
-            if (v_motor_left_drive_front.getMode() == DcMotorController.RunMode.RESET_ENCODERS && v_motor_left_drive_back.getMode() == DcMotorController.RunMode.RESET_ENCODERS)
-            {
-                v_motor_left_drive_front.setMode
-                        ( DcMotorController.RunMode.RUN_WITHOUT_ENCODERS
-                        );
-                v_motor_left_drive_back.setMode
-                        (DcMotorController.RunMode.RUN_WITHOUT_ENCODERS
-                        );
-            }
-        }
-
-    } // run_without_left_drive_encoder
-
-    //--------------------------------------------------------------------------
-    //
-    // run_without_right_drive_encoder
-    //
-    /**
-     * Set the right drive wheel encoder to run, if the mode is appropriate.
-     */
-    public void run_without_right_drive_encoder ()
-
-    {
-        if (v_motor_right_drive_front != null && v_motor_right_drive_back != null)
-        {
-            if (v_motor_right_drive_front.getMode() == DcMotorController.RunMode.RESET_ENCODERS && v_motor_right_drive_back.getMode() == DcMotorController.RunMode.RESET_ENCODERS)
-            {
-                v_motor_right_drive_front.setMode
-                        ( DcMotorController.RunMode.RUN_WITHOUT_ENCODERS
-                        );
-                v_motor_right_drive_back.setMode
-                        (DcMotorController.RunMode.RUN_WITHOUT_ENCODERS
-                        );
-            }
-        }
-
-    } // run_without_right_drive_encoder
-
-    //--------------------------------------------------------------------------
-    //
-    // run_without_drive_encoders
-    //
-    /**
-     * Set both drive wheel encoders to run, if the mode is appropriate.
-     */
-    public void run_without_drive_encoders ()
-
-    {
-        //
-        // Call other members to perform the action on both motors.
-        //
-        run_without_left_drive_encoder ();
-        run_without_right_drive_encoder ();
-
-    } // run_without_drive_encoders
-
-    //--------------------------------------------------------------------------
-    //
-    // reset_left_drive_encoder
-    //
-    /**
-     * Reset the left drive wheel encoder.
-     */
-    public void reset_left_drive_encoder ()
-
-    {
-        if (v_motor_left_drive_front != null && v_motor_left_drive_back != null)
-        {
-            v_motor_left_drive_front.setMode
-                    ( DcMotorController.RunMode.RESET_ENCODERS
-                    );
-            v_motor_left_drive_back.setMode
-                    (DcMotorController.RunMode.RESET_ENCODERS
-                    );
-        }
-
-    } // reset_left_drive_encoder
-
-    //--------------------------------------------------------------------------
-    //
-    // reset_right_drive_encoder
-    //
-    /**
-     * Reset the right drive wheel encoder.
-     */
-    public void reset_right_drive_encoder ()
-
-    {
-        if (v_motor_right_drive_front != null && v_motor_right_drive_back != null)
-        {
-            v_motor_right_drive_front.setMode
-                    ( DcMotorController.RunMode.RESET_ENCODERS
-                    );
-            v_motor_right_drive_back.setMode
-                    ( DcMotorController.RunMode.RESET_ENCODERS
-                    );
-        }
-
-    } // reset_right_drive_encoder
-
-    //--------------------------------------------------------------------------
-    //
-    // reset_drive_encoders
-    //
-    /**
-     * Reset both drive wheel encoders.
-     */
-    public void reset_drive_encoders ()
-
-    {
-        //
-        // Reset the motor encoders on the drive wheels.
-        //
-        reset_left_drive_encoder ();
-        reset_right_drive_encoder ();
-
-    } // reset_drive_encoders
-
-    //--------------------------------------------------------------------------
-    //
-    // a_left_encoder_count
-    //
-    /**
-     * Access the left encoder's count.
-     */
-    int a_left_encoder_count ()
-    {
-        int l_return = 0;
-
-        if (v_motor_left_drive_front != null)
-        {
-            l_return = v_motor_left_drive_front.getCurrentPosition ();
-        }
-
-        return l_return;
-
-    } // a_left_encoder_count
-
-    //--------------------------------------------------------------------------
-    //
-    // a_right_encoder_count
-    //
-    /**
-     * Access the right encoder's count.
-     */
-    int a_right_encoder_count ()
-
-    {
-        int l_return = 0;
-
-        if (v_motor_right_drive_front != null)
-        {
-            l_return = v_motor_right_drive_front.getCurrentPosition ();
-        }
-
-        return l_return;
-
-    } // a_right_encoder_count
-
-    //--------------------------------------------------------------------------
-    //
-    // has_left_drive_encoder_reached
-    //
-    /**
-     * Indicate whether the left drive motor's encoder has reached a value.
-     */
-    boolean has_left_drive_encoder_reached (double p_count)
-
-    {
-        //
-        // Assume failure.
-        //
-        boolean l_return = false;
-
-        if (v_motor_left_drive_front != null)
-        {
-            //
-            // Has the encoder reached the specified values?
-            //
-            // TODO Make a TODO
-            //
-            if (Math.abs (v_motor_left_drive_front.getCurrentPosition ()) > p_count)
-            {
-                //
-                // Set the status to a positive indication.
-                //
-                l_return = true;
-            }
-        }
-
-        //
-        // Return the status.
-        //
-        return l_return;
-
-    } // has_left_drive_encoder_reached
-
-    //--------------------------------------------------------------------------
-    //
-    // has_right_drive_encoder_reached
-    //
-    /**
-     * Indicate whether the right drive motor's encoder has reached a value.
-     */
-    boolean has_right_drive_encoder_reached (double p_count)
-
-    {
-        //
-        // Assume failure.
-        //
-        boolean l_return = false;
-
-        if (v_motor_right_drive_front != null)
-        {
-            //
-            // Have the encoders reached the specified values?
-            //
-            // TODO Implement stall code using these variables.
-            //
-            if (Math.abs (v_motor_right_drive_front.getCurrentPosition ()) > p_count)
-            {
-                //
-                // Set the status to a positive indication.
-                //
-                l_return = true;
-            }
-        }
-
-        //
-        // Return the status.
-        //
-        return l_return;
-
-    } // has_right_drive_encoder_reached
-
-    //--------------------------------------------------------------------------
-    //
-    // have_drive_encoders_reached
-    //
-    /**
-     * Indicate whether the drive motors' encoders have reached a value.
-     */
-    boolean have_drive_encoders_reached
-    ( double p_left_count
-            , double p_right_count
-    )
-
-    {
-        //
-        // Assume failure.
-        //
-        boolean l_return = false;
-
-        //
-        // Have the encoders reached the specified values?
-        //
-        if (has_left_drive_encoder_reached (p_left_count) &&
-                has_right_drive_encoder_reached (p_right_count))
-        {
-            //
-            // Set the status to a positive indication.
-            //
-            l_return = true;
-        }
-
-        //
-        // Return the status.
-        //
-        return l_return;
-
-    } // have_encoders_reached
-
-    //--------------------------------------------------------------------------
-    //
-    // drive_using_encoders
-    //
-    /**
-     * Indicate whether the drive motors' encoders have reached a value.
-     */
-    boolean drive_using_encoders
-    ( double p_left_power
-            , double p_right_power
-            , double p_left_count
-            , double p_right_count
-    )
-
-    {
-        //
-        // Assume the encoders have not reached the limit.
-        //
-        boolean l_return = false;
-
-        //
-        // Tell the system that motor encoders will be used.
-        //
-        run_using_encoders ();
-
-        //
-        // Start the drive wheel motors at full power.
-        //
-        set_drive_power (p_left_power, p_right_power);
-
-        //
-        // Have the motor shafts turned the required amount?
-        //
-        // If they haven't, then the op-mode remains in this state (i.e this
-        // block will be executed the next time this method is called).
-        //
-        if (have_drive_encoders_reached (p_left_count, p_right_count))
-        {
-            //
-            // Reset the encoders to ensure they are at a known good value.
-            //
-            reset_drive_encoders ();
-
-            //
-            // Stop the motors.
-            //
-            set_drive_power (0.0f, 0.0f);
-
-            //
-            // Transition to the next state when this method is called
-            // again.
-            //
-            l_return = true;
-        }
-
-        //
-        // Return the status.
-        //
-        return l_return;
-
-    } // drive_using_encoders
-
-    //--------------------------------------------------------------------------
-    //
-    // has_left_drive_encoder_reset
-    //
-    /**
-     * Indicate whether the left drive encoder has been completely reset.
-     */
-    boolean has_left_drive_encoder_reset ()
-    {
-        //
-        // Assume failure.
-        //
-        boolean l_return = false;
-
-        //
-        // Has the left encoder reached zero?
-        //
-        if (a_left_encoder_count () == 0)
-        {
-            //
-            // Set the status to a positive indication.
-            //
-            l_return = true;
-        }
-
-        //
-        // Return the status.
-        //
-        return l_return;
-
-    } // has_left_drive_encoder_reset
-
-    //--------------------------------------------------------------------------
-    //
-    // has_right_drive_encoder_reset
-    //
-    /**
-     * Indicate whether the left drive encoder has been completely reset.
-     */
-    boolean has_right_drive_encoder_reset ()
-    {
-        //
-        // Assume failure.
-        //
-        boolean l_return = false;
-
-        //
-        // Has the right encoder reached zero?
-        //
-        if (a_right_encoder_count () == 0)
-        {
-            //
-            // Set the status to a positive indication.
-            //
-            l_return = true;
-        }
-
-        //
-        // Return the status.
-        //
-        return l_return;
-
-    } // has_right_drive_encoder_reset
-
-    //--------------------------------------------------------------------------
-    //
-    // have_drive_encoders_reset
-    //
-    /**
-     * Indicate whether the encoders have been completely reset.
-     */
-    boolean have_drive_encoders_reset ()
-    {
-        //
-        // Assume failure.
-        //
-        boolean l_return = false;
-
-        //
-        // Have the encoders reached zero?
-        //
-        if (has_left_drive_encoder_reset () && has_right_drive_encoder_reset ())
-        {
-            //
-            // Set the status to a positive indication.
-            //
-            l_return = true;
-        }
-
-        //
-        // Return the status.
-        //
-        return l_return;
-
-    } // have_drive_encoders_reset
-
-    //--------------------------------------------------------------------------
-    //
-    // v_warning_generated
-    //
-    /**
-     * Indicate whether a message is a available to the class user.
-     */
     private boolean v_warning_generated = false;
 
     //--------------------------------------------------------------------------
@@ -1046,5 +510,10 @@ public class RobotHardware extends OpMode
     private DcMotor v_motor_right_drive_front;
     private DcMotor v_motor_right_drive_back;
 
+    public ColorSensor leftColorSensor;
+    public ColorSensor rightColorSensor;
+    public ColorSensor beaconColorSensor;
+    public ModernRoboticsI2cRangeSensor rangeSensor;
 
-} // PushBotHardware
+
+}
