ba69229706d4be2a10c4248ffa99994049e95dc4
FTC Geared UP 6347 <ftcgearedup@gmail.com>
2016-09-26 19:54:23 -0400

    Implemented holonomic teleop program
    This class is based off of Matt's holonomic teleop implementation.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareHolonomic.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by ftc6347 on 9/25/16.
+ */
+public class HardwareHolonomic {
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    public void init(HardwareMap hardwareMap) {
+        frontLeft = hardwareMap.dcMotor.get("fl");
+        frontRight = hardwareMap.dcMotor.get("fr");
+        backLeft = hardwareMap.dcMotor.get("bl");
+        backRight = hardwareMap.dcMotor.get("br");
+    }
+
+    public DcMotor getFrontLeft() {
+        return frontLeft;
+    }
+
+    public DcMotor getFrontRight() {
+        return frontRight;
+    }
+
+    public DcMotor getBackLeft() {
+        return backLeft;
+    }
+
+    public DcMotor getBackRight() {
+        return backRight;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicDriveOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HolonomicDriveOp.java
@@ -1,55 +1,67 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Created by ftc6347 on 9/22/16.
  */
 @TeleOp(name = "Holonomic Drive", group = "Tests")
-public class HolonomicDriveOp extends OpMode {
+public class HolonomicDriveOp extends LinearOpMode {
 
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
+    private HardwareHolonomic robot = new HardwareHolonomic();
+
+    private float frontLeftPower;
+    private float frontRightPower;
+    private float backLeftPower;
+    private float backRightPower;
 
     @Override
-    public void init() {
-        frontLeft = hardwareMap.dcMotor.get("fl");
-        frontRight = hardwareMap.dcMotor.get("fr");
-        backLeft = hardwareMap.dcMotor.get("bl");
-        backRight = hardwareMap.dcMotor.get("br");
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+        gamepad1.setJoystickDeadzone(0.2f);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            // Reset variables
+            frontLeftPower = 0;
+            frontRightPower = 0;
+            backLeftPower = 0;
+            backRightPower = 0;
+
+            handleStrafe();
+            handlePivot();
+
+            if(gamepad1.right_trigger > 0) {
+                frontLeftPower /= 4;
+                frontRightPower /= 4;
+                backLeftPower /= 4;
+                backRightPower /= 4;
+            }
+
+            // Set the actual motor powers
+            robot.getFrontLeft().setPower(frontLeftPower);
+            robot.getFrontRight().setPower(frontRightPower);
+            robot.getBackLeft().setPower(backLeftPower);
+            robot.getBackRight().setPower(backRightPower);
+
+            idle();
+        }
     }
 
-    @Override
-    public void loop() {
-        if(gamepad1.dpad_right) {
-            frontLeft.setPower(-1);
-            frontRight.setPower(-1);
-            backLeft.setPower(-1);
-            backRight.setPower(-1);
-        } else if(gamepad1.dpad_left) {
-            frontLeft.setPower(1);
-            frontRight.setPower(1);
-            backLeft.setPower(1);
-            backRight.setPower(1);
-        } else if(gamepad1.dpad_up) {
-             frontLeft.setPower(0.5);
-             backLeft.setPower(0.5);
-             frontRight.setPower(-0.5);
-             backRight.setPower(-0.5);
-        } else if(gamepad1.dpad_down) {
-            frontLeft.setPower(-0.5);
-            backLeft.setPower(-0.5);
-            frontRight.setPower(0.5);
-            backRight.setPower(0.5);
-        } else {
-            frontLeft.setPower(0);
-            frontRight.setPower(0);
-            backLeft.setPower(0);
-            backRight.setPower(0);
-        }
+    private void handlePivot() {
+        frontLeftPower += gamepad1.left_stick_x;
+        frontRightPower += gamepad1.left_stick_x;
+        backLeftPower += gamepad1.left_stick_x;
+        backRightPower += gamepad1.left_stick_x;
+    }
+
+    private void handleStrafe() {
+        frontLeftPower += -gamepad1.right_stick_y + gamepad1.right_stick_x;
+        frontRightPower += gamepad1.right_stick_y + gamepad1.right_stick_x;
+        backLeftPower += -gamepad1.right_stick_y - gamepad1.right_stick_x;
+        backRightPower += gamepad1.right_stick_y - gamepad1.right_stick_x;
     }
 }
