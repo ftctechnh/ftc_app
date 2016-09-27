ce082ba0fa6c9108b380d4e0a5c928f85528cebb
jeremyyao <jeremysemailusa@gmail.com>
2016-09-27 18:50:35 -0400

    Added an interface for attatchments

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AttachmentsInterface.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by jeremy on 9/27/2016.
+ */
+
+public interface AttachmentsInterface
+{
+    void launchParticle();
+    void hitBeacon();
+    void pushParticle();
+    void raiseCapBall();
+    void detectBeaconColor();
+    void detectBeaconPicture();
+}
