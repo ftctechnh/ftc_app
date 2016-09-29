c0e90e23203031c59c82d785e0aaf49d5228646d
billcipher1344 <baconrulz101@gmail.com>
2016-09-29 16:31:22 -0700

    key

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/vuforiaKey.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.modules;
+
+/**
+ * Created by billcipher1344 on 9/29/2016.
+ */
+public class vuforiaKey {
+
+    public static final String key = "AYYlzbv/////AAAAGVuIwy4xcUzHo8C0G/OeRbRJnsxEWV4lE/p6iB2hM6zRKwfeHOCORAknKPwAGkr9HfMdkpKwrL3xwnyWZQch+X8DhIBW9JD9CZA7nWOLzfSOSAW71/6ZmKc+9Lcu5FxekvCQcWx7Ghj/Un7ULh/PbVqK2Qt+FLjc8RB6D+vd9M4EW3GA7NQcWocUn8BdSLKJDZwsOab3p7MB+HcYTWEEk/8fB8qbYtGsUd+YzU+2U/dOOzI/fQkI7wcIBBl6RVxgdLjmNVyi8Q2NgQV/0ETxhizPql6/T8kCutUwQDEbjpbAg19N9++WazmIYktjmXSbDWtpASQl9CVW40pPX6GVs27W2v44Of92NoaP3faSY7FF";
+}
