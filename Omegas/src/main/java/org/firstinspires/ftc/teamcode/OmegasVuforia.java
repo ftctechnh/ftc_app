2ca0e0bbfd0cf907e035ba782d0c0bd5f89bf81b
Eli Joesph Bradley <elijos03@gmail.com>
2016-10-01 12:29:53 -0500

    Added template OpModes
    - Fix Hardware map
    - Added Tetrix Ranger OpMode
    - Added HardwareOmegas.init calls.
    
    Signed-off-by: Eli Joesph Bradley <elijos03@gmail.com>

--- Omegas/src/main/java/org/firstinspires/ftc/teamcode/OmegasVuforia.java
+++ Omegas/src/main/java/org/firstinspires/ftc/teamcode/OmegasVuforia.java
@@ -133,6 +133,8 @@ public class OmegasVuforia extends LinearOpMode {
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
 
+        Ω.init(hardwareMap);
+
         /**
          * Load the data sets that for the trackable objects we wish to track. These particular data
          * sets are stored in the 'assets' part of our application (you'll see them in the Android
