d8aa914de739f9619e36dd2cea603b642367b658
Eric Adams <smadagotu@gmail.com>
2016-10-02 15:44:02 -0400

    OpModes updates
    All the opmodes from the previous version have been updated into the new
    folders

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/AutonomousBlueDelayed.java
@@ -0,0 +1,133 @@
+/* Copyright (c) 2014 Qualcomm Technologies Inc
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Qualcomm Technologies Inc nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Example autonomous program.
+ * <p>
+ * This example program uses elapsed time to determine how to move the robot.
+ * The OpMode.java class has some class members that provide time information
+ * for the current op mode.
+ * The public member variable 'time' is updated before each call to the run() event.
+ * The method getRunTime() returns the time that has elapsed since the op mode
+ * starting running to when the method was called.
+ */
+public class AutonomousBlueDelayed extends OpMode {
+
+	final static double MOTOR_POWER = 0.15;
+
+	double armPosition;
+	double clawPosition;
+
+	DcMotor motorRight;
+	DcMotor motorLeft;
+
+
+	/**
+	 * Constructor
+	 */
+	public AutonomousBlueDelayed() {//change auto red to auto blue, and vice versa
+
+	}
+
+	/*
+	 * Code to run when the op mode is first enabled goes here
+	 * 
+	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
+	 */
+	@Override
+	public void init() {
+
+
+		motorRight = hardwareMap.dcMotor.get("motor_2");
+		motorLeft = hardwareMap.dcMotor.get("motor_1");
+		motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
+
+
+	}
+
+	/*
+	 * This method will be called repeatedly in a loop
+	 * 
+	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#run()
+	 */
+	@Override
+	public void loop() {
+
+		double left = 0, right = 0;
+
+
+		if (this.time > 10 && this.time <= 13) {
+			// from 1 to 5 seconds, run the motors for five seconds.
+			left = 0.75;
+			right = 0.75;
+		} else if (this.time > 14.3 && this.time <= 16) {
+			// between 5.5 and 7 seconds, idle.
+			left = 0.0;
+			right = 0.0;
+		} else if (this.time > 16.d && this.time <= 17.25d) {
+			// between 7 and 8.25 seconds, point turn left.
+			left = 0.35;
+			right = -0.35;
+		} else if (this.time > 17.25d && this.time <= 25.5d){
+			// between 8.25 seconds and 11.5 seconds, run the drive motors so the robot goes up the mountain
+			left = -.25;
+			right = -.25;
+		}
+
+		/*
+		 * set the motor power
+		 */
+		motorRight.setPower(-left);
+		motorLeft.setPower(-right);
+
+		telemetry.addData("Text", "*** Robot Data***");
+		telemetry.addData("time", "elapsed time: " + Double.toString(this.time));
+		telemetry.addData("left tgt pwr",  "left  pwr: " + Double.toString(left));
+		telemetry.addData("right tgt pwr", "right pwr: " + Double.toString(right));
+	}
+
+	/*
+	 * Code to run when the op mode is first disabled goes here
+	 * 
+	 * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
+	 */
+	@Override
+	public void stop() {
+
+	}
+
+}
