0627a0e485a246e4d885b94c6c94f8961dd16309
= <=>
2016-10-09 22:33:33 -0400

    Added comments to Protobot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProtoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProtoBot.java
@@ -43,26 +43,35 @@ public class ProtoBot extends OpMode {
         RS.setDirection(DcMotor.Direction.REVERSE);
     }
     public void loop(){
-        // Lets do some math :)
-        
+
+        // Used to get spin speed of shooting motors
+        // lDist - last read encoder value
+        // cDist - current encoder value
+        // dDist - encoder degrees between lDist and cDist (distance traveled)
+        // tDist - total distance traveled since we reset
+        // resetTime - time since reset
         lDist = cDist;
         cDist = (
                   RS.getCurrentPosition() +
                   LS.getCurrentPosition()
                 ) / 2;
-        dDist = cDist - lDist;
+        dDist = cDist - lDist; 
         tDist += dDist;
+        double motorSpeed = tDist/(getRuntime() - resetTime);
 
-        LT.setPower(gamepad1.left_stick_x);
-        LB.setPower(gamepad1.left_stick_x);
-
-        RT.setPower(gamepad1.left_stick_y);
-        RB.setPower(gamepad1.left_stick_y);
-        if(gamepad1.left_bumper){
+        // Resets motor speed readings 
+        if(gamepad1.left_bumper){ 
           resetTime = getRuntime();
           tDist = 0;
         }
 
+        // Drivetrain controls
+        LT.setPower(gamepad1.left_stick_x);
+        LB.setPower(gamepad1.left_stick_x);
+        RT.setPower(gamepad1.left_stick_y);
+        RB.setPower(gamepad1.left_stick_y);
+
+        // Failsafe to prevent slight stick drift.
         if(Math.abs(gamepad1.left_stick_y) < .1 && Math.abs(gamepad1.left_stick_x) < .1){
             LT.setPower(0);
             RT.setPower(0);
@@ -70,30 +79,34 @@ public class ProtoBot extends OpMode {
             RB.setPower(0);
         }
         
+        // Turns the bot 
         if(gamepad1.left_trigger > .1 || gamepad1.right_trigger > 1){
             if(gamepad1.left_trigger > gamepad1.right_trigger){
-                LT.setPower(gamepad1.left_trigger);
-                RT.setPower(gamepad1.left_trigger);
+                LT.setPower(-gamepad1.left_trigger);
+                RT.setPower(-gamepad1.left_trigger);
                 LB.setPower(gamepad1.left_trigger);
                 RB.setPower(gamepad1.left_trigger);
             }else{
-                LT.setPower(-gamepad1.right_trigger);
-                RT.setPower(-gamepad1.right_trigger);
+                LT.setPower(gamepad1.right_trigger);
+                RT.setPower(gamepad1.right_trigger);
                 LB.setPower(-gamepad1.right_trigger);
                 RB.setPower(-gamepad1.right_trigger);
             }
         }
         
+        // Activates shooters
         if(gamepad1.a){
             RS.setPower(1);
             LS.setPower(-1);
         }else if(gamepad1.b){
-            RS.setPower(1);
-            LS.setPower(-1);
+            RS.setPower(-1);
+            LS.setPower(1);
         }else{
             RS.setPower(0);
             LS.setPower(0);
         }
+
+        // Activates collectors
         if(gamepad1.x){
             C.setPosition(1);
         }else if(gamepad1.y){
@@ -102,8 +115,7 @@ public class ProtoBot extends OpMode {
             C.setPosition(.5);
         }
         
-        telemetry.addData("Left Stick Y", gamepad1.left_stick_y);
-        telemetry.addData("Right Stick Y", gamepad1.right_stick_y);
-        telemetry.addData("motor speed", lDist/(getRuntime()-resetTime));
+        // Put telemetry here
+        telemetry.addData("motor speed", motorSpeed);
     }
 }
