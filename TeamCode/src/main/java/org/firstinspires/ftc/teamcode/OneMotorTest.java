9fe1654d7984704de557fba40ff2c8d91332ba6a
ftc359 <ftc.359@gmail.com>
2016-10-14 14:42:40 -0700

    Fixed stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneMotorTest.java
@@ -6,27 +6,19 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.modules.GamepadV2;
 
-
-/**
- * Created by billcipher1344 on 9/13/2016.
- */
-
-@TeleOp
-public class yayayayayayay extends OpMode {
+@TeleOp (group = "MotorTest")
+public class OneMotorTest extends OpMode {
 
     GamepadV2 pad1 = new GamepadV2();
     DcMotor motor;
 
     public void init() {
-
-    motor = hardwareMap.dcMotor.get("motor");
+        motor = hardwareMap.dcMotor.get("motor");
     }
 
     public void loop(){
-
         pad1.update(gamepad1);
         motor.setPower(pad1.left_stick_y_exponential(1));
 
     }
 }
-
