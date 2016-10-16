fd5c63ead12a2c08b0b6d6fab8eef6a922595f95
Alec Matthews <alec500oo@gmail.com>
2016-10-15 20:11:28 -0700

    Put a turn in place and drive straight. Next I will add a color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoButtonPusher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoButtonPusher.java
@@ -55,101 +55,146 @@ import java.text.DecimalFormat;
 //@Disabled
 public class AutoButtonPusher extends LinearOpMode {
 
-  private final int NAVX_DIM_I2C_PORT = 0;
-  private AHRS navxDevice;
-  private navXPIDController yawPIDController;
-  private ElapsedTime runtime = new ElapsedTime();
+    private final int NAVX_DIM_I2C_PORT = 0;
+    private AHRS navxDevice;
+    private navXPIDController yawPIDController;
+    private ElapsedTime runtime = new ElapsedTime();
 
-  private final byte NAVX_UPDATE_RATE_HZ = 50;
-  private final double TOLERANCE_DEGREES = 2.0;
-  private final double MAX_OUTPUT_VAL = 0.25;
-  private final double MIN_OUTPUT_VAL = -0.25;
-  private final double YAW_PID_P = 0.06;
-  private final double YAW_PID_I = 0.0012;
-  private final double YAW_PID_D = 0.85;
+    private final byte NAVX_UPDATE_RATE_HZ = 50;
+    private final double TOLERANCE_DEGREES = 2.0;
+    private final double YAW_PID_P = 0.06;
+    private final double YAW_PID_I = 0.0012;
+    private final double YAW_PID_D = 0.85;
 
-  private boolean calibrationComplete = false;
-  private boolean rotateComplete = false;
+    private double MAX_OUTPUT_VAL = 0.25;
+    private double MIN_OUTPUT_VAL = -0.25;
 
-  @Override
-  public void runOpMode() throws InterruptedException {
+    private boolean calibrationComplete = false;
+    private boolean rotateComplete = false;
+    private boolean lineReached = false;
 
-    HardwareSteelheadMainBot robot = new HardwareSteelheadMainBot();
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        HardwareSteelheadMainBot robot = new HardwareSteelheadMainBot();
 
     /* get the navx device from a device interface module named "dim"
      * Then create a new PID controller with the timestamped source YAW
      */
 
-    navxDevice = AHRS.getInstance(hardwareMap.deviceInterfaceModule.get("dim"),
-            NAVX_DIM_I2C_PORT,
-            AHRS.DeviceDataType.kProcessedData,
-            NAVX_UPDATE_RATE_HZ);
+        navxDevice = AHRS.getInstance(hardwareMap.deviceInterfaceModule.get("dim"),
+                NAVX_DIM_I2C_PORT,
+                AHRS.DeviceDataType.kProcessedData,
+                NAVX_UPDATE_RATE_HZ);
 
-    yawPIDController = new navXPIDController(navxDevice,
-            navXPIDController.navXTimestampedDataSource.YAW);
+        yawPIDController = new navXPIDController(navxDevice,
+                navXPIDController.navXTimestampedDataSource.YAW);
 
-    //Configure the device
-    yawPIDController.setContinuous(true);
-    yawPIDController.setOutputRange(MIN_OUTPUT_VAL, MAX_OUTPUT_VAL);
-    yawPIDController.setTolerance(navXPIDController.ToleranceType.ABSOLUTE, TOLERANCE_DEGREES);
-    yawPIDController.setPID(YAW_PID_P, YAW_PID_I, YAW_PID_D);
+        //Configure the device
+        yawPIDController.setContinuous(true);
+        yawPIDController.setOutputRange(MIN_OUTPUT_VAL, MAX_OUTPUT_VAL);
+        yawPIDController.setTolerance(navXPIDController.ToleranceType.ABSOLUTE, TOLERANCE_DEGREES);
+        yawPIDController.setPID(YAW_PID_P, YAW_PID_I, YAW_PID_D);
 
-    robot.init(hardwareMap);
+        robot.init(hardwareMap);
 
-    robot.rightMotor_1.setDirection(DcMotor.Direction.FORWARD);
-    robot.rightMotor_2.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.rightMotor_1.setDirection(DcMotor.Direction.FORWARD);
+        robot.rightMotor_2.setDirection(DcMotorSimple.Direction.FORWARD);
 
-    robot.rightMotor_1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-    robot.rightMotor_2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-    robot.leftMotor_1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-    robot.leftMotor_2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        robot.rightMotor_1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        robot.rightMotor_2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        robot.leftMotor_1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        robot.leftMotor_2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
-    telemetry.addData("STATUS:", "init complete");
-    telemetry.update();
-    //wait for start of the match
-    waitForStart();
+        telemetry.addData("STATUS:", "init complete");
+        telemetry.update();
+        //wait for start of the match
+        waitForStart();
 
-    while (!calibrationComplete) {
-      calibrationComplete = !navxDevice.isCalibrating();
-      telemetry.addData("CAL:", "Calibration in progress");
-    }
-    navxDevice.zeroYaw();
-
-    try {
-      yawPIDController.setSetpoint(90.0);
-      yawPIDController.enable(true);
-
-      navXPIDController.PIDResult PIDResult = new navXPIDController.PIDResult();
-      int DEVICE_TIMEOUT_MS = 500;
-
-      DecimalFormat df = new DecimalFormat("#.##");
-
-      while (opModeIsActive() && !rotateComplete && !Thread.currentThread().isInterrupted()) {
-        if (yawPIDController.waitForNewUpdate(PIDResult, DEVICE_TIMEOUT_MS)) {
-          if (PIDResult.isOnTarget()) {
-            robot.rightMotor_1.setPower(0);
-            robot.rightMotor_2.setPower(0);
-            robot.leftMotor_1.setPower(0);
-            robot.leftMotor_2.setPower(0);
-            telemetry.addData("PID OUT:", df.format(0.00));
-          } else {
-            double output = PIDResult.getOutput();
-            robot.rightMotor_1.setPower(output);
-            robot.rightMotor_2.setPower(output);
-            robot.leftMotor_1.setPower(-output);
-            robot.leftMotor_2.setPower(-output);
-            telemetry.addData("PID OUT:", df.format(output) + " , " + df.format(-output));
-          }
-
-        } else {
-          Log.w("navx", "YAW PID TIMEOUT");
+        while (!calibrationComplete) {
+            calibrationComplete = !navxDevice.isCalibrating();
+            telemetry.addData("CAL:", "Calibration in progress");
         }
-      }
-    } catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
-    } finally {
-      navxDevice.close();
-      telemetry.addData("STATUS:", "Complete");
+        navxDevice.zeroYaw();
+
+        try {
+            yawPIDController.setSetpoint(90.0);
+            yawPIDController.enable(true);
+
+            navXPIDController.PIDResult PIDResult = new navXPIDController.PIDResult();
+            int DEVICE_TIMEOUT_MS = 500;
+
+            DecimalFormat df = new DecimalFormat("#.##");
+
+            while (opModeIsActive() && !rotateComplete && !Thread.currentThread().isInterrupted()) {
+                if (yawPIDController.waitForNewUpdate(PIDResult, DEVICE_TIMEOUT_MS)) {
+                    if (PIDResult.isOnTarget()) {
+                        robot.rightMotor_1.setPower(0);
+                        robot.rightMotor_2.setPower(0);
+                        robot.leftMotor_1.setPower(0);
+                        robot.leftMotor_2.setPower(0);
+                        telemetry.addData("PID OUT:", df.format(0.00));
+                        rotateComplete = true;
+                    } else {
+                        double output = PIDResult.getOutput();
+                        robot.rightMotor_1.setPower(output);
+                        robot.rightMotor_2.setPower(output);
+                        robot.leftMotor_1.setPower(-output);
+                        robot.leftMotor_2.setPower(-output);
+                        telemetry.addData("PID OUT:", df.format(output) + " , " + df.format(-output));
+                    }
+
+                } else {
+                    Log.w("navx", "YAW PID TIMEOUT");
+                }
+            }
+            yawPIDController.enable(false);
+            MIN_OUTPUT_VAL = -0.5;
+            MAX_OUTPUT_VAL = 0.5;
+
+
+            robot.leftMotor_1.setDirection(DcMotorSimple.Direction.REVERSE);
+            robot.leftMotor_2.setDirection(DcMotorSimple.Direction.FORWARD);
+            robot.rightMotor_1.setDirection(DcMotorSimple.Direction.REVERSE);
+            robot.rightMotor_2.setDirection(DcMotorSimple.Direction.REVERSE);
+
+            yawPIDController.setOutputRange(MIN_OUTPUT_VAL, MAX_OUTPUT_VAL);
+            yawPIDController.enable(true);
+
+            double driveSpeed = 0.25;
+
+            while (opModeIsActive()&& !lineReached && !Thread.currentThread().isInterrupted()) {
+                if (yawPIDController.waitForNewUpdate(PIDResult, DEVICE_TIMEOUT_MS)) {
+                    if (PIDResult.isOnTarget()) {
+                        robot.rightMotor_1.setPower(driveSpeed);
+                        robot.rightMotor_2.setPower(driveSpeed);
+                        robot.leftMotor_1.setPower(driveSpeed);
+                        robot.leftMotor_2.setPower(driveSpeed);
+                        telemetry.addData("PID OUT:", df.format(0.00));
+                    } else {
+                        double output = PIDResult.getOutput();
+                        robot.leftMotor_1.setPower(limit(driveSpeed + output));
+                        robot.leftMotor_2.setPower(limit(driveSpeed + output));
+                        robot.rightMotor_1.setPower(limit(driveSpeed - output));
+                        robot.rightMotor_2.setPower(limit(driveSpeed - output));
+                        telemetry.addData("MOTOR OUT:", df.format(limit(driveSpeed + output))
+                                + " , " + df.format(limit(driveSpeed - output)));
+                    }
+
+                } else {
+                    Log.w("navx", "YAW PID TIMEOUT");
+                }
+            }
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            navxDevice.close();
+            telemetry.addData("STATUS:", "Complete");
+        }
     }
-  }
+
+    private double limit(double a) {
+       return Math.min(Math.max(a, MIN_OUTPUT_VAL), MAX_OUTPUT_VAL);
+    }
 }
