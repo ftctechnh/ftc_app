b0912d4ecd6822f226b67e8db0d55ac8edc2198a
Peterohio <peterohio1@gmail.com>
2016-10-16 15:58:21 -0400

    AutonomousTestOp now increments through different angles to test cal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTestOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTestOp.java
@@ -2,11 +2,12 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 /**
  * Created by 111 on 9/29/2016.
  */
-@Autonomous(name = "AutonomousTestOp", group = "Autonomous")
+@TeleOp(name = "AutonomousTestOp", group = "Autonomous")
 public class AutonomousTestOp extends LinearOpMode
 {
     private OmniDriveBot robot = new OmniDriveBot();
@@ -15,19 +16,22 @@ public class AutonomousTestOp extends LinearOpMode
     {
         robot.init(hardwareMap);
         waitForStart();
-        robot.driveStraight(12, 45);
-        telemetry.addData("Front Left 1", robot.getfL().getCurrentPosition());
-        telemetry.addData("Front Right 1", robot.getfR().getCurrentPosition());
-        telemetry.update();
-        sleep(100);
-        robot.driveStraight(12, -45);
-        telemetry.addData("Front Left 2", robot.getfL().getCurrentPosition());
-        telemetry.addData("Front Right 2", robot.getfR().getCurrentPosition());
-        telemetry.update();
-        sleep(5000);
+        int i = 0;
+        boolean aButton = false;
+        while(i < 7)
+        {
+            if(gamepad1.a == true)
+            {
+                aButton = true;
+            }
+            if(aButton == true)
+            {
+                int degree = 15 * i;
+                robot.driveStraight(12, degree);
+                i++;
+                aButton = false;
+            }
+        }
     }
-
-
-
 }
 
