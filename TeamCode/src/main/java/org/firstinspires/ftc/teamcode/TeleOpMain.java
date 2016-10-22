e90227cfea7a1f9afe17628c50035203cf7e77c0
WHSRobotics11 <wilsonhighschoolrobotics@gmail.com>
2016-10-21 20:03:53 -0700

    Added main teleop program, added basic drivetrain functionality - Benjamin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
@@ -0,0 +1,42 @@
+/*
+    Basic rat bot program for testing
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "RatBot", group = "Misc.")
+//@Disabled
+public class TeleOpMain extends OpMode {
+
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+
+    @Override
+    public void init() {
+
+        telemetry.addData("Status", "Initialized");
+
+        // hardware maps
+        frontLeftMotor = hardwareMap.dcMotor.get("front_left");
+        frontRightMotor = hardwareMap.dcMotor.get("front_right");
+
+        // change directions if necessary
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    @Override
+    public void start() {}
+
+    @Override
+    public void loop() {
+
+        // run the drive train motors
+        frontLeftMotor.setPower(gamepad1.left_stick_y);
+        frontRightMotor.setPower(gamepad1.right_stick_y);
+    }
+}
