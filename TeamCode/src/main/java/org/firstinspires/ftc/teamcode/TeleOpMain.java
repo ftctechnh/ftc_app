1a6ec5b56d96ff3b3953f900d5fdc98d26b8e0be
WHSRobotics11 <wilsonhighschoolrobotics@gmail.com>
2016-10-21 20:06:09 -0700

    Added main teleop program with basic drivetrain funtionality - Benjamin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
@@ -1,19 +1,21 @@
 /*
-    Basic rat bot program for testing
+    Main robot teleop program
  */
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "RatBot", group = "Misc.")
+@TeleOp(name = "TeleOpMain", group = "Main")
 //@Disabled
 public class TeleOpMain extends OpMode {
 
     DcMotor frontLeftMotor;
     DcMotor frontRightMotor;
+    DcMotor backLeftMotor;
+    DcMotor backRightMotor;
 
     @Override
     public void init() {
@@ -23,10 +25,14 @@ public class TeleOpMain extends OpMode {
         // hardware maps
         frontLeftMotor = hardwareMap.dcMotor.get("front_left");
         frontRightMotor = hardwareMap.dcMotor.get("front_right");
+        backLeftMotor = hardwareMap.dcMotor.get("back_left");
+        backRightMotor = hardwareMap.dcMotor.get("back_right");
 
         // change directions if necessary
         frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
     }
 
     @Override
@@ -35,8 +41,24 @@ public class TeleOpMain extends OpMode {
     @Override
     public void loop() {
 
-        // run the drive train motors
-        frontLeftMotor.setPower(gamepad1.left_stick_y);
-        frontRightMotor.setPower(gamepad1.right_stick_y);
+        // run drivetrain motors
+        if(gamepad1.dpad_left) {
+            frontLeftMotor.setPower(-1.0);
+            frontRightMotor.setPower(-1.0);
+            backLeftMotor.setPower(1.0);
+            backRightMotor.setPower(1.0);
+        }
+        else if(gamepad1.dpad_right) {
+            frontLeftMotor.setPower(1.0);
+            frontRightMotor.setPower(1.0);
+            backLeftMotor.setPower(-1.0);
+            backRightMotor.setPower(-1.0);
+        }
+        else {
+            frontLeftMotor.setPower(gamepad1.left_stick_y);
+            frontRightMotor.setPower(gamepad1.right_stick_y);
+            backLeftMotor.setPower(gamepad1.left_stick_y);
+            backRightMotor.setPower(gamepad1.right_stick_y);
+        }
     }
 }
