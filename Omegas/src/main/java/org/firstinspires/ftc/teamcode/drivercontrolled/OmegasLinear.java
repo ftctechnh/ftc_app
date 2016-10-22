06107d39a201b1c902dbf7df60807de82e48feed
Eli Joesph Bradley <elijos03@gmail.com>
2016-10-22 10:48:06 -0500

    Move OmegasLinear to drivercontrolled package.

--- Omegas/src/main/java/org/firstinspires/ftc/teamcode/drivercontrolled/OmegasLinear.java
+++ Omegas/src/main/java/org/firstinspires/ftc/teamcode/drivercontrolled/OmegasLinear.java
@@ -30,13 +30,15 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
 TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.drivercontrolled;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.HardwareOmegas;
 
 /**
  * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
