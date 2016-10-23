c7a57aab7526f4c710e6c159f7b1f901f203e3b3
balajiathreye <balajiathreye@hotmail.com>
2016-10-23 17:54:34 -0500

    Package names had to be changed to teamcodesamples from teamcode for these sample files. If you copy paste this, make sure you are changing the package name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcodesamples/NullPushbot_demo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcodesamples/NullPushbot_demo.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcodesamples;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
