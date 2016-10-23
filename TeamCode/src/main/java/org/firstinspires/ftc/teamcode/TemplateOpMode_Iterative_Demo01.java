03ea8c3a263b8c36d764e84087c1291b753da1f4
FTC10609 <ftc10609@gmail.com>
2016-10-23 14:30:31 -0500

    Hello World...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateOpMode_Iterative_Demo01.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateOpMode_Iterative_Demo01.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-// Adding a simple change. Hello World
+// Adding a simple change. Hello World!!
 
 @TeleOp(name="Template: Iterative OpMode", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
 @Disabled
