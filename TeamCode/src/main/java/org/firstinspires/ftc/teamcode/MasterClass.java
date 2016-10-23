a560beaeee7a4b58a9545ecf79f5c5cbe47a0cd8
FTC10609 <ftc10609@gmail.com>
2016-10-23 14:34:21 -0500

    Added My prior files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MasterClass.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Path;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+/**
+ * Created by Kyle on 10/22/2016.
+ */
+
+
+public class MasterClass extends OpMode {
+
+    DcMotor BackRight;
+
+    DcMotor BackLeft;
+
+    DcMotor FrontRight;
+
+    DcMotor FrontLeft;
+
+    @Override
+    public void init() {
+
+        BackLeft = hardwareMap.dcMotor.get("BackLeft");
+
+        BackRight = hardwareMap.dcMotor.get("BackRight");
+
+        FrontLeft = hardwareMap.dcMotor.get("FrontLeft");
+
+        FrontRight = hardwareMap.dcMotor.get("FrontRight");
+
+        FrontRight.setDirection(DcMotor.Direction.REVERSE);
+
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        float leftY = -gamepad1.left_stick_y;
+
+        float rightY = -gamepad1.right_stick_y;
+
+        BackRight.setPower(rightY);
+
+        FrontRight.setPower(leftY);
+
+        BackLeft.setPower(leftY);
+
+        FrontLeft.setPower(rightY);
+    }
+}
