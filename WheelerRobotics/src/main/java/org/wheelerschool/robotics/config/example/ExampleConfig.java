9b53ffbf23de9f4e30f7fe2667741597a0e42d38
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-10-24 22:25:42 -0400

    Add config for storing Git untracked values
    Added ‘ExampleConfig’. Following the instructions in the README, this
    is then copied to an untracked java file, which contains the actual
    values.
    This can be used to store keys, that shouldn’t be pushed to a public
    repository.

---
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/config/example/ExampleConfig.java
@@ -0,0 +1,15 @@
+package org.wheelerschool.robotics.config.example;
+
+/**
+ * Created by luciengaitskell on 10/24/16.
+ */
+
+/*
+USAGE INSTRUCTIONS:
+ [1]: Copy this file to "../Config.java" (use Android Studio refactor to handle class name change too)
+ [2]: Fill in values in the class below
+ */
+
+public class ExampleConfig {
+    public static String EXAMPLE_VALUE = "VALUE";
+}
