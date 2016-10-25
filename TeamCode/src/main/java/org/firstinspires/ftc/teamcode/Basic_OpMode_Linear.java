97d5af78d6c849c53cedcb7883d9562b4b1587d9
PhantomMike <mikesch@msn.com>
2016-10-25 14:19:41 -0700

    Made minor change to output string.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basic_OpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Basic_OpMode_Linear.java
@@ -82,7 +82,7 @@ public class Basic_OpMode_Linear extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Basic_Status", "Run Time: " + runtime.toString());
             telemetry.update();
 
             // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
