010c19f898eb4ba3187f97832b1f2d551cab94fa
Sean Ovens <whyguy19@gmail.com>
2016-10-26 16:03:13 -0400

    Added first blue autonomus program.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoB1.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by USER on 10/26/2016.
+ */
+
+public class AutoB1 extends AutonomousBase{
+    @Override
+    public void gameState() {
+        super.gameState();
+
+        switch(gameState){
+            case 0: //start
+        }
+    }
+}
