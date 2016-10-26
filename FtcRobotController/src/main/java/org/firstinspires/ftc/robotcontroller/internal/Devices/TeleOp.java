c42a2166172f758119072e8042e73360c78b08df
AbdullahNaveedAbdullahNaveed <weeheejee@gmail.com>
2016-10-25 20:02:22 -0500

    Getting Joystick Value

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Devices/TeleOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Devices/TeleOp.java
@@ -1,9 +1,23 @@
 package org.firstinspires.ftc.robotcontroller.internal.Devices;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 /**
  * Created by abnaveed on 10/13/2016.
  */
-public class TeleOp {
+public class TeleOp extends OpMode {
     // TeleOp class
-    
+
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        double Left_Joystick = -gamepad1.left_stick_y;
+        double Right_Joystick= -gamepad1.left_stick_y;
+    }
 }
