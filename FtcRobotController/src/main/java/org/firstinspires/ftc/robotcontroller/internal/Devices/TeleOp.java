8133f40f2199374554d2c14565c117a5f41b4de8
AbdullahNaveedAbdullahNaveed <weeheejee@gmail.com>
2016-10-25 20:54:33 -0500

    Wrote Motor Control

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Devices/TeleOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Devices/TeleOp.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.robotcontroller.internal.Devices;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Created by abnaveed on 10/13/2016.
@@ -9,6 +10,10 @@ public class TeleOp extends OpMode {
     // TeleOp class
 
 
+
+
+    public DcMotor  leftMotor   = null;
+    public DcMotor  rightMotor  = null;
 
     @Override
     public void init() {
@@ -19,5 +24,8 @@ public class TeleOp extends OpMode {
     public void loop() {
         double Left_Joystick = -gamepad1.left_stick_y;
         double Right_Joystick= -gamepad1.left_stick_y;
+        //converting joystick values to motor power values
+        rightMotor.setPower(Right_Joystick);
+        leftMotor.setPower(Left_Joystick);
     }
 }
