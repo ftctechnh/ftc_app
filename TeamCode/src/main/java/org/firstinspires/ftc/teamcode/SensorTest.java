03dbadf7a24854ec6116ea95c81208b534314156
ADMcK <mckinnon@computer.org>
2016-10-26 20:12:15 -0700

    TouchTest works (code and comments were cleaned up) SensorTest works--allows for both gamepad and touch sensor input vv8099.xml configured for both TouchTest and SensorTest opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorTest.java
@@ -39,21 +39,26 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DigitalChannelController;
 
 /*
- * This is an example LinearOpMode that shows how to use the touch sensor on the
+ * This is an example LinearOpMode that shows how to use the digital inputs and outputs on the
  * the Modern Robotics Device Interface Module.  In addition, it shows how to use the Red and Blue LED
  *
  * This op mode assumes that there is a Device Interface Module attached, named 'dim'.
- * On this DIM there is a digital input named 'touch0'
+ * On this DIM there is a digital input named 'digin' and an output named 'digout'
  *
- * The Red/Blue LED will be used to indicate the state of the touch sensor.
+ * To fully exercise this sample, connect pin 3 of the digin connector to pin 3 of the digout.
+ * Note: Pin 1 is indicated by the black stripe, so pin 3 is at the opposite end.
+ *
+ * The X button on the gamepad will be used to activate the digital output pin.
+ * The Red/Blue LED will be used to indicate the state of the digital input pin.
  * Blue = false (0V), Red = true (5V)
+ * If the two pins are linked, the gamepad will change the LED color.
  *
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
 */
-@TeleOp(name = "TouchTest", group = "Testing")
+@TeleOp(name = "SensorTest", group = "Testing")
 //@Disabled
-public class TouchTest extends LinearOpMode {
+public class SensorTest extends LinearOpMode {
 
   final int BLUE_LED_CHANNEL = 0;      // address for BLUE LED on the Device Interface Module
   final int RED_LED_CHANNEL = 1;       // address for RED LED on the Device Interface Module
@@ -61,30 +66,40 @@ public class TouchTest extends LinearOpMode {
   @Override
   public void runOpMode() {
 
-    boolean               inputPin;             // Input State
+    boolean               inputGamepad;         // Input State from Gamepad ("X" button)
+    boolean               inputTouch;           // Input State from touch sensor ("touch")
     boolean               outputPin;            // Output State
     DeviceInterfaceModule dim;                  // Device Object
+    DigitalChannel        digInTouch;           // Device Object
     DigitalChannel        digIn;                // Device Object
     DigitalChannel        digOut;               // Device Object
 
     // get a reference to a Modern Robotics DIM, and IO channels.
     dim = hardwareMap.get(DeviceInterfaceModule.class, "dim");   //  Use generic form of device mapping
-    digIn = hardwareMap.get(DigitalChannel.class, "touch");    //  Use generic form of device mapping
+    digInTouch = hardwareMap.get(DigitalChannel.class, "touch"); //  Use generic form of device mapping
+    digIn  = hardwareMap.get(DigitalChannel.class, "digin");     //  Use generic form of device mapping
+    digOut = hardwareMap.get(DigitalChannel.class, "digout");    //  Use generic form of device mapping
 
     digIn.setMode(DigitalChannelController.Mode.INPUT);          // Set the direction of each channel
+    digOut.setMode(DigitalChannelController.Mode.OUTPUT);
 
     // wait for the start button to be pressed.
-    telemetry.addData("", "Test program for touch sensor");
-    telemetry.addData("", "Uses Device Interface Modeule <dim> with <touch0> sensor");
+    telemetry.addData(">", "Press play, and then user X button to toggle LEDs");
+    telemetry.addData(">", "or press the touch sensor");
     telemetry.update();
     waitForStart();
 
     while (opModeIsActive())  {
 
-        inputPin = digIn.getState();    //  Read the input pin
+        inputGamepad = gamepad1.x ;             //  Read the gamepad 'x' button state
+        inputTouch = digInTouch.getState();     // Read the touch sensor
 
-        // Display input pin state on LEDs
-        if (inputPin) {
+        outputPin = inputGamepad | inputTouch;  // If either input is on, set the output
+
+        digOut.setState(outputPin);             // set the output
+
+        // Turn on/off the LEDs
+        if (outputPin) {
             dim.setLED(RED_LED_CHANNEL, true);
             dim.setLED(BLUE_LED_CHANNEL, false);
         }
@@ -93,11 +108,10 @@ public class TouchTest extends LinearOpMode {
             dim.setLED(BLUE_LED_CHANNEL, true);
         }
 
-        telemetry.addData("Input", inputPin );
-        telemetry.addData("LED",   inputPin ? "Red" : "Blue" );
+        telemetry.addData(" Output", outputPin );
+        telemetry.addData("  Touch",   inputTouch ? "Red" : "Blue" );
+        telemetry.addData("Gamepad",   inputGamepad ? "X" : "-" );
         telemetry.update();
-
-        idle();     // allow something else to run (aka, release the CPU)
     }
   }
 }
