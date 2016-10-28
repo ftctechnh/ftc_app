fe5711a2a144a2c93caa7cfc5047bdcf5832df3f
Eamonn Ryan <earyan@student.linnmar.org>
2016-10-27 20:37:57 -0500

    Creation of the Sweeper Mechanic system

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Devices/SweeperMechanic.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.robotcontroller.internal.Devices;
+
+/**
+ * Created by Eamonn on 10/27/2016.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class SweeperMechanic {
+    private axelRotation
+}
