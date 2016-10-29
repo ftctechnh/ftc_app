1a1c586624d402f6910585c872f81d8d1524e151
WHSRobotics5 <WilsonRobots@gmail.com>
2016-10-28 19:32:58 -0700

    vuforia teleop test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMainVuf.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMainVuf.java
@@ -1,40 +1,12 @@
 /*
-Copyright (c) 2016 Robert Atkinson
+    Main robot teleop program
+ */
 
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Robert Atkinson nor the names of his contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.ftcrobotcontroller.R;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
@@ -53,42 +25,14 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * This OpMode illustrates the basics of using the Vuforia localizer to determine
- * positioning and orientation of robot on the FTC field.
- * The code is structured as a normal OpMode
- *
- * Vuforia uses the phone's camera to inspect its surroundings, and attempt to locate target images.
- *
- * When images are located, Vuforia is able to determine the position and orientation of the
- * image relative to the camera.  This sample code than combines that information with a
- * knowledge of where the target images are on the field, to determine the location of the camera.
- *
- * This example assumes a "diamond" field configuration where the red and blue alliance stations
- * are adjacent on the corner of the field furthest from the audience.
- * From the Audience perspective, the Red driver station is on the right.
- * The two vision target are located on the two walls closest to the audience, facing in.
- * The Stones are on the RED side of the field, and the Chips are on the Blue side.
- *
- * A final calculation then uses the location of the camera on the robot to determine the
- * robot's location and orientation on the field.
- *
- * @see VuforiaLocalizer
- * @see VuforiaTrackableDefaultListener
- * see  ftc_app/doc/tutorial/FTC_FieldCoordinateSystemDefinition.pdf
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
- *
- * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
- * is explained below.
- */
-
-@Autonomous(name="Concept: Vuforia Navigation 1", group ="Concept")
+@TeleOp(name = "TeleOpVuf", group = "Concept")
 //@Disabled
-public class OpmodeVuforiaNavigationTest1 extends OpMode {
+public class TeleOpMainVuf extends OpMode {
 
-    public static final String TAG = "Vuforia Test1";
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backLeftMotor;
+    DcMotor backRightMotor;
 
     OpenGLMatrix lastLocation = null;
 
@@ -96,12 +40,29 @@ public class OpmodeVuforiaNavigationTest1 extends OpMode {
      * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
      * localization engine.
      */
+
     VuforiaLocalizer vuforia;
     VuforiaTrackables FTC_2016_17 = null;
 
     List<VuforiaTrackable> allTrackables;
 
-    @Override public void init() {
+    @Override
+    public void init() {
+
+        telemetry.addData("Status", "Initialized");
+
+        // hardware maps
+        frontLeftMotor = hardwareMap.dcMotor.get("front_left");
+        frontRightMotor = hardwareMap.dcMotor.get("front_right");
+        backLeftMotor = hardwareMap.dcMotor.get("back_left");
+        backRightMotor = hardwareMap.dcMotor.get("back_right");
+
+        // change directions if necessary
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
         /**
          * Start up Vuforia, telling it the id of the view that we wish to use as the parent for
          * the camera monitor feedback; if no camera monitor feedback is desired, use the parameterless
@@ -125,7 +86,7 @@ public class OpmodeVuforiaNavigationTest1 extends OpMode {
          * and paste it in to your code as the value of the 'vuforiaLicenseKey' field of the
          * {@link Parameters} instance with which you initialize Vuforia.
          */
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(com.qualcomm.ftcrobotcontroller.R.id.cameraMonitorViewId);
         parameters.vuforiaLicenseKey = "ARf809H/////AAAAGRswBQwUCUJ5nqfgZxGbDEQ8oO7YP5GdnbReYr8ZHinqQ74OsP7UdOxNZJDmhaF2OeGD20jpSexpr2CcXGSGuHXNB2p9Z6zUNLDTfEggL+yg4ujefoqdkSpCqZf1medpwh3KXcK76FcfSJuqEudik2PC6kQW/cqJXnnHofVrrDTzJmWMnK3hlqTMjig81DEPMAHbRnA5wn7Eu0irnmqqboWyOlQ0xTF+P4LVuxaOUFlQC8zPqkr1Gvzvix45paWtyuLCnS9YDWMvI1jIM4giMrTRCT0lG8F+vkuKMiK647KJp9QIsFdWQ0ecQhau3ODNQ03pcTzprVN72b9VObpv6FNBpjGKRAcA59xlZiM2l6fc";
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
@@ -238,7 +199,7 @@ public class OpmodeVuforiaNavigationTest1 extends OpMode {
                         AxesReference.EXTRINSIC, AxesOrder.XZX,
                         AngleUnit.DEGREES, 90, 90, 0));
         wheelsTarget.setLocation(wheelsTargetLocationOnField);
-        RobotLog.ii(TAG, "Wheels Target=%s", format(wheelsTargetLocationOnField));
+        //RobotLog.ii(TAG, "Wheels Target=%s", format(wheelsTargetLocationOnField));
 
        /*
         * To place the Tools Target on the wall:
@@ -255,7 +216,7 @@ public class OpmodeVuforiaNavigationTest1 extends OpMode {
                         AxesReference.EXTRINSIC, AxesOrder.XZX,
                         AngleUnit.DEGREES, 90, 180, 0));
         toolsTarget.setLocation(toolsTargetLocationOnField);
-        RobotLog.ii(TAG, "Tools Target=%s", format(toolsTargetLocationOnField));
+        //RobotLog.ii(TAG, "Tools Target=%s", format(toolsTargetLocationOnField));
 
         /*
         * To place the Legos Target on the wall:
@@ -272,7 +233,7 @@ public class OpmodeVuforiaNavigationTest1 extends OpMode {
                         AxesReference.EXTRINSIC, AxesOrder.XZX,
                         AngleUnit.DEGREES, 90, 90, 0));
         legosTarget.setLocation(legosTargetLocationOnField);
-        RobotLog.ii(TAG, "Legos Target=%s", format(legosTargetLocationOnField));
+        //RobotLog.ii(TAG, "Legos Target=%s", format(legosTargetLocationOnField));
 
         /*
         * To place the Gears Target on the wall:
@@ -289,14 +250,14 @@ public class OpmodeVuforiaNavigationTest1 extends OpMode {
                         AxesReference.EXTRINSIC, AxesOrder.XZX,
                         AngleUnit.DEGREES, 90, 180, 0));
         gearsTarget.setLocation(gearsTargetLocationOnField);
-        RobotLog.ii(TAG, "Gears Target=%s", format(gearsTargetLocationOnField));
+        //RobotLog.ii(TAG, "Gears Target=%s", format(gearsTargetLocationOnField));
 
         /**
          * Create a transformation matrix describing where the phone is on the robot. Here, we
          * put the phone with the back camera facing the front of the robot (see our
          * choice of BACK camera above) and in landscape mode. Starting from alignment between the
          * robot's and phone's axes, assumed to be phone lying on the robot in portrait mode with
-         * the long axis point at the front of the robot this is a rotation of -90deg around the Y axis
+         * the long axis point at the front of the robot this is a rotation of 90deg around the Y axis
          * followed by 90 around Z, where robot axes are x->right, y->front, z->up.
          *
          * When determining whether a rotation is positive or negative, consider yourself as looking
@@ -310,7 +271,7 @@ public class OpmodeVuforiaNavigationTest1 extends OpMode {
                 .multiplied(Orientation.getRotationMatrix(
                         AxesReference.EXTRINSIC, AxesOrder.YZX,
                         AngleUnit.DEGREES, 90, 90, 0));
-        RobotLog.ii(TAG, "phone=%s", format(phoneLocationOnRobot));
+        //RobotLog.ii(TAG, "phone=%s", format(phoneLocationOnRobot));
 
         /**
          * Let the trackable listeners we care about know where the phone is. We know that each
@@ -342,42 +303,112 @@ public class OpmodeVuforiaNavigationTest1 extends OpMode {
          */
     }
 
-    @Override public void start()
-    {
+    @Override
+    public void start() {
         /** Start tracking the data sets we care about. */
         FTC_2016_17.activate();
     }
 
-    @Override public void loop()
-    {
-            for (VuforiaTrackable trackable : allTrackables) {
-                /**
-                 * getUpdatedRobotLocation() will return null if no new information is available since
-                 * the last time that call was made, or if the trackable is not currently visible.
-                 * getRobotLocation() will return null if the trackable is not currently visible.
-                 */
-                telemetry.addData(trackable.getName(), ((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible() ? "Visible" : "Not Visible");    //
+    @Override
+    public void loop() {
 
-                OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
-                if (robotLocationTransform != null) {
-                    lastLocation = robotLocationTransform;
-                }
-            }
-            /**
-             * Provide feedback as to where the robot was last located (if we know).
+        // run drivetrain motors
+        /*if(gamepad1.dpad_left) {
+            frontLeftMotor.setPower(-1.0);
+            frontRightMotor.setPower(-1.0);
+            backLeftMotor.setPower(1.0);
+            backRightMotor.setPower(1.0);
+        }
+        else if(gamepad1.dpad_right) {
+            frontLeftMotor.setPower(1.0);
+            frontRightMotor.setPower(1.0);
+            backLeftMotor.setPower(-1.0);
+            backRightMotor.setPower(-1.0);
+        }
+        else if(gamepad1.x) {
+            frontLeftMotor.setPower(1.0);
+            frontRightMotor.setPower(1.0);
+            backLeftMotor.setPower(0.0);
+            backRightMotor.setPower(0.0);
+        }
+        else if(gamepad1.b) {
+            frontLeftMotor.setPower(0.0);
+            frontRightMotor.setPower(0.0);
+            backLeftMotor.setPower(1.0);
+            backRightMotor.setPower(1.0);
+        }*/
+        if(gamepad1.a) {
+            /*
+
+                         1,1,1,1
+                            |
+                 0,0,1,1    |    1,1,0,0
+                            |
+            -1,-1,1,1 ------------- 1,1,-1,-1
+                            |
+               -1,-1,0,0    |    0,0,-1,-1
+                            |
+                       -1,-1,-1,-1
+
              */
-            if (lastLocation != null) {
-                //  RobotLog.vv(TAG, "robot=%s", format(lastLocation));
-                telemetry.addData("Position:", formatPosition(lastLocation));
-                telemetry.addData("Orientation:", formatOrientation(lastLocation));
-            } else {
-                telemetry.addData("Position:", "Unknown");
-                telemetry.addData("Orientation:", "Unknown");
-            }
-    }
 
-    @Override public void stop()
-    {
+            float x = gamepad1.left_stick_x;
+            float y = -1 * gamepad1.left_stick_y;
+
+            float ySign = (y >= 0) ? 1 : -1;
+
+            float frontPower = (ySign * x > 0) ? ySign : (ySign * Math.abs(x) * -2 + ySign);
+            float backPower = (ySign * x < 0) ? ySign : (ySign * Math.abs(x) * -2 + ySign);
+
+            // left and right edge cases don't work
+            //float frontPower = (ySign * x > 0) ? y : (y * Math.abs(x) * -2 + y);
+            //float backPower = (ySign * x < 0) ? y : (y * Math.abs(x) * -2 + y);
+
+            if(x == 0 && y == 0){
+                frontPower = 0;
+                backPower = 0;
+            }
+
+            telemetry.addData("front", frontPower);
+            telemetry.addData("back", backPower);
+
+            frontLeftMotor.setPower(frontPower);
+            frontRightMotor.setPower(frontPower);
+            backLeftMotor.setPower(backPower);
+            backRightMotor.setPower(backPower);
+
+        }
+        else {
+            frontLeftMotor.setPower(gamepad1.left_stick_y);
+            frontRightMotor.setPower(gamepad1.right_stick_y);
+            backLeftMotor.setPower(gamepad1.left_stick_y);
+            backRightMotor.setPower(gamepad1.right_stick_y);
+        }
+
+        for (VuforiaTrackable trackable : allTrackables) {
+            /**
+             * getUpdatedRobotLocation() will return null if no new information is available since
+             * the last time that call was made, or if the trackable is not currently visible.
+             * getRobotLocation() will return null if the trackable is not currently visible.
+             */
+            telemetry.addData(trackable.getName(), ((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible() ? "Visible" : "Not Visible");    //
+
+            OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
+            if (robotLocationTransform != null) {
+                lastLocation = robotLocationTransform;
+            }
+        }
+        /**
+         * Provide feedback as to where the robot was last located (if we know).
+         */
+        if (lastLocation != null) {
+            //  RobotLog.vv(TAG, "robot=%s", format(lastLocation));
+            telemetry.addData("Position:", formatPosition(lastLocation));
+            telemetry.addData("Orientation:", formatOrientation(lastLocation));
+        } else {
+            telemetry.addData("Position:", "Unknown");
+            telemetry.addData("Orientation:", "Unknown");
+        }
     }
 
     /**
