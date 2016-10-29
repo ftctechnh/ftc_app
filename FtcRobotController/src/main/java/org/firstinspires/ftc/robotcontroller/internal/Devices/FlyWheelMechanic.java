502d0b798b974e35dc8e55ebf56da475688c400d
Eamonn Ryan <earyan@student.linnmar.org>
2016-10-29 10:07:24 -0500

    Imported Hardware Map and Assigned axelRotation "null"

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Devices/FlyWheelMechanic.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Devices/FlyWheelMechanic.java
@@ -4,5 +4,9 @@ package org.firstinspires.ftc.robotcontroller.internal.Devices;
  * Created by Eamonn on 10/29/2016.
  */
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 public class FlyWheelMechanic {
 }
--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Devices/SweeperMechanic.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Devices/SweeperMechanic.java
@@ -4,12 +4,17 @@ package org.firstinspires.ftc.robotcontroller.internal.Devices;
  * Created by Eamonn on 10/27/2016.
  */
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class SweeperMechanic {
-    public DcMotor[] axelRotation;
+    public DcMotor[] axelRotation = null;
 
     public SweeperMechanic(HardwareMap hardwareMap)
+    {
+        axelRotation =
+    }
+
 
 }
