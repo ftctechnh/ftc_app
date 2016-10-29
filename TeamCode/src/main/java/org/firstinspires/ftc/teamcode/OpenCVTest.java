e42b7a0584bc3fd64844e6c800ce75f533a81d4a
WHSRobotics11 <wilsonhighschoolrobotics@gmail.com>
2016-10-28 20:48:31 -0700

    ok for realsies this time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTest.java
@@ -1,8 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 /**
  * Created by Robotics on 10/28/2016.
  */
 
-public class OpenCVTest {
+@TeleOp(name = "OpenCVTest", group = "Test")
+//@Disabled
+public class OpenCVTest extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start() {}
+
+    @Override
+    public void loop() {
+
+    }
+
 }
