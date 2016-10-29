4e47b97c26bf5099d363c036aea420836e12e60c
Eli Joesph Bradley <elijos03@gmail.com>
2016-10-29 10:20:56 -0500

    Add Tetrix Ranger test case.

---
+++ Omegas/src/main/java/org/firstinspires/ftc/omegas/drivercontrolled/OmegasRanger.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.omegas.drivercontrolled;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.omegas.HardwareOmegas;
+
+/**
+ * Created by ethertyper on 10/29/16.
+ */
+
+@TeleOp(name = "Test: Textrix Ranger", group = "Test")
+public class OmegasRanger extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+    HardwareOmegas Ω       = new HardwareOmegas();
+
+    // IPS Units
+    static final double     FORWARD_SPEED = 0.6;
+    static final double     TURN_SPEED    = 0.5;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        /**
+         * Initialize the hardware variables. Note that the strings used here as parameters
+         * to 'get' must correspond to the names assigned during the robot configuration
+         * step (using the FTC Robot Controller app on the phone).
+         */
+        Ω.init(hardwareMap);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            for (DcMotor motor: Ω.motors) {
+                motor.setPower(FORWARD_SPEED);
+            }
+        }
+
+        for (DcMotor motor: Ω.motors) {
+            motor.setPower(0.0f);
+        }
+    }
+}
