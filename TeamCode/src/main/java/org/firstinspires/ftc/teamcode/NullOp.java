54642f4a3560abe853c2c40176c8efe1f5ce3a00
Nathan Pollon <nathanpollon@gmail.com>
2016-10-30 12:29:48 -0700

    Fixed package names
    New location for old Op Modes. Needed to change package name to work.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NullOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NullOp.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.robotcontroller.internal.opmodes;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
