c969ac881ffb25240ec4a5249fd998ac3b90c967
Eamonn Ryan <earyan@student.linnmar.org>
2016-10-30 22:26:28 -0500

    Started the TrapDoorMechanic
    :large_orange_diamond:

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Devices/TrapDoorMechanic.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Devices/TrapDoorMechanic.java
@@ -4,6 +4,18 @@ package org.firstinspires.ftc.robotcontroller.internal.Devices;
  * Created by Eamonn on 10/29/2016. Funtastic
  */
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-public class TrapDoorMechanic {
+public class TrapDoorMechanic
+{
+    private DcMotor[] trapDoor = null;
+
+    public TrapDoorMechanic(HardwareMap hardwareMap)
+    {
+        trapDoor = new DcMotor[1];
+    }
+    public synchronized void
 }
+
