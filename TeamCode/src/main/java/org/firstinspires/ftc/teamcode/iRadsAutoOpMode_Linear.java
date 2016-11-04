e8eb4207e960d8345e957ff8014c8fef0057c52e
AJTrowell <ajtrowell@gmail.com>
2016-11-03 22:06:50 -0500

    Removed Garbage Comments
    These comments are confusing, because they allude to an architecture that we aren't going to use.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iRadsAutoOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/iRadsAutoOpMode_Linear.java
@@ -44,16 +44,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 
 /**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
 @Autonomous(name="iRads: AutoOp Vision Linear", group="iRads")  // @Autonomous(...) is the other common choice
@@ -63,8 +53,6 @@ public class iRadsAutoOpMode_Linear extends LinearOpMode {
     /* Declare OpMode members. */
     private VisualNavigation visualNav = new VisualNavigation();
     private ElapsedTime runtime = new ElapsedTime();
-    // DcMotor leftMotor = null;
-    // DcMotor rightMotor = null;
 
     @Override
     public void runOpMode() {
@@ -74,17 +62,8 @@ public class iRadsAutoOpMode_Linear extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
-         * to 'get' must correspond to the names assigned during the robot configuration
-         * step (using the FTC Robot Controller app on the phone).
-         */
-        // leftMotor  = hardwareMap.dcMotor.get("left_drive");
-        // rightMotor = hardwareMap.dcMotor.get("right_drive");
 
-        // eg: Set the drive motor directions:
-        // "Reverse" the motor that runs backwards when connected directly to the battery
-        // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        // rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -130,9 +109,6 @@ public class iRadsAutoOpMode_Linear extends LinearOpMode {
 
 
 
-            // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
-            // leftMotor.setPower(-gamepad1.left_stick_y);
-            // rightMotor.setPower(-gamepad1.right_stick_y);
         } // while(opModeIsActive())
     } // runOpMode()
 
