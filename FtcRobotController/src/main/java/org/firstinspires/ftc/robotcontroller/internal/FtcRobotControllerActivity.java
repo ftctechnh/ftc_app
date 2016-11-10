389691d24747c3643c0350bea4831b234aec1a20
Noah <noah@koontzs.com>
2016-11-10 15:36:08 -0800

    OpenCV algorithm demo works! (on my nexus at least)

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -92,26 +92,21 @@ import com.qualcomm.robotcore.wifi.NetworkType;
 import com.qualcomm.robotcore.wifi.WifiDirectAssistant;
 
 import org.firstinspires.ftc.robotcore.internal.AppUtil;
+import org.firstinspires.ftc.robotcore.internal.opengl.models.VertexArray;
 import org.firstinspires.inspection.RcInspectionActivity;
-import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
+
 
 import java.io.File;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class FtcRobotControllerActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+public class FtcRobotControllerActivity extends Activity {
 
   public static final String TAG = "RCActivity";
 
@@ -151,123 +146,15 @@ public class FtcRobotControllerActivity extends Activity implements CameraBridge
   protected Queue<UsbDevice> receivedUsbAttachmentNotifications;
 
   //user code
-  public static final String cvTAG = "OpenCVInit";
+  protected static Context lastContext;
+  protected static View cameraView;
 
-  private static Mat cStore;
-
-  private static CameraBridgeViewBase mOpenCvCameraView;
-
-  //edge kernel (stolen from http://roboreal.com)
-   private static final float[][] kernel = {{-1, -1, -1, -1, -1},
-                                            {-1,  0,  0,  0, -1},
-                                            {-1,  0, 16,  0, -1},
-                                            {-1,  0,  0,  0, -1},
-                                            {-1, -1, -1, -1, -1}};
-
-  private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-    @Override
-    public void onManagerConnected(int status) {
-      switch (status) {
-        case LoaderCallbackInterface.SUCCESS: {
-          //mOpenCvCameraView.enableView();
-        }
-        break;
-        default: {
-          super.onManagerConnected(status);
-        }
-        break;
-      }
-    }
-  };
-
-  public void onCameraViewStarted(int width, int height){
-    RobotLog.vv(cvTAG, "Camera view started: " + width + ", " + height);
-    JavaCameraView mainLayout=(JavaCameraView) this.findViewById(R.id.image_manipulations_activity_surface_view);
-    mainLayout.setAlpha(1.0f);
+  public static Context getAppContext(){
+    return lastContext;
   }
 
-  public void onCameraViewStopped(){
-    RobotLog.vv(cvTAG, "Camera view stopped");
-    JavaCameraView mainLayout=(JavaCameraView) this.findViewById(R.id.image_manipulations_activity_surface_view);
-    mainLayout.setAlpha(0.0f);
-  }
-
-  public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame){
-    //here we gooooooooo!
-    //my not very simple edge detection algorithm!
-    Mat src = inputFrame.gray();
-    Mat gaus = new Mat();
-    Mat edge = new Mat();
-    Mat pass = new Mat();
-
-    Mat edgeKernel = new Mat(5, 5, CvType.CV_32FC1);
-    for (int i=0; i<kernel.length; i++)
-      edgeKernel.put(i,0, kernel[i]);
-
-    Imgproc.GaussianBlur(src, gaus, new Size(7,7), 0);
-
-    Imgproc.filter2D(gaus, edge, -1, edgeKernel);
-
-    pass = edge.clone();
-
-    pass.convertTo(pass, CvType.CV_64FC3);
-
-    int size = (int) (pass.total() * pass.channels());
-
-    double[] buff = new double[size];
-
-    int imgRow = edge.rows();
-    int imgCol = edge.cols();
-
-    long totalRow = 0;
-    long totalCol = 0;
-    long totalPix = 0;
-
-    pass.get(0, 0, buff);
-
-    int thresh = 100;
-
-    for(int i = 0; i < imgCol; i++){
-      for(int o = 0; o < imgRow; o++){
-        if(buff[i * imgRow + o] > thresh){
-          buff[i * imgRow + o] = 0;
-
-          totalCol += i;
-          totalRow += o;
-          totalPix++;
-        }
-        else{
-          buff[i * imgRow + o] = 255;
-        }
-      }
-    }
-
-    pass.put(0, 0, buff);
-
-    totalCol /= totalPix;
-    totalRow /= totalPix;
-
-    pass.convertTo(pass, CvType.CV_8UC1);
-
-    Imgproc.circle(pass, new Point(totalCol, totalRow), 4, new Scalar(0,0,0));
-
-    Imgproc.circle(pass, new Point(imgCol/2, imgRow/2), 10, new Scalar(0,0,0));
-
-    return pass;
-  }
-
-  public static void startCamera(){
-    mOpenCvCameraView.enableView();
-    while(!mOpenCvCameraView.isActivated() && !mOpenCvCameraView.isEnabled());
-    RobotLog.vv(cvTAG, "Camera Activation Success!");
-  }
-
-  public static void stopCamera(){
-    mOpenCvCameraView.disableView();
-  }
-
-  public static Mat getCameraMat(){
-    return cStore;
+  public static View getCameraView(){
+    return cameraView;
   }
   //end user code
 
@@ -395,14 +282,10 @@ public class FtcRobotControllerActivity extends Activity implements CameraBridge
     callback.networkConnectionUpdate(WifiDirectAssistant.Event.DISCONNECTED);
 
     //user code
-    mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.image_manipulations_activity_surface_view);
-    mOpenCvCameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_BACK);
-    mOpenCvCameraView.setMaxFrameSize(400,400);
-    //mOpenCvCameraView.setVisibility(CameraBridgeViewBase.INVISIBLE);
-    mOpenCvCameraView.setCvCameraViewListener(this);
-
-    JavaCameraView mainLayout=(JavaCameraView) this.findViewById(R.id.image_manipulations_activity_surface_view);
-    mainLayout.setAlpha(0.0f);
+    lastContext = context;
+    cameraView = findViewById(R.id.image_manipulations_activity_surface_view);
+    View thing = findViewById(R.id.image_manipulations_activity_surface_view);
+    thing.setAlpha(0.0f);
     //end user code
 
     bindToService();
@@ -446,16 +329,6 @@ public class FtcRobotControllerActivity extends Activity implements CameraBridge
     super.onResume();
     RobotLog.vv(TAG, "onResume()");
     readNetworkType(NETWORK_TYPE_FILENAME);
-
-    //user code
-    if (!OpenCVLoader.initDebug()) {
-      RobotLog.vv(cvTAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-      OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
-    } else {
-      RobotLog.vv(cvTAG, "OpenCV library found inside package. Using it!");
-      mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-    }
-    //end user code
   }
 
   @Override
@@ -465,13 +338,6 @@ public class FtcRobotControllerActivity extends Activity implements CameraBridge
     if (programmingModeController.isActive()) {
       programmingModeController.stopProgrammingMode();
     }
-
-    //user code
-    if (mOpenCvCameraView != null) {
-      mOpenCvCameraView.disableView();
-      RobotLog.vv(cvTAG, "Camera Disabled");
-    }
-    //end user code
   }
 
   @Override
@@ -483,26 +349,12 @@ public class FtcRobotControllerActivity extends Activity implements CameraBridge
 
     // We *do* shutdown the robot even when we go into configuration editing
     controllerService.shutdownRobot();
-
-    //user code
-    if (mOpenCvCameraView != null) {
-      mOpenCvCameraView.disableView();
-      RobotLog.vv(cvTAG, "Camera Disabled");
-    }
-    //end user code
   }
 
   @Override
   public void onDestroy() {
     super.onDestroy();
     RobotLog.vv(TAG, "onDestroy()");
-
-    //user code
-    if (mOpenCvCameraView != null) {
-      mOpenCvCameraView.disableView();
-      RobotLog.vv(cvTAG, "Camera Disabled");
-    }
-    //end user code
 
     unbindFromService();
     wifiLock.release();
@@ -522,7 +374,7 @@ public class FtcRobotControllerActivity extends Activity implements CameraBridge
     }
   }
 
-  public void writeNetworkTypeFile(String fileName, String fileContents){
+  public void writeNetworkTypeFile(String fileName, String fileContents) {
     ReadWriteFile.writeFile(AppUtil.FIRST_FOLDER, fileName, fileContents);
   }
 
@@ -549,13 +401,13 @@ public class FtcRobotControllerActivity extends Activity implements CameraBridge
   }
 
   @Override
-  public void onWindowFocusChanged(boolean hasFocus){
+  public void onWindowFocusChanged(boolean hasFocus) {
     super.onWindowFocusChanged(hasFocus);
     // When the window loses focus (e.g., the action overflow is shown),
     // cancel any pending hide action. When the window gains focus,
     // hide the system UI.
     if (hasFocus) {
-      if (ImmersiveMode.apiOver19()){
+      if (ImmersiveMode.apiOver19()) {
         // Immersive flag only works on API 19 and above.
         immersion.hideSystemUI();
       }
@@ -579,11 +431,11 @@ public class FtcRobotControllerActivity extends Activity implements CameraBridge
       if (cfgFileMgr.getActiveConfig().isNoConfig()) {
         // Tell the user they must configure the robot before starting programming mode.
         AppUtil.getInstance().showToast(
-            context, context.getString(R.string.toastConfigureRobotBeforeProgrammingMode));
+                context, context.getString(R.string.toastConfigureRobotBeforeProgrammingMode));
       } else {
         Intent programmingModeIntent = new Intent(ProgrammingModeActivity.launchIntent);
         programmingModeIntent.putExtra(
-            LaunchActivityConstantsList.PROGRAMMING_MODE_ACTIVITY_NETWORK_TYPE, networkType);
+                LaunchActivityConstantsList.PROGRAMMING_MODE_ACTIVITY_NETWORK_TYPE, networkType);
         startActivity(programmingModeIntent);
       }
       return true;
@@ -591,41 +443,35 @@ public class FtcRobotControllerActivity extends Activity implements CameraBridge
       Intent inspectionModeIntent = new Intent(RcInspectionActivity.rcLaunchIntent);
       startActivity(inspectionModeIntent);
       return true;
-    }
-    else if (id == R.id.action_blocks) {
+    } else if (id == R.id.action_blocks) {
       Intent blocksIntent = new Intent(BlocksActivity.launchIntent);
       startActivity(blocksIntent);
       return true;
-    }
-    else if (id == R.id.action_restart_robot) {
+    } else if (id == R.id.action_restart_robot) {
       dimmer.handleDimTimer();
       AppUtil.getInstance().showToast(context, context.getString(R.string.toastRestartingRobot));
       requestRobotRestart();
       return true;
-    }
-    else if (id == R.id.action_configure_robot) {
+    } else if (id == R.id.action_configure_robot) {
       EditParameters parameters = new EditParameters();
       Intent intentConfigure = new Intent(FtcLoadFileActivity.launchIntent);
       parameters.putIntent(intentConfigure);
       startActivityForResult(intentConfigure, LaunchActivityConstantsList.FTC_CONFIGURE_REQUEST_CODE_ROBOT_CONTROLLER);
-    }
-    else if (id == R.id.action_settings) {
+    } else if (id == R.id.action_settings) {
       Intent settingsIntent = new Intent(FtcRobotControllerSettingsActivity.launchIntent);
       startActivityForResult(settingsIntent, LaunchActivityConstantsList.FTC_CONFIGURE_REQUEST_CODE_ROBOT_CONTROLLER);
       return true;
-    }
-    else if (id == R.id.action_about) {
+    } else if (id == R.id.action_about) {
       Intent intent = new Intent(AboutActivity.launchIntent);
       intent.putExtra(LaunchActivityConstantsList.ABOUT_ACTIVITY_CONNECTION_TYPE, networkType);
       startActivity(intent);
       return true;
-    }
-    else if (id == R.id.action_exit_app) {
+    } else if (id == R.id.action_exit_app) {
       finish();
       return true;
     }
 
-   return super.onOptionsItemSelected(item);
+    return super.onOptionsItemSelected(item);
   }
 
   @Override
@@ -706,122 +552,6 @@ public class FtcRobotControllerActivity extends Activity implements CameraBridge
           }
         }
       });
-    }
-  }
-
-  private class Thinning {
-
-    boolean B[][];
-
-    public Mat doJaniThinning(Mat Image) {
-      B = new boolean[Image.rows()][Image.cols()];
-      boolean [][] B_ = new boolean[Image.rows()][Image.cols()];
-      for(int i=0; i<Image.rows(); i++)
-        for(int j=0; j<Image.cols(); j++)
-          B[i][j] = (Image.get(i, j)[0]>100?false:true); //not a mistake, in matlab first invert and then morph
-
-      for(int iter = 0; iter < 1000; iter++) {
-        //Iteration #1
-        for(int i=0; i<Image.rows(); i++)
-          for(int j=0; j<Image.cols(); j++)
-            if(B[i][j] && G1(i, j) && G2(i, j) && G3(i, j)) B_[i][j] = false;
-            else B_[i][j] = B[i][j];;
-
-        for(int i=0; i<Image.rows(); i++)
-          for(int j=0; j<Image.cols(); j++) B[i][j] = B_[i][j];
-        //Iteration #2
-        for(int i=0; i<Image.rows(); i++)
-          for(int j=0; j<Image.cols(); j++)
-            if(B[i][j] && G1(i, j) && G2(i, j) && G3_(i, j)) B_[i][j] = false;
-            else B_[i][j] = B[i][j];
-
-
-        for(int i=0; i<Image.rows(); i++)
-          for(int j=0; j<Image.cols(); j++) B[i][j] = B_[i][j];
-      }
-
-
-      Mat r = new Mat(Image.rows(), Image.cols(), CvType.CV_32SC1);
-
-      for(int i=0; i<Image.rows(); i++)
-        for(int j=0; j<Image.cols(); j++) {
-          int[] a = new int[1];
-          a[0] = B[i][j]?255:0;
-          r.put(i, j, a);
-        }
-
-
-      return r;
-    }
-
-    private boolean x(int a, int i, int j) {
-      try {
-        switch(a) {
-          case 1:
-            return B[i+1][j];
-
-          case 2:
-            return B[i+1][j+1];
-
-          case 3:
-            return B[i][j+1];
-
-          case 4:
-            return B[i-1][j+1];
-
-          case 5:
-            return B[i-1][j];
-
-          case 6:
-            return B[i-1][j-1];
-
-          case 7:
-            return B[i][j-1];
-
-          case 8:
-            return B[i+1][j-1];
-
-
-        }
-      } catch(IndexOutOfBoundsException e) {
-        return false;
-      }
-      return false;
-    }
-
-    private boolean G1(int i, int j) {
-      int X = 0;
-      for(int q=1; q<=4; q++) {
-        if(!x(2*q-1, i, j) && (x(2*q, i, j) || x(2*q+1, i, j))) X++;
-      }
-      return X==1;
-    }
-
-    private boolean G2(int i, int j) {
-      int m = Math.min(n1(i, j), n2(i, j));
-      return (m==2 || m==3);
-    }
-
-    private int n1(int i, int j) {
-      int r = 0;
-      for(int q=1; q<=4; q++)
-        if(x(2*q-1, i, j) || x(2*q, i, j)) r++;
-      return r;
-    }
-
-    private int n2(int i, int j) {
-      int r = 0;
-      for(int q=1; q<=4; q++)
-        if(x(2*q, i, j) || x(2*q+1, i, j)) r++;
-      return r;
-    }
-
-    private boolean G3(int i, int j) {
-      return (x(2, i, j) || x(3, i, j) || !x(8, i, j)) && x(1, i, j);
-    }
-
-    private boolean G3_(int i, int j) {
-      return (x(6, i, j) || x(7, i, j) || !x(4, i, j)) && x(5, i, j);
     }
   }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVLib.java
@@ -0,0 +1,105 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.RobotLog;
+
+import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+/**
+ * Created by Noah on 11/10/2016.
+ */
+
+public abstract class OpenCVLib extends OpMode implements CameraBridgeViewBase.CvCameraViewListener2{
+
+    public static final String cvTAG = "OpenCVInit";
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private Context mContext;
+    private View mView;
+
+    OpenCVLib(){
+        mContext = FtcRobotControllerActivity.getAppContext();
+        mView = FtcRobotControllerActivity.getCameraView();
+    }
+
+    OpenCVLib(Context context, View view){
+        mContext = context;
+        mView = view;
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(mContext) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
+    public void initOpenCV(){
+        if (!OpenCVLoader.initDebug()) {
+            RobotLog.vv(cvTAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, mContext, mLoaderCallback);
+        } else {
+            RobotLog.vv(cvTAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+
+        mOpenCvCameraView = (CameraBridgeViewBase) mView;
+        mOpenCvCameraView.setAlpha(0.0f);
+        mOpenCvCameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_BACK);
+        mOpenCvCameraView.setMaxFrameSize(400,400);
+        //mOpenCvCameraView.setVisibility(CameraBridgeViewBase.INVISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    public void stopOpenCV(){
+        if (mOpenCvCameraView != null) {
+            mOpenCvCameraView.disableView();
+            RobotLog.vv(cvTAG, "Camera Disabled");
+        }
+    }
+
+
+    public void onCameraViewStarted(int width, int height){
+        RobotLog.vv(cvTAG, "Camera view started: " + width + ", " + height);
+        mOpenCvCameraView.setAlpha(1.0f);
+    }
+
+    public void onCameraViewStopped(){
+        RobotLog.vv(cvTAG, "Camera view stopped");
+        mOpenCvCameraView.setAlpha(0.0f);
+    }
+
+    public void startCamera(){
+        mOpenCvCameraView.enableView();
+        while(!mOpenCvCameraView.isActivated() && !mOpenCvCameraView.isEnabled());
+        RobotLog.vv(cvTAG, "Camera Activation Success!");
+    }
+
+    public void stopCamera(){
+        stopOpenCV();
+    }
+}
