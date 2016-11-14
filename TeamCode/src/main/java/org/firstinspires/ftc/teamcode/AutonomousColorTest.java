1a02e6ad7a6d0439a9cd0cd212705fdfc881ebfa
= <=>
2016-11-13 19:10:14 -0500

    Added basic color sensor test to press beacon buttons when the bot is started in front of it

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousColorTest.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+/**
+ * Created by Travis on 11/9/16
+ */
+@Autonomous(name="Color Tester", group="Test")
+public class AutonomousColorTest extends AutonomousBase{
+    public void gameState(){
+        super.gameState();
+        if(colorLeft1.blue() > colorLeft2.blue()){
+            moveState = MoveState.SERVO_PORT_R;
+        }else{
+            moveState = MoveState.SERVO_PORT_L;
+        }
+    }
+}
