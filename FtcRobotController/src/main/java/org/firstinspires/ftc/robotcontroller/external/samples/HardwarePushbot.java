6490302ef89d6e941701e3efd705eb08ad68a14a
Arinerron <Arinerron@users.noreply.github.com>
2016-11-16 22:31:59 -0800

    Another convention issue
    All of the other samples have the `{` on the same line as the class name. Why not this one?

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/HardwarePushbot.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/HardwarePushbot.java
@@ -21,8 +21,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Servo channel:  Servo to open left claw:  "left_hand"
  * Servo channel:  Servo to open right claw: "right_hand"
  */
-public class HardwarePushbot
-{
+public class HardwarePushbot {
     /* Public OpMode members. */
     public DcMotor  leftMotor   = null;
     public DcMotor  rightMotor  = null;
