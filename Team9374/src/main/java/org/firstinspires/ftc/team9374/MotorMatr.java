29eb06933444d719025b8c4bbe78689ab110d2b9
Camden Greenhalgh <Camden Greenhalgh>
2016-11-21 16:15:54 -0800

    Cam's Test #1

--- Team9374/src/main/java/org/firstinspires/ftc/team9374/MotorMatr.java
+++ Team9374/src/main/java/org/firstinspires/ftc/team9374/MotorMatr.java
@@ -1,6 +1,14 @@
 package org.firstinspires.ftc.team9374;
 
+import com.qualcomm.hardware.adafruit.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.Locale;
 
 /**
  * Created by lego7_000 on 11/21/2016.
@@ -8,11 +16,28 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class MotorMatr {
 
+    Orientation[] angles = new Orientation[2];
+
     public DcMotor[][] Motors = new DcMotor[10][4];
 
     public void runMotors(int column, float power) {
         for (int i = 0; i < this.Motors.length; i++) {
             this.Motors[i][column-1].setPower(power);
         }
+    }
+
+    public void rotateDegrees(BNO055IMU imu, float degrees, int left, int right) {
+        angles[0] = imu.getAngularOrientation().toAxesReference(AxesReference.INTRINSIC).toAxesOrder(AxesOrder.ZYX);
+        double init = formatAngle(angles[0].angleUnit, angles[0].firstAngle);
+        double cur = formatAngle(angles[0].angleUnit, angles[0].firstAngle);
+        while (Math.abs(init-cur) != init + degrees) {
+            runMotors(left, -(degrees/Math.abs(degrees)));
+            runMotors(right, (degrees/Math.abs(degrees)));
+            cur = formatAngle(angles[0].angleUnit, angles[0].firstAngle);
+        }
+    }
+
+    double formatAngle(AngleUnit angleUnit, double angle) {
+        return AngleUnit.DEGREES.fromUnit(angleUnit, angle);
     }
 }
