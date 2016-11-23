b529fc16b6b8b7208e082adcc5cce4c31e1d0596
Jack Silberman <jack@silbermans.net>
2016-11-22 17:27:24 -0800

    no message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jack/JackCompetitionTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jack/JackCompetitionTeleOp.java
@@ -10,8 +10,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "#11183: Jack TeleOp Competition", group = "Robot")
 public class JackCompetitionTeleOp extends OpMode {
 
-    //---------------------------------------------------------------------------------------------
 /*
+    ---------------------------------------------------------------------------------------------
+
    Define the actuators we use in the robot here
 */
     DcMotor leftWheelMotorFront;
@@ -20,12 +21,11 @@ public class JackCompetitionTeleOp extends OpMode {
     DcMotor rightWheelMotorBack;
     DcMotor ballCollectorMotor;
     DcMotor ballShooterMotor;
+/*
+ ----------------------------------------------------------------------------------------------
 
-// ----------------------------------------------------------------------------------------------
-
-    /*
         Get references to the hardware installed on the robot and name them here
-    */
+*/
     @Override
     public void init() {
         leftWheelMotorFront = hardwareMap.dcMotor.get("leftWheelMotorFront");
@@ -39,34 +39,34 @@ public class JackCompetitionTeleOp extends OpMode {
         rightWheelMotorFront.setDirection(DcMotor.Direction.REVERSE);
         rightWheelMotorBack.setDirection(DcMotor.Direction.REVERSE);
     }
-//---------------------------------------------------------------------------------------------
+/*
+---------------------------------------------------------------------------------------------
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
+      Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+*/
     @Override
     public void init_loop() {
     }
 
-//---------------------------------------------------------------------------------------------
+/*
+ ---------------------------------------------------------------------------------------------
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
+      Code to run ONCE when the driver hits PLAY
+*/
     @Override
     public void start() {
     }
-//---------------------------------------------------------------------------------------------
 /*
+---------------------------------------------------------------------------------------------
+
 Main code loop goes here
+
  */
     @Override
-<<<<<<< HEAD
-    public void loop() {
-=======
     public void loop(){
-        // read the gamepad values and put into variables
->>>>>>> d1af4182201c10d2e493898c6c5a20866d31a98f
+        /*
+        read the gamepad values and put into variables
+         */
         float leftY_gp1 = -gamepad1.left_stick_y;
         float rightY_gp1 = -gamepad1.right_stick_y;
 
@@ -76,15 +76,10 @@ Main code loop goes here
         rightWheelMotorFront.setPower(rightY_gp1);
         rightWheelMotorBack.setPower(rightY_gp1);
     }
-<<<<<<< HEAD
-
-    public void CollectBalls() {
-=======
-//---------------------------------------------------------------------------------------------
-
+/*---------------------------------------------------------------------------------------------
+*/
 
     public void CollectBalls(){
->>>>>>> d1af4182201c10d2e493898c6c5a20866d31a98f
         boolean intake = gamepad1.right_bumper;
         boolean outtake = gamepad1.left_bumper;
 
@@ -95,23 +90,20 @@ Main code loop goes here
         } else {
             ballCollectorMotor.setPower(0);
         }
-<<<<<<< HEAD
     }
-}
-=======
 
-//---------------------------------------------------------------------------------------------
->>>>>>> d1af4182201c10d2e493898c6c5a20866d31a98f
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    /*@Override
+/*
+---------------------------------------------------------------------------------------------
+
+     Code to run ONCE after the driver hits STOP
+*/
+    @Override
     public void stop() {
     }
-   */
 
-<<<<<<< HEAD
-=======
+/*
+---------------------------------------------------------------------------------------------
+ */
+
 }
-//---------------------------------------------------------------------------------------------
->>>>>>> d1af4182201c10d2e493898c6c5a20866d31a98f
+
