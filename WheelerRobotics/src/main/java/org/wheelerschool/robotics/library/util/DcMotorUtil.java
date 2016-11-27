d4686c596923a62c19829e792230c864bb547bbd
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-11-27 18:12:57 -0500

    Add 'DcMotorUtil' class

---
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/library/util/DcMotorUtil.java
@@ -0,0 +1,15 @@
+package org.wheelerschool.robotics.library.util;
+
+/**
+ * Helpful functions for "DcMotor"s.
+ *
+ * @see com.qualcomm.robotcore.hardware.DcMotor
+ *
+ * @author luciengaitskell
+ * @version 1.0
+ * @since 161127
+ */
+
+public class DcMotorUtil {
+    private DcMotorUtil() {}  // Prevent instantiation
+}
