84399e5f969289af4e64e0b171804f03e405dc89
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-11-27 18:13:40 -0500

    Add 'setMotorsPower' to 'DcMotorUtil'

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/library/util/DcMotorUtil.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/library/util/DcMotorUtil.java
@@ -1,5 +1,9 @@
 package org.wheelerschool.robotics.library.util;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.List;
+
 /**
  * Helpful functions for "DcMotor"s.
  *
@@ -12,4 +16,13 @@ package org.wheelerschool.robotics.library.util;
 
 public class DcMotorUtil {
     private DcMotorUtil() {}  // Prevent instantiation
+
+    public static void setMotorsPower(List<DcMotor> motors, double power) {
+        /**
+         * Set the power of all motors in a list.
+         */
+        for (DcMotor mtr : motors) {
+            mtr.setPower(power);
+        }
+    }
 }
