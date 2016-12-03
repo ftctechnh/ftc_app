ae57d3a63597bcc8d05b0d28ab761863c9f1c751
prestonraab <prestonraab@gmail.com>
2016-12-03 15:51:13 -0800

    Started two classes for pushing the beacons
    They use most of the methods from the DriveEngine class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OneButtonPusher.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class OneButtonPusher
+{
+
+
+    private ElapsedTime timer = null;
+
+    DcMotor frontPusher = null;
+    Gamepad gamepad = null;
+    Sensors sensors = null;
+
+    private DcMotor.Direction frontDirection = DcMotor.Direction.REVERSE;
+
+    private double frontPower = 0;  //[0.0, 1.0]
+    private double backPower = 0; //[0.0, 1.0]
+
+
+
+    public OneButtonPusher(HardwareMap hardwareMap, Gamepad gamepad, Sensors sensors)
+    {
+        this.gamepad = gamepad;
+
+        this.sensors = sensors;
+
+        frontPusher  = hardwareMap.dcMotor.get("front_push");
+
+        frontPusher.setPower(frontPower);
+
+        frontPusher.setDirection(frontDirection);
+        timer = new ElapsedTime();
+    }
+
+    public Double getFrontPower()
+    {
+        return frontPower;
+    }
+
+    public Double getBackPower()
+    {
+        return backPower;
+    }
+
+    public void invertDirection()
+    {
+        if (frontDirection == DcMotor.Direction.FORWARD)
+            frontDirection = DcMotor.Direction.REVERSE;
+        else
+            frontDirection = DcMotor.Direction.FORWARD;
+    }
+
+    public void setPower(double fPower) {
+        frontPower = fPower;
+
+        frontPusher.setPower(frontPower);
+    }
+
+    public void stop()
+    {
+        frontPower = 0;
+        frontPusher.setPower(frontPower);
+    }
+}
