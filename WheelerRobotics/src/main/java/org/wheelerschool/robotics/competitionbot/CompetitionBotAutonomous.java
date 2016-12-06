458477733047658a9af5b4fa0d8f52e5e0cd49bd
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-12-06 18:35:01 -0500

    Add 'Drive to Position' to 'CompetitionBotAutonomous'

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/CompetitionBotAutonomous.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/CompetitionBotAutonomous.java
@@ -1,13 +1,165 @@
 package org.wheelerschool.robotics.competitionbot;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.wheelerschool.robotics.library.navigation.TranslationMotorNavigation;
+import org.wheelerschool.robotics.library.util.DcMotorUtil;
+import org.wheelerschool.robotics.library.vision.VuforiaLocation;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by luciengaitskell on 11/24/16.
  */
 
 public abstract class CompetitionBotAutonomous extends LinearOpMode {
-    public void runOpMode() throws InterruptedException {
+    // Variables to set when extending class:
+    /**
+     * What to set:
+     *  1. closeMotors
+     *  2. fartherMotors
+     *  3. sideUltrasonicSensor
+     *  4. INITAL_LOCATION
+     */
+    //      Wall Follow Motors:
+    public List<DcMotor> closeMotors;
+    public double closeMotorGain;
+    public List<DcMotor> fartherMotors;
+    public double fartherMotorGain;
+    //      Sensors:
+    public UltrasonicSensor sideUltrasonicSensor;
+    //      Other:
+    public VectorF INITAL_LOCATION;
 
+
+    // Hardware Setup:
+    //      Motor:
+    public List<DcMotor> leftMotors = new ArrayList<>();
+    public List<DcMotor> rightMotors = new ArrayList<>();
+
+    // Config:
+    public static String LOG_TAG = "Comp Bot Auto";
+    private static long MAX_TIME_TIMEOUT = 200; // MAX time until TIMEOUT when running OpMode (millis)
+    private static double NO_BEACON_ROTATE_SPEED = 0.25;
+
+
+    public void runOpMode() throws InterruptedException {
+        // Hardware Setup:
+        //      Motors:
+        this.leftMotors.add(hardwareMap.dcMotor.get("frontLeft"));
+        this.leftMotors.add(hardwareMap.dcMotor.get("backLeft"));
+        this.rightMotors.add(hardwareMap.dcMotor.get("frontRight"));
+        this.rightMotors.add(hardwareMap.dcMotor.get("backRight"));
+
+        // Set phone location:
+        OpenGLMatrix phoneLocation = OpenGLMatrix
+                .translation((15 * VuforiaLocation.MM_PER_INCH) / 2,
+                        -1.75f * VuforiaLocation.MM_PER_INCH,
+                        4 * VuforiaLocation.MM_PER_INCH)
+                .multiplied(Orientation.getRotationMatrix(
+                        AxesReference.EXTRINSIC, AxesOrder.XYZ,
+                        AngleUnit.DEGREES, 0, -90, 0));
+
+        // Vuforia Target Setup:
+        VuforiaLocation vuforia = new VuforiaLocation(phoneLocation);
+
+        // Translation Navigation Setup:
+        TranslationMotorNavigation translationNavigation = new TranslationMotorNavigation();
+
+        // Wait for start button to be pushed:
+        waitForStart();
+
+        /*---------------------------------DRIVE TO INITIAL POINT---------------------------------*/
+        long time = System.currentTimeMillis();
+        while (opModeIsActive()) {
+            // Set 'Phase' in the telemetry:
+            telemetry.addData("Phase", "Drive to Initial Position");
+
+            // Read and get robot location data using vuforia:
+            vuforia.readData();
+            VectorF lastLocation = vuforia.lastLocationXYZ;
+            Orientation lastRotation = vuforia.lastRotationXYZ;
+
+            // If robot location is known:
+            if (lastLocation != null && lastRotation != null) {
+                time = System.currentTimeMillis();
+
+                // Get specific values from robot location data:
+                float x = lastLocation.get(0);
+                float y = lastLocation.get(1);
+                float robotRot = lastRotation.thirdAngle;
+
+                // Log data for debug:
+                Log.d(LOG_TAG, String.format("X: %.3f, Y: %.3f, Rot: " + lastRotation.toString(), x, y));
+
+                // Calculate translation required to get to target:
+                VectorF translation = INITAL_LOCATION.subtracted(lastLocation);
+
+                // Calculate motor power for navigation to target:
+                TranslationMotorNavigation.NavigationData calculationData =
+                        translationNavigation.calculateNavigationData(translation.get(0), translation.get(1), robotRot);
+
+                // Break if on target:
+                if (calculationData.onTarget) {
+                    break;
+                }
+
+                // Add data to telemetry for debug:
+                telemetry.addData("On Target", calculationData.onTarget);
+                telemetry.addData("Left Power", calculationData.leftMotorPower);
+                telemetry.addData("Right Power", calculationData.rightMotorPower);
+                telemetry.addData("Needed Distance", calculationData.translationDistance);
+                telemetry.addData("Needed Angle", calculationData.translationAngle);
+                telemetry.addData("Robot Angle", robotRot);
+                telemetry.addData("Needed Angle Change", calculationData.rotationAmount);
+                telemetry.addData("Forward Power", calculationData.forwardPower);
+                telemetry.addData("Rotation Power", calculationData.rotationPower);
+                telemetry.addData("Robot X", x);
+                telemetry.addData("Robot Y", y);
+
+                // Drive Motors:
+                DcMotorUtil.setMotorsPower(this.leftMotors, calculationData.leftMotorPower);
+                DcMotorUtil.setMotorsPower(this.rightMotors, calculationData.rightMotorPower);
+            }
+
+            // If target are not seen after some amount of time, rotate to find one:
+            if (System.currentTimeMillis() - time > MAX_TIME_TIMEOUT) {
+                // Log data and add to telemetry for debug:
+                telemetry.addData("ERROR", "Target has not been seen in " + MAX_TIME_TIMEOUT + "ms");
+                Log.d("Vuforia Data", "Lost beacon (" + MAX_TIME_TIMEOUT + "ms). Rotating...");
+
+                // Set motors to rotate:
+                DcMotorUtil.setMotorsPower(this.leftMotors, NO_BEACON_ROTATE_SPEED);
+                DcMotorUtil.setMotorsPower(this.rightMotors, NO_BEACON_ROTATE_SPEED);
+
+                // Sleep to allow for rotation:
+                Thread.sleep(200);
+
+                // Stop motors:
+                DcMotorUtil.setMotorsPower(this.leftMotors, 0);
+                DcMotorUtil.setMotorsPower(this.rightMotors, 0);
+                Log.d("Vuforia Data", "Ended Rotation");
+
+                // Wait to allow camera to adjust and acquire a lock on a target
+                Thread.sleep(500);
+            }
+
+            // Update telemetry:
+            telemetry.update();
+
+            // Sleep to control loop:
+            Thread.sleep(50);
+        }
     }
 }
