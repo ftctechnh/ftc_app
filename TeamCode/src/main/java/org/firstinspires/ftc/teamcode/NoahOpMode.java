52df65b1dcdbccdedda67db029171b2881acca8a
Noah Pietrafesa <thepizzaofdeath@gmail.com>
2016-12-05 19:02:46 -0500

    Added sweeper support

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NoahOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NoahOpMode.java
@@ -5,25 +5,29 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
- * Created by mikepietrafesa1 on 12/3/16.
+ * Created by Noah Pietrafesa on 12/3/16.
  */
 @TeleOp
 public class NoahOpMode extends OpMode {
 
     DcMotor left;
     DcMotor right;
+    DcMotor sweeper;
 
     @Override
     public void init()
     {
         left = hardwareMap.dcMotor.get("left");
         right = hardwareMap.dcMotor.get("right");
+        sweeper = hardwareMap.dcMotor.get("sweeper");
     }
+    //Noah was here
 
     @Override
     public void loop()
     {
         left.setPower(gamepad1.left_stick_y);
         right.setPower(gamepad1.right_stick_y);
+        sweeper.setPower(gamepad1.right_trigger);
     }
 }
