52f386f5fc62d67413485b4cb635173b61b3ac96
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-12-06 18:34:15 -0500

    Add 'CompetitionBotAutonomous'

---
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/CompetitionBotAutonomous.java
@@ -0,0 +1,13 @@
+package org.wheelerschool.robotics.competitionbot;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by luciengaitskell on 11/24/16.
+ */
+
+public abstract class CompetitionBotAutonomous extends LinearOpMode {
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
