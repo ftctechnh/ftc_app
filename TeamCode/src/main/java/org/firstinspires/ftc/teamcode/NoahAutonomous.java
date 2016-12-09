f289b39f894f02e415b76501a39e6a0069a83710
Noah Pietrafesa <thepizzaofdeath@gmail.com>
2016-12-09 14:54:44 -0500

    added onto autonomus program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NoahAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NoahAutonomous.java
@@ -1,8 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
- * Created by mikepietrafesa1 on 12/5/16.
+ * Created by Noah Pietrafesa on 12/5/16.
  */
 
-public class NoahAutonomous {
+public class NoahAutonomous extends OpMode {
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
+
+
