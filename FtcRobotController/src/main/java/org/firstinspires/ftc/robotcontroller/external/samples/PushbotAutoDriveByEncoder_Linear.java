a3215ae05991aa13d2a5b951aff53c217610bd18
James Fegan <james@ifspace.org>
2016-12-09 01:41:57 -0500

    Revert "Merge remote-tracking branch 'refs/remotes/origin/master' into Invaders"
    This reverts commit 24c051a6e3c18f87ef3d90fcc8868b8332d59885, reversing
    changes made to 1ada4380ee340c118fb9661d79d91da3d8440f9b.

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotAutoDriveByEncoder_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotAutoDriveByEncoder_Linear.java
@@ -82,11 +82,7 @@ public class PushbotAutoDriveByEncoder_Linear extends LinearOpMode {
     static final double     TURN_SPEED              = 0.5;
 
     @Override
-<<<<<<< HEAD
     public void runOpMode() throws InterruptedException {
-=======
-    public void runOpMode() {
->>>>>>> refs/remotes/ftctechnh/master
 
         /*
          * Initialize the drive system variables.
@@ -138,11 +134,7 @@ public class PushbotAutoDriveByEncoder_Linear extends LinearOpMode {
      */
     public void encoderDrive(double speed,
                              double leftInches, double rightInches,
-<<<<<<< HEAD
                              double timeoutS) throws InterruptedException {
-=======
-                             double timeoutS) {
->>>>>>> refs/remotes/ftctechnh/master
         int newLeftTarget;
         int newRightTarget;
 
@@ -175,12 +167,9 @@ public class PushbotAutoDriveByEncoder_Linear extends LinearOpMode {
                                             robot.leftMotor.getCurrentPosition(),
                                             robot.rightMotor.getCurrentPosition());
                 telemetry.update();
-<<<<<<< HEAD
 
                 // Allow time for other processes to run.
                 idle();
-=======
->>>>>>> refs/remotes/ftctechnh/master
             }
 
             // Stop all motion;
