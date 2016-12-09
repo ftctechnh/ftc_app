d256145720caacd4ca58ef610237aa6caae8344c
Noah Pietrafesa <thepizzaofdeath@gmail.com>
2016-12-09 18:32:40 -0500

    rewrote for 4 motor compatability, fixed issue where right side motors moved backwards

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NoahOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NoahOpMode.java
@@ -10,24 +10,30 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp
 public class NoahOpMode extends OpMode {
 
-    DcMotor left;
-    DcMotor right;
-    DcMotor sweeper;
+    DcMotor left1;
+    DcMotor left2;
+    DcMotor right1;
+    DcMotor right2;
+    DcMotor sweeper2;
 
     @Override
     public void init()
     {
-        left = hardwareMap.dcMotor.get("left");
-        right = hardwareMap.dcMotor.get("right");
-        sweeper = hardwareMap.dcMotor.get("sweeper");
+        left1 = hardwareMap.dcMotor.get("left1");
+        left2 = hardwareMap.dcMotor.get("left2");
+        right1 = hardwareMap.dcMotor.get("right1");
+        right2 = hardwareMap.dcMotor.get("right2");
+        sweeper2 = hardwareMap.dcMotor.get("sweeper2");
     }
     //Noah was here
 
     @Override
     public void loop()
     {
-        left.setPower(gamepad1.left_stick_y);
-        right.setPower(gamepad1.right_stick_y);
-        sweeper.setPower(gamepad1.right_trigger);
+        left1.setPower(gamepad1.left_stick_y);
+        left2.setPower(gamepad1.left_stick_y);
+        right2.setPower(-1*(gamepad1.right_stick_y));
+        right1.setPower(-1*(gamepad1.right_stick_y));
+        sweeper2.setPower(gamepad1.right_trigger);
     }
 }
