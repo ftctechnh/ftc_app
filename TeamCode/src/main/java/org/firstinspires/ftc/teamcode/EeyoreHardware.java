15f99f471436e240fbb4a2df1ab2eaa8db691244
matursam <matursam@my.waketech.edu>
2016-12-12 19:21:53 -0500

    Added hardware for Eeyore (the primary robot)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EeyoreHardware.java
@@ -0,0 +1,94 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class EeyoreHardware
+{
+    /* Public OpMode members. */
+    public DcMotor l1   = null;
+    public DcMotor l2   = null;
+    public DcMotor r1   = null;
+    public DcMotor r2   = null;
+    public DcMotor shooter   = null;
+    public DcMotor collection   = null;
+    public ColorSensor color   = null;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           = null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public EeyoreHardware() {
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        l1 = hwMap.dcMotor.get("l1");
+        l2 = hwMap.dcMotor.get("l2");
+        r1 = hwMap.dcMotor.get("r1");
+        r2 = hwMap.dcMotor.get("r2");
+        shooter = hwMap.dcMotor.get("shooter");
+        collection = hwMap.dcMotor.get("collection");
+
+        color = hwMap.colorSensor.get("color");
+
+        // Set motor direction
+        l1.setDirection(DcMotor.Direction.REVERSE);
+        l2.setDirection(DcMotor.Direction.REVERSE);
+        r1.setDirection(DcMotor.Direction.FORWARD);
+        r2.setDirection(DcMotor.Direction.FORWARD);
+        shooter.setDirection(DcMotor.Direction.FORWARD);
+        collection.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set all motors to zero power
+        l1.setPower(0);
+        l2.setPower(0);
+        r1.setPower(0);
+        r2.setPower(0);
+        shooter.setPower(0);
+        collection.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        l1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        l2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        r1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        r2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        shooter.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        collection.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // Define and initialize ALL installed servos.
+        // None yet
+    }
+
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
+     * @throws InterruptedException
+     */
+    public void waitForTick(long periodMs) throws InterruptedException {
+
+        long  remaining = periodMs - (long)period.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0)
+            Thread.sleep(remaining);
+
+        // Reset the cycle clock for the next pass.
+        period.reset();
+    }
+}
+
