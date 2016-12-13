6414177ca551ed5cd61e1126befcd1dc66765cb4
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-12-13 17:20:56 -0500

    Update initial location in 'CompetitionBotAutonomousBlue'

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/SideSpecific/CompetitionBotAutonomousBlue.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/competitionbot/SideSpecific/CompetitionBotAutonomousBlue.java
@@ -17,7 +17,7 @@ public class CompetitionBotAutonomousBlue extends CompetitionBotAutonomous {
     @Override
     public void runOpMode() throws InterruptedException {
         // Sensor Setup:
-        this.INITAL_LOCATION = new VectorF(-56 * VuforiaLocation.MM_PER_INCH, -28 * VuforiaLocation.MM_PER_INCH, 0);
+        this.INITAL_LOCATION = new VectorF(-60 * VuforiaLocation.MM_PER_INCH, -12 * VuforiaLocation.MM_PER_INCH, 0);
         Log.d("CompBotAutoBlue", this.INITAL_LOCATION.toString());
         this.sideUltrasonicSensor = hardwareMap.ultrasonicSensor.get("leftUltrasonicSensor");
         this.closeMotorGain = 1;
