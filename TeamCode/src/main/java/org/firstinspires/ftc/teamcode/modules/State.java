778d4f414760adafe242e8021a8c71e61100aabc
ftc359 <ftc.359@gmail.com>
2016-12-13 14:49:52 -0800

    Added capabilities to attach variables to states

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/State.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/State.java
@@ -3,13 +3,21 @@ package org.firstinspires.ftc.teamcode.modules;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public abstract class State {
     final String id;
     StateMachine stateMachine;
 
+    private Map<String, Integer> parameterInt;
+    private Map<String, Boolean> parameterBool;
+
     public State(@NonNull String id) {
         this.id = id;
+        parameterInt = new HashMap<>();
+        parameterBool = new HashMap<>();
     }
 
     public abstract void run();
@@ -21,5 +29,21 @@ public abstract class State {
         }
 
         stateMachine.changeState(stateId);
+    }
+
+    public void sendData(String key, int anInt) {
+        parameterInt.put(key, anInt);
+    }
+
+    public void sendData(String key, boolean aBool) {
+        parameterBool.put(key, aBool);
+    }
+
+    protected int getInt(String key) {
+        return parameterInt.get(key);
+    }
+
+    protected boolean getBool(String key) {
+        return parameterBool.get(key);
     }
 }
