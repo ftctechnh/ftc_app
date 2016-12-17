26103b0d209b7842e97c3f9765b3e39c83bbf4ff
Noah Pietrafesa <thepizzaofdeath@gmail.com>
2016-12-17 12:00:46 -0500

    rewriting code, will clean up later

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 /**
  * Created by Noah Pietrafesa on 12/17/16.
@@ -11,26 +11,40 @@ import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
 @Autonomous
 
 public class ColorSensorTest extends OpMode {
-
     DcMotor left1;
     DcMotor left2;
     DcMotor right1;
     DcMotor right2;
-    ModernRoboticsI2cColorSensor red;
-
+    ColorSensor red;
+    boolean lookForRed;
+    boolean 
     @Override
     public void init() {
-
         left1 = hardwareMap.dcMotor.get("left1");
         left2 = hardwareMap.dcMotor.get("left2");
         right1 = hardwareMap.dcMotor.get("right1");
         right2 = hardwareMap.dcMotor.get("right2");
-        red = (ModernRoboticsI2cColorSensor) hardwareMap.colorSensor.get("red");
+        red = hardwareMap.colorSensor.get("red");
+    }
 
+    @Override
+    public void start()
+    {
+        lookForRed = true;
+        resetStartTime();
     }
 
     @Override
     public void loop() {
+        if (lookForRed)
+        {
+            right1.setPower(-1);
+            right2.setPower(-1);
+            left1.setPower(1);
+            left2.setPower(1);
+            red.red
+        }
+
         if (time < 5)
         {
             right1.setPower(-1);
@@ -38,12 +52,19 @@ public class ColorSensorTest extends OpMode {
             left1.setPower(1);
             left2.setPower(1);
         }
-        else if ()
+        if (red.red() > 100)
         {
             right1.setPower(0);
             right2.setPower(0);
             left1.setPower(0);
             left2.setPower(0);
+        }
+        else
+        {
+            right1.setPower(1);
+            right2.setPower(1);
+            left1.setPower(-1);
+            left2.setPower(-1);
         }
     }
 }
