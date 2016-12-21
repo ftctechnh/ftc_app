5c5d4d54b34a17e9d2f0e4bc361f051b8fe98a59
MitchellFTC <mitchellftc@gmail.com>
2016-12-21 16:50:44 -0500

    Started MitchellStraight and MitchellTurn for auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MitchellStraight.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+/**
+ * Created by Robotics on 12/21/2016.
+ */
+
+public class MitchellStraight extends OpMode {
+
+    //CONST
+
+    public static final double OneEncRotation = 1440;
+    public static final double DistanceOneRotation = 0;
+    public static final double ninetyTurn = 0;
+    public static final double speed = .75d;
+    public static final double turnSpeed = .5d;
+
+    //Hardware init
+    HardwarePushbot robot = new HardwarePushbot();
+
+    //init
+    public void init() {
+
+    }
+    //loop
+    public void loop() {
+
+    }
+}
