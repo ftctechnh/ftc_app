2e440dcba738e3aecc78d1797ed740a93c812df2
matursam <matursam@my.waketech.edu>
2016-12-22 15:42:58 -0500

    Eeyore Teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EeyoreTeleop.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.EeyoreHardware;
+
+@TeleOp(name="Eeyore TeleOp", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
+//@Disabled
+public class EeyoreTeleop extends OpMode {
+    EeyoreHardware robot = new EeyoreHardware();
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        double left;
+        double right;
+
+        // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+        left = Range.clip(gamepad1.left_stick_y - gamepad1.left_stick_x, -1, 1);
+        right = Range.clip(gamepad1.left_stick_y + gamepad1.left_stick_x, -1, 1);
+        robot.l1.setPower(left);
+        robot.l2.setPower(left);
+        robot.r1.setPower(right);
+        robot.r2.setPower(right);
+
+        if(gamepad1.a)
+        {
+            robot.collection.setPower(1);
+        }
+        else if(gamepad1.y)
+        {
+            robot.collection.setPower(-1);
+        }
+
+        if(gamepad1.x)
+        {
+            robot.shooter.setPower(1);
+        }
+        else if(gamepad1.b)
+        {
+            robot.shooter.setPower(-1);
+        }
+        // Send telemetry message to signify robot running;
+        telemetry.addData("left", "%.2f", left);
+        telemetry.addData("right", "%.2f", right);
+        telemetry.update();
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+}
