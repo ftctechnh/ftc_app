9bc2de71a8464b4a608382e7af89070efbdd0301
James Fegan <james@ifspace.org>
2017-01-02 21:10:31 -0500

    Adding in a class for enabling/disabling the I2C-based sensors. Reference the following post: http://ftcforum.usfirst.org/showthread.php?7724-Gyro-Read-Latency-Development-team-update&p=33112&viewfull=1#post33112 for details on how to use this class.  Their example assumes you'll have this stuff in your opmode class, but now that we are using the shared 'VelocityVortexBot' class, you can add the new variables there.
    Specifically, you need to add a FtcI2cDeviceState variable for each of the two color sensors, the gyro, and the distance sensor in the VelocityVortexBot class.  Then, add setEnabled(true) / setEnabled(false) calls at the top and bottom of your new functions (GyroTurn, DistanceDrive, etc) for the sensors that you will be using in that method.  The goal is to leave

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcI2cDeviceState.java
@@ -0,0 +1,40 @@
+import com.qualcomm.robotcore.hardware.I2cController;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+
+public class FtcI2cDeviceState
+{
+    private I2cDevice device;
+    private I2cController.I2cPortReadyCallback deviceCallback;
+    private boolean deviceEnabled;
+
+    public FtcI2cDeviceState(I2cDevice device)
+    {
+        this.device = device;
+        deviceCallback = device.getI2cPortReadyCallback();
+        deviceEnabled = true;
+        if (device.getI2cController().getI2cPortReadyCallback(device.getPort()) != deviceCallback)
+            throw new IllegalStateException("PortReadyCallback don't match!");
+    }   //FtcI2cDeviceState
+
+public boolean isEnabled()
+    {
+        return deviceEnabled;
+    }   //isEnabled
+
+public void setEnabled(boolean enabled)
+    {
+        if (deviceEnabled != enabled)
+        {
+            if (enabled)
+            {
+                device.registerForI2cPortReadyCallback(deviceCallback);
+            }
+            else
+{
+                device.deregisterForPortReadyCallback();
+            }
+            deviceEnabled = enabled;
+        }
+    }   //setEnabled
+
+}   //class FtcI2cDeviceState
