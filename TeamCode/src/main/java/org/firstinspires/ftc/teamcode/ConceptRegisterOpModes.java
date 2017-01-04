b8a974d9362215d80077d81e7bd43a1fce98ab25
MitchellFTC <mitchellftc@gmail.com>
2017-01-04 18:47:30 -0500

    Started on auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptRegisterOpModes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptRegisterOpModes.java
@@ -73,6 +73,7 @@ public class ConceptRegisterOpModes
     // Driving Samples
     // manager.register("Teleop POV",             PushbotTeleopPOV_Linear.class);
      manager.register("Teleop Tank",            PushbotTeleopTank_Iterative.class);
+      manager.register("test auto",            MitchellStraight.class);
     // manager.register("Auto Drive Gyro",        PushbotAutoDriveByGyro_Linear.class);
     // manager.register("Auto Drive Encoder",     PushbotAutoDriveByEncoder_Linear.class);
      manager.register("Auto Drive Time",        PushbotAutoDriveByTime_Linear.class);
