1ead30f1d11e927f49d8ef56cd691b6d73c6dfd7
Wired5035 <WiredFTC@gmail.com>
2017-01-07 14:27:11 -0600

    autonomous code to beacon from saturdays
    works till we want it to drive in reverse

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDoubleBeaconBlue2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDoubleBeaconBlue2.java
@@ -25,6 +25,8 @@ public class AutoDoubleBeaconBlue2 extends LinearOpMode {
         robot.ballBooster2.setPower(1);
         robot.driveForward((OneFoot * 2.25) - 1);
 
+        //shooting the balls
+
         sleep(2000);
         robot.triggered();
         sleep(500);
@@ -33,21 +35,34 @@ public class AutoDoubleBeaconBlue2 extends LinearOpMode {
         robot.triggered();
         sleep(500);
         robot.detriggered();
+
+        //turns off shooter
+
         robot.ballBooster1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         robot.ballBooster1.setPower(0);
         robot.ballBooster2.setPower(0);
-        robot.driveForward(OneFoot * 1);
-        robot.turnDegrees(-90);
-        robot.driveForward(OneFoot * 2.25);
-        while(robot.leftLightSensor.getLightDetected() < .25 || robot.rightLightSensor.getLightDetected() < .25){
+
+        //Turns to right to drive directly towards beacon, without hitting the ball
+        robot.turnDegrees(145);
+        robot.driveForward(OneFoot * -1.00);
+
+        //code for meet 1 pushing the ball and driving toward beacon
+        // robot.driveForward(OneFoot * 1);
+       // robot.turnDegrees(-90);
+       // robot.driveForward(OneFoot * 2.25);
+
+       // UNCOMMENT OUT THiS CODE LATER
+       while(robot.leftLightSensor.getLightDetected() < .25 || robot.rightLightSensor.getLightDetected() < .25){
             robot.setDrivePower(.25);
         }
         robot.setDrivePower(0);
-        robot.turnDegrees(-90);
+        robot.turnDegrees(-105);
+        /*
         robot.driveForward(.5);
         telemetry.addData("value of driveReverse", OneFoot + " inches");
         telemetry.update();
+        */
+        robot.setDrivePower(0);
 
     }
 }
-
