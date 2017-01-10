ab54c6c4e1cc4f5d63b6aeecff5b0a35394b03ed
matursam <matursam@my.waketech.edu>
2017-01-10 13:23:20 -0500

    Gyro code does not work at all. Some fixes have been implemented. Problem appears to be in the first loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeaconFinderAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeaconFinderAuto.java
@@ -12,7 +12,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.ColorSensor;
-
+import com.qualcomm.robotcore.util.Range;
+import java.util.concurrent.TimeUnit;
 
 @Autonomous(name="Beacon Finder", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
 public class BeaconFinderAuto extends LinearOpMode {
@@ -70,7 +71,7 @@ public class BeaconFinderAuto extends LinearOpMode {
 
         //reachBeacon();
         //pushBeacons();
-        GyroMovement(0.5, 90, 5000); //pull forward off the wall
+        GyroMovement(0.3, 90, 5000); //pull forward off the wall
 
         ModernRoboticsI2cGyro Gyro;   // Hardware Device Object
         Gyro = (ModernRoboticsI2cGyro)hardwareMap.gyroSensor.get("gyro");
@@ -81,10 +82,7 @@ public class BeaconFinderAuto extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            while(true) {
-                telemetry.addData("Gyro output", Gyro.getHeading());
-                telemetry.update();
-            }
+            idle();
         }
     }
 
@@ -128,14 +126,19 @@ public class BeaconFinderAuto extends LinearOpMode {
         GyroMovement(0.5, 0, 5000); //pull forward off the wall
     }
 
-    public void GyroMovement(double speed, int direction, int time) //Speed is from -1 to 1 and direction is 0 to 360 degrees
+    public void GyroMovement(double speed, int targetDirection, int time) //Speed is from -1 to 1 and direction is 0 to 360 degrees
     {
         ModernRoboticsI2cGyro Gyro;   // Hardware Device Object
         Gyro = (ModernRoboticsI2cGyro)hardwareMap.gyroSensor.get("gyro");
         Gyro.calibrate();
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
 
-        int currentHeading = 0;
-        long startTime = System.currentTimeMillis(); //Determine the time as we enter the loop
+
+        int currentDirection = Gyro.getHeading();
 
         int error_prior = 0;
         int integral = 0;
@@ -143,30 +146,78 @@ public class BeaconFinderAuto extends LinearOpMode {
         double I = 0.1;
         double D = 0.1;
 
+        //First, check to see if we are pointing in the correct direction
+        while(Math.abs(targetDirection - currentDirection) > 5) //If we are more than 5 degrees off target, make corrections before moving
+        {
+            currentDirection = Gyro.getHeading();
+            int error = targetDirection - currentDirection;
+            // In order to prevent issues when crossing from 360 to zero or vice versa,
+            // we need to adjust error to compensate
+
+            if(error > 360)
+            {
+                error = error - 360;
+            }
+
+            integral = integral + error;
+            int derivative = error-error_prior;
+            double pidOutput = 0.1 * P * error; //+ I * integral + D * derivative; //Value will be based off the amount of degrees we've deviated from the target, but there'll be some amount of modification done to this value
+
+            //If pidOutput is negative, we need to turn left, and if it's positive we need to turn right
+
+            double leftPower = -pidOutput;
+            double rightPower = pidOutput;
+
+            //Finally, assign these values to the motors
+
+            robot.r1.setPower(rightPower);
+            robot.r2.setPower(rightPower);
+            robot.l1.setPower(leftPower);
+            robot.l2.setPower(leftPower);
+            String currentMode = "Turning";
+            telemetry.addData("Current Mode", currentMode);
+            telemetry.addData("rightPower", rightPower);
+            telemetry.addData("leftPower", leftPower);
+            telemetry.addData("PID Output", pidOutput);
+            telemetry.addData("Gyro output", currentDirection);
+
+            telemetry.update();
+        }
+
+        //Now we can move forward, making corrections as needed
+
+        long startTime = System.currentTimeMillis(); //Determine the time as we enter the loop
+
         while(System.currentTimeMillis() - startTime < time) //Loop for the desired amount of time
         {
-            currentHeading = Gyro.getHeading();
-            int error = direction - currentHeading;
+            currentDirection = Gyro.getHeading();
+            int error = targetDirection - currentDirection;
+
+            if(error > 360)
+            {
+                error = error - 360;
+            }
+
             integral = integral + error;
             int derivative = error-error_prior;
             double pidOutput = P * error; //+ I * integral + D * derivative; //Value will be based off the amount of degrees we've deviated from the target, but there'll be some amount of modification done to this value
             //Now we just need to use this error value to determine our motor speeds
             //We need to determine whether we are turning left or right first
-            double leftPower = 0;
-            double rightPower = 0;
-            if(direction - currentHeading > currentHeading - direction)
-            leftPower = speed + (pidOutput * 0.2);
-            rightPower = speed + (pidOutput * 0.2);
+            double leftPower = speed; //+ (-pidOutput * 0.006);
+            double rightPower = speed; //+ (pidOutput * 0.006);
+
             //Finally, assign these values to the motors
             robot.r1.setPower(rightPower);
             robot.r2.setPower(rightPower);
             robot.l1.setPower(leftPower);
             robot.l2.setPower(leftPower);
 
+            String currentMode = "Moving Forward";
+            telemetry.addData("Current Mode", currentMode);
             telemetry.addData("rightPower", rightPower);
             telemetry.addData("leftPower", leftPower);
             telemetry.addData("PID Output", pidOutput);
-            telemetry.addData("Gyro output", currentHeading);
+            telemetry.addData("Gyro output", currentDirection);
             telemetry.update();
         }
         robot.r1.setPower(0);
