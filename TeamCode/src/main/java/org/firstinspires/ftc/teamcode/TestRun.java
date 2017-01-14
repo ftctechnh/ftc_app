1c25238bcb239a79a684e87a654dd428dc29da33
Noah Pietrafesa <thepizzaofdeath@gmail.com>
2017-01-14 12:19:57 -0500

    fleshed out

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestRun.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestRun.java
@@ -1,8 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
  * Created by mikepietrafesa1 on 1/14/17.
  */
 
-public class TestRun {
+public class TestRun extends OpMode{
+
+    DcMotor left1;
+    DcMotor left2;
+    DcMotor right1;
+    DcMotor right2;
+
+    @Override
+    public void init() {
+        left1 = hardwareMap.dcMotor.get("left1");
+        left2 = hardwareMap.dcMotor.get("left2");
+        right1 = hardwareMap.dcMotor.get("right1");
+        right2 = hardwareMap.dcMotor.get("right2");
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
+
