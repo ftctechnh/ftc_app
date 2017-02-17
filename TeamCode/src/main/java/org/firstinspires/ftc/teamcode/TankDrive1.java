79860d8d970b7768ce21dfd5c5a310d13c95a109
Bright A <bright.a19@bronxsoftware.org>
2017-02-17 01:57:09 +0000

    Organization and additions
    I used a lot of new lines to organize the code, as well as adding and correcting comments.
    I made a lot of changes. Look for yourself...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive1.java
@@ -36,97 +36,106 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-//import org.firstinspires.ftc.robotcontroller.external.samples.HardwareK9bot;
-
 /**
- * This OpMode uses the common HardwareK9bot class to define the devices on the robot.
- * All device access is managed through the HardwareK9bot class. (See this class for device names)
- * The code is structured as a LinearOpMode
- *
- * This particular OpMode executes a basic Tank Drive Teleop for the K9 bot
- * It raises and lowers the arm using the Gampad Y and A buttons respectively.
- * It also opens and closes the claw slowly using the X and B buttons.
- *
- * Note: the configuration of the servos is such that
- * as the arm servo approaches 0, the arm position moves up (away from the floor).
- * Also, as the claw servo approaches 0, the claw opens up (drops the game element).
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+
+                What the WHOLE teleop driver mode does:
+
+
+ - Driver 1 actually moves the robot using the left and right sticks for the
+   left and right motors, respectively.
+ - Driver 1 can use up on the dpad to move both wheels on the robot forward,
+   and down for backwards.
+ 
+ - Driver 2 controls the rest of the robot                                    
+ - Driver 2 toggles the harvester and wheel shooters on and off using the "A"
+   and "Y" buttons, respectively.
+ - Driver 2 controls the trigger (servo) using up and down of the dpad.
+ 
  */
 
-@TeleOp(name="Golden Eagles: TankDrive1", group="K9bot")
+@TeleOp(name="Golden Eagles: TeleOp", group="K9bot")
 //@Disabled
 public class TankDrive1 extends LinearOpMode {
 
-    // Declare OpMode members.
-    HardwareK9bot robot = new HardwareK9bot();       // Use a K9'shardware
+    // Use robot hardware specified in the file "HardwareK9bot.java".
+    HardwareK9bot robot = new HardwareK9bot();
 
     @Override
     public void runOpMode() {
         
+        
         // Some variables to initialize
-        double left, right, leftShooter, rightShooter, harvester;
+        double left, right;
         
         // The position of the "trigger" servo
-        double triggerPos = 0.2;
+        double triggerPos = 1.0;
         
-        // The on / off status of the harvestor and wheel shooters
+        // The on / off status of the harvester and wheel shooters
         boolean harvesterOn = false;
         boolean shootersOn = false;
         
-        // The previous state of 2's "A" and "Y" button
+        // The previous state of gamepad2's "A" and "Y" buttons
         boolean GP2a = false;
         boolean GP2y = false;
         
 
-        /* Initialize the hardware variables.
-         * The init() method of the hardware class does all the work here
-         */
+        // Initialize the hardware variables.
         robot.init(hardwareMap);
 
-        // Send telemetry message to signify robot waiting;
+
+        // Send telemetry message to signify robot ready.
         telemetry.addData("Say", "Hello Driver");
         telemetry.update();
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        // run until the end of the match (driver presses STOP)
+
+        // Run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
-            left = gamepad1.left_stick_y;
-            right = gamepad1.right_stick_y;
+            
+            /*
+            If up is pressed on gamepad1's DPAD, move forward.
+            If down is pressed on the DPAD, move backward.
+            If neither, move using gamepad1's left and right sticks.
+            */
+            if (gamepad1.dpad_up) {
+                left = 1;
+                right = 1;
+            }
+            else if (gamepad1.dpad_down) {
+                left = -1;
+                right = -1;
+            }
+            else {
+                left = gamepad1.left_stick_y;
+                right = gamepad1.right_stick_y;
+            }
+            
             robot.leftMotor.setPower(left);
             robot.rightMotor.setPower(right);
 
 
             /*
-            If gamepad 2 was false before, but is now true,
-            toggle the state of the harvester.
-            
-            If it was true before, this doesn't do anything.
+            If a button on gamepad2 wasn't pressed during the last loop,
+            but is being pressed in this loop, toggle the state of the motors.
             */
             if (!GP2a && gamepad2.a) {
-                harvesterOn = !harvestorOn;
+                harvesterOn = !harvesterOn;
             }
-            
-            // Same thing but with the wheel shooters
             if (!GP2y && gamepad2.y) {
                 shootersOn = !shootersOn;
             }
             
             
-            // Turn harvester on based on harvesterOn variable
+            // Turn the motors on or off based on their state.
             if (harvesterOn) {
                 robot.harvester.setMotorPower(1, -1);
             }
             else {
                 robot.harvester.setMotorPower(1, 0);
             }
-            
-            // Turn shooters on based on shootersOn variable
             if (shootersOn) {
                robot.leftShooter.setPower(-1);
                robot.rightShooter.setPower(-1);
@@ -137,42 +146,54 @@ public class TankDrive1 extends LinearOpMode {
             }
 
             
-            // Increase "trigger" servo position when the left stick is up
-            // Decrease when the stick is down
-            if (gamepad2.left_stick_y > 0.2) {
-                if (triggerPos != 1.0) {
-                    triggerPos += 0.02;
-                }
+            /*
+            Increase "trigger"'s' servo position when up is pressed on the DPAD.
+            Decrease the position when down is pressed on the DPAD.
+            */
+            if (gamepad2.dpad_up) {
+                triggerPos += 0.05;
             }
-            if (gamepad2.left_stick_y < -0.2) {
-                if (triggerPos != -1.0) {
-                    triggerPos -= 0.02;
-                }
+            else if (gamepad2.dpad_down) {
+                triggerPos -= 0.05;
+            }
+            
+            
+            // If it exceeds below 0 or above 1, set it to 0 or 1, respectively.
+            if (triggerPos < 0) {
+                triggerPos = 0;
+            }
+            else if (triggerPos > 1) {
+                triggerPos = 1;
             }
 
-            // Set position of "trigger" servo
+
+            // Set position of the "trigger" servo
             robot.trigger.setPosition(triggerPos);
 
-            // Make the "now" into the "previous" for the next loop.
+            /*
+            Make the "now" into the "before" for the next loop.
+            If that makes sense.
+            */
             GP2a = gamepad2.a;
             GP2y = gamepad2.y;
+            
 
-
-            // Get the motors' power (for telemetry)
-            leftShooter = robot.leftShooter.getPower();
-            rightShooter = robot.rightShooter.getPower();
-
-            // Send telemetry message to signify robot running;
-            telemetry.addData("left",  "%.2f", left);
-            telemetry.addData("right", "%.2f", right);
-            telemetry.addData("left shooter", "%.2f", leftShooter);
-            telemetry.addData("right shooter", "%.2f", rightShooter);
-            telemetry.addData("trigger", "%.2f", triggerPos);
-
+            // Send telemetry message of robot data.
+            telemetry.addData("Left wheel power",  "%.2f", left);
+            telemetry.addData("Right wheel power", "%.2f", right);
+            telemetry.addData("Harvester", (harvesterOn) ? "On" : "Off");
+            telemetry.addData("Wheel shooters", (shootersOn) ? "On" : "Off");
+            telemetry.addData("Trigger position", "%.2f", triggerPos);
             telemetry.update();
+            
 
             // Pause for metronome tick.  40 mS each cycle = update 25 times a second.
             robot.waitForTick(40);
+            
+            
         }
+        
+        
     }
+    
 }
