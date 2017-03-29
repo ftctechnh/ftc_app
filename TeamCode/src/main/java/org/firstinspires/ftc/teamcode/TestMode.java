3df5a2c321f5f022d33f9000f9782c92a0137f44
sfChen1029 <steven.fx.chen@gmail.com>
2017-03-28 20:17:23 -0700

    shortened the distance the robot goes backwards in the beginning of autonomous (blue)
    wrote test mode to test strafing to the left and right in autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMode.java
@@ -3,29 +3,36 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
+import org.firstinspires.ftc.teamcode.Main.AutonomousGeneral_charlie;
 import org.firstinspires.ftc.teamcode.Main.beta.AutonomousGeneral;
 
 /**
  * Created by inspirationteam on 2/19/2017.
  */
 @Autonomous(name = "autonomous")
-@Disabled
-public class TestMode extends AutonomousGeneral {
+///@Disabled
+public class TestMode extends AutonomousGeneral_charlie
+{
 
     public void runOpMode(){
         initiate();
         waitForStart();
-        setMotorsModeToEncDrive();
-        stopMotors();
-        encoderDrive(.7,100,100,3);
-        sleep(5000);
-        Turn360Right();
-        sleep(5000);
-        Turn360Left();
-        sleep(5000);
-        setMotorsModeToEncDrive();
-        stopMotors();
-        encoderDrive(.7,-100,-100,3);
+        sleep(1000);
+        encoderMecanumDrive(0.8,50,50,10,1);
+        encoderMecanumDrive(0.8,50,50,10,-1);
+//        initiate();
+//        waitForStart();
+//        setMotorsModeToEncDrive();
+//        stopMotors();
+//        encoderDrive(.7,100,100,3);
+//        sleep(5000);
+//        Turn360Right();
+//        sleep(5000);
+//        Turn360Left();
+//        sleep(5000);
+//        setMotorsModeToEncDrive();
+//        stopMotors();
+//        encoderDrive(.7,-100,-100,3);
 
 
     }
