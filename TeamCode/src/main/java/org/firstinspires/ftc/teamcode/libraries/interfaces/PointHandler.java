5aeb9feed9820455bcb7a9f8fbd45907482d675b
Noah <noah@koontzs.com>
2017-04-09 15:10:42 -0700

    hmmmmm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/interfaces/PointHandler.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.libraries.interfaces;
+
+/**
+ * Created by Noah on 4/9/2017.
+ */
+
+public interface PointHandler {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/demo/OpticalMouseCanvasFun.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.opmodes.demo;
+
+/**
+ * Created by Noah on 4/9/2017.
+ */
+
+public class OpticalMouseCanvasFun {
+}
