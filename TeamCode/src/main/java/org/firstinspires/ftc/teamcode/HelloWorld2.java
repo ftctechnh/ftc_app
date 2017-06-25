f0401f00dfe7797de8c08c2b910598347d12cbc4
Eric D'Urso <eric@casadurso.com>
2017-06-25 08:36:09 -0400

    We now only have 1 "Practice" class for notes and tutorials.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld2.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-/**
- * Created by Eric D'Urso on 6/24/2017.
- */
-
-public class HelloWorld2 {
-}
-//ignore all of this, it is just a test
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice.java
@@ -4,7 +4,7 @@ package org.firstinspires.ftc.teamcode;
  * Created by Eric D'Urso on 6/24/2017.
  */
 
-public class HelloWorld {
+public class Practice {
 }
 //ignore all of this, it is just a test
 //TODO start working on learning code
