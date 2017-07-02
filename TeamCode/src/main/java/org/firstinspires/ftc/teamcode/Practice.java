449427dcc0250f6f71e63e6bf680a31e20614028
Eric D'Urso <eric@casadurso.com>
2017-07-01 20:58:13 -0400

    Had an idea, think we could make a java class of todo files only and with links to resources and what not. Haven't done it yet though.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice.java
@@ -8,4 +8,6 @@ public class Practice {
 }
 //ignore all of this, it is just a test
 //TODO start working on learning code
+//TODO get all members set up with source tree and android studio
+// TODO See if we should use this todo thing as an "internal website" add links and stuff and keep it from OpModes
 //See above for how to create a to-do 
