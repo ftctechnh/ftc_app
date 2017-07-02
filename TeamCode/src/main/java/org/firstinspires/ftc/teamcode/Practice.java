b9bb05ae8ee8a53b3a34efbeb46851fea3889949
Eric D'Urso <eric@casadurso.com>
2017-07-02 17:16:23 -0400

    Bored. Playing Around. Only Change is a fixed Typo :|

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Practice.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 /**
  * Created by Eric D'Urso on 6/24/2017.
  */
@@ -9,5 +11,5 @@ public class Practice {
 //ignore all of this, it is just a test
 //TODO start working on learning code
 //TODO get all members set up with source tree and android studio
-// TODO See if we should use this todo thing as an "internal website" add links and stuff and keep it from OpModes
+//TODO See if we should use this todo thing as an "internal website" add links and stuff and keep it from OpModes
 //See above for how to create a to-do 
