f0152139889d0efefa2ebf978136e8e2533bb619
Jake Mueller <jakemueller1008@gmail.com>
2017-08-10 00:30:27 -0500

    Good for testing Two motors specifically drive motors setup phone with "LeftDrive" and "RightDrive"

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopV1.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/**
+ * Created by jakemueller on 8/10/17.
+ */
+@TeleOp(name = "teleOp One", group = "Test")
+public class TeleopV1 extends LinearOpMode
+{
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
+
+
+    public void runOpMode() throws InterruptedException
+    {
+        motorLeft = hardwareMap.dcMotor.get("LeftDrive");
+        motorRight = hardwareMap.dcMotor.get("RightDrive");
+
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+
+
+
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            motorLeft.setPower(-gamepad1.left_stick_y);
+            motorRight.setPower(-gamepad1.right_stick_y);
+
+
+
+
+
+            idle();
+
+        }
+    }
+}
