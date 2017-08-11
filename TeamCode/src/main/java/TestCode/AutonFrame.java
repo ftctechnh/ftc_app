999672f21b4120f0926e4dcbf4081a8679ebcd79
JAKES-PC\Jake Mueller <jakemueller1008@gmail.com>
2017-08-11 10:31:53 -0500

    -0

--- TeamCode/src/main/java/TestCode/AutonFrame.java
+++ TeamCode/src/main/java/TestCode/AutonFrame.java
@@ -1,8 +1,27 @@
 package TestCode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
  * Created by Jake Mueller on 8/11/2017.
  */
 
-public class AutonFrame {
+public class AutonFrame extends LinearOpMode {
+
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
+
+    public AutonFrame(){
+
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        motorLeft = hardwareMap.dcMotor.get("LeftDrive");
+        motorRight = hardwareMap.dcMotor.get("RightDrive");
+
+        //insert any autonomous code here
+
+    }
 }
