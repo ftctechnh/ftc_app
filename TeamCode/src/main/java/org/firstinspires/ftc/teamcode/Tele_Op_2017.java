ab8426d880cf1b0a7f52984210ebdf95a98acafc
Cassie <cassiemaenash@gmail.com>
2017-08-31 18:19:19 -0400

    test commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele_Op_2017.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele_Op_2017.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  *
- * test
+ * test test2
  */
 
 @TeleOp(name="Tele Op 2017", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
