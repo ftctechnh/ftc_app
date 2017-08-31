cd7cf12d674715ba2683c1e706ff64f1c8c4bf2a
stlgolfer <stlgolfer@live.com>
2017-08-30 22:22:47 -0500

    Test method on DocTesting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/DocTestingclass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/DocTestingclass.java
@@ -5,4 +5,11 @@ package org.firstinspires.ftc.teamcode.teleops;
  * @since 8/30/17
  */
 public class DocTestingclass {
+
+    /**
+     * This is test method
+     */
+    public void testMethod() {
+        System.out.println("this is a test method");
+    }
 }
