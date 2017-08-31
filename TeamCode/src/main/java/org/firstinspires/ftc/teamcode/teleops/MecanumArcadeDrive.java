4ebe13e731410097439e45c3e7e2c93be9013a1c
Blake Abel <blakecabel@gmail.com>
2017-08-31 15:09:18 -0500

    updated mecanumdrive doc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/MecanumArcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/MecanumArcadeDrive.java
@@ -10,14 +10,10 @@ import org.firstinspires.ftc.teamcode.robotplus.hardware.Robot;
 
 /**
  * @author Blake Abel, Alex Migala
- * @since 8------
+ * @since 8/24/17
 */
 @TeleOp(name="Mecanum Arcade Drive", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
 public class MecanumArcadeDrive extends OpMode {
-
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
 
     private ElapsedTime runtime = new ElapsedTime();
 
@@ -33,40 +29,21 @@ public class MecanumArcadeDrive extends OpMode {
         telemetry.addData("Status", "Initialized");
 
         robot = new Robot(hardwareMap);
-
-        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
-         * to 'get' must correspond to the names assigned during the robot configuration
-         * step (using the FTC Robot Controller app on the phone).
-         */
-        // leftMotor  = hardwareMap.dcMotor.get("left_drive");
-        // rightMotor = hardwareMap.dcMotor.get("right_drive");
-
-        // eg: Set the drive motor directions:
-        // Reverse the motor that runs backwards when connected directly to the battery
-        // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        //  rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
-        // telemetry.addData("Status", "Initialized");
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
+    // Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
     @Override
     public void init_loop() {
 
     }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
+    // Code to run ONCE when the driver hits PLAY
     @Override
     public void start() {
         runtime.reset();
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
+    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
     @Override
     public void loop() {
 
@@ -77,9 +54,7 @@ public class MecanumArcadeDrive extends OpMode {
         }
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
+    // Code to run ONCE after the driver hits STOP
     @Override
     public void stop() {
     }
