c129798c50adb1c0908220459f77e1b362bb330a
Migala <amigala@micds.org>
2017-09-14 16:16:16 -0500

    Can differentiate image

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomii/TestVuforiaAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomii/TestVuforiaAutonomous.java
@@ -1,27 +1,36 @@
 package org.firstinspires.ftc.teamcode.autonomii;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.teamcode.robotplus.autonomoushelper.VuforiaWrapper;
 
-/**
- * Created by amigala on 9/9/2017.
- */
-
+@Autonomous(name = "VuforiaTester", group = "Autononomousous")
 public class TestVuforiaAutonomous extends LinearOpMode {
     private VuforiaWrapper identifier;
 
     public void runOpMode() {
         identifier = new VuforiaWrapper(hardwareMap);
 
+        identifier.getLoader().getTrackables().activate();
+
         waitForStart();
 
-        while (opModeIsActive()) {
-            // run the opMode
-            RelicRecoveryVuMark relicRecoveryVuMark = RelicRecoveryVuMark.from(identifier.getLoader().getTrackables().get(0));
+        telemetry.addData("Status", "Ready");
+        telemetry.update();
 
-            telemetry.addData("Relic Visibility", relicRecoveryVuMark);
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            // run the opMode
+            RelicRecoveryVuMark relicRecoveryVuMark = RelicRecoveryVuMark.from(this.identifier.getLoader().getRelicTemplate());
+
+            if (relicRecoveryVuMark != RelicRecoveryVuMark.UNKNOWN) {
+                telemetry.addData("Relic Visibility", relicRecoveryVuMark);
+                telemetry.addData("VuMark Column", relicRecoveryVuMark.name());
+            }
+
+            telemetry.update();
         }
     }
 }
