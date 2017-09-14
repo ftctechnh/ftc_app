2ea6f7520ee3b8738c98ae366ddbc5a64ad2435b
The_Senate <Nazucca7@gmail.com>
2017-09-13 19:35:58 -0500

    test commit

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Iterative.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Iterative.java
@@ -49,7 +49,7 @@ import com.qualcomm.robotcore.util.Range;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-
+//test comment
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
 @Disabled
 public class BasicOpMode_Iterative extends OpMode
