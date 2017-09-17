25bb0a2e63c5eeb8139104c5028158bc0c08e324
Carson Storm <stormymcstorm@gmail.com>
2017-09-17 10:20:52 -0400

    adding ChathamRoboticsCommon

---
+++ ChathamRoboticsCommon/src/main/java/org/chathamrobotics/common/Robot.java
@@ -0,0 +1,11 @@
+package org.chathamrobotics.common;
+
+/*!
+ * FTC_APP_2018
+ * Copyright (c) 2017 Chatham Robotics
+ * MIT License
+ * @Last Modified by: storm
+ * @Last Modified time: 9/17/2017
+ */
+public abstract class Robot {
+}
