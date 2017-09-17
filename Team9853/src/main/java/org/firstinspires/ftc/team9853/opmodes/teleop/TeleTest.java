3fd9ee8d225eb80839e7450c431bb33c72d23162
Carson Storm <stormymcstorm@gmail.com>
2017-09-17 12:47:44 -0400

    switching to Android Studio Canary

---
+++ Team9853/src/main/java/org/firstinspires/ftc/team9853/opmodes/teleop/TeleTest.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.team9853.opmodes.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * FTC_APP_2018
+ * Copyright (c) 2017 Chatham Robotics
+ * MIT License
+ *
+ * @Last Modified by: storm
+ * @Last Modified time: 9/17/2017
+ */
+public class TeleTest extends OpMode {
+}
