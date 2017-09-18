f8ea5b76c452039ddd8f9b0a4cd307a59bf95caa
nathanlesser <nathanlesser@outlook.com>
2017-09-18 15:31:09 -0400

    In theory the app works now

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriver.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class TestDriver extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+    }
+}
