92d8253dcc79b9a23820aa748db44e17dcae8e28
gasman7 <31522065+gasman7@users.noreply.github.com>
2017-09-18 18:58:34 -0400

    Stubbing in drive motion functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -1,8 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
  * Created by gstaats on 18/09/17.
  */
 
 public class Drive {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor rearLeft;
+    private DcMotor rearRight;
+
+    // function to move forward
+    public forward()
+    {
+    }
+
+    // function to move backward
+    public backward()
+    {
+    }
+
+    // function to move left (not turn!)
+    public left()
+    {
+    }
+
+    // function to move right (not turn!)
+    public right()
+    {
+
+    }
+
+    // function to move diagonal
+    public diagonal()
+    {
+    }
+
+    // function to turn
+    public turn()
+    {
+    }
+
 }
+
