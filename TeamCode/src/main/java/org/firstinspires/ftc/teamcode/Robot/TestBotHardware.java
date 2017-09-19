20a64f56c21ad5d9705c6c8558cd2d13a86a2918
StephenButters <32017706+Stephen013@users.noreply.github.com>
2017-09-19 16:19:59 -0400

    ColorSensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/TestBotHardware.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by butterss21317 on 9/19/2017.
+ */
+
+public class TestBotHardware {
+ColorSensor colorSensor = null;
+    public void init (HardwareMap hwMap){
+        colorSensor = hwMap.colorSensor.get("color_sensor");
+    }
+}
+
