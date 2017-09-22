be1b86aeb58f8944cd4c2c818ef8aa63ababe634
david-wiles6 <davidcwiles6@gmail.com>
2017-09-22 12:06:26 -0400

    Tweaks to mechanum wheel drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DavidMechanumWheelDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DavidMechanumWheelDrive.java
@@ -84,10 +84,10 @@ public class DavidMechanumWheelDrive extends LinearOpMode {
 
             double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
             double rightX = gamepad1.right_stick_x;
-            final double v1 = r * Math.cos(robotAngle) + rightX;
-            final double v2 = r * Math.sin(robotAngle) - rightX;
-            final double v3 = r * Math.sin(robotAngle) + rightX;
-            final double v4 = r * Math.cos(robotAngle) - rightX;
+            final double v1 = gamepad1.left_stick_y + gamepad1.right_stick_x + gamepad1.left_stick_x;
+            final double v2 = gamepad1.left_stick_y + gamepad1.right_stick_x - gamepad1.left_stick_x;
+            final double v3 = gamepad1.left_stick_y - gamepad1.right_stick_x - gamepad1.left_stick_x;
+            final double v4 = gamepad1.left_stick_y - gamepad1.right_stick_x + gamepad1.left_stick_x;
             frontLeft=v1;
             frontRight=v2;
             backLeft=v3;
