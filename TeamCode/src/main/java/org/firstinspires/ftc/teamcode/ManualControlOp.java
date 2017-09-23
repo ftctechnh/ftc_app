5e544e26884fffe11e6ad3e90da940333e76e348
Unknown <jacobsnarr@gmail.com>
2017-09-23 14:05:48 -0400

    Moved Old Code and Revised
    Revised old Vortex code to work with a new simple bot. Got rid of Mecanum specific lines of code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualControlOp.java
@@ -0,0 +1,115 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.robot.Robot;
+
+
+public class ManualControlOp extends OpMode {
+    private static final String KEY = "Manual";
+
+    private Robot robot;
+
+    private int loopCounter = 0;
+
+
+    public ManualControlOp() {
+    }
+
+    @Override
+    public void init() {
+
+        this.robot = new Robot(hardwareMap, telemetry);
+    }
+
+
+    /*
+     * This method will be called repeatedly in a loop
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#run()
+     */
+    @Override
+    public void loop() {
+
+        loopCounter = loopCounter + 1;
+
+        driver_controlDriveMotors();
+//        operator_controlBeaconButton();
+
+    }
+
+    private void operator_controlBeaconButton() {
+
+        if (gamepad1.y) {
+
+        } else {
+        }
+    }
+
+
+    private void driver_controlDriveMotors() {
+
+        // write the values to the motors
+        robot.motorRight.setPower(limitValue(gamepad1.right_stick_y));
+        robot.motorLeft.setPower(limitValue(gamepad1.left_stick_y));
+
+        Log.i(KEY, "--Robot");
+        Log.i(KEY, "WHEELS: [" + String.format("%.0f", robot.motorRight.getPower() * 100) + "]---[" + String.format("%.0f", robot.motorLeft.getPower() * 100) + "]");
+        Log.i(KEY, "------------------------------------------");
+
+    }
+
+    private float limitValue(float input) {
+
+        if (input > 1.0) {
+
+            return 1.0f;
+        } else if (input < -1.0) {
+            return -1.0f;
+        }
+        return input;
+    }
+
+
+    /*
+     * Code to run when the op mode is first disabled goes here
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#stop()
+     */
+    @Override
+    public void stop() {
+
+        telemetry.addData("TextStop", "***Stop happened**" + loopCounter);
+
+    }
+
+    /*
+     * This method scales the joystick inputValue so for low joystick values, the
+     * scaled value is less than linear.  This is to make it easier to drive
+     * the robot more precisely at slower speeds.
+     */
+    double scaleOutput(double inputValue, double[] scaleArray) {
+
+
+        // get the corresponding index for the scaleOutput array.
+        int index = (int) (inputValue * 16.0);
+        if (index < 0) {
+            index = -index;
+        } else if (index > 16) {
+            index = 16;
+        }
+
+        double dScale = 0.0;
+        if (inputValue < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        return dScale;
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.java
@@ -0,0 +1,190 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+//   TODO consider using the robot for single loopCounter and delayUntilLoopCount
+// and stillWaiting.
+
+public class Robot {
+
+    private HardwareMap hardwareMap;
+    public int HARDWARE_DELAY = 30;
+
+    public int loopCounter;
+    private int delayUntilLoopCount = 0;
+
+
+    public Telemetry telemetry;
+
+    public DcMotor motorRight;
+    public DcMotor motorLeft;
+
+    public Robot(HardwareMap hardwareMap, Telemetry telemetry) {
+
+        this.hardwareMap = hardwareMap;
+        this.telemetry = telemetry;
+
+        if (this.hardwareMap == null) {
+
+            Log.i("ROBOT", "OUCH!!!! hardwareMap is null");
+
+        }
+
+
+        this.motorLeft = this.hardwareMap.dcMotor.get("motorLeft");
+        this.motorRight = this.hardwareMap.dcMotor.get("motorRight");
+
+        resetHardwarePositions();
+    }
+
+
+    public void resetDriveMotors() {
+
+        setDriveMotorForwardDirection();
+        runWithoutEncoders();
+    }
+
+
+    public void resetEncodersAndStopMotors() {
+
+        Log.w("ROBOT", "resetEncodersAndStopMotors..." + loopCounter);
+        this.motorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.motorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+    }
+
+
+    private void resetHardwarePositions() {
+
+        resetDriveMotors();
+        resetServos();
+
+    }
+
+
+    public void resetServos() {
+
+    }
+
+    public void setDriveMotorForwardDirection() {
+
+        this.motorLeft.setDirection(DcMotor.Direction.REVERSE);
+        this.motorRight.setDirection(DcMotor.Direction.FORWARD);
+
+
+    }
+
+    public void setDriveMotorReverseDirection() {
+
+        this.motorLeft.setDirection(DcMotor.Direction.FORWARD);
+        this.motorRight.setDirection(DcMotor.Direction.REVERSE);
+
+
+    }
+
+    public void runWithoutEncoders() {
+
+        this.motorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.motorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+
+
+    public void runWithEncoders_MAINTAINS_SPEED() {
+
+        this.motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public boolean isStillWaiting() {
+
+        if (delayUntilLoopCount > loopCounter) {
+            Log.i("ROBOT", "Waiting..." + loopCounter);
+            return true;
+        }
+        return false;
+    }
+
+    public void setLoopDelay() {
+
+        this.delayUntilLoopCount = loopCounter + HARDWARE_DELAY;
+    }
+
+    public static enum ColorEnum {
+
+        RED,
+
+        BLUE,
+
+        WHITE
+
+
+    }
+
+    public enum TurnEnum {
+
+        RIGHT,
+
+        LEFT
+    }
+
+
+    public enum TeamEnum {
+        RED,
+
+        BLUE
+    }
+
+    public enum StrafeEnum {
+
+        RIGHT,
+
+        LEFT
+    }
+
+
+    public enum DirectionEnum {
+
+        FORWARD,
+
+        REVERSE
+    }
+
+
+    public enum MotorPowerEnum {
+
+        LowLow(0.1),
+
+        Low(0.2),
+
+        Med(0.4),
+
+        High(0.6),
+
+        FTL(0.8);
+
+        private double motorPower;
+
+        private MotorPowerEnum(double motorPower) {
+            this.motorPower = motorPower;
+
+        }
+
+        public double getValue() {
+            return this.motorPower;
+
+        }
+
+    }
+
+
+}
+
+
