9d60a51d193821607ab8b17834de0c925dc86e2c
FTC Geared UP 6347 <ftcgearedup@gmail.com>
2017-09-24 14:21:34 -0400

    Add code to initialize H-drive motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/drivetrain/HDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/drivetrain/HDriveTrain.java
@@ -1,17 +1,25 @@
 package org.firstinspires.ftc.teamcode.mechanism.drivetrain;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class HDriveTrain implements IDirectionalDriveTrain {
-    @Override
-    public void directionalDrive(double speedX, double speedY, double rotationSpeed, int targetDistance) {
 
-    }
+    private static final int WHEEL_DIAMETER_INCHES = 4;
 
-    @Override
-    public void initialize(LinearOpMode opMode) {
+    private static final int COUNTS_PER_MOTOR_REV = 1120;
 
-    }
+    private static final double GYRO_ERROR_THRESHOLD = 1.0;
+
+    private static final double P_GYRO_TURN_COEFF = 0.01;
+    private static final double P_GYRO_DRIVE_COEFF = 0.008;
+
+    protected static final int COUNTS_PER_INCH = (int)(COUNTS_PER_MOTOR_REV /
+            (WHEEL_DIAMETER_INCHES * Math.PI));
+
+
+    private DcMotor frontLeft, frontRight, backLeft, backRight, middleLeft, middleRight;
 
     @Override
     public void pivot(double pivotSpeed) {
@@ -36,5 +44,34 @@ public class HDriveTrain implements IDirectionalDriveTrain {
     @Override
     public boolean encodersEnabled() {
         return false;
+    }
+
+    @Override
+    public void initialize(LinearOpMode opMode) {
+        HardwareMap hWMap = opMode.hardwareMap;
+
+        frontLeft = hWMap.dcMotor.get("fl");
+        frontRight = hWMap.dcMotor.get("fr");
+        backRight = hWMap.dcMotor.get("br");
+        backLeft = hWMap.dcMotor.get("bl");
+        middleLeft = hWMap.dcMotor.get("ml");
+        middleRight = hWMap.dcMotor.get("mr");
+
+    }
+
+    @Override
+    public void directionalDrive(double speedX, double speedY, double pivotSpeed, int targetDistance) {
+        middleLeft.setPower(speedX);
+        middleRight.setPower(speedX);
+        frontLeft.setPower(speedY);
+        frontRight.setPower(speedY);
+        backRight.setPower(speedY);
+        backLeft.setPower(speedY);
+
+
+        int encoderTargetCounts = COUNTS_PER_INCH * targetDistance;
+
+
+
     }
 }
