579f395473cef09e488d099a943c35e7a8d4ae78
nathanlesser <nathanlesser@outlook.com>
2017-09-25 09:23:20 -0400

    Kotlin Support, new Gradle, and 2 demo OpModes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriver.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-public class TestDriver extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
-    }
-}
