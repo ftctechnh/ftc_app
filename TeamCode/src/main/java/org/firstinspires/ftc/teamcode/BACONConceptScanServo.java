2700ce39dc90c48692d1b8b486c655a65347d6df
david-wiles6 <davidcwiles6@gmail.com>
2017-09-27 16:12:56 -0400

    Tweaks to mechanum wheel drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BACONConceptScanServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BACONConceptScanServo.java
@@ -46,7 +46,7 @@ import com.qualcomm.robotcore.hardware.Servo;
  * connected servos are able to move freely before running this test.
  *
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ * Remove or comment out the @Disabled line to add this opmode to the Station OpMode list
  */
 @Autonomous(name = "Concept: Scan Servo", group = "Concept")
 @Disabled
