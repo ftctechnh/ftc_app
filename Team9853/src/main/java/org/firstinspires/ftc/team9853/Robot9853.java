ecbcdbffd5f76d4588db97d493f72777babf61d6
Carson Storm <stormymcstorm@gmail.com>
2017-09-28 18:31:12 -0400

    BTSN

--- Team9853/src/main/java/org/firstinspires/ftc/team9853/Robot9853.java
+++ Team9853/src/main/java/org/firstinspires/ftc/team9853/Robot9853.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.team9853;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
 
 import org.chathamrobotics.common.Robot;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -17,6 +21,11 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 @SuppressWarnings({"unused", "WeakerAccess"})
 public class Robot9853 extends Robot {
 
+    private DcMotor frontRightMotor;
+    private DcMotor frontLeftMotor;
+    private DcMotor backRightMotor;
+    private DcMotor backLeftMotor;
+
     public static Robot9853 build(OpMode opMode) {
         return new Robot9853(opMode.hardwareMap, opMode.telemetry);
     }
@@ -27,11 +36,39 @@ public class Robot9853 extends Robot {
 
     @Override
     public void init() {
+        frontRightMotor = hardwareMap.dcMotor.get("FrontRight");
+        frontLeftMotor = hardwareMap.dcMotor.get("FrontLeft");
+        backRightMotor = hardwareMap.dcMotor.get("BackRight");
+        backLeftMotor = hardwareMap.dcMotor.get("BackLeft");
 
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
     }
 
     @Override
     public void start() {
 
+    }
+
+    public void driveWithControls(Gamepad gp) {
+        double rightMotorPower = Range.clip(-gp.left_stick_y, -1, 1);
+        double leftMotorPower = Range.clip(-gp.left_stick_y, -1, 1);
+
+        if (gp.right_stick_x != 0) {
+            rightMotorPower = rightMotorPower == 0 ? 1 : rightMotorPower;
+            leftMotorPower = leftMotorPower == 0 ? 1 : leftMotorPower;
+
+            rightMotorPower *= -gp.right_stick_x;
+            leftMotorPower *= gp.right_stick_x;
+        }
+
+        log.debug("Right Stick X", gp.right_stick_x);
+        log.debug("Left Stick Y", -gp.right_stick_y);
+
+        frontRightMotor.setPower(rightMotorPower);
+        backRightMotor.setPower(rightMotorPower);
+
+        frontLeftMotor.setPower(leftMotorPower);
+        backLeftMotor.setPower(leftMotorPower);
     }
 }
