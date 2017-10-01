cad94e9f1f43694b6756f9c685eb66820906bbc1
ThunderPengwins <31020436+ThunderPengwins@users.noreply.github.com>
2017-10-01 16:24:16 -0500

    Updated gradle.build to lateest build version

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base_mecanum_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base_mecanum_test.java
@@ -5,101 +5,132 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import static java.lang.Math.abs;
-import java.math.*;
 
 /**
  * Created by Eric on 9/20/2017!
  */
 
-@TeleOp(name="mecanum_test",group="BBBot" )
+@TeleOp(name="base_mecanum_test",group="mecanum" )
 public class base_mecanum_test extends LinearOpMode {
     HardwareSensorMap robot   = new HardwareSensorMap();   // Use a Pushbot's hardware
     //power variables
-    DcMotor lf;
-    DcMotor rf;
-    DcMotor lb;
-    DcMotor rb;
+    DcMotor leftFrontMotor;
+    DcMotor rightFrontMotor;
+    DcMotor leftBackMotor;
+    DcMotor rightBackMotor;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         //get motors
-        lb = hardwareMap.dcMotor.get("m0"); //left back
-        rb = hardwareMap.dcMotor.get("m1"); //right back
-        lf = hardwareMap.dcMotor.get("m2"); //left front
-        rf = hardwareMap.dcMotor.get("m3"); //right front
+        leftBackMotor = hardwareMap.dcMotor.get("m0"); //left back
+        rightBackMotor = hardwareMap.dcMotor.get("m1"); //right back
+        leftFrontMotor = hardwareMap.dcMotor.get("m2"); //left front
+        rightFrontMotor = hardwareMap.dcMotor.get("m3"); //right front
 
         //neat variables
         double drive; //turn power
         double turn; //turn direction
         double power; //move power
         double leftX; //left x: joystick
-        double p1; //lb
-        double p2; //rb
-        double p3; //lf
-        double p4; //rf
+        double leftBackMotorPower; //leftBackMotor
+        double rightBackMotorPower; //rightBackMotor
+        double leftFrontMotorPower; //leftFrontMotor
+        double rightFrontMotorPower; //rightMotorPower
 
-        waitForStart();
-        drive = gamepad1.left_stick_y;
-        turn = gamepad1.right_stick_x;
-        leftX = gamepad1.left_stick_x;
-        power = getPathagorus(leftX, drive);
+        waitForStart(); //wait for the button to be pushed
 
-        //main if/then
-        if (turn > 0){
+        while (opModeIsActive()) {
+            drive = gamepad1.left_stick_y; //left joystick moving up and down
+            turn = gamepad1.right_stick_x; //right joystick left and right
+            leftX = gamepad1.left_stick_x; //left joystick moving right and left
+            power = getPathagorus(leftX, drive); //the current joystick position
 
-            //turn right
-            p1 = drive;
-            p2 = -drive;
-            p3 = drive;
-            p4 = -drive;
+            boolean turningRight = turn > 0;
+            boolean notTurning = turn == 0;
+            boolean movingVertical = Math.abs(drive) > Math.abs(leftX);
+            boolean strafingRight = leftX > 0;
+            //main if/then
+            if (turningRight) {
+                //pushing right joystick to the right
+                //turn right by left wheels going forward and right going backwards
+                turnRight(power);
+            } else if (notTurning) {
+                //no movement in right joystick
 
-        }else if (turn == 0){
-
-            //drive stuff
-            if (Math.abs(drive) > Math.abs(leftX)){ //forward/back or left/right?
-                if (drive > 0){ //forward
-                    p1 = power;
-                    p2 = power;
-                    p3 = power;
-                    p4 = power;
-                }else{ //back
-                    p1 = -power;
-                    p2 = -power;
-                    p3 = -power;
-                    p4 = -power;
+                //start of driving section
+                if (movingVertical) { //forward/back or left/right?
+                    if (drive > 0) { //forward
+                       driveForward(power);
+                    } else { //back
+                        driveBackward(power);
+                    }
+                } else {
+                    if (strafingRight) { //right
+                        strafeRight(power);
+                    } else { //left
+                        strafeLeft(power);
+                    }
                 }
-            }else{
-                if (drive > 0){ //right
-                    p1 = power;
-                    p2 = -power;
-                    p3 = -power;
-                    p4 = power;
-                }else{ //left
-                    p1 = -power;
-                    p2 = power;
-                    p3 = power;
-                    p4 = -power;
-                }
+            } else {
+                //turn left
+                turnLeft(power);
             }
-        }else {
-
-            //turn left
-            p1 = -drive;
-            p2 = drive;
-            p3 = -drive;
-            p4 = drive;
-
+            telemetry.addData("front left", leftFrontMotor.getPower());
+            telemetry.addData("front right", rightFrontMotor.getPower());
+            telemetry.addData("back left", leftBackMotor.getPower());
+            telemetry.addData("back right", rightBackMotor.getPower());
+            telemetry.update();
         }
-        telemetry.addData("front left", p3);
-        telemetry.addData("front right", p4);
-        telemetry.addData("back left", p1);
-        telemetry.addData("back right", p2);
-        telemetry.update();
     }
+
+    private void turnRight(double power){
+        leftBackMotor.setPower(power);
+        leftFrontMotor.setPower(power);
+        rightBackMotor.setPower(-power);
+        rightFrontMotor.setPower(-power);
+    }
+
+    private void turnLeft(double power){
+        leftBackMotor.setPower(-power);
+        leftFrontMotor.setPower(-power);
+        rightBackMotor.setPower(power);
+        rightFrontMotor.setPower(power);
+    }
+
+    private void driveForward(double power){
+        leftBackMotor.setPower(power);
+        leftFrontMotor.setPower(power);
+        rightBackMotor.setPower(power);
+        rightFrontMotor.setPower(power);
+    }
+
+    private void driveBackward(double power){
+        leftBackMotor.setPower(-power);
+        leftFrontMotor.setPower(-power);
+        rightBackMotor.setPower(-power);
+        rightFrontMotor.setPower(-power);
+    }
+
+    private void strafeLeft(double power){
+        leftBackMotor.setPower(-power);
+        leftFrontMotor.setPower(power);
+        rightBackMotor.setPower(power);
+        rightFrontMotor.setPower(-power);
+    }
+
+    private void strafeRight(double power){
+        leftBackMotor.setPower(power);
+        leftFrontMotor.setPower(-power);
+        rightBackMotor.setPower(-power);
+        rightFrontMotor.setPower(power);
+    }
+
     private double getPathagorus(double a, double b){//Define Pythagorean Theorem
         double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
         return c;
+
+        //Finding the joystick position using pythagorean theorem (a2 + b2 = c2)
     }
 
 
