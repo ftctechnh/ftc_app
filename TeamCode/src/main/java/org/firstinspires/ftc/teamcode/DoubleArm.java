33d07848076f290dc79f96d0cbb2ff4d19e2f1c0
cooler9711 <cooler9711@outlook.com>
2017-10-05 16:51:56 -0600

    Kaden added a file which will control the arm he built. So far it's just the servo on the claw itself.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleArm.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+/**
+ * Created by BeehiveRobotics-3648 on 9/19/2017.
+ */
+@TeleOp(name = "DoubleArm", group = "linear OpMode")
+public class DoubleArm extends OpMode{
+    Servo Claw;
+    Servo UpDown;
+    Servo BigRotation;
+    
+    @Override
+
+    public void init(){
+        Claw = hardwareMap.servo.get("s1" );
+
+    }
+
+    @Override
+    public void loop(){
+        if (gamepad1.a){
+            if (Claw.getPosition() < 0.5) {
+                Claw.setPosition(Claw.getPosition() + 0.05);
+            }
+        }
+        if (gamepad1.b){
+            if (Claw.getPosition() > 0.3) {
+                Claw.setPosition(Claw.getPosition() - 0.05);
+            }
+        }
+    }
+}
