2428835536a2752f970c9f727c17acf2677f8638
Scott3.0 <bremmerscottw@gmail.com>
2017-10-06 17:40:01 -0700

    Worked on TeleOp, need to add tewo motors, program was designed for 2 motors (tank treads)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScottsTeleOP.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.opmodes.HardwareRatbot;
+
+/**
+ * Created by bremm on 10/6/2017.
+ */
+
+@TeleOp(name="ratbot2017", group="Ratbot")
+public class ScottsTeleOP extends OpMode {
+
+    HardwareRatbot robot       = new HardwareRatbot();
+
+    @Override
+    public void init() {
+
+        robot.init(hardwareMap);
+
+        telemetry.addData("Say", "Hello Driver");    //
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+
+    @Override
+    public void start() {
+    }
+
+
+    @Override
+    public void loop() {
+        double left;
+        double right;
+
+        left = -gamepad1.left_stick_y;
+        right = -gamepad1.right_stick_y;
+
+        robot.leftDrive.setPower(left);
+        robot.rightDrive.setPower(right);
+
+    }
+    @Override
+    public void stop() {
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/HardwareRatbot.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Created by bremm on 10/6/2017.
+ */
+
+public class HardwareRatbot {
+    /* Public OpMode members. */
+    public DcMotor leftDrive   = null;
+    public DcMotor  rightDrive  = null;
+
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+
+    public HardwareRatbot(){
+
+    }
+
+    public void init(HardwareMap ahwMap) {
+
+        hwMap = ahwMap;
+
+        leftDrive  = hwMap.get(DcMotor.class, "left_drive");
+        rightDrive = hwMap.get(DcMotor.class, "right_drive");
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+}
