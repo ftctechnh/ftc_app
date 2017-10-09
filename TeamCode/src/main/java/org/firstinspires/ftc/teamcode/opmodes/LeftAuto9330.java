38d30f93ab5813dba5e49d120cc9470f61cdbbd4
Jakob Stickles <jnlstickles@yahoo.com>
2017-10-09 15:30:42 -0700

    Added some empty subsystem and opmode classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LeftAuto9330.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+/**
+ * Created by robot on 10/9/2017.
+ */
+
+public class LeftAuto9330 {
+
+    //Will rely on color sensor to detect side and then turn the right direction
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/RightAuto9330.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+/**
+ * Created by robot on 10/9/2017.
+ */
+
+public class RightAuto9330 {
+    //Will rely on cs to detect side and turn accordingly
+}
