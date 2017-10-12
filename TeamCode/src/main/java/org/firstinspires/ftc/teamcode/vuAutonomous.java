2c4de711ebd53f694201f48eedd379d8693cefbe
cooler9711 <cooler9711@outlook.com>
2017-10-12 17:21:01 -0600

    i removed one enter space because OCD

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vuAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vuAutonomous.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
