fc39dc4911b87050d9d59e396e62434db7aa5326
Arleth Salinas <arlethssbb@gmail.com>
2017-10-12 09:33:58 -0400

    Thrusday Morning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmHardwareClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmHardwareClass.java
@@ -3,27 +3,18 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
-import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 
 //This class defines all the specific hardware for a the BACONbot robot.
 
-public class HardwareBACONbot {
+public class ArmHardwareClass {
     /* Public OpMode members. */
-    public DcMotor frontLeftMotor = null;
+//    public DcMotor frontLeftMotor = null;
     public DcMotor frontRightMotor = null;
     public DcMotor backLeftMotor = null;
     public DcMotor backRightMotor = null;
-//    public DcMotor verticalArmMotor = null;
-//    public Servo clawServo = null;
-
-//    public Servo servo = null;
-//    public ColorSensor colorSensor = null;
-//    public TouchSensor touchSensor = null;
-//    public boolean enableLed = true;
+    public DcMotor verticalArmMotor = null;
+    public Servo   clawServo = null;
 
 
     /* Give place holder values for the motors and the grabber servo */
@@ -37,7 +28,7 @@ public class HardwareBACONbot {
     private ElapsedTime period = new ElapsedTime();
 
     /* Constructor */
-    public HardwareBACONbot() {
+    public ArmHardwareClass() {
 
     }
 
@@ -49,30 +40,25 @@ public class HardwareBACONbot {
         // Define and Initialize Hardware
 
         // Define and Initialize Hardware
-        frontLeftMotor = hwMap.dcMotor.get("FL");
+//        frontLeftMotor = hwMap.dcMotor.get("FL");
         frontRightMotor = hwMap.dcMotor.get("FR");
         backLeftMotor = hwMap.dcMotor.get("BL");
         backRightMotor = hwMap.dcMotor.get("BR");
-//        verticalArmMotor = hwMap.dcMotor.get("VAM");
-//        clawServo =  hwMap.servo.get("CS");
-
-//      colorSensor = hwMap.colorSensor.get("colorSensor");
-//        servo = hwMap.servo.get("servo0");
-//        touchSensor = hwMap.touchSensor.get("touchSensor");
+        verticalArmMotor = hwMap.dcMotor.get("VAM");
+        clawServo =  hwMap.servo.get("CS");
 
         // Set all hardware to default position
         // Set all hardware to default position
-        frontLeftMotor.setPower(0);
+//        frontLeftMotor.setPower(0);
         frontRightMotor.setPower(0);
         backLeftMotor.setPower(0);
         backRightMotor.setPower(0);
-//        verticalArmMotor.setPower(0);
-//        clawServo.setPosition(0);
-//        servo.setPosition(0);
+        verticalArmMotor.setPower(0);
+        clawServo.setPosition(0);
 
 
         // Set proper encoder state for all motor
-        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+//        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
