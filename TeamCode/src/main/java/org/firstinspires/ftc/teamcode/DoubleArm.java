1abfce68466d20f57f622f5aa822c135dea35f46
KadenGordon <kaden.m.gordon@gmail.com>
2017-10-13 20:30:33 -0600

    so... long story short, turns out I was using a continuous servo for the BigRotation this whole time. oops. I updated the code to reflect that realization. At this point, who knows if it works. I will test on Saturday

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleArm.java
@@ -2,7 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.Range;
+
 /**
  * Created by BeehiveRobotics-3648 on 9/19/2017.
  */
@@ -10,32 +13,36 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 public class DoubleArm extends OpMode{
     Servo Claw;
     Servo UpDown;
-    Servo BigRotation;
+    CRServo BigRotation;
     double clawSpeed = 0.075;
     double clawPosition = 0.4;
     double clawHighEnd = 1.00;
     double clawLowEnd = 0.3;
-    double upDownSpeed = 0.05;
+    double upDownSpeed = 0.075;
     double upDownPosition = 0.5;
-    double bigRotationSpeed = 0.025;
-    double bigRotationPosition = 0.43;
+    double upDownHighEnd = 1.00;
+    double upDownLowEnd = 0.00;
+    double bigRotationSpeed = 0;
+    double bigRotationInit = 0.3;
+    double bigRotationMaxSpeed = 0.03;
     double bigRotationHighEnd = 0.49;
-    double bigRotationLowEnd = 0.38;
+    double bigRotationLowEnd = 0.43;
     
     @Override
-
     public void init(){
         Claw = hardwareMap.servo.get("s1");
         Claw.setPosition(clawPosition);
         UpDown = hardwareMap.servo.get("s2");
         UpDown.setPosition(upDownPosition);
-        BigRotation = hardwareMap.servo.get("s3");
-        BigRotation.setPosition(bigRotationPosition);
+        BigRotation = hardwareMap.crservo.get("s3");
+        BigRotation.setPower(bigRotationInit);
     }
 
     @Override
     public void loop(){
-        bigRotationPosition = (gamepad1.left_stick_y*bigRotationSpeed/16) + bigRotationPosition;
+        bigRotationSpeed = Range.scale(gamepad1.left_stick_y,-1.0,1.0,bigRotationLowEnd,bigRotationHighEnd);
+        upDownPosition = (gamepad1.right_stick_y*upDownSpeed/16) + upDownPosition;
+        BigRotation.setPower(bigRotationSpeed);
 
         if (gamepad1.a){
             if (Claw.getPosition() < clawHighEnd) {
@@ -52,22 +59,23 @@ public class DoubleArm extends OpMode{
         Claw.setPosition(clawPosition);
         UpDown.setPosition(upDownPosition);
         UpDown.setPosition(upDownPosition);
-        if (bigRotationPosition < bigRotationHighEnd && bigRotationPosition > bigRotationLowEnd) {
-            BigRotation.setPosition(bigRotationPosition);
-            BigRotation.setPosition(bigRotationPosition);
-            bigRotationPosition = BigRotation.getPosition();
+
+        if (upDownPosition < upDownHighEnd && upDownPosition > upDownLowEnd) {
+            UpDown.setPosition(upDownPosition);
+            UpDown.setPosition(upDownPosition);
+            upDownPosition = UpDown.getPosition();
         }
-        else if (bigRotationPosition < bigRotationLowEnd){
-            bigRotationPosition = bigRotationLowEnd;
+        else if (upDownPosition < upDownLowEnd){
+            upDownPosition = upDownLowEnd;
         }
-        else if (bigRotationPosition > bigRotationHighEnd){
-            bigRotationPosition = bigRotationHighEnd;
+        else if (upDownPosition > upDownHighEnd){
+            upDownPosition = upDownHighEnd;
         }
 
         telemetry.addData("ClawServoPos",Claw.getPosition());
         telemetry.addData("Wrist servo position",UpDown.getPosition());
-        telemetry.addData("Big Rotation goal",bigRotationPosition);
-        telemetry.addData("big rotation position",BigRotation.getPosition());
+        telemetry.addData("Big Rotation goal",bigRotationSpeed);
+        telemetry.addData("big rotation speed",BigRotation.getPower());
         telemetry.update();
     }
 }
