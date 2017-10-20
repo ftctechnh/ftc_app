dd0b2a5168dac32c9806fe9da45f6d4e8e3f5b25
chsbacon <matthew.shields@charlottesvilleschools.org>
2017-10-20 10:36:35 -0400

    color and distance sensor test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestHardwareClass.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//This class defines all the specific hardware for a the BACONbot robot.
+
+public class TestHardwareClass {
+    /* Public OpMode members. */
+    ColorSensor sensorColor;
+    DistanceSensor sensorDistance;
+    /* local OpMode members. */
+    HardwareMap hwMap = null;
+    private ElapsedTime period = new ElapsedTime();
+
+    /* Constructor */
+    public TestHardwareClass() {
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+        // Define and Initialize Hardware
+        sensorColor = hwMap.get(ColorSensor.class, "colorsensor");
+        // get a reference to the distance sensor that shares the same name.
+        sensorDistance = hwMap.get(DistanceSensor.class, "colorsensor");
+
+
+    }
+}
