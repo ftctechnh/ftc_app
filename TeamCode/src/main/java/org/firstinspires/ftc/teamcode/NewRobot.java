df9912af26bddf7cf63f286737c25ec3f3cde1be
Jeremy <jeremy.yao.info@gmail.com>
2017-10-20 15:54:27 -0400

    10/20 Start Vision Track, layout autonomous op mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewRobot.java
@@ -5,22 +5,57 @@ import android.graphics.Color;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
 /**
  * Created by Jeremy and Raghav on 10/18/2017.
  */
 
 public class NewRobot
 {
+
     ColorSensor topColorSens = null;
     ColorSensor forwardColorSens = null;
     ColorSensor floorColorSens = null;
 
-    public NewRobot(HardwareMap hMap)
-    {
-        topColorSens = hMap.colorSensor.get("topColorSens");
-        forwardColorSens = hMap.colorSensor.get("forwardColorSens");
-        floorColorSens = hMap.colorSensor.get("floorColorSens");
+    public static final String VUFORIA_KEY = "AepnoMf/////AAAAGWsPSj5vh0WQpMc0OEApBsgbZVwduMSeEZFjXMlBPW7WiZRgwGXsOTLiGMxL4qjU0MYpZitHxs4E/nOUHseMX+SW0oopu6BnWL3cAqFIptSrdMpy4y6yB3N6l+FPcGFZxzadvRoiOfAuYIu5QMHSeulfQ1XApDhBQ79lNUXv9LZ7bngBI3BEYVB+slmTGHKhRW2NI5fUtF+rLRiou4ZcNir2eZh0OxEW4zAnTnciVB2R28yyHkYz8xJtACm+4heWLdpw/zf66LRpvTGLwkASci7ZkGJp4NrG5Of4C0b3+iq/EeEmX2PiY5lq2fkUE0dejdztmkFWYBW7c/Y+bIYGER/3gt6I8UhAB78cR7p2mOaY"; //Key used for Vuforia.
 
+    OpenGLMatrix lastLocation = null;
+
+    VuforiaLocalizer vuforia;
+    VuforiaLocalizer.Parameters parameters;
+    VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+    VuforiaTrackable relicTemplate = relicTrackables.get(0);
+
+
+    public NewRobot(HardwareMap hardwareMap)
+    {
+       // topColorSens = hardwareMap.colorSensor.get("topColorSens");
+        //forwardColorSens = hardwareMap.colorSensor.get("forwardColorSens");
+        //floorColorSens = hardwareMap.colorSensor.get("floorColorSens");
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        relicTemplate.setName("relicVuMarkTemplate");
+
+        relicTrackables.activate();
+    }
+
+    public String getGlyphCipher()
+    {
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        return vuMark.toString();
     }
 
     public float getHueValue(ColorSensor in_ColorSens)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSpinPivot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSpinPivot.java
@@ -1,19 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 /**
  * Created by Jeremy on 10/15/2017.
  */
 
+@Autonomous(name = "Vuforia letters", group = "Test")
 public class TestSpinPivot extends LinearOpMode
 {
-    private TankBase robot;
+    private NewRobot robot;
 
     public void runOpMode() throws InterruptedException
     {
-        robot = new TankBase(hardwareMap);
+        robot = new NewRobot(hardwareMap);
         waitForStart();
+        while(opModeIsActive())
+        {
+            telemetry.addData("Cipher", robot.getGlyphCipher());
+            telemetry.update();
+        }
 
     }
 }
