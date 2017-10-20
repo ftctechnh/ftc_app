0a51ab1586cf5932f455f5d2e8ad5d891773ca22
Jakob Stickles <jnlstickles@yahoo.com>
2017-10-20 16:17:40 -0700

    Yay! We coded the glyph and relic servos.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RelicPickup9330.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RelicPickup9330.java
@@ -1,8 +1,48 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import org.firstinspires.ftc.teamcode.Hardware9330;
+
 /**
  * Created by robot on 10/13/2017.
  */
 
 public class RelicPickup9330 {
+    //We will need to add 2 extra servos
+    private boolean isHandClosed;
+    private boolean isWristUp;
+    private Hardware9330 hwMap;
+
+
+    static final Double CLOSED_POS = 0.8;
+    static final Double OPEN_POS = 0.25;
+
+    public RelicPickup9330(Hardware9330 robotMap){
+        hwMap = robotMap;
+        isHandClosed = false;
+        isWristUp = false;
+    }
+
+    public void openHand(){
+        if(isHandClosed){
+            hwMap.handServo.setPosition(OPEN_POS);
+        }
+    }
+
+    public void closeHand(){
+        if(!isHandClosed){
+            hwMap.handServo.setPosition(CLOSED_POS);
+        }
+    }
+
+    public void raiseWrist(){
+        if(!isHandClosed){
+            hwMap.wristServo.setPosition(OPEN_POS);
+        }
+    }
+
+    public void lowerWrist(){
+        if(isHandClosed){
+            hwMap.wristServo.setPosition(CLOSED_POS);
+        }
+    }
 }
