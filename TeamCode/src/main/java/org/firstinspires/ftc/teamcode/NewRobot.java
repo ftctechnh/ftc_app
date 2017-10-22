9eeaccf761508ca7e6480f6c3b12dc5477cb4cba
Jeremy <jeremy.yao.info@gmail.com>
2017-10-22 18:29:07 -0400

    10/22 Vision track function to return column of picture

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewRobot.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
@@ -18,44 +17,38 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 public class NewRobot
 {
-
     private ColorSensor topColorSens = null;
     private ColorSensor forwardColorSens = null;
     private ColorSensor floorColorSens = null;
 
     public static final String VUFORIA_KEY = "AepnoMf/////AAAAGWsPSj5vh0WQpMc0OEApBsgbZVwduMSeEZFjXMlBPW7WiZRgwGXsOTLiGMxL4qjU0MYpZitHxs4E/nOUHseMX+SW0oopu6BnWL3cAqFIptSrdMpy4y6yB3N6l+FPcGFZxzadvRoiOfAuYIu5QMHSeulfQ1XApDhBQ79lNUXv9LZ7bngBI3BEYVB+slmTGHKhRW2NI5fUtF+rLRiou4ZcNir2eZh0OxEW4zAnTnciVB2R28yyHkYz8xJtACm+4heWLdpw/zf66LRpvTGLwkASci7ZkGJp4NrG5Of4C0b3+iq/EeEmX2PiY5lq2fkUE0dejdztmkFWYBW7c/Y+bIYGER/3gt6I8UhAB78cR7p2mOaY"; //Key used for Vuforia.
-
-    private VuforiaLocalizer vuforia;
-    private VuforiaLocalizer.Parameters parameters;
-    private VuforiaTrackables relicTrackables;
-    private VuforiaTrackable relicTemplate;
-    private RelicRecoveryVuMark vuMark;
-    private int cameraMonitorViewId;
-
+    private VuforiaLocalizer vuforia = null;
+    private RelicRecoveryVuMark vuMark = null;
+    private VuforiaTrackables relicTrackables = null;
+    private VuforiaTrackable relicTemplate = null;
 
     public NewRobot(HardwareMap hardwareMap)
     {
-       // topColorSens = hardwareMap.colorSensor.get("topColorSens");
-        //forwardColorSens = hardwareMap.colorSensor.get("forwardColorSens");
-        //floorColorSens = hardwareMap.colorSensor.get("floorColorSens");
-
-        cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
 
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
 
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         vuforia = ClassFactory.createVuforiaLocalizer(parameters);
 
-        relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        relicTrackables = vuforia.loadTrackablesFromAsset("RelicVuMark");
         relicTemplate = relicTrackables.get(0);
         relicTemplate.setName("relicVuMarkTemplate");
 
         relicTrackables.activate();
+        vuMark = RelicRecoveryVuMark.from(relicTemplate);
     }
 
     public char getGlyphCipher()
     {
+        vuMark = RelicRecoveryVuMark.from(relicTemplate);
+
         if (vuMark.equals(RelicRecoveryVuMark.CENTER))
             return 'c';
         else if (vuMark.equals(RelicRecoveryVuMark.LEFT))
@@ -63,7 +56,7 @@ public class NewRobot
         else if (vuMark.equals(RelicRecoveryVuMark.RIGHT))
             return 'r';
         else
-            return 'u';
+            return '?';
     }
 
     public float getHueValue(ColorSensor in_ColorSens)
