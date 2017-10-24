14500cf1a3552931813c0035723a649af65cb4fc
Carson Storm <stormymcstorm@gmail.com>
2017-10-24 09:54:31 -0400

    cleaned up robot class

--- Team9853/src/main/java/org/firstinspires/ftc/team9853/Robot9853.java
+++ Team9853/src/main/java/org/firstinspires/ftc/team9853/Robot9853.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
 import org.chathamrobotics.common.Robot;
+import org.chathamrobotics.common.systems.HolonomicDriver;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 /**
@@ -21,10 +22,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 @SuppressWarnings({"unused", "WeakerAccess"})
 public class Robot9853 extends Robot {
 
-    private DcMotor frontRightMotor;
-    private DcMotor frontLeftMotor;
-    private DcMotor backRightMotor;
-    private DcMotor backLeftMotor;
+    private HolonomicDriver driver;
 
     public static Robot9853 build(OpMode opMode) {
         return new Robot9853(opMode.hardwareMap, opMode.telemetry);
@@ -36,13 +34,7 @@ public class Robot9853 extends Robot {
 
     @Override
     public void init() {
-        frontRightMotor = hardwareMap.dcMotor.get("FrontRight");
-        frontLeftMotor = hardwareMap.dcMotor.get("FrontLeft");
-        backRightMotor = hardwareMap.dcMotor.get("BackRight");
-        backLeftMotor = hardwareMap.dcMotor.get("BackLeft");
-
-        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        driver = HolonomicDriver.build(this);
     }
 
     @Override
@@ -51,24 +43,9 @@ public class Robot9853 extends Robot {
     }
 
     public void driveWithControls(Gamepad gp) {
-        double rightMotorPower = Range.clip(-gp.left_stick_y, -1, 1);
-        double leftMotorPower = Range.clip(-gp.left_stick_y, -1, 1);
+        double magnitude = Math.hypot(gp.left_stick_x, gp.left_stick_y);
+        double direction = Math.atan2(gp.left_stick_y, gp.left_stick_x);
 
-        if (gp.right_stick_x != 0) {
-            rightMotorPower = rightMotorPower == 0 ? 1 : rightMotorPower;
-            leftMotorPower = leftMotorPower == 0 ? 1 : leftMotorPower;
-
-            rightMotorPower *= -gp.right_stick_x;
-            leftMotorPower *= gp.right_stick_x;
-        }
-
-        log.debug("Right Stick X", gp.right_stick_x);
-        log.debug("Left Stick Y", -gp.right_stick_y);
-
-        frontRightMotor.setPower(rightMotorPower);
-        backRightMotor.setPower(rightMotorPower);
-
-        frontLeftMotor.setPower(leftMotorPower);
-        backLeftMotor.setPower(leftMotorPower);
+        driver.setDrivePower(direction, magnitude, gp.right_stick_x);
     }
 }
