8db3ef686a57e4905720bdf69a62ca29b3486a51
jefflscott <jeff_l_scott@yahoo.com>
2017-10-26 18:19:09 -0400

    Change teleop to use MoveSimple

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele_Op_2017.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele_Op_2017.java
@@ -194,7 +194,7 @@ public class Tele_Op_2017 extends OpMode
            wep.retract();
            */
 
-        go.VectorMove( gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x );
+        go.MoveSimple( gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x );
 
         /*
         if (gamepad1.left_stick_y > 0.0)
