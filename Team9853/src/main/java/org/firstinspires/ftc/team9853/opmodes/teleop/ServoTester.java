5928ef3e30e6df3d89b771a0366614527b56e725
Carson Storm <stormymcstorm@gmail.com>
2017-10-26 07:55:44 -0400

    gradles stuff

---
+++ Team9853/src/main/java/org/firstinspires/ftc/team9853/opmodes/teleop/ServoTester.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.team9853.opmodes.teleop;
+
+/**
+ * Created by carsonstorm on 10/25/2017.
+ */
+
+public class ServoTester {
+}
---
+++ Team9853/src/main/java/org/firstinspires/ftc/team9853/systems/RackAndPinion.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.team9853.systems;
+
+/**
+ * Created by carsonstorm on 10/25/2017.
+ */
+
+public class RackAndPinion {
+}
