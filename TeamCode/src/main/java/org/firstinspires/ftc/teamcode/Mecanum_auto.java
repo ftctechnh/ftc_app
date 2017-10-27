8e6eff1a6c58046264ba2d9d71421f33de0cec34
ftcteam8045 <pleasanthillftc@gmail.com>
2017-10-26 17:45:58 -0700

    work to mecanum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum_auto.java
@@ -1,34 +1,9 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -36,7 +11,20 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+
+
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+
+import java.util.Locale;
 
 import static java.lang.Math.abs;
 
@@ -82,6 +70,12 @@ public class Mecanum_auto extends LinearOpMode {
 //                                                      (WHEEL_DIAMETER_INCHES * 3.1415);
 //    static final double     DRIVE_SPEED             = 0.6;
 //    static final double     TURN_SPEED              = 0.5;
+// The IMU sensor object
+    BNO055IMU imu;
+
+    // State used for updating telemetry
+    Orientation angles;
+    Acceleration gravity;
 
     @Override
     public void runOpMode() {
@@ -91,6 +85,32 @@ public class Mecanum_auto extends LinearOpMode {
          * The init() method of the hardware class does all the work here
          */
         gromit.init(hardwareMap);
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        // Set up our telemetry dashboard
+        composeTelemetry();
+
+        // Wait until we're told to go
+        //waitForStart();
+
+        // Start the logging of measured acceleration
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
 
         // Send telemetry message to signify robot waiting;
 //        telemetry.addData("Status", "Resetting Encoders");    //
@@ -109,19 +129,32 @@ public class Mecanum_auto extends LinearOpMode {
 
         /** Wait for the game to start (driver presses PLAY)**/
         waitForStart();
+            double error=.3;
+            double power=0.3;
+        resetStartTime();      // start timer
 
+        mecanumDriveGyro(1.0,0.0,0.0,0);
+//        while (opModeIsActive()) {
+//            telemetry.update();
+//            error=(angles.firstAngle-0)/90;
+//            gromit.left_front.setPower(power + error/1.0);
+//            gromit.left_back.setPower(power + error/1.0);
+//            gromit.right_front.setPower(power - error/1.0);
+//            gromit.right_back.setPower(power - error/1.0);
+//
+//
+//
+//        }
 
-    sleep(1000);
-
-        telemetry.addData("Path", "Complete");
-        telemetry.update();
-
-        mecanumDrive(1,25,34);
-        sleep(2000);
-        stop_drivetrain();
-        mecanumDrive(-1,45,34);
-        sleep(2000);
-        stop_drivetrain();
+//        telemetry.addData("Path", "Complete");
+//        telemetry.update();
+//
+//        mecanumDrive(1,25,34);
+//        sleep(2000);
+//        stop_drivetrain();
+//        mecanumDrive(-1,45,34);
+//        sleep(2000);
+//        stop_drivetrain();
 
 
     }
@@ -175,6 +208,7 @@ public class Mecanum_auto extends LinearOpMode {
      */
     public void mecanumDriveGyro(double speed,double direction, double orientation, double distance)
     {
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
         if(abs(speed)> 1) speed = speed/abs(speed);                                                 //Correct Speed if outside of Range
 
         /**
@@ -183,7 +217,7 @@ public class Mecanum_auto extends LinearOpMode {
         double max;
         double multiplier;
         double turn_correction;
-        double correction_speed =0;
+        double correction_speed ;
         double lfpower;
         double lrpower;
         double rfpower;
@@ -195,11 +229,10 @@ public class Mecanum_auto extends LinearOpMode {
         /**
         * Drive Loop of this method
         */
-        if(opModeIsActive()) {                                                                     // Ensure that the opmode is still active
-             /**
-             * READ IMU
-             */
-            turn_correction = orientation /**- IMU reading*/;
+        while(opModeIsActive()) {                                                                     // Ensure that the opmode is still active
+            telemetry.update();
+            double heading = angles.firstAngle;
+            turn_correction = orientation - heading;
 
             if (turn_correction <= -180) turn_correction +=360;   // correction should be +/- 180 (to the left negative, right positive)
             if (turn_correction >=  180) turn_correction -=360;
@@ -210,10 +243,10 @@ public class Mecanum_auto extends LinearOpMode {
 
 
 
-            lfpower = Math.sin(Math.toRadians(direction+45)); //+ correction_speed;
-            lrpower = Math.cos(Math.toRadians(direction+45)); //+ correction_speed;
-            rfpower = Math.cos(Math.toRadians(direction+45)); //- correction_speed;
-            rrpower = Math.sin(Math.toRadians(direction+45)); //- correction_speed;
+            lfpower = Math.sin(Math.toRadians(direction+45)) + correction_speed;
+            lrpower = Math.cos(Math.toRadians(direction+45)) + correction_speed;
+            rfpower = Math.cos(Math.toRadians(direction+45)) - correction_speed;
+            rrpower = Math.sin(Math.toRadians(direction+45)) - correction_speed;
 
             //Determine largest power being applied
             max = lfpower;
@@ -277,5 +310,81 @@ public class Mecanum_auto extends LinearOpMode {
         gromit.left_back.setPower(0.0);
         gromit.right_front.setPower(0.0);
         gromit.right_back.setPower(0.0);
+    }
+
+
+    //----------------------------------------------------------------------------------------------
+    // Telemetry Configuration
+    //----------------------------------------------------------------------------------------------
+
+    void composeTelemetry() {
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(new Runnable() { @Override public void run()
+        {
+            // Acquiring the angles is relatively expensive; we don't want
+            // to do that in each of the three items that need that info, as that's
+            // three times the necessary expense.
+            angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            gravity  = imu.getGravity();
+        }
+        });
+
+        telemetry.addLine()
+                .addData("status", new Func<String>() {
+                    @Override public String value() {
+                        return imu.getSystemStatus().toShortString();
+                    }
+                })
+                .addData("calib", new Func<String>() {
+                    @Override public String value() {
+                        return imu.getCalibrationStatus().toString();
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("heading", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.firstAngle);
+                    }
+                })
+                .addData("roll", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.secondAngle);
+                    }
+                })
+                .addData("pitch", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.thirdAngle);
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("grvty", new Func<String>() {
+                    @Override public String value() {
+                        return gravity.toString();
+                    }
+                })
+                .addData("mag", new Func<String>() {
+                    @Override public String value() {
+                        return String.format(Locale.getDefault(), "%.3f",
+                                Math.sqrt(gravity.xAccel*gravity.xAccel
+                                        + gravity.yAccel*gravity.yAccel
+                                        + gravity.zAccel*gravity.zAccel));
+                    }
+                });
+    }
+
+    //----------------------------------------------------------------------------------------------
+    // Formatting
+    //----------------------------------------------------------------------------------------------
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
     }
 }
