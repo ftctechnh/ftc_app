df35692250591c873639445a9f0c6d7a99f7ddde
nithyagh <nithyas729@gmail.com>
2017-10-30 20:37:12 -0700

    Set up motors and servos for the robot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NS_Robot_GoldenGears.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NS_Robot_GoldenGears.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Nithya on 10/29/2017.
@@ -12,7 +15,23 @@ public class NS_Robot_GoldenGears {
     // Hardware Map of robot
     private HardwareMap hardwareMap = null;
 
+    DcMotor driveLeftMotor = null;
+    DcMotor driveRightMotor = null;
+    DcMotor armElevationMotor = null;
+    Servo clawLeftServo = null;
+    Servo clawRightServo = null;
+
     public NS_Robot_GoldenGears(HardwareMap hm){
         hardwareMap = hm;
+
+        driveLeftMotor = hardwareMap.dcMotor.get("driveLeftMotor");
+        driveRightMotor = hardwareMap.dcMotor.get("driveRightMotor");
+        driveLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        armElevationMotor = hardwareMap.dcMotor.get("armElevationMotor");
+
+        clawLeftServo = hardwareMap.servo.get("clawLeftServo");
+        clawRightServo = hardwareMap.servo.get("clawRightServo");
+        clawRightServo.setDirection(Servo.Direction.REVERSE);
     }
 }
