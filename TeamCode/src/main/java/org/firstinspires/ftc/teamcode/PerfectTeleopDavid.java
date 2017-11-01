90a435128aea02758c6d57330b3ddc29265c5268
david-wiles6 <davidcwiles6@gmail.com>
2017-11-01 16:18:15 -0400

    tweaks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PerfectTeleopDavid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PerfectTeleopDavid.java
@@ -112,7 +112,7 @@ MasterHardwareClass robot = new MasterHardwareClass();
 
 
                     final double v1 = +GRX;
-                    final double v2 = +GRX;
+                    final double v2 = -GRX;
                     final double v3 = -GRX;
                     final double v4 = -GRX;
                     frontLeft = v1;
