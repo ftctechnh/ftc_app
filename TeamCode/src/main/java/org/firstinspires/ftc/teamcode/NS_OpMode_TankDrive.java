91bc9eb4cf18f128cab5b91990b5c362f923fb77
nithilangh <nithilans929@gmail.com>
2017-11-01 19:28:35 -0700

    Implemented Tank Drive OpMode using Golden Gears robot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NS_OpMode_TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NS_OpMode_TankDrive.java
@@ -9,14 +9,37 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class NS_OpMode_TankDrive extends OpMode {
     NS_Robot_GoldenGears GGRobot = null;
+
     @Override
     public void init() {
         GGRobot = new NS_Robot_GoldenGears(hardwareMap);
+        telemetry.addData("Status", "Robot Initalized");
+    }
 
+    @Override
+    public void start(){
+        GGRobot.Reset();
+        telemetry.addData("Status", "Robot Started");
     }
 
     @Override
     public void loop() {
+        GGRobot.DriveRobot(-gamepad1.left_stick_y, -gamepad1.right_stick_y);
 
+        GGRobot.RotateArm(-gamepad2.left_stick_y);
+
+        double tolerance = 0.15;
+        if (-gamepad2.right_stick_y > tolerance){
+            GGRobot.ActuateClaw(true);
+        }
+        else if (-gamepad2.right_stick_y < -tolerance){
+            GGRobot.ActuateClaw(false);
+        }
+    }
+
+    @Override
+    public void stop(){
+        GGRobot.Reset();
+        telemetry.addData("Status", "This Is AWESOME!");
     }
 }
