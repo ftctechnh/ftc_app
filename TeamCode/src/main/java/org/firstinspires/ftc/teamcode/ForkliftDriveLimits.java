d8293dc3826de39d4478ab6e4158b299238f5480
cooler9711 <cooler9711@outlook.com>
2017-11-02 16:40:53 -0600

    programmed a new opmode for the forklift, hopefully with button limits working.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ForkliftDriveLimits.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by FTC on 11/2/17.
+ */
+@TeleOp(name = "ForkliftDriveWithLimits", group = "linear OpMode")
+public class ForkliftDriveLimits extends OpMode {
+    private Servo Claw;
+    private double clawPosition = 0.0;
+    private double clawHighEnd = 1.0;
+    private double clawLowEnd = 0.3;
+    private DcMotor FrontLeft;
+    private DcMotor FrontRight;
+    private DcMotor RearLeft;
+    private DcMotor RearRight;
+    private DcMotor DrawerSlide;
+    private DigitalChannel TopButton;
+    private DigitalChannel BottomButton;
+    private double DrawerSlideLowEnd;
+    private double DrawerSlideHighEnd;
+    private double DrawerSlideSpeed = 0;
+    private double up = 0;
+    private double down = 0;
+
+    @Override
+    public void init() {
+        FrontLeft = hardwareMap.dcMotor.get("m1");
+        FrontRight = hardwareMap.dcMotor.get("m2");
+        RearLeft = hardwareMap.dcMotor.get("m3");
+        RearRight = hardwareMap.dcMotor.get("m4");
+        DrawerSlide = hardwareMap.dcMotor.get("m5");
+        Claw = hardwareMap.servo.get("s1");
+        Claw.setPosition(clawPosition);
+        TopButton = hardwareMap.get(DigitalChannel.class, "b1");
+        TopButton = hardwareMap.get(DigitalChannel.class, "b2");
+        reverseMotor(FrontRight);
+        reverseMotor(RearRight);
+    }
+
+    @Override
+    public void loop() {
+        double right = gamepad1.right_stick_y;
+        double left = gamepad1.left_stick_y;
+        FrontRight.setPower(right);
+        FrontLeft.setPower(left);
+        RearRight.setPower(right);
+        RearLeft.setPower(left);
+
+        if (gamepad1.a) {
+            clawPosition = clawHighEnd;
+            //clawPosition = clawPosition + 0.001;
+
+        }
+        if (gamepad1.b) {
+            clawPosition = clawLowEnd;
+            //clawPosition = clawPosition - 0.001;
+        }
+        Claw.setPosition(clawPosition);
+        Claw.setPosition(clawPosition);
+        up = gamepad1.right_trigger;
+        down = gamepad1.left_trigger;
+        DrawerSlideSpeed = up - down;
+        if(!((TopButton.getState()==false && DrawerSlideSpeed > 0)||(BottomButton.getState()==false && DrawerSlideSpeed < 0))) {
+            DrawerSlide.setPower(DrawerSlideSpeed);
+        }
+        else {
+            DrawerSlide.setPower(0.0);
+        }
+        telemetry.addData("Current Claw Position", Claw.getPosition());
+    }
+
+    public void reverseMotor(DcMotor motor) {
+
+        motor.setDirection(DcMotor.Direction.REVERSE);
+    }
+}
