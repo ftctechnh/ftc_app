fac222234e3d6cf419a3416278f73a7b12bfb4b5
Gavin Uberti <gavin.uberti@gmail.com>
2017-11-02 14:57:37 -0700

    Don't knock off a ball unless robot is certain about positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompleteAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompleteAutonomous.java
@@ -26,15 +26,16 @@ public class CompleteAutonomous extends LinearOpMode {
     VuforiaLocalizer vuforia;
     List<VuforiaTrackable> trackables;
 
-    final double INITIAL_DESIRED_HEADING = Math.PI/2;
+    final double INITIAL_DESIRED_HEADING = Math.PI / 2;
     final double TURN_VOLATILITY = 1.5 * Math.PI;
-    final double ACCEPTABLE_HEADING_VARIATION = Math.PI/180; // 1 degree
+    final double ACCEPTABLE_HEADING_VARIATION = Math.PI / 180; // 1 degree
     final int DISTANCE_TO_DRIVE = 400;
     final int ROTATION = 1120; // 2880 encoder clicks per wheel rotation
 
     PixyCam pixyCam;
     PixyCam.Block redBall;
     PixyCam.Block blueBall;
+    Alliance rightMostBall; // Which alliance the rightmost ball belongs to, from robot POV
     double difference;
 
     @Override
@@ -65,9 +66,7 @@ public class CompleteAutonomous extends LinearOpMode {
         telemetry.log().add("Robot started");
 
         updateBlocks();
-
-        Alliance rightMostBall; // Which alliance the rightmost ball belongs to, from robot POV
-
+        
         // Higher x-values are on the right
 
         if (redBall.averageX() > blueBall.averageX()) {
@@ -82,9 +81,135 @@ public class CompleteAutonomous extends LinearOpMode {
         robot.closeBlockClaw();
         robot.setLiftMode(DcMotor.RunMode.RUN_TO_POSITION);
         robot.lift.setPower(0.5);
-        robot.lift.setTargetPosition(-ROTATION/2);
+        robot.lift.setTargetPosition(-ROTATION / 2);
         robot.sleep(1000);
 
+
+        // We have now hit the gem
+
+        // We now need to read the pictograph
+        initializeVuforia();
+        String pictographLocation = null;
+        int pictograph = 0;
+
+        while (opModeIsActive() && pictographLocation == null) {
+
+            for (VuforiaTrackable trackable : trackables) {
+                boolean seen = ((VuforiaTrackableDefaultListener) trackable.getListener()).isVisible();
+                telemetry.addData(trackable.getName(), seen ? "Visible" : "Not Visible");
+                if (seen) {
+                    pictographLocation = trackable.getName();
+                    break;
+                }
+            }
+            telemetry.update();
+        }
+        switch (pictographLocation) {
+            case "leftTarget":
+                pictograph = 0;
+                break;
+            case "centerTarget":
+                pictograph = 1;
+                break;
+            case "rightTarget":
+                pictograph = 2;
+                break;
+        }
+        // We now need to move into the corner
+
+        robot.sleep(3000);
+
+        // Up and left three wheel rotations
+        robot.frontLeft.setTargetPosition(robot.frontLeft.getTargetPosition() + ROTATION * 3);
+        //robot.backLeft.setTargetPosition(robot.backLeft.getTargetPosition() + ROTATION*2);
+        //robot.frontRight.setTargetPosition(robot.frontRight.getTargetPosition() + ROTATION*2);
+        robot.backRight.setTargetPosition(robot.backRight.getTargetPosition() + ROTATION * 3);
+
+        for (DcMotor m : robot.motorArr) {
+            m.setPower(0.35);
+        }
+        robot.sleep(3000); // Wait for this to be completed
+
+        // Up and right five wheel rotations
+        //robot.frontLeft.setTargetPosition(robot.frontLeft.getTargetPosition() + ROTATION*3);
+        robot.backLeft.setTargetPosition(robot.backLeft.getTargetPosition() + ROTATION * 3);
+        robot.frontRight.setTargetPosition(robot.frontRight.getTargetPosition() + ROTATION * 3);
+        //robot.backRight.setTargetPosition(robot.backRight.getTargetPosition() + ROTATION*3);
+
+        robot.sleep(3000); // Wait for this to be completed
+
+        robot.frontLeft.setTargetPosition(robot.frontLeft.getTargetPosition() - ROTATION / 2);
+        robot.backLeft.setTargetPosition(robot.backLeft.getTargetPosition() - ROTATION / 2);
+        robot.frontRight.setTargetPosition(robot.frontRight.getTargetPosition() - ROTATION / 2);
+        robot.backRight.setTargetPosition(robot.backRight.getTargetPosition() - ROTATION / 2);
+
+        robot.sleep(1000); // Wait for this to be completed
+
+        int driveDist = ROTATION + ROTATION / 2 * pictograph;
+
+        robot.frontLeft.setTargetPosition(robot.frontLeft.getTargetPosition() + driveDist);
+        robot.backLeft.setTargetPosition(robot.backLeft.getTargetPosition() - driveDist);
+        robot.frontRight.setTargetPosition(robot.frontRight.getTargetPosition() - driveDist);
+        robot.backRight.setTargetPosition(robot.backRight.getTargetPosition() + driveDist);
+
+        robot.sleep(3000);
+
+        robot.frontLeft.setTargetPosition(robot.frontLeft.getTargetPosition() + ROTATION);
+        robot.backLeft.setTargetPosition(robot.backLeft.getTargetPosition() + ROTATION);
+        robot.frontRight.setTargetPosition(robot.frontRight.getTargetPosition() + ROTATION);
+        robot.backRight.setTargetPosition(robot.backRight.getTargetPosition() + ROTATION);
+
+        robot.sleep(1000);
+        robot.lift.setTargetPosition(0);
+
+        robot.sleep(1000);
+        robot.openRelicClaw();
+        robot.sleep(500);
+
+        robot.frontLeft.setTargetPosition(robot.frontLeft.getTargetPosition() - ROTATION);
+        robot.backLeft.setTargetPosition(robot.backLeft.getTargetPosition() - ROTATION);
+        robot.frontRight.setTargetPosition(robot.frontRight.getTargetPosition() - ROTATION);
+        robot.backRight.setTargetPosition(robot.backRight.getTargetPosition() - ROTATION);
+        robot.openRelicClaw();
+
+
+    }
+
+    public void initializeVuforia() {
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = "Ac4jpF3/////AAAAGYER4VUDLEYGlD++ha+MStuNhKORp/7DQz1D1+tQwcrsMnbQwLqRgpkFtCOIGrZ942gdL179juAJmdXeeH+Dk0pVgxLFq6O0AzY1MS3wS5JHvSLppO9v8W//finYio3hQk+TFKD+qWq9Q1nAZx0bMWFeF6IuIjUPQLioBzC/lYzI/L7oi/AJAbFlf6wue3gDs0dgwrAgpe+JFHTgM3g2+y4hS6O0mcJjobAWSNeRxq9caOGfl/q6f09Eu2EccSmHLAaqje0i70eAIZ4Tbg5C31sPZxBOPTEGTQ9NvFhP4FNAXlvPCdiBt6XYE8P17UzPN72p7lRKyp4xR1oC8B/4dYbivso+rQUed5/H7AnQYOdA";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        VuforiaTrackables pictographAssets = this.vuforia.loadTrackablesFromAsset("FirstRelicRecoveryPictographs");
+        VuforiaTrackable blueCryptoBox = pictographAssets.get(0);
+        blueCryptoBox.setName("BlueCryptoBox");
+
+        VuforiaTrackable rightPictograph = pictographAssets.get(1);
+        rightPictograph.setName("RightTarget");
+
+        VuforiaTrackable centerPictograph = pictographAssets.get(2);
+        centerPictograph.setName("CenterTarget");
+
+        VuforiaTrackable leftPictograph = pictographAssets.get(3);
+        leftPictograph.setName("LeftTarget");
+
+        trackables = new ArrayList<VuforiaTrackable>();
+        trackables.addAll(pictographAssets);
+
+        pictographAssets.activate();
+    }
+
+    public void updateBlocks() {
+        redBall = pixyCam.GetBiggestBlock(1);
+        blueBall = pixyCam.GetBiggestBlock(2);
+    }
+
+    public boolean ballPositionsKnown() {
+        return redBall.isSeen() && blueBall.isSeen();
+    }
+
+    public void knockOffBlocks() {
         if (robot.color == BLUE) {
             robot.lowerLeftWhipSnake();
         } else {
@@ -115,122 +240,5 @@ public class CompleteAutonomous extends LinearOpMode {
         for (DcMotor m : robot.motorArr) {
             m.setTargetPosition(0);
         }
-
-        // We have now hit the gem
-
-        // We now need to read the pictograph
-        initializeVuforia();
-        String pictographLocation = null;
-        int pictograph = 0;
-
-        while (opModeIsActive() && pictographLocation == null) {
-
-            for (VuforiaTrackable trackable : trackables) {
-                boolean seen = ((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible();
-                telemetry.addData(trackable.getName(), seen ? "Visible" : "Not Visible");
-                if (seen) {
-                    pictographLocation = trackable.getName();
-                    break;
-                }
-            }
-            telemetry.update();
-        }
-        switch(pictographLocation) {
-            case "leftTarget":
-                pictograph = 0;
-                break;
-            case "centerTarget":
-                pictograph = 1;
-                break;
-            case "rightTarget":
-                pictograph = 2;
-                break;
-        }
-        // We now need to move into the corner
-
-        robot.sleep(3000);
-
-        // Up and left three wheel rotations
-        robot.frontLeft.setTargetPosition(robot.frontLeft.getTargetPosition() + ROTATION*3);
-        //robot.backLeft.setTargetPosition(robot.backLeft.getTargetPosition() + ROTATION*2);
-        //robot.frontRight.setTargetPosition(robot.frontRight.getTargetPosition() + ROTATION*2);
-        robot.backRight.setTargetPosition(robot.backRight.getTargetPosition() + ROTATION*3);
-
-        for (DcMotor m : robot.motorArr) {
-            m.setPower(0.35);
-        }
-        robot.sleep(3000); // Wait for this to be completed
-
-        // Up and right five wheel rotations
-        //robot.frontLeft.setTargetPosition(robot.frontLeft.getTargetPosition() + ROTATION*3);
-        robot.backLeft.setTargetPosition(robot.backLeft.getTargetPosition() + ROTATION*3);
-        robot.frontRight.setTargetPosition(robot.frontRight.getTargetPosition() + ROTATION*3);
-        //robot.backRight.setTargetPosition(robot.backRight.getTargetPosition() + ROTATION*3);
-
-        robot.sleep(3000); // Wait for this to be completed
-
-        robot.frontLeft.setTargetPosition(robot.frontLeft.getTargetPosition() - ROTATION/2);
-        robot.backLeft.setTargetPosition(robot.backLeft.getTargetPosition() - ROTATION/2);
-        robot.frontRight.setTargetPosition(robot.frontRight.getTargetPosition() - ROTATION/2);
-        robot.backRight.setTargetPosition(robot.backRight.getTargetPosition() - ROTATION/2);
-
-        robot.sleep(1000); // Wait for this to be completed
-
-        int driveDist = ROTATION + ROTATION/2 * pictograph;
-
-        robot.frontLeft.setTargetPosition(robot.frontLeft.getTargetPosition() + driveDist);
-        robot.backLeft.setTargetPosition(robot.backLeft.getTargetPosition() - driveDist);
-        robot.frontRight.setTargetPosition(robot.frontRight.getTargetPosition() - driveDist);
-        robot.backRight.setTargetPosition(robot.backRight.getTargetPosition() + driveDist);
-
-        robot.sleep(3000);
-
-        robot.frontLeft.setTargetPosition(robot.frontLeft.getTargetPosition() + ROTATION);
-        robot.backLeft.setTargetPosition(robot.backLeft.getTargetPosition() + ROTATION);
-        robot.frontRight.setTargetPosition(robot.frontRight.getTargetPosition() + ROTATION);
-        robot.backRight.setTargetPosition(robot.backRight.getTargetPosition() + ROTATION);
-
-        robot.sleep(1000);
-        robot.lift.setTargetPosition(0);
-
-        robot.sleep(1000);
-        robot.openRelicClaw();
-        robot.sleep(500);
-
-        robot.frontLeft.setTargetPosition(robot.frontLeft.getTargetPosition() - ROTATION);
-        robot.backLeft.setTargetPosition(robot.backLeft.getTargetPosition() - ROTATION);
-        robot.frontRight.setTargetPosition(robot.frontRight.getTargetPosition() - ROTATION);
-        robot.backRight.setTargetPosition(robot.backRight.getTargetPosition() - ROTATION);
-        robot.openRelicClaw();
-
-
-    }
-    public void initializeVuforia() {
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
-        parameters.vuforiaLicenseKey = "Ac4jpF3/////AAAAGYER4VUDLEYGlD++ha+MStuNhKORp/7DQz1D1+tQwcrsMnbQwLqRgpkFtCOIGrZ942gdL179juAJmdXeeH+Dk0pVgxLFq6O0AzY1MS3wS5JHvSLppO9v8W//finYio3hQk+TFKD+qWq9Q1nAZx0bMWFeF6IuIjUPQLioBzC/lYzI/L7oi/AJAbFlf6wue3gDs0dgwrAgpe+JFHTgM3g2+y4hS6O0mcJjobAWSNeRxq9caOGfl/q6f09Eu2EccSmHLAaqje0i70eAIZ4Tbg5C31sPZxBOPTEGTQ9NvFhP4FNAXlvPCdiBt6XYE8P17UzPN72p7lRKyp4xR1oC8B/4dYbivso+rQUed5/H7AnQYOdA";
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
-
-        VuforiaTrackables pictographAssets = this.vuforia.loadTrackablesFromAsset("FirstRelicRecoveryPictographs");
-        VuforiaTrackable blueCryptoBox  = pictographAssets.get(0);
-        blueCryptoBox.setName("BlueCryptoBox");
-
-        VuforiaTrackable rightPictograph = pictographAssets.get(1);
-        rightPictograph.setName("RightTarget");
-
-        VuforiaTrackable centerPictograph  = pictographAssets.get(2);
-        centerPictograph.setName("CenterTarget");
-
-        VuforiaTrackable leftPictograph  = pictographAssets.get(3);
-        leftPictograph.setName("LeftTarget");
-
-        trackables = new ArrayList<VuforiaTrackable>();
-        trackables.addAll(pictographAssets);
-
-        pictographAssets.activate();
-    }
-    public void updateBlocks() {
-        redBall = pixyCam.GetBiggestBlock(1);
-        blueBall = pixyCam.GetBiggestBlock(2);
     }
 }
