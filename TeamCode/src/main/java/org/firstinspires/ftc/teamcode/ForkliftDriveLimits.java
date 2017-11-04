6f8c332c2f313fbc224dbaad355fd2ac89ca4134
cooler9711 <cooler9711@outlook.com>
2017-11-04 13:23:16 -0600

    made adjustments match all forklift opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ForkliftDriveLimits.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ForkliftDriveLimits.java
@@ -15,8 +15,8 @@ import com.qualcomm.robotcore.util.Range;
 public class ForkliftDriveLimits extends OpMode {
     private Servo Claw;
     private double clawPosition = 0.0;
-    private double clawHighEnd = 1.0;
-    private double clawLowEnd = 0.3;
+    private double clawHighEnd = 0.45;
+    private double clawLowEnd = 0.225;
     private DcMotor FrontLeft;
     private DcMotor FrontRight;
     private DcMotor RearLeft;
