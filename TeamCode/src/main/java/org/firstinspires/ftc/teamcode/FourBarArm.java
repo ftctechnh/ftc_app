ae56685fdc6ca804b914dd31e765db17c6510409
GamingMage <josephliang@roadrunner.com>
2017-11-04 15:32:41 -0400

    New Things 1.2 :octocat:
    Sorted some files
    Worked on autonomous
    Fixed some things
    Broke some things... probably

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FourBarArm.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
+/**
+ * Created by Joseph Liang on 11/4/2017.
+ */
+
+public class FourBarArm {
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RlcRcvryAutoSwitch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RlcRcvryAutoSwitch.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -13,28 +13,33 @@ import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
  */
 
 @Autonomous(name="Relic Recovery: State Machine", group="Pushbot")
-public class RlcRcvryAutoSwitch {
+//@Disabled
+public class RlcRcvryAutoSwitch extends OpMode{
 
     private int stateMachineFlow;
     HardwarePushbot robot       = new HardwarePushbot();
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.7;
+    static final double     TURN_SPEED              = 0.65;
 
     @Override
-    public void runOpMode(){
+    public void init() {
         robot.init(hardwareMap);
-    }
-    @Override
-    public void init_loop(){
-
         stateMachineFlow = 0;
     }
-    @Override
-    public void start(){
 
-    }
+
     @Override
     public void loop() {
         switch(stateMachineFlow){
             case 0:
+
                 stateMachineFlow++;
                 break;
             case 1:
@@ -42,5 +47,67 @@ public class RlcRcvryAutoSwitch {
                 break;
         }
     }
+    //encoder drive method
+    public void encoderDrive(double speed,
+                             double leftSide, double rightSide,
+                             double timeoutS) {
+        int newLeftTarget;
+        int newRightTarget;
+        telemetry.addData("Path", "before opmode is active");
+        telemetry.update();
+        sleep(500);
 
-}
+        // Ensure that the opmode is still active
+        //if (opModeIsActive()) {
+            telemetry.addData("Path", "inside opmode: %7d :%7d",robot.leftDrive.getCurrentPosition(),robot.rightDrive.getCurrentPosition());
+            telemetry.update();
+            sleep(500);
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = robot.leftDrive.getCurrentPosition() + (int)(leftSide * COUNTS_PER_INCH);
+            newRightTarget = robot.rightDrive.getCurrentPosition() + (int)(rightSide * COUNTS_PER_INCH);
+            robot.leftDrive.setTargetPosition(newLeftTarget);
+            robot.rightDrive.setTargetPosition(newRightTarget);
+
+            telemetry.addData("after new target set",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+            telemetry.update();
+            sleep(500);
+
+            // Turn On RUN_TO_POSITION
+            robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.leftDrive.setPower(Math.abs(speed));
+            robot.rightDrive.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+           // while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.leftDrive.isBusy() && robot.rightDrive.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLeftTarget,  newRightTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d",
+                        robot.leftDrive.getCurrentPosition(),
+                        robot.rightDrive.getCurrentPosition());
+                telemetry.update();
+            }
+
+            //Stop all motion;
+            robot.leftDrive.setPower(0);
+            robot.rightDrive.setPower(0);
+
+            telemetry.addData("PathJ", "inside opmode: %7d :%7d",robot.leftDrive.getCurrentPosition(),robot.rightDrive.getCurrentPosition());
+            telemetry.update();
+            sleep(500);
+
+            // Turn off RUN_TO_POSITION
+            robot.leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //  sleep(250);   // optional pause after each move
+        } //end of encoder drive code
+    }//end of class
+
