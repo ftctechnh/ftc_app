27d1b9ed384682c88ee98d535668cd387712d220
Ftc Team 8045 <pleasanthillftc@gmail.com>
2017-11-05 13:34:22 -0800

    Ana Controller Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AnaControllerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AnaControllerTest.java
@@ -1,98 +1,127 @@
-
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="AnaOnBot ControllerTest", group="Pushbot")
+
+public class AnaControllerTL extends LinearOpMode {
+
+    // Set all status flags to false
+    public boolean dpadup = false;
+    public boolean dpaddown = false;
+    public boolean dpadleft = false;
+    public boolean dpadright = false;
+    public boolean button_a = false;
+    public boolean button_b = false;
+    public boolean button_x = false;
+    public boolean button_y = false;
+    public boolean start = false;
+    public boolean back = false;
+    public boolean righttrigger = false;
+    public boolean lefttrigger = false;
+    public boolean rightbumper = false;
+    public boolean leftbumper = false;
 
 
-@TeleOp(name="Ana Controller Test", group="Pushbot")
-//@Disabled
-public class AnaControllerTest extends OpMode{
-
-    /* Declare OpMode members. */
-    //HardwareGromit gromit      = new HardwareGromit(); // use the class created to define a Pushbot's hardware
-                                                         // could also use HardwarePushbotMatrix class.
-   
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
     @Override
-    public void init() {
+    public void runOpMode() {
+
+
         /* Initialize the hardware variables.
          * The init() method of the hardware class does all the work here
          */
-       // gromit.init(hardwareMap);
+        //robot.init(hardwareMap);
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData("To Start Test", "Press Play");    //
-    }
+        telemetry.update();
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
-    public boolean dpadup = false;
-    public boolean dpaddown = false;
-  
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
-    }
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
-    @Override
-    public void loop() {
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addLine("*** Controller Test ****");
 
-
-        telemetry.addLine("*** Controller Test ****");
-
-        telemetry.addLine().addData("", dpadup).addData("Dpad Up    ", gamepad1.dpad_up);
-        telemetry.addLine().addData("", dpaddown).addData("Dpad Down  ", gamepad1.dpad_down);
-        
+            // show status flag and current value
+            telemetry.addLine().addData("", dpadup).addData("Dpad Up    ", gamepad1.dpad_up);
+            telemetry.addLine().addData("", dpaddown).addData("Dpad Down  ", gamepad1.dpad_down);
+            telemetry.addLine().addData("", dpadleft).addData("Dpad Left  ", gamepad1.dpad_left);
+            telemetry.addLine().addData("", dpadright).addData("Dpad Right  ", gamepad1.dpad_right);
+            telemetry.addLine().addData("", button_a).addData("Button A  ", gamepad1.a);
+            telemetry.addLine().addData("", button_b).addData("Button B  ", gamepad1.b);
+            telemetry.addLine().addData("", button_x).addData("Button X  ", gamepad1.x);
+            telemetry.addLine().addData("", start).addData("Start  ", gamepad1.start);
+            telemetry.addLine().addData("", back ).addData("Back  ", gamepad1.back);
+            telemetry.addLine().addData("", righttrigger ).addData(" Right Trigger  ", gamepad1.right_trigger);
+            telemetry.addLine().addData("", lefttrigger ).addData(" Left Trigger  ", gamepad1.left_trigger);
+            telemetry.addLine().addData("", rightbumper ).addData(" Right Bumper  ", gamepad1.right_bumper);
+            telemetry.addLine().addData("", leftbumper ).addData(" Left Bumper  ", gamepad1.left_bumper);
 
 
-        if (gamepad1.dpad_up) dpadup = true;
-        if (gamepad1.dpad_down) dpaddown = true;
+
+
+            // set status flag true if pressed
+            if (gamepad1.dpad_up) dpadup = true;
+            if (gamepad1.dpad_down) dpaddown = true;
+            if (gamepad1.dpad_left) dpadleft = true;
+            if (gamepad1.dpad_right) dpadright = true;
+            if (gamepad1.a) button_a = true;
+            if (gamepad1.b) button_b = true;
+            if (gamepad1.x) button_x = true;
+            if (gamepad1.y) button_y = true;
+            if (gamepad1.start) start = true;
+            if (gamepad1.back) back = true;
+            if (gamepad1.right_trigger) righttrigger = true;
+            if (gamepad1.left_trigger) lefttrigger = true;
+            if (gamepad1.right_bumper) rightbumper = true;
+            if (gamepad1.left_bumper) leftbumper = true;
+
 /**    controller buttons I can think of
  *      gamepad1.dpad_left
-        gamepad1.dpad_right
-        gamepad1.a
-        gamepad1.b
-        gamepad1.x
-        gamepad1.y
-        gamepad1.back
-        gamepad1.start
-        gamepad1.right_stick_button
-        gamepad1.left_stick_button
-        gamepad1.right_bumper
-        gamepad1.left_bumper
-        gamepad1.right_trigger
-        gamepad1.left_trigger
-        gamepad1.right_stick_x
-        gamepad1.right_stick_y
-        gamepad1.left_stick_x
-        gamepad1.left_stick_y   */
+ gamepad1.dpad_right
+ gamepad1.a
+ gamepad1.b
+ gamepad1.x
+ gamepad1.y
+ gamepad1.back
+ gamepad1.start
+ gamepad1.right_stick_button
+ gamepad1.left_stick_button
+ gamepad1.right_bumper
+ gamepad1.left_bumper
+ gamepad1.right_trigger
+ gamepad1.left_trigger
+ gamepad1.right_stick_x
+ gamepad1.right_stick_y
+ gamepad1.left_stick_x
+ gamepad1.left_stick_y   */
 
 
-       // test to see if all have been set
-        if (    (dpadup) &&
-                (dpaddown)     ) {
+            // test to see if all have been set
+            if (    (dpadup) &&
+                    (dpaddown) &&
+                    (button_a) &&
+                    (button_b) &&
+                    (button_x) &&
+                    (button_y) &&
+                    (start) &&
+                    (back) &&
+                    (righttrigger) &&
+                    (lefttrigger) &&
+                    (rightbumper) &&
+                    (leftbumper) )
+            {
 
-            telemetry.addLine("****** All TESTS COMPLETE*******");
+                telemetry.addLine("****** All TESTS COMPLETE*******");
+            }
+            telemetry.update();
+
+
+
         }
-       telemetry.update();
-
-    }
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
     }
 }
