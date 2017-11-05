6ecc4f47baa9ce9fd3f145e5056deb09139f448b
Jeremy <jeremy.yao.info@gmail.com>
2017-11-05 15:17:15 -0500

    11/5 Autonomus changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoAudi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoAudi.java
@@ -33,18 +33,18 @@ public class RedAutoAudi extends LinearOpMode
         switch (newRobot.getGlyphCipher()) //Switch statement; it also decodes the image at the same time and
                                             //returns the column in a character
         {
-            case 'l': robot.driveStraight_In(x); //lowercase L represents left;
+            case 'l': robot.driveStraight_In(30); //lowercase L represents left;
                 break;
-            case 'c': robot.driveStraight_In(x); // represents the center; robot drives distance to center column
+            case 'c': robot.driveStraight_In(36); // represents the center; robot drives distance to center column
                 break;
-            case 'r': robot.driveStraight_In(x);//represents right; robot drives dist to right column
+            case 'r': robot.driveStraight_In(42);//represents right; robot drives dist to right column
                 break;
             default: robot.driveStraight_In(center); //robot goes into center if it can't find a glyph
                 break;
         }
         robot.pivot_IMU(-90, .25);
-        robot.driveStraight_In(x); //Drive to the glyph to column at normal speed first, but not into it
-        robot.driveStraight_In(x, .25);
+        robot.driveStraight_In(16); //Drive to the glyph to column at normal speed first, but not into it
+        robot.driveStraight_In(8, .25);
         //lower door attachment to ground
         //open door attachment to release glyph
         robot.driveStraight_In(-2); // back up
