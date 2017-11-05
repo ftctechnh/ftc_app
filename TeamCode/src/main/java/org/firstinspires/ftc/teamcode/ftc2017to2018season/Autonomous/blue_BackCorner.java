5fdcd17af31b7aa166c3a8f02888487078442288
sfChen1029 <steven.fx.chen@gmail.com>
2017-11-04 20:05:45 -0700

    asdfasdf

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc2017to2018season/Autonomous/blue_BackCorner.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode.ftc2017to2018season.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+
+//10-28-17
+@Autonomous(name="Autonomous Blue Test")
+public class blue_frontCorner extends Autonomous_General {
+
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor leftBack;
+    DcMotor rightBack;
+    public double rsBuffer = 20.00;
+
+
+    @Override
+    public void runOpMode() {
+
+
+        vuforiaInit(true, true);
+        telemetry.addData("","Vuforia Initiated");
+        telemetry.update();
+        initiate();
+        telemetry.addData("--->", "Gyro Calibrating");
+        telemetry.update();
+        gyro.calibrate();
+
+
+        while(gyro.isCalibrating()){
+            sleep(50);
+            idle();
+
+        }
+
+        telemetry.addData("---->","Gyro Calibrated. Good to go...");
+        telemetry.update();
+
+        waitForStart();
+
+        gyro.resetZAxisIntegrator();
+
+        startTracking();
+        telemetry.addData("","READY TO TRACK");
+        telemetry.update();
+
+        while(!vuMarkFound()){
+
+        }
+        //returnImage();
+        telemetry.addData("Vumark" , vuMark);
+        telemetry.update();
+
+        encoderMecanumDrive(0.5,65,65,1000,0);
+        sleep(1000);
+        gyroTurn(0.3,88);
+        sleep(1000);
+        encoderMecanumDrive(0.75,45,45,1000,0);
+        if (vuMark == RelicRecoveryVuMark.CENTER){
+            strafeRangeDistance(88, 1, rsBuffer, false);
+        }
+        else if (vuMark == RelicRecoveryVuMark.LEFT){
+            strafeRangeDistance(71, 1, rsBuffer, false);
+
+        }
+        else if (vuMark == RelicRecoveryVuMark.RIGHT){
+            strafeRangeDistance(108, 1, rsBuffer, false);
+
+        }
+
+
+        sleep(1000);
+
+
+
+    }
+
+
+}
