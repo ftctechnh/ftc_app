032b0b04f65494bd81ba59af3664766addf0acec
pahels <pahel.srivastava@gmail.com>
2017-11-05 19:42:19 -0800

    Added diagonal movements to teleOP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc2017to2018season/TeleOp/charlieTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc2017to2018season/TeleOp/charlieTeleOp.java
@@ -148,8 +148,29 @@ public class charlieTeleOp extends OpMode {
         float strafeStickLeft = (-gamepad1.left_stick_x);//*leftWheelMotorFront.getMaxSpeed();
         float strafeStickRight = (-gamepad1.right_stick_x);//*leftWheelMotorFront.getMaxSpeed();
         //run the motors by setting power to the motors with the game pad value
+        if(( Math.abs(strafeStickRight) > 0) && (rightY_gp1 != 0)){
+            if (strafeStickRight>0 && rightY_gp1>0){
+                leftWheelMotorBack.setPower(1);
+                rightWheelMotorFront.setPower(1);
+            }
+        else if (strafeStickRight <0 && rightY_gp1 >0){
+                leftWheelMotorFront.setPower(1);
+                rightWheelMotorBack.setPower(1);
+            }
+         else if (strafeStickRight >0 && rightY_gp1 <0){
+                leftWheelMotorBack.setPower(-1);
+                rightWheelMotorFront.setPower(1);
 
-        if (Math.abs(strafeStickLeft) > 0) {
+            }
+         else (strafeStickRight<0 && rightY_gp1<0){
+             leftWheelMotorFront.setPower(-1);
+             rightWheelMotorBack.setPower(-1);
+            }
+            
+
+
+        }
+        else if (Math.abs(strafeStickLeft) > 0) {
 
             leftWheelMotorFront.setPower(-strafeStickLeft);
             leftWheelMotorBack.setPower(strafeStickLeft);
@@ -173,5 +194,7 @@ public class charlieTeleOp extends OpMode {
             rightWheelMotorFront.setPower(rightY_gp1);
             rightWheelMotorBack.setPower(rightY_gp1);
         }
+
+
     }
 }
