06e9baa929babc6d64966613532305d9f66edf6a
Jeremy <jeremy.yao.info@gmail.com>
2017-11-10 16:10:55 -0500

    Edits in RedAutoRef and RedAuto Audi regarding the case statement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoRef.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutoRef.java
@@ -16,43 +16,43 @@ public class RedAutoRef extends LinearOpMode
 
     public void runOpMode()
     {
-        float center = 0;
-        float x = 0;
         robot = new TankBase(hardwareMap);
         newRobot = new NewRobot(hardwareMap);
         waitForStart();
-        sleep(2000);
+        sleep(4000);
         // Close doors
         // Lift up doors to get glyph
         // Lower the right arm
         char cipher = newRobot.getGlyphCipher();
+        telemetry.addData("Cipherr = ", cipher);
+        telemetry.update();
         // Look at the color of platform
         // Sense color of the jewel
         // if the jewel has the same color as the platform; move backwards; return to original position
         // else if the jewel isn't same color; move forward; return to original position
         // else (no color sense); do nothing
         // raise/retract arm
-        robot.driveStraight_In(24);
-        robot.pivot_IMU(90, .25);
+        robot.driveStraight_In(26);
+        robot.pivot_IMU(82, .25);
         switch (cipher)
         {
             case 'l':
-                robot.driveStraight_In(18);
+                robot.driveStraight_In(22); //needs testing, fix on 11/12
                 break;
             case 'c':
-                robot.driveStraight_In(12);
+                robot.driveStraight_In(11.5f);
                 break;
             case 'r':
-                robot.driveStraight_In(6);
+                robot.driveStraight_In(5); //positioning is really off, need to fix on 11/12
                 break;
             default:
                 robot.driveStraight_In(12);
                 break;
         }
 
-        robot.pivot_IMU(-90, .25);
-        robot.driveStraight_In(8);
-        robot.driveStraight_In(8, .25);
+        robot.pivot_IMU(-85, .25);
+        robot.driveStraight_In(8,.1);
+        robot.driveStraight_In(8, .1);
         // lower door attachment to the ground
         //lower door attachment to ground
         //open door attachment to release glyph
