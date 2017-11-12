3a468c1f610c7a40b78979d8ec993b07ece12c8d
Andor16 <pstoney16@gmail.com>
2017-11-12 13:07:36 -0500

    Drive train integration (#1) Fixed Issues in DriveTrain
    Integrated DriveTrain into the main method, fixing small issues relating to formatting. Removed the AHRS Library, as it's been integrated into the main program.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpModes/DriveTrainTeleOpTest.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode.TestOpModes;
+
+import com.qualcomm.hardware.kauailabs.NavxMicroNavigationSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
+
+import org.firstinspires.ftc.teamcode.DriveTrain;
+
+/**
+ * Created by FTC 4316 on 10/29/2017
+ */
+@TeleOp(name="Drive Train TeleOp test", group="Test")
+public class DriveTrainTeleOpTest extends OpMode {
+
+    private DriveTrain driveTrain;
+
+    private double x;
+    private double y;
+    private double z;
+
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor leftRear;
+    DcMotor rightRear;
+
+    private double power;
+    private double finches;
+
+    private boolean isFinished;
+
+    NavxMicroNavigationSensor gyroReference;
+    IntegratingGyroscope gyro;
+
+    @Override
+    public void init() {
+        
+        leftFront = hardwareMap.dcMotor.get("leftfront");
+        rightFront = hardwareMap.dcMotor.get("rightfront");
+        leftRear = hardwareMap.dcMotor.get("leftrear");
+        rightRear = hardwareMap.dcMotor.get("rightrear");
+
+        power = 0.1;
+        finches = 5;
+
+        gyroReference = hardwareMap.get(NavxMicroNavigationSensor.class, "navx");
+        gyro = gyroReference;
+
+        driveTrain = new DriveTrain(leftFront, rightFront, leftRear, rightRear, gyro,  telemetry);
+
+        isFinished = true;
+    }
+
+    @Override
+    public void loop() {
+
+            if (isFinished) {
+                x = gamepad1.left_stick_x;
+                y = -gamepad1.left_stick_y;
+                z = gamepad1.right_stick_x;
+
+                driveTrain.setMotorPower(x, y, z);
+            }
+
+            telemetry.addData("Current Yaw", driveTrain.getYaw());
+            driveTrain.displayEncoders();
+
+        }
+    }
