21e4f7432fddc563982edab89278d28aced0ca9a
Daniel P. Taillard <robotbuilder@rocketmail.com>
2017-11-12 16:59:47 -0500

    Update TrainingTeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/seasons/relicrecovery/TrainingTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/seasons/relicrecovery/TrainingTeleOp.java
@@ -1,8 +1,45 @@
 package org.firstinspires.ftc.teamcode.seasons.relicrecovery;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
 /**
+ *
  * Created by daniel on 11/12/17.
  */
 
-public class TrainingTeleOp {
+public class TrainingTeleOp extends LinearOpMode {
+
+    private DcMotor fLeft;
+    private DcMotor bLeft;
+    private DcMotor fRight;
+    private DcMotor bRight;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //set deadzones
+        gamepad1.setJoystickDeadzone(.02f);
+
+        //initiate motors
+        fLeft = hardwareMap.dcMotor.get("fl");
+        fRight = hardwareMap.dcMotor.get("fr");
+        bLeft = hardwareMap.dcMotor.get("bl");
+        bRight = hardwareMap.dcMotor.get("bl");
+
+        waitForStart();
+        double speedLeft;
+        double speedRight;
+
+        while (opModeIsActive()) {
+            speedLeft = gamepad1.left_stick_x;
+            speedRight = gamepad1.right_stick_x;
+
+            fLeft.setPower(speedLeft);
+            bLeft.setPower(speedLeft);
+            fRight.setPower(speedRight);
+            bRight.setPower(speedRight);
+
+        }
+
+    }
 }
