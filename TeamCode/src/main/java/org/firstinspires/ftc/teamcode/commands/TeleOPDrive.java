bcfa7eb34d87cf303e3a5f8200a08fab6975bb6d
Mahim <mahim216@gmail.com>
2017-11-13 16:14:53 -0500

    created drive system and teleOP command

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/TeleOPDrive.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.systems.DriveSystem;
+
+/**
+ * Created by Mahim on 11/4/2017.
+ */
+
+@TeleOp
+public class TeleOPDrive extends OpMode {
+    private DriveSystem driveSystem;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void init() {
+        this.driveSystem = new DriveSystem(hardwareMap, gamepad1);
+    }
+
+    @Override
+    public void init_loop() {}
+
+    @Override
+    public void start() { runtime.reset(); }
+
+    @Override
+    public void loop() {
+        this.driveSystem.teleOPDrive();
+    }
+
+    @Override
+    public void stop() {}
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/DriveSystem.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.systems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by Mahim on 11/4/2017.
+ */
+
+public class DriveSystem {
+    private DcMotor rightMotor, leftMotor;
+    private HardwareMap hardwareMap;
+    private Gamepad gamepad;
+
+    public DriveSystem (HardwareMap hardwareMap, Gamepad gamepad) {
+        this.hardwareMap = hardwareMap;
+        this.gamepad = gamepad;
+        this.rightMotor = hardwareMap.get(DcMotor.class, "left motor");
+        this.leftMotor = hardwareMap.get(DcMotor.class, "right motor");
+    }
+
+    public void drive(double leftSpeed, double rightSpeed) {
+        this.rightMotor.setPower(rightSpeed);
+        this.leftMotor.setPower(leftSpeed);
+    }
+
+    public void teleOPDrive() {
+        drive(gamepad.left_stick_y, gamepad.right_stick_y);
+    }
+}
