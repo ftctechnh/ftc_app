e56361e06ceae49b33930536abd2fed5f216d584
Gavin Uberti <gavin.uberti@gmail.com>
2017-11-13 19:48:45 -0800

    Give relic claw and flipper controls to second controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -5,10 +5,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import static org.firstinspires.ftc.teamcode.ConstrainedPIDMotor.Direction.BACKWARD;
-import static org.firstinspires.ftc.teamcode.ConstrainedPIDMotor.Direction.COAST;
-import static org.firstinspires.ftc.teamcode.ConstrainedPIDMotor.Direction.FORWARD;
-import static org.firstinspires.ftc.teamcode.ConstrainedPIDMotor.Direction.HOLD;
+import java.io.IOException;
+
 import static org.firstinspires.ftc.teamcode.NullbotHardware.clamp;
 import static org.firstinspires.ftc.teamcode.NullbotHardware.getAngleDifference;
 
@@ -34,6 +32,8 @@ public class MainTeleOp extends LinearOpMode {
 
     boolean wasLeftBumperPressed;
     boolean wasRightBumperPressed;
+    boolean wasGP2LeftBumperPressed;
+    boolean wasGP2RightBumperPressed;
     boolean wasRightTriggerPressed;
     boolean wasLeftTriggerPressed;
     boolean flashMode;
@@ -50,16 +50,15 @@ public class MainTeleOp extends LinearOpMode {
 
     ElapsedTime totalElapsedTime;
 
-    ElapsedTime timeAPressed;
     boolean wasBackPressed;
     boolean slowMode;
 
     @Override
     public void runOpMode() {
-        robot.init(hardwareMap, this, true, gamepad2);
+        robot.init(hardwareMap, this, gamepad1, gamepad2);
 
-        lift = new ConstrainedPIDMotor(robot.lift, 100, 0.6, 0.6, 0, -2500);
-        zType = new ConstrainedPIDMotor(robot.zType, 100, 0.4, 0.3, 0, 12288);
+        lift = new ConstrainedPIDMotor(robot.lift, 100, 0.6, 0.6, 0, -2500, telemetry);
+        zType = new ConstrainedPIDMotor(robot.zType, 100, 0.4, 0.3, 0, 12288, telemetry);
 
         waitForStart();
 
@@ -68,28 +67,28 @@ public class MainTeleOp extends LinearOpMode {
 
         wasLeftBumperPressed = false;
         wasRightBumperPressed = false;
+        wasGP2LeftBumperPressed = false;
+        wasGP2RightBumperPressed = false;
+
         flashMode = false;
         timeTillHeadingLock = new ElapsedTime();
 
         timeSinceDriveModeToggle = new ElapsedTime();
         timeSinceSlowModeToggle = new ElapsedTime();
         totalElapsedTime = new ElapsedTime();
-        timeAPressed = new ElapsedTime();
         wasRightTriggerPressed = false;
         wasLeftTriggerPressed = false;
         wasBackPressed = false;
         slowMode = false;
 
-
         while (opModeIsActive()) {
-
+            robot.updateReadings();
             // Toggle control mode
             if (gamepad1.right_trigger > triggerThreshold && !wasRightTriggerPressed &&
                     timeSinceDriveModeToggle.milliseconds() > 100) {
                 nonrelativeDriveModeEnabled = !nonrelativeDriveModeEnabled;
                 timeSinceDriveModeToggle.reset();
             }
-
             wasRightTriggerPressed = gamepad1.right_trigger > triggerThreshold;
 
             scale = true;
@@ -97,70 +96,66 @@ public class MainTeleOp extends LinearOpMode {
             desiredMax = 1;
 
             // Toggle slow mode
-            if (gamepad1.left_trigger > triggerThreshold || robot.zType.getCurrentPosition() > 3000) {// Left trigger activates slow mode
+            if (gamepad1.left_trigger > triggerThreshold) {// Left trigger activates slow mode
                 desiredMax = minSlowModePower + ((1 - minSlowModePower) * (1 - gamepad1.left_trigger));
                 robot.setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
             } else {
                 robot.setDriveMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             }
 
-
-            if (!robot.isTestChassis) {
-
-                if (!gamepad1.a) {
-                    timeAPressed.reset();
-                }
-                if (gamepad1.back && !wasBackPressed) {
-                    timeAPressed.reset();
-                }
+            if (true) {
 
                 setOverride((gamepad1.a && !gamepad1.start));
 
                 if (gamepad1.start && !gamepad1.back && !gamepad1.a) { // Back coasts all motors
-                    lift.setDirection(COAST);
-                    zType.setDirection(COAST);
+                    lift.setDirection(ConstrainedPIDMotor.Direction.COAST);
+                    zType.setDirection(ConstrainedPIDMotor.Direction.COAST);
                 } else {
                     // Adjust lift
                     if (gamepad1.dpad_up || gamepad2.dpad_up) {
-                        lift.setDirection(FORWARD);
+                        lift.setDirection(ConstrainedPIDMotor.Direction.FORWARD);
                     } else if (gamepad1.dpad_down || gamepad2.dpad_down) {
-                        lift.setDirection(BACKWARD);
+                        lift.setDirection(ConstrainedPIDMotor.Direction.BACKWARD);
                     } else {
-                        lift.setDirection(HOLD);
+                        lift.setDirection(ConstrainedPIDMotor.Direction.HOLD);
                     }
 
                     if (gamepad1.dpad_right || gamepad2.dpad_right) {
-                        zType.setDirection(FORWARD);
+                        zType.setDirection(ConstrainedPIDMotor.Direction.FORWARD);
                     } else if (gamepad1.dpad_left || gamepad2.dpad_left) {
-                        zType.setDirection(BACKWARD);
+                        zType.setDirection(ConstrainedPIDMotor.Direction.BACKWARD);
                     } else {
-                        zType.setDirection(HOLD);
+                        zType.setDirection(ConstrainedPIDMotor.Direction.HOLD);
                     }
 
-                    if (gamepad1.b) {
+                    if (gamepad1.b || gamepad2.b) {
                         robot.openBlockClaw();
-                    } else if (gamepad1.x) {
+                    } else if (gamepad1.x || gamepad2.x) {
                         robot.closeBlockClaw();
                     }
 
-                    if (gamepad1.left_bumper && !wasLeftBumperPressed) {
+                    if ((gamepad1.left_bumper && !wasLeftBumperPressed) || (gamepad2.left_bumper && !wasGP2LeftBumperPressed)) {
                         robot.toggleRelicClaw();
                     }
-                    if (gamepad1.right_bumper) {
-                        if (gamepad1.back && gamepad1.a && timeAPressed.milliseconds() > 500) {
-                            robot.relicFipperPosition -= 1;
+
+                    if (gamepad1.right_bumper || gamepad2.right_bumper) {
+                        if (gamepad2.back && gamepad2.a) {
+                            robot.relicFipperPosition -= 0.02;
                             robot.updateFlipperPos();
 
-                        } else if (!gamepad1.back && gamepad1.a && timeAPressed.milliseconds() > 500) { // Explicitly stated for clarity
-                            robot.relicFipperPosition += 1;
+                        } else if (!gamepad2.back && gamepad2.a) { // Explicitly stated for clarity
+                            robot.relicFipperPosition += 0.02;
                             robot.updateFlipperPos();
 
-                        } else if (!wasRightBumperPressed) {
+                        } else if ((gamepad1.right_bumper && !wasRightBumperPressed) || (gamepad2.right_bumper && !wasGP2RightBumperPressed)) {
                             robot.toggleRelicClawFlipper();
                         }
                     }
+
                     wasLeftBumperPressed = gamepad1.left_bumper;
                     wasRightBumperPressed = gamepad1.right_bumper;
+                    wasGP2LeftBumperPressed = gamepad2.left_bumper;
+                    wasGP2RightBumperPressed = gamepad2.right_bumper;
 
                     // Taunt code
                     if (!gamepad1.y) {
@@ -168,13 +163,12 @@ public class MainTeleOp extends LinearOpMode {
                     } else {
                         robot.lowerWhipSnake();
                     }
-
-                    if (gamepad1.start && gamepad1.back) {
+                    /*if (gamepad1.start && gamepad1.back) {
 
                         robot.gyroError = robot.getGyroHeadingRaw();
-                    }
+                    }*/
 
-                    if (gamepad1.start && gamepad1.a) {
+                    if ((gamepad1.start && gamepad1.a) || gamepad2.start && gamepad2.a) {
                         lift.encoderOffset = robot.lift.getCurrentPosition();
                     }
 
@@ -200,13 +194,13 @@ public class MainTeleOp extends LinearOpMode {
                 if (turnRelevant) {
                     turnSpeed = gamepad1.right_stick_x;
                     if (gamepad1.left_trigger > triggerThreshold) {
-                        turnSpeed /= 10.0;
+                        turnSpeed *= 0.6;
                     }
                     timeTillHeadingLock.reset();
                 }
             } else {
                 // Auto turning
-                if (true) {
+                if (false) {
                     difference = getAngleDifference(desiredHeading, heading);
                     turnSpeed = difference / (Math.PI / turnVolatility);
                     turnSpeed = clamp(turnSpeed);
@@ -226,11 +220,6 @@ public class MainTeleOp extends LinearOpMode {
                 }
             }
 
-            // Turning around a point bypasses all previous settings
-            if (gamepad1.left_trigger < triggerThreshold) { // If there's nothing that needs precision running
-                robot.adjustMotorEncoders(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            }
-
             if (scale) {
                 // Now, let's scale those powers
                 double greatest = 0;
@@ -246,17 +235,16 @@ public class MainTeleOp extends LinearOpMode {
             robot.setMotorSpeeds(unscaledMotorPowers);
 
             telemetry.addLine()
-                    .addData("Current lift position", robot.lift.getCurrentPosition());
-            telemetry.addLine()
-                    .addData("Locked position", lift.lockPos);
-            telemetry.addLine()
                     .addData("Current tick", totalElapsedTime.milliseconds());
             telemetry.update(); // Send telemetry data to driver station
 
             // Run above code at 1Khz
-            robot.writeLogTick(gamepad1);
-            robot.waitForTick(1000 / robot.hz);
+            //robot.writeLogTick();
+            //robot.waitForTick(1000 / robot.hz);
         }
+        try {
+            robot.closeLog();
+        } catch (IOException e) {}
 
     }
     private double chop(double d) { // Cutoff all signals being sent to the motor below a threshold
