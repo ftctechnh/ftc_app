e36e6b889fe8e25520b9d52deb47155b77125370
spmcenaney <spmcenaney@outlook.com>
2017-11-14 13:22:12 -0600

    created power functions for motors and servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/Power.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/Power.java
@@ -5,4 +5,28 @@ package org.firstinspires.ftc.teamcode.Utilities;
  */
 
 public class Power {
+  
+    Telemetry tele;
+  
+    public Power(Telemetry tele) {
+        telemetry = tele;
+    }
+  
+    public void power(HardwareDevice motor, double power) {
+        telemetry.addData("motor power", power);
+        try {
+            motor.setPower(power);
+        } catch (Exception opModeException) {
+            telemetry.addData("Can't power (not mapped)", "motor");
+        }
+    }
+    
+    public void position(HardwareDevice servo, double power) {
+        telemetry.addData("servo power", power);
+        try {
+            motor.setPosition(power);
+        } catch (Exception opModeException) {
+            telemetry.addData("Can't power (not mapped)", "servo");
+        }
+    }
 }
