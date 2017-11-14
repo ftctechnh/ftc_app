95ee1641b3843ab66ffd0709a42c1c7921986d11
AArJ <arjunjobe@me.com>
2017-11-14 11:10:39 -0800

    idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAlgoMagnitude.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAlgoMagnitude.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -40,6 +41,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 @TeleOp(name="Mecanum Magnitude Algorithm Concept", group="Concept")
+@Disabled
 public class ConceptAlgoMagnitude extends OpMode
 {
     // move that gear up
