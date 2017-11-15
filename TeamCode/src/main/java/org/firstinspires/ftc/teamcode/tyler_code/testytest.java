c39381e0fff1173007223158fd67ba0430e0f79b
rollameniacalmechanics <Tyler@Tylers-MacBook-Pro.local>
2017-11-15 16:53:14 -0600

    gggg

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tyler_code/testytest.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.tyler_code;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class testytest extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
