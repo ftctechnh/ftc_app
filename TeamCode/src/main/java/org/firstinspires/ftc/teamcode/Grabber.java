3d4fd7cc525f405dd345e8ef38bbc38ba22efe77
Diego Gutierrez <DiegoGutiDev@Diegos-MBP-68.home>
2017-11-15 18:49:52 -0500

    Hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grabber.java
@@ -13,17 +13,17 @@ public class Grabber {
     public boolean closed = false;
     public boolean isPressed = false;
 
-    private final double LEFT_CLOSED_POSITION = 0.5 - (6.0/180);
-    private final double LEFT_OPEN_POSITION = 1 - (6.0/180);
+    private final double LEFT_OPEN_POSITION = (174.0/180);
+    private final double LEFT_CLOSED_POSITION = (84.0/180);
     private final double RIGHT_OPEN_POSITION = (28.0/180);
-    private final double RIGHT_CLOSED_POSITION = RIGHT_OPEN_POSITION + 0.5;
+    private final double RIGHT_CLOSED_POSITION = (118.0/180);
 
     public Grabber(Servo left, Servo right) {
 
         this.leftServo = left;
         this.rightServo = right;
 
-        leftServo.setPosition(LEFT_OPEN_POSITION);
+        leftServo.setPosition(Position(30/180, LEFT_OPEN_POSITION, LEFT_CLOSED_POSITION));
         rightServo.setPosition(RIGHT_OPEN_POSITION);
 
     }
@@ -52,6 +52,18 @@ public class Grabber {
         if (change == 0) {
             isPressed = false;
         }
+    }
+
+    private double Position(double angle, double open, double close) {
+
+        double range = open - close;
+
+        if (range < 0) {
+            return 0 - (range * angle) + open;
+        } else {
+            return range * angle + close;
+        }
+
     }
 
 }
