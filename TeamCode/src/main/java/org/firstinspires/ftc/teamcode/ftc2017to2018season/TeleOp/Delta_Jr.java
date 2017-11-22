572e7312c9799d704b6ab7cce2daa05ba1ecd446
DESKTOP-H8TOJAT\boswo <bosworthrohan@gmail.com>
2017-11-22 15:15:20 -0800

    Added Delta_Jr for the new pushbot.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc2017to2018season/TeleOp/Delta_Jr.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.ftc2017to2018season.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by Pahel and Rohan on 11/22/2017.
+ */
+
+@TeleOp(name = "Delta_Jr")
+
+public class Delta_Jr extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+    @Override
+    public void loop() {
+    }
+
+}
