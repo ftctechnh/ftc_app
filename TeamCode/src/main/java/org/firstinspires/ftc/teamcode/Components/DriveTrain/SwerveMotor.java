94e8127f6c55b7ec7ca08ec384a85010ec7ce540
spmcenaney <spmcenaney@gmail.com>
2017-11-26 21:45:45 -0600

    swerve motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/DriveTrain/SwerveMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/DriveTrain/SwerveMotor.java
@@ -10,7 +10,7 @@ import org.firstinspires.ftc.teamcode.Utilities.SetRobot;
  *
  * This drive train is used for robots with a four motor drive train
  */
-public class FourMotor extends DriveTrainHardware {
+public class SwerveMotor extends DriveTrainHardware {
     // ---------------------- Hardware Devices ----------------------
     /**
      * back left motor
@@ -20,6 +20,22 @@ public class FourMotor extends DriveTrainHardware {
      * back right motor
      */
     private DcMotor mBackRight;
+    /**
+     * motor that rotates left wheel
+     */
+    private DcMotor mSwerveLeft;
+    /**
+     * motor that rotates right wheel
+     */
+    private DcMotor mSwerveRight;
+    /**
+     * motor that rotates back left wheel
+     */
+    private DcMotor mSwerveBackLeft;
+    /**
+     * motor that rotates back right wheel
+     */
+    private DcMotor mSwerveBackRight;
     // --------------------- Hardware Variables ---------------------
     /**
      * this variable is used to set power the back left motor
@@ -29,6 +45,22 @@ public class FourMotor extends DriveTrainHardware {
      * this variable is used to set power the back right motor
      */
     public double backRightPower;
+    /**
+     * this variable is used to set power the swerve left motor
+     */
+    public double swerveLeftPower;
+    /**
+     * this variable is used to set power the swerve right motor
+     */
+    public double swerveRightPower;
+    /**
+     * this variable is used to set power the swerve back left motor
+     */
+    public double swerveBackLeftPower;
+    /**
+     * this variable is used to set power the swerve back right motor
+     */
+    public double swerveBackRightPower;
     // ------------------------ Constructor -------------------------
     /**
      * Constructs a four motor drive train
@@ -36,13 +68,21 @@ public class FourMotor extends DriveTrainHardware {
      * @param map object that is used to map the drive train
      * @param setRobot object that is used to set the power to the drive train
      */
-    public FourMotor(Map map, SetRobot setRobot) {
+    public SwerveMotor(Map map, SetRobot setRobot) {
         this.map = map;
         this.setRobot = setRobot;
-        mBackLeft  = null;
-        mBackRight = null;
-        backLeftPower  = 0;
-        backRightPower = 0;
+        mBackLeft        = null;
+        mBackRight       = null;
+        mSwerveLeft      = null;
+        mSwerveRight     = null;
+        mSwerveBackLeft  = null;
+        mSwerveBackRight = null;
+        backLeftPower        = 0;
+        backRightPower       = 0;
+        swerveLeftPower      = 0;
+        swerveRightPower     = 0;
+        swerveBackLeftPower  = 0;
+        swerveBackRightPower = 0;
     }
     // -------------------------- Mapping ---------------------------
     /**
@@ -54,6 +94,10 @@ public class FourMotor extends DriveTrainHardware {
         mRight = map.motor("r");
         mBackLeft  = map.revMotor("bl");
         mBackRight = map.motor("br");
+        mSwerveLeft  = map.revMotor("sl");
+        mSwerveRight = map.motor("sr");
+        mSwerveBackLeft  = map.revMotor("sbl");
+        mSwerveBackRight = map.motor("sbr");
     }
     // --------------------- Set Hardware Power ---------------------
     /**
@@ -65,5 +109,9 @@ public class FourMotor extends DriveTrainHardware {
         setRobot.power(mRight,rightPower,"right motor");
         setRobot.power(mBackLeft,backLeftPower,"back left motor");
         setRobot.power(mBackRight,backRightPower,"back right motor");
+        setRobot.power(mSwerveLeft,swerveLeftPower,"swerve left motor");
+        setRobot.power(mSwerveRight,swerveRightPower,"swerve right motor");
+        setRobot.power(mSwerveBackLeft,swerveBackLeftPower,"swerve back left motor");
+        setRobot.power(mSwerveBackRight,swerveBackRightPower,"swerve back right motor");
     }
 }
