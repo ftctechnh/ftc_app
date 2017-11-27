76fd26ebb10d2bc04cc2927c6ecd526846ea3716
Jeremy <jeremy.yao.info@gmail.com>
2017-11-27 07:35:41 -0500

    Format

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAutoAudi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAutoAudi.java
@@ -8,7 +8,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
  * Created by Sahithi and Harshini on 11/1/17.
  */
 @Autonomous (name = "BlueAutoAudi", group = "Auto")
-
 public class BlueAutoAudi extends LinearOpMode
 {
     private TankBase robot;
