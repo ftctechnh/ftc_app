3c2de677290d8ef55245d68c438517a9c9d2f6f0
KadenGordon <kaden.m.gordon@gmail.com>
2017-11-28 15:25:27 -0700

    added jewelarm class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelArm.java
@@ -1,8 +1,48 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 /**
  * Created by Kaden on 11/28/2017.
  */
 
 public class JewelArm {
+    private Servo servo;
+    private ColorSensor cs;
+    private double servoDownPos = 0.39;
+    private double servoUpPos = 0.9;
+    public enum Color {Red, Blue}
+
+    public void JewelArm(Servo servo, ColorSensor cs) {
+        this.servo = servo;
+        this.cs = cs;
+    }
+    public void down() {
+        servo.setPosition(servoDownPos);
+        servo.setPosition(servoDownPos);
+    }
+    public void up() {
+        servo.setPosition(servoUpPos);
+        servo.setPosition(servoUpPos);
+    }
+    public Color findJewel() {
+        while (cs.red() < 2 && cs.blue() < 2) {
+
+        }
+        int blue = 0;
+        int red = 0;
+        for (int i = 0; i<=5; i++) {
+            if (cs.blue() > cs.red()) {
+                blue += 1;
+            } else {
+                red += 1;
+            }
+        }
+        if (blue > red) {
+            return Color.Red;
+        } else {
+            return Color.Blue;
+        }
+    }
 }
