f9b1a8bb65511d2b9d2b7a46a0ea53312bac4a10
anthonyTesoriero <anthony.tesoriero>
2017-11-28 22:15:07 -0500

    Jewel Auto

--- Team11248/src/main/java/org/firstinspires/ftc/team11248/Autonomous/Park/Auto.java
+++ Team11248/src/main/java/org/firstinspires/ftc/team11248/Autonomous/Park/Auto.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.team11248.Autonomous;
+package org.firstinspires.ftc.team11248.Autonomous.Park;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
