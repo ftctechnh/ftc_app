00eb8f5689226fa5f254a70841d5c10e4a5a71bd
AArJ <arjunjobe@me.com>
2017-12-03 05:15:19 -0800

    asdfjsdjfklsdf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueOneAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueOneAuto.java
@@ -35,6 +35,7 @@ public class BlueOneAuto extends LinearOpMode {
         encode(15, 0.5, MoveType.STRAIGHT);
         encode(-19, 0.5, MoveType.ROT);
         encode(35, 0.5, MoveType.STRAIGHT);
+        wait(500);
         robot.gripper.setPower(0.25);
         wait(1000);
         robot.gripper.setPower(0);
