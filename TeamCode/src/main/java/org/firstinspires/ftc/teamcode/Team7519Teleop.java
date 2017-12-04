c4635cb9a7a70e84191581de4356e88a10e33dd8
QO Engineering and Robotics Club <31809536+qoerc@users.noreply.github.com>
2017-12-04 11:52:29 -0500

    Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team7519Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team7519Teleop.java
@@ -12,7 +12,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class Team7519Teleop extends LinearOpMode{
 
     private CRServo testServo;
-    private DcMotor motorLift;
+    private DcMotor motorLift, leftFront, rightFront, leftRear, rightRear;
     int clawPosition=0;
     @Override
     public void runOpMode() throws InterruptedException
@@ -20,6 +20,10 @@ public class Team7519Teleop extends LinearOpMode{
 
         //Declare hardwareMap here, example below
         motorLift = hardwareMap.dcMotor.get("motorLift");
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        leftRear = hardwareMap.dcMotor.get("leftRear");
+        rightRear = hardwareMap.dcMotor.get("rightRear");
         testServo = hardwareMap.crservo.get("testServo");
 
 
@@ -30,6 +34,20 @@ public class Team7519Teleop extends LinearOpMode{
         {
 
             //Include commands to run on controller presses here
+
+            //Mecanum Drive
+            double hypot = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+
+            final double leftFrontPower = hypot * Math.cos(robotAngle) + gamepad1.right_stick_x;
+            final double rightFrontPower = hypot * Math.sin(robotAngle) - gamepad1.right_stick_x;
+            final double leftRearPower = hypot * Math.sin(robotAngle) + gamepad1.right_stick_x;
+            final double rightRearPower = hypot * Math.cos(robotAngle) - gamepad1.right_stick_x;
+
+            leftFront.setPower(leftFrontPower);
+            rightFront.setPower(rightFrontPower);
+            leftRear.setPower(leftRearPower);
+            rightRear.setPower(rightRearPower);
 
             //Manual Lift Control w/Trigger
             motorLift.setPower(gamepad1.right_trigger);
