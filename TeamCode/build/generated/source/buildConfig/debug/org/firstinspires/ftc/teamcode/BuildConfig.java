0ef05020443b241444037eb6060f1ca4d3b4c1b3
DESKTOP-H8TOJAT\boswo <bosworthrohan@gmail.com>
2017-12-03 18:17:04 -0800

    Fixing Source Tree

--- TeamCode/build/generated/source/buildConfig/debug/org/firstinspires/ftc/teamcode/BuildConfig.java
+++ TeamCode/build/generated/source/buildConfig/debug/org/firstinspires/ftc/teamcode/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 24;
-  public static final String VERSION_NAME = "3.4";
+  public static final int VERSION_CODE = 25;
+  public static final String VERSION_NAME = "3.5";
 }
