fbcf2c9ac55fa250fbf0edc8e82c26785b04ee13
AArJ <arjunjobe@me.com>
2017-12-07 00:57:10 -0800

    idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedTwoAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedTwoAuto.java
@@ -1,9 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Auto Red Two", group="Linear Auto")
 
