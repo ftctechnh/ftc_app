a7df88687440cab91a4cd76677a673181a284fe6
Gavin Uberti <gavin.uberti@gmail.com>
2017-12-09 16:58:09 -0800

    Move back farther on red

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompleteAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompleteAutonomous.java
@@ -193,6 +193,10 @@ public class CompleteAutonomous extends NullbotGemOnlyAutonomous {
             }
         }
 
+        if (robot.color == Alliance.RED && robot.startingPad == StartingPosition.BACK) {
+            driveTicks -= 100;
+        }
+    
         robot.frontLeft.setTargetPosition(robot.frontLeft.getCurrentPosition() + driveTicks);
         robot.backLeft.setTargetPosition(robot.backLeft.getCurrentPosition() - driveTicks);
         robot.frontRight.setTargetPosition(robot.frontRight.getCurrentPosition() - driveTicks);
