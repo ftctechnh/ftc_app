d2bfb4f842ef208a254935f5975ed7b116062951
keechio <keechidotty123@yahoo.com>
2017-12-10 13:54:11 -0800

    testing encoder

--- TeamCode/src/main/java/teamcode/driveStrightPossiblyFixed.java
+++ TeamCode/src/main/java/teamcode/driveStrightPossiblyFixed.java
@@ -3,6 +3,7 @@ package teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -64,27 +65,24 @@ public class driveStrightPossiblyFixed extends LinearOpMode {
         while (opModeIsActive() ) {
             elapsedTime = runtime.time();
             // on gamepad movement (controls robot wheel movment)
-            if (gamepad1.atRest()) {
-                turnOffMotors();
-            } else if(gamepad1.right_stick_y != 0 || gamepad1.right_stick_x != 0 || gamepad1.left_stick_x != 0) {
-                robotYSpeedRight = -gamepad1.right_stick_y;
-                robotXSpeedRight = gamepad1.right_stick_x/1.2;
-                drive(robotXSpeedRight, robotYSpeedRight);
-                turn(gamepad1.left_stick_x);
-            }
+            if(gamepad1.right_stick_y != 0 || gamepad1.right_stick_x != 0 ) {
+                drive(gamepad1.right_stick_x/1.2, -gamepad1.right_stick_y);
 
-            //Arm Height
-            if (gamepad1.a) {
-                armServo.setPosition(.8);
-            } else if (gamepad1.b) // middle arm position
+            }else if(gamepad1.left_stick_x != 0)
             {
-                armServo.setPosition(.432);
-            } else if (gamepad1.y) {
-                armServo.setPosition(.25);
-            } else if (gamepad1.dpad_down) {
-                lowerArm(armServo.getPosition());
-            } else if (gamepad1.dpad_up) {
-                liftArm(armServo.getPosition());
+                turn(gamepad1.left_stick_x);
+            }else if(gamepad1.a)
+            {
+                driveForwardDistance(1, 100);
+            }else if(gamepad1.b)
+            {
+                driveForwardDistance(1, 500);
+            }else if(gamepad1.x)
+            {
+                driveForwardDistance(1, 1000);
+            }else
+            {
+                turnOffMotors();
             }
 
             //Left to Right Movement
@@ -94,43 +92,6 @@ public class driveStrightPossiblyFixed extends LinearOpMode {
                 drive(1, 0); //drive right
             }
 
-            // The x button can only be pressed when the claw is not active.
-            if (gamepad1.x && !clawActive) // only allow toggling the claws every 100 loops
-            {
-                clawActive = true; // set the claw to active
-                clawActiveTimeInSec = runtime.time(); // get the time we set the claw to active
-
-                // toggles if the arm is clamping every time x is pressed
-                isClamped = !isClamped;
-                if (isClamped) {
-                    rightClampServo.setPosition(0);
-                } else {
-                    rightClampServo.setPosition(1);
-                }
-            }
-            else if (clawActive)
-            {
-                double currentTimeInSec = runtime.time();
-                if ((currentTimeInSec - clawActiveTimeInSec) > cooldown)
-                {
-                    clawActive = false;
-                }
-            }
-
-            if (gamepad1.dpad_down || gamepad1.dpad_right)
-            {
-                if (gamepad1.dpad_right)
-                {
-                    rightClampServo.setPosition(.7);
-                }
-                else if (gamepad1.dpad_left)
-                {
-                    closeClamp(rightClampServo.getPosition());
-                }
-            }
-
-            // Show the elapsed game time and wheel power
-
             updateTelemetry();
         }
     }
@@ -138,16 +99,9 @@ public class driveStrightPossiblyFixed extends LinearOpMode {
     public void updateTelemetry(){
 
         telemetry.addData("Status", "Run Time   : " + runtime.toString());
-        telemetry.addData("Arm servo position   : " , armServo.getPosition());
-        telemetry.addData("Right clamp position : " , rightClampServo.getPosition());
-        //telemetry.addData("x value right stick  : " , gamepad1.right_stick_x);
-        //telemetry.addData("y value right stick  : " , -gamepad1.right_stick_y);
-        //telemetry.addData("left motor power", motor2.getPower());
-        //telemetry.addData("right motor power", motor3.getPower());
-        //telemetry.addData("back motor power", motor1.getPower());
-        //telemetry.addData("left encoder", motor2.getCurrentPosition());
-        //telemetry.addData("right encoder", motor3.getCurrentPosition());
-        //telemetry.addData("back encoder", motor1.getCurrentPosition());
+        telemetry.addData("back wheel encoder values", motor1.getCurrentPosition());
+        telemetry.addData("left wheel encoder values", motor2.getCurrentPosition());
+        telemetry.addData("right wheel encoder values", motor3.getCurrentPosition());
         telemetry.update();
     }
     public double sigmoid(double x) {
@@ -155,6 +109,32 @@ public class driveStrightPossiblyFixed extends LinearOpMode {
     }
 
     //drive method that accepts two values, x and y motion
+
+    public void driveForwardDistance(double power, int distance)
+    {
+        //reset encoders
+        motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //set target position
+        motor1.setTargetPosition(distance);
+        motor2.setTargetPosition(distance);
+
+        motor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        drive(0,power);
+
+        while (motor2.isBusy() && motor3.isBusy())
+        {
+
+        }
+
+        turnOffMotors();
+        motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
 
     public void drive(double x, double y)
     {
@@ -187,30 +167,6 @@ public class driveStrightPossiblyFixed extends LinearOpMode {
         motor1.setPower(-speed/divisor);
         motor3.setPower(-speed/divisor);
         motor2.setPower(-speed/divisor);
-    }
-
-    private void closeClamp(double currentPosition)
-    {
-        double newPosition = currentPosition + .0005;
-        rightClampServo.setPosition(newPosition);
-    }
-
-    private  void openClamp(double currentPosition)
-    {
-        double newPosition = currentPosition - .0005;
-        rightClampServo.setPosition(newPosition);
-    }
-
-    private void liftArm(double currentPosition)
-    {
-        double newPosition = currentPosition - armLiftChange;
-        armServo.setPosition(newPosition);
-    }
-
-    private  void lowerArm(double currentPosition)
-    {
-        double newPosition = currentPosition + armLiftChange;
-        armServo.setPosition(newPosition);
     }
 
 }
