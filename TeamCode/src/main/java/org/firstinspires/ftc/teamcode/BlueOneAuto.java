f773b5da4d2a8c9cdc630ed28ae17ada63cb623f
Aaron <arjunjobe@me.com>
2017-12-12 12:29:30 -0800

    lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueOneAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueOneAuto.java
@@ -10,23 +10,7 @@ public class BlueOneAuto extends AutoMaster {
         robot.init(hardwareMap);
         waitForStart();
         telemetry.addData("skatin fast,", "eatin' ass");
-        encode(5, -0.5, MoveType.STRAIGHT);
-        robot.arm.setPosition(1);
-        wait(1000);
-        if (robot.color.red() > 1 ) {
-            encode(5, 0.25, MoveType.LATERALLY);
-        } else {
-            encode(5, -0.25, MoveType.LATERALLY);
-        }
-        robot.arm.setPosition(0);
-        encode(15, 0.5, MoveType.STRAIGHT);
-        encode(-19, 0.5, MoveType.ROT);
-        encode(42, 0.5, MoveType.STRAIGHT);
-        wait(500);
-        robot.gripper.setPower(-0.25);
-        wait(1000);
-        robot.gripper.setPower(0);
-        encode(2, -0.25, MoveType.STRAIGHT);
 
+        encodeInd(-0.25, MoveType.LATERALLY);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedOneAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedOneAuto.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 @Autonomous(name="Auto Red One", group="Linear Auto")
 
 public class RedOneAuto extends AutoMaster {
@@ -10,22 +12,8 @@ public class RedOneAuto extends AutoMaster {
         robot.init(hardwareMap);
         waitForStart();
         telemetry.addData("skatin fast,", "eatin' ass");
-        encode(5, -0.5, MoveType.STRAIGHT);
-        robot.arm.setPosition(1);
-        wait(1000);
-        if (robot.color.red() > 1) {
-            encode(5, -0.25, MoveType.LATERALLY);
-        } else {
-            encode(5, 0.25, MoveType.LATERALLY);
-        }
-        robot.arm.setPosition(0);
-        encode(15, 0.5, MoveType.STRAIGHT);
-        encode(20, 0.5, MoveType.ROT);
-        encode(40, 0.5, MoveType.STRAIGHT);
-        wait(500);
-        robot.gripper.setPower(-0.25);
-        wait(1000);
-        robot.gripper.setPower(0);
-        encode(2, -0.25, MoveType.STRAIGHT);
+        encodeInd(0.25, MoveType.LATERALLY);
+        while (robot.rangeSensor.getDistance(DistanceUnit.INCH) < 11) {}
+        robot.setAllDriveMotors(0);
     }
 }
