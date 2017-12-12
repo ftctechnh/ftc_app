8e500f512f2ee7839ba67ec3e16bef6a4e3a9921
Gavin Uberti <gavin.uberti@gmail.com>
2017-12-11 16:18:21 -0800

    Improve formatting and comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Demos/DriveWhileSpinning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Demos/DriveWhileSpinning.java
@@ -16,12 +16,15 @@ public class DriveWhileSpinning extends MainTeleOp {
     @Override
     public void runOpMode() {
         robot.init(hardwareMap, this, gamepad1, gamepad2);
+
+        // Enables hardware-controlled PID wheel speeds
         robot.setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
         while (opModeIsActive()) {
 
+            // Use the bumpers to compute a turn speed
             double turnSpeed = 0;
             if (gamepad1.left_bumper && !gamepad1.right_bumper) {
                 turnSpeed = 0.3;
@@ -32,10 +35,14 @@ public class DriveWhileSpinning extends MainTeleOp {
             double[] driveSpeeds;
 
             if (getLeftStickDist(gamepad1) > 0.15) {
-                double controllerAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) + Math.PI / 2;
+
+                double controllerAngle =
+                        Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) + Math.PI / 2;
                 controllerAngle = robot.normAngle(controllerAngle);
-                robot.updateReadings();
+
+                robot.updateReadings(); // Tells the robot to read from the IMU
                 double robotAngle = robot.normAngle(controllerAngle + robot.getGyroHeading());
+
                 driveSpeeds = robot.getDrivePowersFromAngle(robotAngle);
 
             } else {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Demos/SlewDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Demos/SlewDrive.java
@@ -22,20 +22,31 @@ public class SlewDrive extends MainTeleOp {
 
         while (opModeIsActive()) {
 
+
+            // Decide on a speed to turn, which by default is 35% of maximum speed capacity,
+            // but can be made faster or slower depending on the position of the right stick
             double turnDegree = gamepad1.right_stick_y * 0.15 + 0.35;
 
+            // Decide on a speed with which to move, depending on the position of our left joystick
+            // Since we could be turning with up to 50% of our robot's movement capacity, we need
+            // to ensure that we never use more than 50% for movement either
             double moveSpeed = gamepad1.left_stick_x * 0.5;
 
             double[] driveSpeeds = new double[]{0, 0, 0, 0};
 
-            if (getLeftStickDist(gamepad1) > 0.15) {
+            if (getLeftStickDist(gamepad1) > 0.15) { // If we should move at all
+
                 for (int i = 0; i < 4; i++) {
+                    // By default, the robot should move at the desired movement speed
                     driveSpeeds[i] = moveSpeed;
 
+                    // We need to negate motors 1 and 2 in order to move sideways
                     if (i == 1 || i == 2) {
                         driveSpeeds[i] *= -1;
                     }
 
+                    // Add the turn speed value to the left motors (with even motor numbers),
+                    // and subtract it from the right motors (with odd motor numbers)
                     if (i % 2 == 0) {
                         driveSpeeds[i] += turnDegree;
                     } else {
@@ -44,6 +55,8 @@ public class SlewDrive extends MainTeleOp {
 
                 }
             }
+
+            // Apply these speeds to the robot
             robot.setMotorSpeeds(driveSpeeds);
         }
     }
