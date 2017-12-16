d0cd4ef6b02aeeff9b82a23f9929da1bdb4bc937
Aaron <31283314+AArJ@users.noreply.github.com>
2017-12-15 21:15:52 -0800

    Update BlueOneAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueOneAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueOneAuto.java
@@ -10,7 +10,15 @@ public class BlueOneAuto extends AutoMaster {
         robot.init(hardwareMap);
         waitForStart();
         telemetry.addData("skatin fast,", "eatin' ass");
-
-        encodeInd(-0.25, MoveType.LATERALLY);
+        encodeInd(0.25, MoveType.LATERALLY);
+        while (robot.rangeSensor.getDistance(DistanceUnit.INCH) > 10.5) {}
+        robot.setallDriveMotors(0);
+        wait(1000);
+        robot.arm.setPosition(1);
+        if (robot.color.red() > 1) {
+            encode(5, -0.5, MoveType.STRAIGHT);
+        } else {
+            encode(5, 0.5, MoveType.STRAIGHT);
+        }
     }
 }
