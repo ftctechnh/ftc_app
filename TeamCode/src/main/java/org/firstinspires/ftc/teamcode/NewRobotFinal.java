eaae2241c95dd00496661d919c0422abac6b9d54
sahithithumuluri <sahithi.thumuluri@gmail.com>
2017-12-29 15:36:29 -0500

    updated lift value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewRobotFinal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewRobotFinal.java
@@ -644,7 +644,7 @@ public class NewRobotFinal
         else
         {
             wingMotor.setPower(1f);
-            while(wingMotor.getCurrentPosition() < 69)
+            while(wingMotor.getCurrentPosition() < 90)
             {
              //   if (System.currentTimeMillis() > endTime)
                  //   break;
