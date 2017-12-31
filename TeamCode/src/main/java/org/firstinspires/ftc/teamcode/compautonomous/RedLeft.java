f1c9a14fb48108ec058e5e6939c5b763a1c39b8c
Blabel <blakecabel@gmail.com>
2017-12-30 22:11:17 -0600

    Added jewels to the competition opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/compautonomous/RedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/compautonomous/RedLeft.java
@@ -1,10 +1,102 @@
 package org.firstinspires.ftc.teamcode.compautonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.teamcode.robotplus.autonomous.VuforiaWrapper;
+import org.firstinspires.ftc.teamcode.robotplus.hardware.ColorSensorWrapper;
+import org.firstinspires.ftc.teamcode.robotplus.hardware.IMUWrapper;
+import org.firstinspires.ftc.teamcode.robotplus.hardware.MecanumDrive;
+import org.firstinspires.ftc.teamcode.robotplus.hardware.Robot;
+
 /**
  * Created by Alex on 12/30/2017.
  */
 
 @Autonomous(name="RedLeft", group="compauto")
 public class RedLeft extends LinearOpMode {
+
+    private Robot robot;
+    private DcMotor raiser;
+    private Servo grabber;
+    private MecanumDrive drivetrain;
+    private IMUWrapper imuWrapper;
+    private VuforiaWrapper vuforiaWrapper;
+
+    private Servo armExtender;
+    private Servo armRotator;
+    private ColorSensorWrapper colorSensorWrapper;
+
+    private RelicRecoveryVuMark relicRecoveryVuMark;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //Initialize hardware
+        robot = new Robot(hardwareMap);
+        drivetrain = (MecanumDrive) robot.getDrivetrain();
+        raiser = hardwareMap.dcMotor.get("raiser");
+        grabber = hardwareMap.servo.get("grabber");
+        imuWrapper = new IMUWrapper(hardwareMap);
+        vuforiaWrapper = new VuforiaWrapper(hardwareMap);
+
+        //Assuming other hardware not yet on the robot
+        armRotator = hardwareMap.servo.get("armRotator");
+        armExtender = hardwareMap.servo.get("armExtender");
+
+        armRotator.scaleRange(0.1, 0.9);
+        armExtender.scaleRange(0.16, 0.75);
+
+        armExtender.setPosition(1.0);
+        armRotator.setPosition(0.5);
+
+        colorSensorWrapper = new ColorSensorWrapper(hardwareMap);
+
+        vuforiaWrapper.getLoader().getTrackables().activate();
+
+        waitForStart();
+
+        //STEP 1: Scan vuforia pattern
+        relicRecoveryVuMark = RelicRecoveryVuMark.from(vuforiaWrapper.getLoader().getRelicTemplate());
+
+        if (relicRecoveryVuMark != RelicRecoveryVuMark.UNKNOWN) {
+            telemetry.addData("VuMark Column", relicRecoveryVuMark.name());
+        } else {
+            telemetry.addData("VuMark Column", "borked");
+        }
+        telemetry.update();
+
+        //STEP 2: Hitting the jewel
+        armExtender.setPosition(0); //servo in 'out' position
+
+        sleep(2000);
+
+        telemetry.addData("Color Sensor", "R: %f \nB: %f ", colorSensorWrapper.getRGBValues()[0], colorSensorWrapper.getRGBValues()[2]);
+        //Checks that blue jewel is closer towards the cryptoboxes (assuming color sensor is facing forward
+        if (Math.abs(colorSensorWrapper.getRGBValues()[2] - colorSensorWrapper.getRGBValues()[0]) < 30) {
+            telemetry.addData("Jewels", "Too close.");
+        } else if (colorSensorWrapper.getRGBValues()[2] > colorSensorWrapper.getRGBValues()[0]) {
+            armRotator.setPosition(1);
+            telemetry.addData("Jewels", "Blue Team!");
+        } else {
+            armRotator.setPosition(0);
+            telemetry.addData("Jewels", "Red Team!");
+        }
+        telemetry.update();
+
+        sleep(1000);
+
+        armExtender.setPosition(1);
+        armRotator.setPosition(0.5);
+
+        sleep(1000);
+
+        imuWrapper.getIMU().initialize(imuWrapper.getIMU().getParameters());
+
+        sleep(1000);
+    }
 
 }
