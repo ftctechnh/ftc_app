ccde6bda404a35bfe6f3823e73ce8dd9a113326a
FTC Geared UP 6347 <ftcgearedup@gmail.com>
2017-12-31 15:37:08 -0500

    Added Gson + began JSON file for autonomous program values

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/seasons/relicrecovery/GsonTesting.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.seasons.relicrecovery;
+
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+/**
+ * Created by ftc6347 on 12/29/17.
+ */
+
+public class GsonTesting {
+
+    
+
+}
