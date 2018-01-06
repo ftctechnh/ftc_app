daf6c8de073d2428db28db766e6856e9142b5f48
KnifesmithCode <alex.knifesmith@gmail.com>
2018-01-06 11:48:06 -0600

    Implement the Iterative Op Mode instead because I like it more; Eliminate some warnings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveRev.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveRev.java
@@ -34,7 +34,6 @@ import android.hardware.Camera;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
@@ -43,17 +42,19 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
  * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
  * class is instantiated on the Robot Controller and executed.
- *
+ * <p>
  * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
  * It includes all the skeletal structure that all linear OpModes contain.
- *
+ * <p>
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Test Motors REV", group="Manual")
+@TeleOp(name = "Test Drive REV", group = "Manual")
 //@Disabled
-public class TestMotorsRev extends LinearOpMode {
+
+@SuppressWarnings("unused")
+public class TestDriveRev extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -70,7 +71,7 @@ public class TestMotorsRev extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "leftDrive");
+        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
         rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
 
         // Most robots need the motor on one side to be reversed to drive forward
@@ -79,11 +80,11 @@ public class TestMotorsRev extends LinearOpMode {
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         //Turn on the torch when the robot is ready
-         camera = Camera.open();
-         Camera.Parameters p = camera.getParameters();
-         p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-         camera.setParameters(p);
-         camera.startPreview();
+        camera = Camera.open();
+        Camera.Parameters p = camera.getParameters();
+        p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(p);
+        camera.startPreview();
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -107,12 +108,12 @@ public class TestMotorsRev extends LinearOpMode {
 
             // Tank Mode uses one stick to control each wheel.
             // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            leftPower  = gamepad1.left_stick_y * speedMultiplier;
+            leftPower = gamepad1.left_stick_y * speedMultiplier;
             rightPower = gamepad1.right_stick_y * speedMultiplier;
 
-            if(gamepad1.dpad_up && speedMultiplier + 0.0001 <= 1) {
+            if (gamepad1.dpad_up && speedMultiplier + 0.0001 <= 1) {
                 speedMultiplier += 0.0001;
-            } else if(gamepad1.dpad_down&& speedMultiplier - 0.0001 >= 0) {
+            } else if (gamepad1.dpad_down && speedMultiplier - 0.0001 >= 0) {
                 speedMultiplier -= 0.0001;
             }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveRevIterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDriveRevIterative.java
@@ -30,16 +30,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.hardware.Camera;
-import android.os.Debug;
-import android.util.Log;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 
 /**
@@ -47,111 +42,111 @@ import com.qualcomm.robotcore.util.Range;
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
  * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
  * class is instantiated on the Robot Controller and executed.
- *
+ * <p>
  * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
  * It includes all the skeletal structure that all linear OpModes contain.
- *
+ * <p>
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Test Motors", group="Manual")
+@TeleOp(name = "Test Drive REV", group = "Manual")
 //@Disabled
-public class TestMotors extends LinearOpMode {
+
+@SuppressWarnings("unused")
+public class TestDriveRevIterative extends OpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
-    private DcMotor clawMotor = null;
-    private Servo clawServo = null;
     private float speedMultiplier = 1.0f;
-    int updatesPerSecond;
-    Camera camera;
+    private Camera camera;
+    private Camera.Parameters p;
 
     @Override
-    public void runOpMode() {
+    public void init() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "leftDrive");
+        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
         rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
-        clawMotor = hardwareMap.get(DcMotor.class, "armHeight");
-        clawServo = hardwareMap.get(Servo.class, "clawGrip");
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
+    }
 
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
         //Turn on the torch when the robot is ready
-         camera = Camera.open();
-         Camera.Parameters p = camera.getParameters();
-         p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-         camera.setParameters(p);
-         camera.startPreview();
+        camera = Camera.open();
+        p = camera.getParameters();
+        p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(p);
+        camera.startPreview();
 
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
         runtime.reset();
+    }
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            updatesPerSecond++;
+    // run until the end of the match (driver presses STOP)
+    public void loop() {
+        // Setup a variable for each drive wheel to save power level for telemetry
+        double leftPower;
+        double rightPower;
 
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
-            double clawPower;
+        // Choose to drive using either Tank Mode, or POV Mode
+        // Comment out the method that's not used.  The default below is POV.
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
+        // POV Mode uses left stick to go forward, and right stick to turn.
+        // - This uses basic math to combine motions and is easier to drive straight.
+        //double drive = -gamepad1.left_stick_y;
+        //double turn  =  gamepad1.right_stick_x;
+        // leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+        // rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            //double drive = -gamepad1.left_stick_y;
-            //double turn  =  gamepad1.right_stick_x;
-            // leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            // rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        // Tank Mode uses one stick to control each wheel.
+        // - This requires no math, but it is hard to drive forward slowly and keep straight.
+        leftPower = gamepad1.left_stick_y * speedMultiplier;
+        rightPower = gamepad1.right_stick_y * speedMultiplier;
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            leftPower  = gamepad1.left_stick_y * speedMultiplier;
-            rightPower = gamepad1.right_stick_y * speedMultiplier;
-
-            if(gamepad1.dpad_up && speedMultiplier + 0.0001 <= 1) {
-                speedMultiplier += 0.0001;
-            } else if(gamepad1.dpad_down&& speedMultiplier - 0.0001 >= 0) {
-                speedMultiplier -= 0.0001;
-            }
-
-            clawPower = (gamepad1.right_trigger - gamepad1.left_trigger) / 2;
-
-            //Open or close the claw
-            if (gamepad1.right_bumper) {
-                clawServo.setPosition(2.0);
-            } else if (gamepad1.left_bumper) {
-                clawServo.setPosition(0.2);
-            }
-
-            // Send calculated power to wheels
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
-            clawMotor.setPower(clawPower);
-
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f), claw (%.2f)",
-                    leftPower, rightPower, clawPower);
-            telemetry.addData("Claw Position", Double.toString(clawServo.getPosition()));
-            telemetry.addData("Speed", Float.toString(speedMultiplier));
-            telemetry.addData("Updates Per Second", (updatesPerSecond / runtime.seconds()));
-            telemetry.update();
+        if (gamepad1.dpad_up && speedMultiplier + 0.0001 <= 1) {
+            speedMultiplier += 0.0001;
+        } else if (gamepad1.dpad_down && speedMultiplier - 0.0001 >= 0) {
+            speedMultiplier -= 0.0001;
         }
 
+        // Send calculated power to wheels
+        leftDrive.setPower(leftPower);
+        rightDrive.setPower(rightPower);
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)",
+                leftPower, rightPower);
+        telemetry.addData("Speed", Float.toString(speedMultiplier));
+        telemetry.update();
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
         //Turn off the torch when the robot is done
         camera = Camera.open();
         p = camera.getParameters();
