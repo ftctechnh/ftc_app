132e665a58a62274aae92a39ae1867e04ff10a9c
Harper Mckerrow <hmckerrow19@taboracademy.org>
2018-01-11 16:55:39 -0500

    SEND IT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlockGrabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlockGrabber.java
@@ -27,37 +27,30 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import java.lang.annotation.Target;
 
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+@TeleOp(name="BlockGrabber Linear OpMode", group="Linear Opmode")
 @Disabled
-public class BasicOpMode_Linear extends LinearOpMode {
+public class BlockGrabber extends LinearOpMode {
 
     // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+    private DcMotor GrabberMotor = null;
+    private Servo s1, s2 = null;
+
+    private int initial_angle = 0;
+    private int final_angle = 45;
+
 
     @Override
     public void runOpMode() {
@@ -67,48 +60,74 @@ public class BasicOpMode_Linear extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        GrabberMotor = hardwareMap.get(DcMotor.class, "Grabber1");
+        s1 = hardwareMap.get(Servo.class, "Servo1");
+        s2 = hardwareMap.get(Servo.class, "Servo2");
+
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        GrabberMotor.setDirection(DcMotor.Direction.FORWARD);
+        double initial_angle_value = Range.clip(initial_angle, 0,1 );
+        double final_angle_value = Range.clip(final_angle, 0,1 );
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        runtime.reset();
+
+        GrabberMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        GrabberMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        GrabberMotor.setTargetPosition(400);
+
+        GrabberMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
+            while (GrabberMotor.isBusy()){
+                GrabberMotor.setPower(-0.2);
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
+            }
+            GrabberMotor.setPower(0.0);
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+            s1.setPosition(initial_angle_value);
+            s2.setPosition(initial_angle_value);
+            boolean pressed = false;
+            if (gamepad1.x && !pressed){
+                s1.setPosition(final_angle_value);
+                s2.setPosition(final_angle_value);
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
+                GrabberMotor.setTargetPosition(400);
+                GrabberMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-            // Send calculated power to wheels
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
+                while (GrabberMotor.isBusy()){
+                    GrabberMotor.setPower(0.2);
 
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-            telemetry.update();
+                }
+                GrabberMotor.setPower(0.0);
+
+                pressed = true;
+            }
+            if (gamepad1.x && pressed) {
+                s1.setPosition(initial_angle_value);
+                s2.setPosition(initial_angle_value);
+
+                GrabberMotor.setTargetPosition(10);
+                GrabberMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                while (GrabberMotor.isBusy()){
+                    GrabberMotor.setPower(-0.2);
+
+                }
+                GrabberMotor.setPower(0.0);
+
+
+            }
+
+
+
         }
     }
 }
