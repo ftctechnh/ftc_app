9be35413f2b77129673bb978c63b6aa8f0f04a7f
Ftc Team 8045 <pleasanthillftc@gmail.com>
2018-01-10 21:44:17 -0800

    add image analysis method to jewel arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qualifier/JewelArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qualifier/JewelArm.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.Qualifier;
 
+import android.graphics.Bitmap;
+
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -11,6 +13,7 @@ public class JewelArm {
     public Servo jewelFlickerServo;
     public ColorSensor sensorColor;
     public DistanceSensor sensorDistance;
+    public double BRRatio;
     public double jewelarmup = 0.22;
 
     // hsvValues is an array that will hold the hue, saturation, and value information.
@@ -88,6 +91,36 @@ public class JewelArm {
 
 
     }
+
+    public boolean blueIsOnLeft(Bitmap rgbImage) {
+        // play images are 1280x720 w x h  origin is lower right looking at vuforia image,  Lower Left from camer pov
+        // x is vertical, y is horizontal (for portrait orientation)
+        int XStart = (int) ((double) rgbImage.getWidth() * 0.75);
+        int XEnd = (int) ((double) rgbImage.getWidth() * 1.0);
+
+        int yStart = (int) ((double) rgbImage.getHeight() * 0.0);
+        int yEnd = (int) ((double) rgbImage.getHeight() * 0.25);
+
+        int RedValue = 0;
+        int BlueValue = 0;
+
+        for (int x = XStart; x < XEnd; x++) {
+            for (int y = yStart; y < yEnd; y++) {
+                int pixel = rgbImage.getPixel(x, y);
+                RedValue += (pixel >> 16) & 0xff;
+                BlueValue += pixel & 0xff;
+            }
+        }
+        double BRratio = (double) BlueValue / RedValue;
+
+        if (BlueValue > RedValue)
+
+            return true;
+        else
+            return false;
+
+    }
+
 
 
 }
