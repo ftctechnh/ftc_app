83b5543b92afdc24eec8f9240b2fee538bd35ccf
Ben <erryche@gmail.com>
2018-01-12 19:59:30 -0500

    autonomous programs for quals

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorsOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorsOnly.java
@@ -1,8 +1,142 @@
+
 package org.firstinspires.ftc.teamcode;
 
-/**
- * Created by Ben on 1/12/18.
- */
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcontroller.external.samples.ConceptVuforiaNavigation;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
-public class SensorsOnly {
+
+@Autonomous(name="Preciousss: SensorsOnly", group="Preciousss")
+
+/*
+ * Created by Josie and Ben on 11/4/17.
+ *
+ */
+public class SensorsOnly extends LinearOpMode {
+
+
+    NormalizedColorSensor colorSensor;
+    static ModernRoboticsI2cGyro gyro;
+    boolean iAmBlue = false;
+    boolean iAmRed = true;
+    boolean isBoxSide = true;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    VuforiaLocalizer vuforia;
+    ModernRoboticsI2cRangeSensor rangeSensor;
+
+    @Override
+    public void runOpMode() {
+
+        // V u f o r i a  s e t u p
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = "Ac+mNz7/////AAAAGarZm7vF6EvSku/Zvonu0Dtf199jWYNFXcXymm3KQ4XngzMBntb" +
+                "NeMXt0qCPqACXugivtrYvwDU3VhMDRJwlwdMi4C2F6Su/8LZBrPIFtxUtr7MMagebQM/+4CSUIOQQdKNpdBttrX8yWM" +
+                "SrdyfnkNhh/vhXpQd7pXWwJ02UcnEVT1CiLeyTcl+bJUo1+xNonNaNEs8861zxmtO2TBtf9gyXhunlM6lpBJjC6nYWQ3" +
+                "BM2DOODFNz2EU3F3N1WxnOvCERQ+c934JKPajgCrNs5dquSo1wpcr0Kkf3u29hzK0DornR8s9j03g8Ea7q5cYN8WLn/e" +
+                "q1dUOFznng+6y2/7/fvw9wrzokOP9nP1QujkUN";
+
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT;
+        vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        VuforiaTrackables relicTrackables = vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+
+        relicTrackables.activate();
+
+        // H a r d w a r e   M a p p i n g
+
+
+
+        rangeSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "sensor_range");
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
+        if (colorSensor instanceof SwitchableLight) {
+            ((SwitchableLight) colorSensor).enableLight(true);
+        }
+
+        // S t a r t
+
+        waitForStart();
+
+        // J e w e l s
+
+        boolean autoClear = false;
+        telemetry.setAutoClear(autoClear);
+        telemetry.addLine("starting");
+        telemetry.update();
+
+
+        boolean iSeeBlue = false;
+        boolean iSeeRed = false;
+
+        NormalizedRGBA colors = colorSensor.getNormalizedColors();
+
+        telemetry.addLine()
+                .addData("r", "%.3f", colors.red)
+                .addData("b", "%.3f", colors.blue);
+
+        telemetry.update();
+
+        // R e a d   V u M a r k
+
+        RelicRecoveryVuMark currentVumark = ReadPicto(relicTemplate);
+        // Move off balance stone
+        telemetry.update();
+        /*telemetry.addData("raw ultrasonic", rangeSensor.rawUltrasonic());
+        telemetry.addData("raw optical", rangeSensor.rawOptical());
+        telemetry.addData("cm optical", "%.2f cm", rangeSensor.cmOptical());
+        telemetry.addData("cm", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM));
+        telemetry.update();*/
+    }
+
+
+
+    RelicRecoveryVuMark ReadPicto(VuforiaTrackable relicTemplate) {
+
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        return vuMark;
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
