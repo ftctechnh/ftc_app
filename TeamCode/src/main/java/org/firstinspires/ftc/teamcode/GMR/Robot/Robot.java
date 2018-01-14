9d8fc8bf1d64740c96a91bda5f750699e7dc4d72
Amoller655 <amoller655@gmail.com>
2018-01-14 14:46:12 -0500

    Added detectPictograph() Method and ColumnDetection subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GMR/Robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GMR/Robot/Robot.java
@@ -3,7 +3,13 @@ package org.firstinspires.ftc.teamcode.GMR.Robot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.teamcode.GMR.Robot.SubSystems.BlockLift;
+import org.firstinspires.ftc.teamcode.GMR.Robot.SubSystems.ColumnDetection;
 import org.firstinspires.ftc.teamcode.GMR.Robot.SubSystems.DriveTrain;
 import org.firstinspires.ftc.teamcode.GMR.Robot.SubSystems.RelicGrab;
 
@@ -20,6 +26,7 @@ public class Robot {
     public DriveTrain driveTrain;
     public BlockLift blockLift;
     public RelicGrab relicGrab;
+    public ColumnDetection columnDetection;
 
     private DcMotor leftFront;
     private DcMotor rightFront;
@@ -41,6 +48,12 @@ public class Robot {
 
     private Servo rightColor;
     private Servo leftColor;
+
+    private OpenGLMatrix lastLocation = null;
+    private VuforiaLocalizer vuforia;
+    private VuforiaTrackables relicTrackables;
+    private VuforiaTrackable relicTemplate;
+    private VuforiaLocalizer.Parameters parameters;
 
 
     public Robot (HardwareMap hardwareMap, Telemetry telemetry) {
@@ -65,6 +78,9 @@ public class Robot {
         relicTilt = hardwareMap.servo.get("relictilt");
         relicClamp = hardwareMap.servo.get("relicclamp");
 
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
         rightColor.setPosition(0);
         leftColor.setPosition(0.85);
 
@@ -73,6 +89,8 @@ public class Robot {
         blockLift = new BlockLift(liftMotor, topLeftGrab, topRightGrab, bottomLeftGrab, bottomRightGrab);
 
         relicGrab = new RelicGrab(relicLift, slideLift, relicTilt, relicClamp);
+
+        columnDetection = new ColumnDetection(vuforia, parameters, relicTrackables, relicTemplate);
 
         blockLift.clamp(false, true, false, false);
     }
