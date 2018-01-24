62585b239b9fd4f80f6010aca4d1eb46cc3938b2
Gavin Uberti <gavin.uberti@gmail.com>
2018-01-24 11:19:55 -0800

    Ultrasonic sensor simple test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Demos/ReadUltrasonicSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Demos/ReadUltrasonicSensors.java
@@ -1,27 +1,43 @@
 package org.firstinspires.ftc.teamcode.Demos;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.MainTeleOp;
-import org.firstinspires.ftc.teamcode.NullbotHardware;
+import org.firstinspires.ftc.teamcode.CompleteAutonomous;
 
 /**
  * Created by guberti on 1/16/2018.
  */
-@Autonomous(name="Test Ultrasonic Sensors", group="Demo")
-public class ReadUltrasonicSensors extends MainTeleOp {
-    private NullbotHardware robot = new NullbotHardware();
+@TeleOp(name="Test Ultrasonic Sensors", group="Demo")
+public class ReadUltrasonicSensors extends CompleteAutonomous {
 
     @Override
     public void runOpMode() {
         robot.init(hardwareMap, this, gamepad1, gamepad2);
-
+        robot.setDriveMode(DcMotor.RunMode.RUN_USING_ENCODER);
         waitForStart();
+        double heading = 0;
 
         while(opModeIsActive()) {
+            double speed = 0;
+            if (gamepad1.dpad_left) {
+                heading += Math.PI/2;
+                turnToPos(heading);
+                robot.sleep(500);
+            } else if (gamepad1.dpad_right) {
+                heading -= Math.PI/2;
+                turnToPos(heading);
+                robot.sleep(500);
+            } else if (gamepad1.dpad_up) {
+                speed = 1;
+            } else if (gamepad1.dpad_down) {
+                speed = -1;
+            }
+
+            robot.setMotorSpeeds(new double []{speed, speed, speed, speed});
+
             telemetry.addData("Front distance: ", robot.frontUltrasonic.getVoltage());
             telemetry.update();
-            robot.sleep(100);
         }
     }
 }
