c8e0142dba1fb6c90570971b2b74e66eca3c3071
Eduardo Lopez <18lopezrodase66@sanmateohigh.org>
2018-01-24 16:48:37 -0800

    big meme

---
+++ TeamCode/src/main/java/KyleAndEduardoGoSuperSaiyan.java
@@ -0,0 +1,14 @@
+/**
+ * Created by eduardo on 1/24/18.
+ */
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class KyleAndEduardoGoSuperSaiyan extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        waitForStart();
+    }
+}
