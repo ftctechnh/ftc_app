477d0150ec07ac7dd355ad5b47156569b1dd2d1b
dchretien <dchretien@theicongroup.com>
2018-01-28 17:06:29 -0500

    Working AutoPivotDumb accurate to .25 degrees, either way, turns left and right.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPivotDumb.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoPivotDumb.java
@@ -12,11 +12,14 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.SwitchableLight;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.Locale;
 
 
 @Autonomous(name="Preciousss: AutoPivotDumb", group="Preciousss")
@@ -84,6 +87,10 @@ public class AutoPivotDumb extends LinearOpMode {
             ((SwitchableLight) colorSensor).enableLight(true);
         }
 
+        // Set up our telemetry dashboard
+        composeTelemetry();
+
+
         // S t a r t
 
         waitForStart();
@@ -93,21 +100,21 @@ public class AutoPivotDumb extends LinearOpMode {
         telemetry.addLine("starting");
         telemetry.update();
 
-        pivotTo(90);
+        pivotTo(-90);
     }
 
     void pivotTo(int target) {
         //Pivot to counterclockwise is positive.
         //Pivot to clockwise is negative.
         double baseWheelPower = .35;
-        double minWheelPower = .15;
+        double minWheelPower = .2;
         double wheelPower = baseWheelPower;
-        float fudgeFactor = 5f;
+        float fudgeFactor = .25f;
         double dif = target;
 
         angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         double currentHeading = angles.firstAngle;
-        wheelPower = .15;
+        wheelPower = .3;
 
 
         while ((currentHeading < (target - fudgeFactor)) || (currentHeading > (target + fudgeFactor))) {
@@ -115,8 +122,9 @@ public class AutoPivotDumb extends LinearOpMode {
             if (angles != null) {
                 currentHeading = angles.firstAngle;
                 dif = (target - currentHeading);
+                telemetry.update();
                 //wheelPower = ( ( ( dif / target ) - minWheelPower ) * baseWheelPower ) + minWheelPower ;
-                if (target - currentHeading > 0) {
+                if (target - currentHeading < 0) {
                     motorFL.setPower(-wheelPower);
                     motorBL.setPower(-wheelPower);
                     motorFR.setPower(wheelPower);
@@ -128,6 +136,8 @@ public class AutoPivotDumb extends LinearOpMode {
                     motorBR.setPower(-wheelPower);
                 }
             }
+            //Wait(.01f);
+            //sR();
         }
 
         sR();
@@ -141,6 +151,93 @@ public class AutoPivotDumb extends LinearOpMode {
         motorFR.setPower(power);
         motorBR.setPower(power);
     }
+
+    //----------------------------------------------------------------------------------------------
+    // Telemetry Configuration
+    //----------------------------------------------------------------------------------------------
+
+    void composeTelemetry() {
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(new Runnable() { @Override public void run()
+        {
+            // Acquiring the angles is relatively expensive; we don't want
+            // to do that in each of the three items that need that info, as that's
+            // three times the necessary expense.
+            angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            gravity  = imu.getGravity();
+        }
+        });
+
+        telemetry.addLine()
+                .addData("status", new Func<String>() {
+                    @Override public String value() {
+                        return imu.getSystemStatus().toShortString();
+                    }
+                })
+                .addData("calib", new Func<String>() {
+                    @Override public String value() {
+                        return imu.getCalibrationStatus().toString();
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("heading", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.firstAngle);
+                    }
+                })
+                .addData("roll", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.secondAngle);
+                    }
+                })
+                .addData("pitch", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.thirdAngle);
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("grvty", new Func<String>() {
+                    @Override public String value() {
+                        return gravity.toString();
+                    }
+                })
+                .addData("mag", new Func<String>() {
+                    @Override public String value() {
+                        return String.format(Locale.getDefault(), "%.3f",
+                                Math.sqrt(gravity.xAccel*gravity.xAccel
+                                        + gravity.yAccel*gravity.yAccel
+                                        + gravity.zAccel*gravity.zAccel));
+                    }
+                });
+    }
+
+
+
+    //----------------------------------------------------------------------------------------------
+    // Formatting
+    //----------------------------------------------------------------------------------------------
+
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+
+    void Wait(double WaitTime) {
+        runtime.reset();
+        while (runtime.seconds() < WaitTime) {
+            //Comment this out to avoid it overwriting other telemetry
+            //telemetry.addData("5", " %2.5f S Elapsed", runtime.seconds());
+            //telemetry.update();
+        }
+    }
+
 
 }
 
