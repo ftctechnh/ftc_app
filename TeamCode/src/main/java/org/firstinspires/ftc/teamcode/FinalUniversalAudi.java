7076064341e47bd2ec76cd9bca37759fb504efe9
sahithithumuluri <sahithi.thumuluri@gmail.com>
2018-01-31 18:52:35 -0500

    added turns to end of autonomus

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalUniversalAudi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FinalUniversalAudi.java
@@ -68,8 +68,8 @@ public class FinalUniversalAudi extends LinearOpMode
                 newRobot.oldMoveLift(-1);
                 newRobot.openOrCloseDoor(false);
                 newRobot.driveStraight_In_Stall(14, .25);
-                //newRobot.driveStraight_In(3,.2);
                 newRobot.driveStraight_In(-6,1);
+                newRobot.pivot(-180,.7);
                 newRobot.stopAllMotors();
                 break;
             case 'r':
@@ -118,6 +118,7 @@ public class FinalUniversalAudi extends LinearOpMode
                 newRobot.openOrCloseDoor(false);
                 newRobot.driveStraight_In_Stall(16,.25);
                 newRobot.driveStraight_In(-6);
+                newRobot.pivot(180,.7);
                 newRobot.stopAllMotors();
                 break;
             default:
