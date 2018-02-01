cb3bb1df5ad12c15fcb14a40152b10e46b69c592
KadenGordon <kaden.m.gordon@gmail.com>
2018-02-01 16:17:25 -0700

    Added findJewel function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelArm.java
@@ -18,6 +18,9 @@ public class JewelArm {
     private final double UP_POSITION = 0.035;
     private final double RIGHT_POSITION = 1;
     private final double LEFT_POSITION = 0;
+    private final double MIDDLE_POSITION = 0.5;
+    private final double TRY_AGAIN_POSITION = 0.35;
+    private final double MIN_COLOR_DETECTION_THRESHOLD = 5;
     private Telemetry telemetry;
 
     public JewelArm(Servo upDownServo, Servo endServo, ColorSensor cs, Telemetry telemetry) {
@@ -49,9 +52,56 @@ public class JewelArm {
         up();
         cs.enableLed(true);
     }
-
-    public void loop() {
+    public void findJewel(Color allianceColor) {
+        down();
+        ElapsedTime time = new ElapsedTime();
+        time.start();
+        boolean tryAgain = false;
+        while(cs.red() < MIN_COLOR_DETECTION_THRESHOLD && cs.blue() < MIN_COLOR_DETECTION_THRESHOLD) {
+            if(time.getElapsedTime() > 3000) {
+                tryAgain = true;
+                break;
+            }
+        }
+        if (tryAgain) {
+            setEndPosition(TRY_AGAIN_POSITION);
+            time.start();
+            while (cs.red() < MIN_COLOR_DETECTION_THRESHOLD && cs.blue() < MIN_COLOR_DETECTION_THRESHOLD) {
+                if(time.getElapsedTime() > 3000) {
+                    return;
+                }
+            }
+        }
+        int red = 0, blue = 0;
+        for (int i=0; i<5; i++) {
+            if (cs.red() > cs.blue()) {
+                red++;
+            }
+            else if (cs.blue()>cs.red()) {
+                blue++;
+            }
+        }
+        if(allianceColor == Color.RED) {
+          if (red > blue) {
+            right();
+          }
+          else if (blue > red) {
+            left();
+          }
+        }
+        if(allianceColor == Color.BLUE) {
+          if (blue > red) {
+            right();
+          }
+          else if (red > blue) {
+            left();
+          }
+        }
+        sleep(750);
+        setEndPosition(MIDDLE_POSITION);
+        up();
     }
+
     public void setUpDownPosition(double postion) {
         upDownServo.setPosition(postion);
         upDownServo.setPosition(postion);
