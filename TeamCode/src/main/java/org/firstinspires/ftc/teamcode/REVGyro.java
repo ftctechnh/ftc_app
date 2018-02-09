7c793dbcf8beaebc0e952bc90c8baaf5a4f8ee25
KadenGordon <kaden.m.gordon@gmail.com>
2018-02-09 09:18:03 -0700

    didn't need that import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/REVGyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/REVGyro.java
@@ -5,7 +5,6 @@ package org.firstinspires.ftc.teamcode;
  */
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.robotcore.util.*;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
