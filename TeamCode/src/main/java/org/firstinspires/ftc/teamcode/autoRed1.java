f0ffe60265f33545ae0c893cd56c199e869a54e3
gjf <glenda.jeffrey@alumni.purdue.edu>
2018-02-08 21:49:54 -0500

    Subclass for autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoRed1.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/**
+ * Created by GJF on 1/28/2018.
+ */
+@Autonomous(name="Preciousss: AutoRed1", group="Preciousss")
+
+class autoRed1 extends superAuto {
+
+    public void runOpMode() {
+
+        iAmRed = true;
+
+        configureGyro();
+
+        mapHardware();
+
+        composeTelemetry();
+
+        waitForStart();
+
+        pivotTo(-90);
+    }
+}
