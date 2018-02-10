3ed8f46ebe6214a7be3f5f8d5fe4af12b55a72c8
Ansh Nanda <anshnanda10@gmail.com>
2018-02-10 17:33:59 +0530

    fixed driving direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumTrigo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumTrigo.java
@@ -17,6 +17,7 @@ public class mecanumTrigo extends LinearOpMode
     private DcMotor motorFrontRight;
     private DcMotor motorBackRight;
 
+
     @Override
     public void runOpMode() throws InterruptedException
     {
@@ -32,18 +33,35 @@ public class mecanumTrigo extends LinearOpMode
 
         while(opModeIsActive())
         {
+//            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+//            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+//            double rightX = gamepad1.right_stick_x;
+//            final double v1 = r * Math.cos(robotAngle) + rightX;
+//            final double v2 = r * Math.sin(robotAngle) - rightX;
+//            final double v3 = r * Math.sin(robotAngle) + rightX;
+//            final double v4 = r * Math.cos(robotAngle) - rightX;
+
             double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+            telemetry.addData("r = ", r);
+            double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+            telemetry.addData("robotAngle = ", robotAngle);
             double rightX = gamepad1.right_stick_x;
+            telemetry.addData("rightX = ", rightX);
             final double v1 = r * Math.cos(robotAngle) + rightX;
+            telemetry.addData("front left power = ", v1);
             final double v2 = r * Math.sin(robotAngle) - rightX;
+            telemetry.addData("front right power = ", v2);
             final double v3 = r * Math.sin(robotAngle) + rightX;
+            telemetry.addData("back left power = ", v3);
             final double v4 = r * Math.cos(robotAngle) - rightX;
+            telemetry.addData("back right power = ", v4);
 
             motorFrontLeft.setPower(v1);
             motorFrontRight.setPower(v2);
-            motorBackRight.setPower(v4);
             motorBackLeft.setPower(v3);
+            motorBackRight.setPower(v4);
+
+            telemetry.update();
 
             idle();
         }
