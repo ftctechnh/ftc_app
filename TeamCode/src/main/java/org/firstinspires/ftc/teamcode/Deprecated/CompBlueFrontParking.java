89faec3f016a8b50c3f63932ff4c90928b2af1e2
Gavin Uberti <gavin.uberti@gmail.com>
2018-02-10 19:13:55 -0800

    Fix build failing due to missing import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Deprecated/CompBlueFrontParking.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Deprecated/CompBlueFrontParking.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Deprecated;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
