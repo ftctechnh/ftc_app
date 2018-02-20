8d69b7185896e84a240f336b16337239b98d5c1e
KadenGordon <kaden.m.gordon@gmail.com>
2018-02-20 16:54:56 -0700

    got stuff ready for Austin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoGlyphs.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoGlyphs.java
@@ -1,11 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.disnodeteam.dogecv.CameraViewDisplay;
 import com.disnodeteam.dogecv.detectors.GlyphDetector;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 /**
  * Created by Aus on 2/20/18.
  */
 
 public class AutoGlyphs extends GlyphDetector {
-    
+    public AutoGlyphs(HardwareMap hardwareMap, Telemetry telemetry) {
+        super();
+        init(hardwareMap.appContext, CameraViewDisplay.getInstance());
+        minScore = 1;
+        downScaleFactor = 0.3;
+        speed = GlyphDetector.GlyphDetectionSpeed.SLOW;
+    }
+    public void enable() {
+        enable();
+    }
+    public void thing() {
+
+    }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoGlyphsTest.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by root on 2/20/18.
+ */
+@TeleOp(name="testing auto glyphs stuff", group="test")
+public class AutoGlyphsTest {
+    AutoGlyphs autoGlyphs;
+    public void init() {
+        autoGlyphs.enable();
+    }
+}
