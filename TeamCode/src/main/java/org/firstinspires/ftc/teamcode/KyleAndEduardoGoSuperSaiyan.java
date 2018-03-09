98f5dab18828fab10d22666dc0df2e5a64c28049
Klye Zhang <zkyle1337@gmail.com>
2018-03-08 16:05:02 -0800

    am i supposed to be productive
    not sure whats going on

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KyleAndEduardoGoSuperSaiyan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KyleAndEduardoGoSuperSaiyan.java
@@ -1,3 +1,4 @@
+//TO DO: better documentation and commenting lol xd kill me
 /* Copyright (c) 2017 FIRST. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
