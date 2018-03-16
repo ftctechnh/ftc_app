a5a326c63d614288dec1bb2868e373e53cf29237
josie <jelly1667@gmail.com>
2018-03-16 18:38:25 -0400

    MoRe TwEaKs...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoPlay.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoPlay.java
@@ -8,15 +8,6 @@ public class autoPlay extends superAuto {
     public void runOpMode() {
         iAmRed = true;
         setUp();
-        //jewel();
-        setUpVuforia();
-       // Wait(3d);
-       // followHeading(0, .47f, 0f, -0.25f);
-       // Wait(3);
-        //pivotTo(-90);
-        Wait(3);
-        cryptoState(0,-.375f, 0f);
-        telemetry.addData("We have gotten past findCrypto", iAmRed);
-        flip();
+        distCorrector(23);
     }
 }
