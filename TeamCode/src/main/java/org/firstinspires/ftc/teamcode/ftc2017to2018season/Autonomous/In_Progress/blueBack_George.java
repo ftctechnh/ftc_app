21d1f7c9358a6d45de31d26b4dc86806271b7e5a
Rohan Lloyd Bosworth <bosworthrohan@gmail.com>
2018-03-29 20:14:10 -0700

    Experimenting with new concepts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc2017to2018season/Autonomous/In_Progress/blueBack_George.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc2017to2018season/Autonomous/In_Progress/blueBack_George.java
@@ -8,6 +8,7 @@ when                                      who                       Purpose/Chan
 3/28/18                                   Rohan                   On line 65 I changed the function value of centimeters for the moveDownGlyph() functions as it was moving down to far causing the program to crash.
 3/28/18                                   Rohan                   Added new object called jewelDectector1 which calls on DogeCV_JewelDetector. Added on line 88 jewelDectector1.init(); which initializes OpenCV. The other lines of initiation are deleted.  Added a sleep afterward as to give time for the initiation.
 3/28/18                                   Rohan                   Deleted the call on init in line 88 as it caused numerous other problems in the program. Went back to old activations methods.
+3/28/18                                   Rohan                   Added a getCurrentOrder function
 
 =============================================================================================================================================*/
 package org.firstinspires.ftc.teamcode.ftc2017to2018season.Autonomous.In_Progress;
@@ -110,6 +111,8 @@ public class blueBack_George extends Autonomous_General_George_ {
         jewelDetector1.minArea = 700;
 
         jewelDetector1.enable();
+
+        jewelDetector1.getCurrentOrder();
        sleep(500);
         switch (jewelDetector1.getCurrentOrder()){
             case BLUE_RED:
