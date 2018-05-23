c632f33f877ca735cfa68782396fb3dc1f988f45
gm_sx2 <shivaansood@gmail.com>
2018-05-23 15:45:46 +0530

    midwork aryaman bs
    commit to show aryaman how it works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewFinal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewFinal.java
@@ -81,6 +81,8 @@ public class NewFinal extends LinearOpMode {
         waitForStart();
 
         while(opModeIsActive()) {
+
+            //----------DRIVING----------
             double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
             telemetry.addData("r: ", r);
             double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x);
@@ -100,6 +102,85 @@ public class NewFinal extends LinearOpMode {
             motorFrontRight.setPower(v2);
             motorBackRight.setPower(v4);
             motorBackLeft.setPower(v3);
+
+            //----------SECONDARY----------
+
+            //MODE SWITCH
+            if (gamepad1.x) {
+                mode = 1;
+            }
+            if (gamepad1.x) {
+                mode = 2;
+            }
+
+            //MODE-1
+            if (mode == 1) {
+                //GRABBER <UP>
+                if (gamepad2.dpad_up && !gamepad2.dpad_down) {
+                    grabMotor.setPower(gamepad2.right_trigger);
+                }
+                //GRABBER <DOWN>
+                if (!gamepad2.dpad_up && gamepad2.dpad_down) {
+                    grabMotor.setPower(-gamepad2.right_trigger);
+                }
+                //GRABBER <STOP>
+                if (!gamepad2.dpad_up && !gamepad2.dpad_down) {
+                    grabMotor.setPower(0);
+                }
+                //GRABBER UPPER LIMIT
+                if (gamepad2.dpad_up) {
+                    while (grabMotor.getCurrentPosition() > grabberTop) {
+                        grabMotor.setPower(0.7);
+                    }
+                    grabMotor.setPower(0);
+                }
+                //GRABBER LOWER LIMIT
+                if (gamepad2.dpad_down) {
+                    while (grabMotor.getCurrentPosition() > grabberMiddle) {
+                        grabMotor.setPower(0.7);
+                    }
+                    grabMotor.setPower(0);
+                }
+//                if (gamepad2.a) {
+//                    while (grabMotor.getCurrentPosition() >grabberRest) {
+//                        grabMotor.setPower(0.7);
+//                    }
+//                    grabMotor.setPower(0);
+//                }
+                //GRABBER POSITION
+                telemetry.addData("Grab Motor Encoder: ", grabMotor.getCurrentPosition());
+
+                //TOP GRAB <CLOSE>
+                if (gamepad2.a) {
+
+                }
+                //TOP GRAB <OPEN>
+                if (gamepad2.b) {
+
+                }
+                //BOTTOM GRAB <CLOSE>
+                if (gamepad2.left_bumper) {
+
+                }
+                //BOTTOM GRAB <EXTREME CLOSE>
+                if (gamepad2.left_stick_button) {
+
+                }
+                //BOTTOM GRAB <OPEN>
+                if (gamepad2.right_bumper) {
+
+                }
+                //BOTTOM GRAB <EXTREME OPEN>
+                if (gamepad2.right_stick_button) {
+
+                }
+            }
+
+            //MODE-2
+            if (mode == 2) {
+                
+            }
+
         }
     }
 
