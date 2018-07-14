384c274f02507ede999c5d1c6dc1f0df5600a73f
trent <sellerstj@bellsouth.net>
2018-07-14 00:04:33 -0400

    Update to Gradle

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class TestOpMode extends LinearOpMode
+{
+    // OpMode commands will go here
+
+    @Override
+    public void runOpMode() {
+        // This method is required as LinearOpMode defines it as abstract
+    }
+}
