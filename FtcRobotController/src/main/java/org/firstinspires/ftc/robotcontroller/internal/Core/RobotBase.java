07d4fcd2c4a6aa50c58f983902b61fedc59d7e19
Unknown <team4964rollapatriots@gmail.com>
2018-08-08 23:46:06 -0500

    Fix a few typos --Pranal

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Core/RobotBase.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Core/RobotBase.java
@@ -2,25 +2,26 @@ package org.firstinspires.ftc.robotcontroller.internal.Core;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
 /**
  *
  * Team 4964 Rolla Patriots
  *
- * Authors: Joel Schott
+ * Authors: Joel Schott and now Pranal Madria ;)
  * Created on 8/8/2018.
  */
 
 public abstract class RobotBase {
 
-    public RobotBase (final HardWareMap hwm, final LinearOpMode op){
+    public RobotBase (final HardwareMap hwm, final LinearOpMode op){
         hardWareMap = hwm;
         opMode = op;
     }
 
-    private HardWareMap hardWareMap;
+    private HardwareMap hardWareMap;
     private LinearOpMode opMode;
 
-    public HardWareMap getRobotHardwareMap(){
+    public HardwareMap getRobotHardwareMap(){
         return hardWareMap;
     }
     public LinearOpMode getRobotOpMode(){
