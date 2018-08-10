062f0a33ad9598fbcc37e652f16184f48aa200f2
Benjamin <1025403@apps.nsd.org>
2018-08-09 18:31:37 -0700

    Test programs

--- TeamCode/src/main/java/teamcode/demoRobotBenjamin.java
+++ TeamCode/src/main/java/teamcode/demoRobotBenjamin.java
@@ -1,64 +1,99 @@
 package teamcode;
 
+//import these things to make the code work
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-
+//the name of the Op Mode in the Driver's Station
 @TeleOp(name="demoRobotBenjamin", group="Linear Opmode")
+
+//okay now onto the class finally
 public class demoRobotBenjamin extends LinearOpMode {
-    private DcMotor motorLeft;
-    private DcMotor motorRight;
-    private Servo armServo;
-
-    private void initialize() {
-
-        motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
-        motorRight = hardwareMap.get(DcMotor.class, "motorRight");
 
 
-        armServo = hardwareMap.get(Servo.class, "armServo");
-
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-
-    }
-
+    // declaring hardware
+    // variable type, Hardware type, variable name
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor motor0;
+    private DcMotor motor1;
+    private Servo servo0;
 
     @Override
     public void runOpMode() {
 
+        //run the initialize block
         this.initialize();
 
-        // run until the end of the match (driver presses STOP)
-        double tgtPowerLeft = 0;
-        double tgtPowerRight = 0;
-        while (opModeIsActive()) {
-            tgtPowerLeft = -this.gamepad1.left_stick_y;
-            tgtPowerRight = -this.gamepad1.right_stick_y;
-            motorLeft.setPower(tgtPowerLeft);
-            motorRight.setPower(tgtPowerRight);
-            //check to see if we need to move the servo.
-            if(gamepad1.y) {
-                //move to 0 degrees
-                armServo.setPosition(0);
-            } else if (gamepad1.x){
-                //move to 90 degrees
-                armServo.setPosition(0.5);
-            } else if (gamepad1.a) {
-                //move to 180 degrees
-                armServo.setPosition(1);
-            } else if (gamepad1.b) {
-                //move to -90 degrees
-                armServo.setPosition(-0.5);
+        waitForStart();
+        runtime.reset();
+
+        while(opModeIsActive()){
+
+            //double is a variable type that supports decimals
+            double leftPower;
+            double rightPower;
+            double drive = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+            double armPosition = servo0.getPosition();
+
+
+            if (drive !=0){
+                leftPower = (-drive/2);
+                rightPower = (-drive/2);
+                //if drive is not 0, set both motor powers to the value of drive
+            }
+            else if (turn !=0){
+                leftPower = (drive - (turn/2));
+                rightPower = (drive + (turn/2));
+                //if the drive if reports false, it runs this turn block
+            }
+            else {
+                leftPower = 0;
+                rightPower = 0;
+                //if neither of these if statements report true it sets the motor powers to 0
+                // if you don't push a stick, the robot don't move
+            }
+            this.motor0.setPower(leftPower);
+            this.motor1.setPower(rightPower);
+
+            //Arm servo control
+            if (gamepad1.left_bumper) {
+                if (armPosition != 0) {
+                    servo0.setPosition(armPosition - 0.05);
+                }
+            }
+            else if (gamepad1.right_bumper) {
+                if (armPosition != 1) {
+                    servo0.setPosition(armPosition + 0.05);
+                }
             }
 
-            telemetry.addData("Status", "Running");
-            telemetry.update();
         }
+
     }
+
+
+    private void initialize(){
+
+        //giving internal hardware an external name for the app config
+        //also initializing the hardware?
+        this.motor0 = hardwareMap.get (DcMotor.class,"Left Motor");
+        this.motor1 = hardwareMap.get (DcMotor.class, "Right Motor");
+        this.servo0 = hardwareMap.get (Servo.class, "Arm Servo");
+
+        // makes the motors that face away from each other move the same direction
+        this.motor0.setDirection(DcMotor.Direction.FORWARD);
+        this.motor1.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Status", "Online");
+        telemetry.update();
+    }
+
+
+    //code under here
+
 
 }
---
+++ TeamCode/src/main/java/teamcode/demoRobotColorBenjamin.java
@@ -0,0 +1,49 @@
+package teamcode;
+
+//import these things to make the code work
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//the name of the Op Mode in the Driver's Station
+@Autonomous(name= "demoRobotColorBenjamin", group= "Linear Opmode")
+
+//okay now onto the class finally
+public class demoRobotColorBenjamin extends LinearOpMode {
+
+    // declaring hardware
+    // variable type, Hardware type, variable name
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor motor0;
+    private DcMotor motor1;
+    private Servo servo0;
+
+    @Override
+    public void runOpMode() {
+
+        //run the initialize block
+        this.initialize();
+
+        waitForStart();
+        runtime.reset();
+    }
+
+    private void initialize(){
+
+        //giving internal hardware an external name for the app config
+        //also initializing the hardware?
+        this.motor0 = hardwareMap.get (DcMotor.class,"Left Motor");
+        this.motor1 = hardwareMap.get (DcMotor.class, "Right Motor");
+        this.servo0 = hardwareMap.get (Servo.class, "Arm Servo");
+
+        // makes the motors that face away from each other move the same direction
+        this.motor0.setDirection(DcMotor.Direction.FORWARD);
+        this.motor1.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Status", "Online");
+        telemetry.update();
+    }
+
+}
