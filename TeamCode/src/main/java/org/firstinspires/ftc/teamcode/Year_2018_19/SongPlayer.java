6e8e2d5a25f661c116a0ad8ad2eb072b3dbc4056
abbajabalroba <abbajabal.roba@gmail.com>
2018-08-11 13:04:41 -0500

    Programmed robot to play R2D2 andd BB8 sounds on init and startup! Haven't been tested yet.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Year_2018_19/SongPlayer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Year_2018_19/SongPlayer.java
@@ -6,20 +6,32 @@ import org.firstinspires.ftc.teamcode.R;
 import java.io.IOException;
 
 public class SongPlayer {
-    //The player handling the audio
+
     private static MediaPlayer mediaPlayer = null;
-    //Start the wubs
-    public static void start(Context context) {
+
+    public static void playMusic(Context context) {
         if (mediaPlayer == null) mediaPlayer = MediaPlayer.create(context, R.raw.music);
         mediaPlayer.seekTo(0);
         mediaPlayer.start();
     }
-    //Stop the wubs
-    public static void stop() {
+
+    public static void stopMusic() {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
             try { mediaPlayer.prepare(); }
             catch (IOException e) {}
         }
+    }
+
+    public static void playBB8Sound(Context context) {
+        if (mediaPlayer == null) mediaPlayer = MediaPlayer.create(context, R.raw.BB8);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.start();
+    }
+
+    public static void playR2D2Sound (Context context) {
+        if (mediaPlayer == null) mediaPlayer = MediaPlayer.create(context, R.raw.R2D2);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.start();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Year_2018_19/TeleOp/TileRunnerTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Year_2018_19/TeleOp/TileRunnerTeleOp.java
@@ -1,17 +1,11 @@
 package org.firstinspires.ftc.teamcode.Year_2018_19.TeleOp;
 
+import com.qualcomm.ftccommon.SoundPlayer;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.teamcode.R;
 import org.firstinspires.ftc.teamcode.Year_2018_19.Robot.TileRunnerRobotHardware;
 import org.firstinspires.ftc.teamcode.Year_2018_19.SongPlayer;
-
-import android.content.Context;
-import android.media.MediaPlayer;
-
-import java.io.IOException;
-import java.util.Timer;
 
 @TeleOp(name="TileRunnerTeleOp", group="TeleOpMode")
 
@@ -19,13 +13,12 @@ public class TileRunnerTeleOp extends OpMode
 {
     //Creates robot hardware.
     TileRunnerRobotHardware robot = new TileRunnerRobotHardware();
-    private boolean pressedLast;
 
     //Robot initiates hardware and components.
     public void init()
     {
         robot.init(hardwareMap);
-        this.pressedLast = false;
+        SongPlayer.playR2D2Sound(this.hardwareMap.appContext);
         telemetry.addData("Status", "Robot has initiated!");
         telemetry.update();
     }
@@ -33,6 +26,7 @@ public class TileRunnerTeleOp extends OpMode
     //Robot starts.
     public void start()
     {
+        SongPlayer.playBB8Sound(this.hardwareMap.appContext);
         telemetry.addData("Status", "Robot has started!");
         telemetry.update();
     }
@@ -54,48 +48,18 @@ public class TileRunnerTeleOp extends OpMode
             robot.flagServo.setPosition(0.3);
         }
         if (this.gamepad1.x) {
-            SongPlayer.start(this.hardwareMap.appContext);
+            SongPlayer.playMusic(this.hardwareMap.appContext);
         }
         else if (gamepad1.y) {
-            SongPlayer.stop();
+            SongPlayer.stopMusic();
         }
     }
 
     //Robot ends.
     public void stop()
     {
-        SongPlayer.stop();
+        SongPlayer.stopMusic();
         telemetry.addData("Status", "Robot has stopped!");
         telemetry.update();
     }
-
-    public void FlagWave() throws InterruptedException
-    {
-        Timer timer = new Timer();
-        
-        Thread.sleep(1000);
-        robot.flagServo.setPosition(0.7);
-        Thread.sleep(1000);
-        robot.flagServo.setPosition(0.3);
-    }
 }
-
-/*public class SoundOpMode extends OpMode {
-    private boolean pressedLast;
-    public void init() {
-        this.pressedLast = false;
-    }
-    public void loop() {
-        if (this.gamepad1.a && !this.pressedLast) {
-            SongPlayer.start(this.hardwareMap.appContext);
-            this.pressedLast = true;
-        }
-        else if (!this.gamepad1.a && this.pressedLast) {
-            SongPlayer.stop();
-            this.pressedLast = false;
-        }
-    }
-    public void stop() {
-        SongPlayer.stop();
-    }
-}*/
