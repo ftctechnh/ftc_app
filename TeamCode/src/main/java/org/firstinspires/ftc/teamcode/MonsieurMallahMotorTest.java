96323f16eecd2f10586f60fa45b4dcb05e0f9473
coolguybri <bri@coolguybri.com>
2018-09-16 01:01:52 -0700

    gyroscope code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MonsieurMallahMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MonsieurMallahMotorTest.java
@@ -1,44 +1,78 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Axis;
+import org.firstinspires.ftc.robotcore.external.navigation.MagneticFlux;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Temperature;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import java.util.Iterator;
+import java.util.Set;
 
 @TeleOp(name="Monsieur Mallah Motor OpMode", group="Iterative Opmode")
 public class MonsieurMallahMotorTest extends OpMode {
-    // Declare OpMode members.
+    // Elapsed time since the opmode started.
     private ElapsedTime runtime = new ElapsedTime();
 
-    private Gyroscope imu;
+    // Internal Gyroscope in the Rev Hub.
+    private BNO055IMU bosch;
+
+    // Motors connected to the hub.
     private DcMotor motor0;
     private DcMotor motor1;
     private DcMotor motor2;
     private DcMotor motor3;
+
+    // Hack stuff.
+    private boolean runMotors = false;
 
     /*
      * Code to run ONCE when the driver hits INIT
      */
     @Override
     public void init() {
-        telemetry.addData("Status", "Initialized");
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        imu = hardwareMap.get(Gyroscope.class, "imu");
-        motor0 = hardwareMap.get(DcMotor.class, "motor0");
-        motor1 = hardwareMap.get(DcMotor.class, "motor1");
-        motor2 = hardwareMap.get(DcMotor.class, "motor2");
-        motor3 = hardwareMap.get(DcMotor.class, "motor3");
+        // Initialize the Bosch IMU, the internal gyroscope/temperature/magnetometer
+        bosch = hardwareMap.get(BNO055IMU.class, "imu");
+        //telemetry.addData("Gyro", "class:" + bosch.getClass().getName());
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+        parameters.loggingTag = "bosch";
+        boolean boschInit = bosch.initialize(parameters);
+        telemetry.addData("Gyro", "bosch init=" + boschInit);
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
-        motor1.setDirection(DcMotor.Direction.REVERSE);
-        motor3.setDirection(DcMotor.Direction.REVERSE);
+        // Initialize the motors.
+        if (runMotors) {
+            motor0 = hardwareMap.get(DcMotor.class, "motor0");
+            motor1 = hardwareMap.get(DcMotor.class, "motor1");
+            motor2 = hardwareMap.get(DcMotor.class, "motor2");
+            motor3 = hardwareMap.get(DcMotor.class, "motor3");
+
+            // Most robots need the motor on one side to be reversed to drive forward
+            // Reverse the motor that runs backwards when connected directly to the battery
+            motor1.setDirection(DcMotor.Direction.REVERSE);
+            motor3.setDirection(DcMotor.Direction.REVERSE);
+        }
 
         // Tell the driver that initialization is complete.
+        telemetry.addData("Gyro", bosch.getCalibrationStatus().toString());
         telemetry.addData("Status", "Initialized");
     }
 
@@ -47,7 +81,7 @@ public class MonsieurMallahMotorTest extends OpMode {
      */
     @Override
     public void init_loop() {
-    }
+     }
 
     /*
      * Code to run ONCE when the driver hits PLAY
@@ -55,6 +89,9 @@ public class MonsieurMallahMotorTest extends OpMode {
     @Override
     public void start() {
         runtime.reset();
+
+        // Start the sampling thread.
+        bosch.startAccelerationIntegration(null, null, 10);
     }
 
     /*
@@ -62,38 +99,65 @@ public class MonsieurMallahMotorTest extends OpMode {
      */
     @Override
     public void loop() {
-        // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+        if (runMotors) {
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
 
-        // Choose to drive using either Tank Mode, or POV Mode
-        // Comment out the method that's not used.  The default below is POV.
+            // Choose to drive using either Tank Mode, or POV Mode
+            // Comment out the method that's not used.  The default below is POV.
 
-        // POV Mode uses left stick to go forward, and right stick to turn.
-        // - This uses basic math to combine motions and is easier to drive straight.
-        //double drive = -gamepad1.left_stick_y;
-        //double turn  =  gamepad1.right_stick_x;
-        double drive = 1.0;
-        double turn = 1.0;
-        //leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        //rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-        leftPower = 0.5;
-        rightPower = -0.5;
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            //double drive = -gamepad1.left_stick_y;
+            //double turn  =  gamepad1.right_stick_x;
+            double drive = 1.0;
+            double turn = 1.0;
+            //leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            //rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+            leftPower = 0.5;
+            rightPower = -0.5;
 
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
-        // leftPower  = -gamepad1.left_stick_y ;
-        // rightPower = -gamepad1.right_stick_y ;
+            // Tank Mode uses one stick to control each wheel.
+            // - This requires no math, but it is hard to drive forward slowly and keep straight.
+            // leftPower  = -gamepad1.left_stick_y ;
+            // rightPower = -gamepad1.right_stick_y ;
 
-        // Send calculated power to wheels
-        motor0.setPower(leftPower);
-        motor1.setPower(rightPower);
-        motor2.setPower(leftPower);
-        motor3.setPower(rightPower);
+            // Send calculated power to wheels
+            motor0.setPower(leftPower);
+            motor1.setPower(rightPower);
+            motor2.setPower(leftPower);
+            motor3.setPower(rightPower);
 
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        }
+
+        /*AngularVelocity velocity = imu.getAngularVelocity(AngleUnit.DEGREES);
+        String mesg = "x: " + velocity.xRotationRate + ", y:" + velocity.yRotationRate + ", z:" + velocity.zRotationRate;
+        telemetry.addData("Gyro", mesg); */
+
+        Orientation angles = bosch.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+        telemetry.addData("Gyro", "angles: " + angles.firstAngle + "," + angles.secondAngle + "," + angles.thirdAngle);
+       // Acceleration oa = bosch.getOverallAcceleration();
+        //telemetry.addData("Gyro", "oa: " + oa);
+        Acceleration la = bosch.getLinearAcceleration();
+        telemetry.addData("Gyro", "la: " + la);
+        Acceleration ga = bosch.getGravity();
+        telemetry.addData("Gyro", "ga: " + ga);
+        Position pos = bosch.getPosition();
+        telemetry.addData("Gyro", "pos: " + pos);
+        Velocity v = bosch.getVelocity();
+        telemetry.addData("Gyro", "v: " + pos);
+        Acceleration accel = bosch.getAcceleration();
+        telemetry.addData("Gyro", "accel: " + accel);
+        Temperature temp = bosch.getTemperature();
+        telemetry.addData("Gyro", "temp: " + temp);
+        MagneticFlux flux = bosch.getMagneticFieldStrength();
+        telemetry.addData("Gyro", "flux: " + flux);
+        BNO055IMU.SystemStatus status = bosch.getSystemStatus();
+        telemetry.addData("Gyro", "status (" + bosch.isSystemCalibrated() + "): " + status);
     }
 
     /*
@@ -101,5 +165,6 @@ public class MonsieurMallahMotorTest extends OpMode {
      */
     @Override
     public void stop() {
+        bosch.stopAccelerationIntegration();
     }
 }
