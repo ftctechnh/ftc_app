5c0bb0236d7f0d48189c8655e6ba126fba2d9803
Eva <Eva@newton.local>
2018-09-16 16:27:16 -0700

    initial commit for auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAutonomous.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by Eva on 9/16/18.
+ */
+
+@Autonomous
+public class MainAutonomous extends LinearOpMode{
+
+    @Override
+    public void runOpMode(){
+
+    }
+
+    public void lower(){}
+
+    public void turnLeft(int degrees){}
+
+    public void turnRight(int degrees){}
+
+    public void moveForward(int inches){}
+
+    public void moveBackward(int inches){}
+}
