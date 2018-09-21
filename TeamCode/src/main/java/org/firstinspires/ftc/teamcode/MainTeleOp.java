0648d06d6e00b03ac043b8c2cf1d8bc37b73cf05
Ethan Liang <e3lcool@gmail.com>
2018-09-20 19:02:40 -0700

    Comment changes, minor refactoring

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -4,14 +4,13 @@ import com.qualcomm.hardware.lynx.LynxI2cColorRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 
 public class MainTeleOp extends LinearOpMode{
     // Motors
-    protected DcMotor motor0;
-    protected DcMotor motor1;
+    protected DcMotor rightDriveMotor;
+    protected DcMotor leftDriveMotor;
 
     // Servos
 
@@ -20,7 +19,8 @@ public class MainTeleOp extends LinearOpMode{
 
     private void initOpMode() {
         //initialize all the motors
-        motor0 = hardwareMap.get(DcMotor.class, "motor0");
+        rightDriveMotor = hardwareMap.get(DcMotor.class, "rightDriveMotor");
+        leftDriveMotor = hardwareMap.get(DcMotor.class, "leftDriveMotor");
 
         // Sensors initialization
         color0 = hardwareMap.get(LynxI2cColorRangeSensor.class, "color0");
@@ -40,8 +40,10 @@ public class MainTeleOp extends LinearOpMode{
         }
     }
 
-    double slow=2;
-//driver 1
+    // slow variable to allow for 'slowmode' - allowing the robot to go slower.
+    double slow = 1.66;
+
+//controller 1
     private void drive() {
         if (gamepad1.right_bumper) {
             slow = 1;
@@ -50,23 +52,26 @@ public class MainTeleOp extends LinearOpMode{
             slow = 1.66;
         }
 
-        motor0.setPower(gamepad1.left_stick_y / slow);
-        motor1.setPower(-1 * gamepad1.left_stick_y / slow);
+        rightDriveMotor.setPower(gamepad1.left_stick_y / slow);
+        leftDriveMotor.setPower(-1 * gamepad1.left_stick_y / slow);
 
         if (gamepad1.right_stick_x != 0) {
-            motor0.setPower(gamepad1.right_stick_x / slow * 2);
-            motor1.setPower(gamepad1.right_stick_x / slow * 2);
+            rightDriveMotor.setPower(gamepad1.right_stick_x / slow * 2);
+            leftDriveMotor.setPower(gamepad1.right_stick_x / slow * 2);
         }
     }
-//driver 2
+
+//controller 2
     private void intake() {
 
     }
-//driver 2
+
+//controller 2
     private void lift() {
 
     }
-//driver 2
+
+//controller 2
     private void deposit() {
 
     }
