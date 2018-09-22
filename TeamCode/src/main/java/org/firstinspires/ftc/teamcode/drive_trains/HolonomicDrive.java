b5c989b03d7a68261a045b8fa574a0b2e527a18f
ashq7 <aislinndream918@gmail.com>
2018-09-22 13:10:12 -0500

    started holonomic drive
    Declared motors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive_trains/HolonomicDrive.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.drive_trains;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by AshQuinn on 9/22/18.
+ */
+
+public class HolonomicDrive {
+    public DcMotor NW;
+    public DcMotor NE;
+    public DcMotor SW;
+    public DcMotor SE;
+
+    public HolonomicDrive (HardwareMap HWMap){
+        
+    }
+    public void init () {
+
+    }
+}
