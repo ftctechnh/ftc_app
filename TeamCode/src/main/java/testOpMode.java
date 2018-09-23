add70a74eb1d4ad9bad273ce1a2c90feb4b5b097
TFD Member <TFD Member>
2018-09-23 15:36:57 -0400

    initial test teleop

---
+++ TeamCode/src/main/java/testOpMode.java
@@ -0,0 +1,34 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+
+/**
+ * Made by Vedang
+ */
+@TeleOp
+public class testOpMode extends LinearOpMode {
+    private Gyroscope imu;
+    private DcMotor motorTest;
+    private DigitalChannel digitalTouch;
+
+    @Override
+    public void runOpMode()  {
+
+        imu = hardwareMap.get(Gyroscope.class, "imu");
+        motorTest = hardwareMap.get(DcMotor.class, "motorTest");
+        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        //Wait for the game to start ( driver presses play)
+        waitForStart();
+
+        //run until the end of the match (driver presses stop)
+        while (opModeIsActive()){
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+        }
+    }
+}
