c3830b2b34313ca7538de7a70b46801ed39deacd
David10238 <davidqcy02@gmail.com>
2018-09-27 17:46:40 -0500

    made an example linearOpmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTest.java
@@ -45,13 +45,13 @@ public class MecanumTest extends LinearOpMode {
         rotate = gamepad1.right_stick_x;
 
         // You might have to play with the + or - depending on how your motors are installed
-        frontLeftPower = drive + strafe - rotate;
-        backLeftPower = drive - strafe - rotate;
-        frontRightPower = drive - strafe + rotate;
-        backRightPower = drive + strafe + rotate;
+        frontLeftPower = drive + strafe + rotate;
+        backLeftPower = drive - strafe + rotate;
+        frontRightPower = drive - strafe - rotate;
+        backRightPower = drive + strafe - rotate;
 
-        leftBack.setPower(backLeftPower);
-        leftFront.setPower(frontLeftPower);
+        leftBack.setPower(-backLeftPower);
+        leftFront.setPower(-frontLeftPower);
         rightBack.setPower(backRightPower);
         rightFront.setPower(frontRightPower);
 
@@ -65,6 +65,11 @@ public class MecanumTest extends LinearOpMode {
         leftFront = hardwareMap.dcMotor.get("leftFront");
         rightFront = hardwareMap.dcMotor.get("rightFront");
         rightBack = hardwareMap.dcMotor.get("rightBack");
+
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpmode.java
@@ -1,8 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 /**
  * Created by Recharged Orange on 9/27/2018.
  */
+@TeleOp(name = "linearOpmode")//register Opmode
 
-public class linearOpmode {
+public class linearOpmode extends LinearOpMode {
+    // declarations go here
+    public void runOpMode () {
+
+        //init goes here
+        waitForStart();//driver hits play
+        while (opModeIsActive()) {//while driver has not hit stop
+
+        }
+
+    }
+    //Methods go here
 }
