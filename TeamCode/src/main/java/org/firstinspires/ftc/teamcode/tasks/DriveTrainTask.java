e0c0b2c0fb4bab74754c4f3c9ec95f8b3fae6f3c
Anton's Mac <antonjlin@gmail.com>
2018-09-29 11:50:15 -0700

    varialbe assignment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/DriveTrainTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/DriveTrainTask.java
@@ -14,28 +14,26 @@ import org.firstinspires.ftc.teamcode.robotutil.IMU;
  * Created by Howard on 10/15/16.
  */
 public class DriveTrainTask extends TaskThread {
-    DriveTrainNew dt;
+    private DriveTrainNew dt;
 
     public DriveTrainTask(LinearOpMode opMode) {
         this.opMode = opMode;
-
         initialize();
     }
 
     @Override
     public void run() {
         timer.reset();
-        while (opMode.opModeIsActive() && running) {
-
+        while (opMode.opModeIsActive() && this.running) {
             dt.setPowers(opMode.gamepad1.left_stick_y,opMode.gamepad1.right_stick_y);
         }
         dt.setPowers(0,0);
     }
     @Override
     public void initialize() {
-
         this.dt = new DriveTrainNew(this.opMode);
         dt.initMotors();
     }
 }
+
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/TaskThread.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/TaskThread.java
@@ -3,32 +3,26 @@ package org.firstinspires.ftc.teamcode.tasks;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 /**
  * Created by Anton on 10/15/17.
  */
 
 public abstract class TaskThread extends Thread{
     public volatile boolean running = true;
-    public LinearOpMode opMode;
-    public volatile boolean teleOp = false;
-    public static volatile double voltage = 13;
-    public static final double EXPECTED_VOLTAGE = 13;
+    LinearOpMode opMode;
     ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
 
     public void sleep (int ms) {
         timer.reset();
-        while (opMode.opModeIsActive() && timer.time() < ms);
+        while (opMode.opModeIsActive() && timer.time() < ms){
+            opMode.telemetry.update();
+        }
     }
-
-    public static void calculateVoltage(LinearOpMode opMode) {
-        double mc7 = opMode.hardwareMap.voltageSensor.get("frontDrive").getVoltage();
-        double mc6 = opMode.hardwareMap.voltageSensor.get("backDrive").getVoltage();
-        double mc3 = opMode.hardwareMap.voltageSensor.get("cap").getVoltage();
-        double mc2 = opMode.hardwareMap.voltageSensor.get("flywheels").getVoltage();
-        voltage = (mc7 + mc6 + mc3 + mc2) / 4;
+    public void stopThread(){
+        this.running = false;
     }
-
-
 
     public abstract void initialize();
 
