7ca83ba0919b42ee1fe86a2e37e4eed72c7d7d5d
unholydeath785 <ivansonofcoul@gmail.com>
2018-10-04 23:27:24 -0700

    added a file logger

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/logging/FileLogger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/logging/FileLogger.java
@@ -2,17 +2,15 @@ package org.firstinspires.ftc.teamcode.systems.logging;
 
 public class FileLogger implements ILogger
 {
-    private static final String LoggingPath = "";
-
     private LogWritter logWritter;
 
     public FileLogger() {
         logWritter = new LogWritter();
     }
 
-    public void log(String name, String data, Object... args)
-    {
-
+    public void log(String name, String data, Object... args) {
+        String line = StringFormatter.format("[{0}]: {1}", name, StringFormatter.format(data, args));
+        logWritter.appendLine(line);
     }
 
     public void write() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/logging/LogWritter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/systems/logging/LogWritter.java
@@ -1,10 +1,48 @@
 package org.firstinspires.ftc.teamcode.systems.logging;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+
 public class LogWritter
 {
+    private static final String LoggingFilePath = "";
+
     private StringBuffer buffer;
+    private File logFile;
 
     public LogWritter() {
-        
+        this.buffer = new StringBuffer();
+    }
+
+    public void appendLine(String line) {
+        buffer.ensureCapacity(line.length() + 1);
+        buffer.append(line + "\n");
+    }
+
+    public void write() {
+        try {
+            writeToFile();
+        } catch (Exception e) {
+            throw new IllegalStateException("Could not write to file", e);
+        }
+    }
+
+    private void writeToFile() throws Exception {
+        FileWriter fileWriter = getFileWriter();
+        appendBufferToFile(fileWriter);
+        buffer = new StringBuffer();
+    }
+
+    private FileWriter getFileWriter() throws Exception {
+        return new FileWriter(logFile);
+    }
+
+    private void appendBufferToFile(FileWriter fileWriter) throws Exception {
+        fileWriter.append(buffer.toString());
+        fileWriter.flush();
+        fileWriter.close();
     }
 }
