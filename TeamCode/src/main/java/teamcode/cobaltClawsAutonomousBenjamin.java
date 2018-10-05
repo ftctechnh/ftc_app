64b13dc1f00342ee3ef6a29474b3ec13e88a3316
1025403 <1025403@apps.nsd.org>
2018-10-04 18:41:46 -0700

    j

--- TeamCode/src/main/java/teamcode/cobaltClawsAutonomousBenjamin.java
+++ TeamCode/src/main/java/teamcode/cobaltClawsAutonomousBenjamin.java
@@ -27,6 +27,12 @@ public class cobaltClawsAutonomousBenjamin extends LinearOpMode {
     boolean leftGrabberOpen;
     boolean rightGrabberOpen;
 
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION)
+                                                        / (WHEEL_DIAMETER_INCHES * 3.1415);
+
     @Override
     public void runOpMode() {
 
