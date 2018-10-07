24c55ab34345c4d4d7822343aeb5e5a1e36f44bb
ashq7 <aislinndream918@gmail.com>
2018-10-07 12:57:48 -0500

    Attempting servo program
    Trying to test servo-activated linear actuator but code is filled with errors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearActTest.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by AshQuinn on 10/6/18.
+ */
+
+public class LinearActTest {
+    public Servo LinAct;
+
+    HardwareMap HWMap;
+
+    public void init (HardwareMap HWMap);
+
+    LinAct.setPosition ();
+
+
+
+}
