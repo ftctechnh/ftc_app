a38ea417a02e5fe1519966141084cce795882c37
Anton's Mac <antonjlin@gmail.com>
2018-10-06 21:31:10 -0700

    works now
    This reverts commit b341b45d78f741bfa504a806872d04d3b4f98504.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotutil/DriveTrainNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotutil/DriveTrainNew.java
@@ -14,31 +14,31 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
-import org.firstinspires.ftc.robotcore.internal.android.dex.util.ExceptionWithContext;
 
 //import com.qualcomm.hardware.adafruit.JustLoggingAccelerationIntegrator;
 
 public class DriveTrainNew {
     private ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
     private DcMotor lDrive,rDrive;
-    private IMUNew imu;
+    private IMU imu;
     private LinearOpMode opMode;
-    private DcMotor[] driveMotors;
+    private DcMotor[] driveMotors  = new DcMotor[2];
 
     public DriveTrainNew(LinearOpMode opMode) {
         this.opMode = opMode;
         lDrive = opMode.hardwareMap.dcMotor.get("lDrive");
         rDrive = opMode.hardwareMap.dcMotor.get("rDrive");
 
-        rDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        lDrive.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        this.imu = new IMUNew("imu",this.opMode);
+        BNO055IMU adaImu = opMode.hardwareMap.get(BNO055IMU.class, "imu");
+        this.imu = new IMU(adaImu);
 
-        this.driveMotors = new DcMotor[]{lDrive,rDrive};
+        this.driveMotors[0] = rDrive;
+        this.driveMotors[1] = lDrive;
+
 
         initMotors();
-        this.imu.init();
+
     }
 
     public void initMotors(){
@@ -87,8 +87,8 @@ public class DriveTrainNew {
     }
 
     public void setPowers(double lPower, double rPower){
-        lDrive.setPower(lPower);
-        rDrive.setPower(rPower);
+        rDrive.setPower(lPower);
+        lDrive.setPower(rPower);
     }
 
 
@@ -165,19 +165,73 @@ public class DriveTrainNew {
         return target - current;
     }
 
-
-//    public double rotateIMU(double degrees,Direction direction, double power, double timeoutS, Telemetry telemetry) throws Exception{
-//        double start = imu.getAngle(Axis.HEADING);
+//
+//    public double rotateIMURamp(int degrees,Direction direction, double power, int timeoutS, Telemetry telemetry) {
+//        //public double rotateIMURamp(int degrees, double power, int timeoutS, IMU imu, Telemetry telemetry) {
+//        //COUNTERCLOCKWISE IS POSITIVE DEGREES
+//        double heading;
+//        int e;
+//        long endtime = System.currentTimeMillis() + (timeoutS * 1000);
+//        double start = imu.getAngle();
 //        double target;
 //        if(direction == Direction.CW){
-//            target = start+degrees;
-//        }else if (direction == Direction.CCW){
-//            target = start - degrees;
+//            target = start + Math.abs(degrees);
 //        }else{
-//
-//            target = start;
+//            target = start - Math.abs(degrees);
 //        }
 //
+//        if ( target < 0) {
+//            target += 360;
+//        } else if (target >=360 ) {
+//            target -=360;
+//        }
+//        telemetry.clear();
+//        telemetry.addData("Start", start);
+//        telemetry.addData("Target", target);
+//
+//        if (Math.abs(degrees) <= Values.gyroTurnErrorMargin ){
+//            telemetry.addData("Too small to turn ", degrees);
+//            telemetry.update();
+//            return start;
+//        }
+//        telemetry.update();
+//        do {
+//            heading = imu.getAngle();
+//            if ( degrees > 0 ) { // Turn clockwise
+//                e = headingCWError((int) start, degrees, (int) heading); // Heading error
+//            } else { // turn counter clockwise
+//                e = headingCCWError((int) start, -degrees, (int) heading); // Heading error
+//            }
+//            if ( e > 0 ) {
+//                setPowers(Math.max(Values.minRotationPower, power * powerAdjust(e)),-Math.max(Values.minRotationPower, power * powerAdjust(e)));
+//            } else {  // overshoot
+//                setPowers(-Math.max(Values.minRotationPower, power * powerAdjust(e)),Math.max(Values.minRotationPower, power * powerAdjust(e)));
+//            }
+//            if (Math.abs(e) <= Values.gyroTurnErrorMargin) {
+//                this.stopAll();
+//                Utils.waitFor(100); // wait for 500 msec.
+//                heading = imu.getAngle(); // read heading again
+//                if ( degrees > 0 ) { // Turn clockwise
+//                    e = headingCWError((int) start, degrees, (int) heading); // Heading error
+//                } else { // turn counter clockwise
+//                    e = headingCCWError((int) start, -degrees, (int) heading); // Heading error
+//                }
+//            }
+//
+//            telemetry.clear();
+//            telemetry.addData("Start", start);
+//            telemetry.addData("Heading", imu.getAngle());
+//            telemetry.addData("Target", target);
+//            telemetry.addData("timeout", timeoutS * 1000);
+//            telemetry.addData("End Time", endtime);
+//            telemetry.addData("Error", e);
+//            telemetry.update();
+//        } while ((Math.abs(e) > Values.gyroTurnErrorMargin) &&  (System.currentTimeMillis() < endtime) && opMode.opModeIsActive());
+//
+//        this.stopAll();
+//        return heading;
+//
+////      setDriveMotorMode(DcMotor.RunMode.RUN_USING_ENCODER);
 //
 //    }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotutil/IMUNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotutil/IMUNew.java
@@ -1,12 +1,19 @@
 package org.firstinspires.ftc.teamcode.robotutil;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import java.util.Locale;
 
 enum Axis {
     PITCH,ROLL,HEADING;
@@ -16,29 +23,24 @@ public class IMUNew{
     private BNO055IMU imu;
     private Orientation angles;
     private OpMode opMode;
-    private Boolean initialized = false;
+    boolean isInitialized = false;
     private BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-
-    public IMUNew(String hwMapName, OpMode opMode){
+    
+    public IMUNew(OpMode opMode){
         this.opMode = opMode;
 
         this.parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
         this.parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
         this.parameters.loggingEnabled      = true;
-        this.parameters.loggingTag          = hwMapName;
-        this.parameters.loggingEnabled      = true;
+        this.parameters.loggingTag          = "IMU";
         // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
         // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
         // and named "imu".
-        imu = this.opMode.hardwareMap.get(BNO055IMU.class, hwMapName);
+        imu = this.opMode.hardwareMap.get(BNO055IMU.class, "imu");
     }
 
-    public boolean isInitialized(){
-        return this.initialized;
-    }
     public void init(){
         imu.initialize(parameters);
-        this.initialized = true;
     }
 
     public double getAngle(Axis axis){
