2f95777347cc7b3aaa9e3cc76324db5ff71df163
Aaron Cook <cookx770@umn.edu>
2018-10-07 14:46:33 -0500

    Remember, opmodes need to extend LinearOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearActTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearActTest.java
@@ -3,20 +3,19 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by AshQuinn on 10/6/18.
  */
 
-public class LinearActTest {
+public class LinearActTest extends LinearOpMode {
     public Servo LinAct;
 
-    HardwareMap HWMap;
-
-    public void init (HardwareMap HWMap);
-
-    LinAct.setPosition ();
-
-
-
+    @Override
+    public void runOpMode() {
+        LinAct = hardwareMap.servo.get("LinAct");
+        waitForStart();
+        LinAct.setPosition (0.0);
+    }
 }
