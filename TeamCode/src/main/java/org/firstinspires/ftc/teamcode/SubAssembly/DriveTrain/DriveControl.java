459ebbb528056ba361f639a2c1bc0a6522e0fbc5
Sammerkins <32140179+Sammerkins@users.noreply.github.com>
2018-10-08 16:23:35 -0400

    added motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubAssembly/DriveTrain/DriveControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubAssembly/DriveTrain/DriveControl.java
@@ -17,8 +17,10 @@ public class DriveControl {
     private String name = "Drive Train";
 
     //initializing motors
-    private DcMotor right;
-    private DcMotor left;
+    private DcMotor rightFront;
+    private DcMotor leftFront;
+    private DcMotor rightBack;
+    private DcMotor leftBack;
 
     /* Declare public class object */
     public VoltageSensor Battery = null;
@@ -34,43 +36,58 @@ public class DriveControl {
         /* Map hardware devices */
         Battery = hardwareMap.voltageSensor.get("Expansion Hub 2");
 
-        right = hardwareMap.dcMotor.get("leftWheel");
-        left = hardwareMap.dcMotor.get("rightWheel");
-        right.setDirection(DcMotor.Direction.REVERSE);
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        rightBack = hardwareMap.dcMotor.get("rightBack");
+        leftBack = hardwareMap.dcMotor.get("leftBack");
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
 
-        right.setPower(0);
-        left.setPower(0);
+        rightFront.setPower(0);
+        leftFront.setPower(0);
+        rightBack.setPower(0);
+        leftBack.setPower(0);
 
     }
 
     //setting power to move forward
     public void moveForward(double speed) {
-        right.setPower(1);
-        left.setPower(1);
+        rightFront.setPower(1);
+        leftFront.setPower(1);
+        rightBack.setPower(1);
+        leftBack.setPower(1);
     }
 
     //setting power to move backward
     public void moveBackward(double speed) {
-        right.setPower(-1);
-        left.setPower(-1);
+        rightFront.setPower(-1);
+        leftFront.setPower(-1);
+        rightBack.setPower(-1);
+        leftBack.setPower(-1);
     }
 
     //setting power to turn left
     public void turnLeft(double speed) {
-        right.setPower(-1);
-        left.setPower(1);
+        rightFront.setPower(-1);
+        leftFront.setPower(1);
+        rightBack.setPower(-1);
+        leftBack.setPower(1);
     }
 
     //setting power to turn right
     public void turnRight(double speed) {
-        right.setPower(1);
-        left.setPower(-1);
+        rightFront.setPower(1);
+        leftFront.setPower(-1);
+        rightBack.setPower(1);
+        leftBack.setPower(-1);
     }
 
     //setting power to 0
     public void stop (double speed) {
-        right.setPower(0);
-        left.setPower(0);
+        rightFront.setPower(0);
+        leftFront.setPower(0);
+        rightBack.setPower(0);
+        leftBack.setPower(0);
     }
 
     /* Subassembly methods */
