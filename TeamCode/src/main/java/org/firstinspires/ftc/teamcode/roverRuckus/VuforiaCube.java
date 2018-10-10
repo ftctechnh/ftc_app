4b1493112fa20447fd769147c79245cd14862477
Eric <3rix40@gmail.com>
2018-10-09 19:41:29 -0500

    Vuforia Beta 0.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/VuforiaCube.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/VuforiaCube.java
@@ -4,10 +4,16 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
+import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
 import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
 
 @TeleOp (name = "VuforiaCube", group = "test")
@@ -18,7 +24,10 @@ public class VuforiaCube extends LinearOpMode {
     private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
     //
     private OpenGLMatrix lastLocation = null;
+    public VectorF translation = null;
     private boolean targetVisible = false;
+    public Orientation orientation = null;
+    private static final float mmPerInch        = 25.4f;
     //
     VuforiaLocalizer vuforia;
     //
@@ -34,17 +43,22 @@ public class VuforiaCube extends LinearOpMode {
         bloackTrackies.activate();
         //
         while (opModeIsActive()){
-            /*if (((VuforiaTrackableDefatListener)blockTrackable.getListener()).isVisible()) {
+            if (((VuforiaTrackableDefaultListener)blockTrackable.getListener()).isVisible()) {
                 //
                 OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)blockTrackable.getListener()).getUpdatedRobotLocation();
                 if (robotLocationTransform != null) {
                     lastLocation = robotLocationTransform;
+                    translation = lastLocation.getTranslation();
+                    orientation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
                 }
                 //
-
-            }*/
-            telemetry.addData("VuMark", "");
+            }
+            telemetry.addData("Cube?", lastLocation != null);
+            telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                    translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
+            telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", orientation.firstAngle, orientation.secondAngle, orientation.thirdAngle);
             telemetry.update();
+            //
         }
     }
     //
