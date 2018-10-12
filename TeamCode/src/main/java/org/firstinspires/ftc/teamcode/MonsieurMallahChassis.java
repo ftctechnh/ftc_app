6d6f5fa41942d63dbf97981d7309457991d4186d
coolguybri <bri@coolguybri.com>
2018-10-11 18:58:22 -0700

    Resolve merge conflicts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MonsieurMallahChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MonsieurMallahChassis.java
@@ -31,7 +31,7 @@ public class MonsieurMallahChassis extends OpMode {
     static final double DRIVE_GEAR_REDUCTION = 2.0;     // This is < 1.0 if geared UP
     static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
     static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * 3.1415);
+            (WHEEL_DIAMETER_INCHES * 4.7);
     static final double DRIVE_SPEED = 0.6;
     static final double TURN_SPEED = 0.5;
 
