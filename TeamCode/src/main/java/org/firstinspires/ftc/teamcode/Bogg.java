b9a11adfc0346e557d7ebc1d136b3005ee284bc3
jodasue <jodasue@gmail.com>
2018-10-17 19:59:19 -0700

    Adds smoothing to the spin, incorporates test alpha

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bogg.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bogg.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -8,21 +9,26 @@ public class Bogg
     Gamepad gamepad;
     HardwareMap hardwareMap;
     DriveEngine driveEngine;
+    DcMotor lift;
+    Camera camera;
 //    Sensors sensors;
 
-    double alpha = .2;
+    double alpha = 0.0039;
+    double alphaInc = 0.000001;
     double xAve = 0;
     double yAve = 0;
+    double SpinAve = 0;
 
     public Bogg(HardwareMap hardwareMap, Gamepad gamepad)
     {
         this.gamepad = gamepad;
         this.hardwareMap = hardwareMap;
         driveEngine = new DriveEngine(hardwareMap);
+        lift  = hardwareMap.dcMotor.get("lift");
 //        sensors = new Sensors(hardwareMap);
     }
 
-    private double smoothX(double x)
+    public double smoothX(double x)
     {
         if(x == 0)
             xAve = 0;
@@ -31,7 +37,7 @@ public class Bogg
         return xAve;
     }
 
-    private double smoothY(double y)
+    public double smoothY(double y)
     {
         if(y == 0)
             yAve = 0;
@@ -39,12 +45,49 @@ public class Bogg
             yAve = alpha * y + (1-alpha) * yAve;
         return yAve;
     }
+    public double smoothSpin(double Spin)
+    {
+        if(Spin == 0)
+            SpinAve = 0;
+        else
+            SpinAve = alpha * Spin + (1-alpha ) * SpinAve;
+        return SpinAve;
+    }
+    public void lift()
+    {
+        if(gamepad.y)
+            lift.setPower(.1);
+        else if(gamepad.a)
+            lift.setPower(-.1);
+        else
+            lift.setPower(0);
+    }
 
     public void manualDrive()
     {
-        driveEngine.drive(smoothX(gamepad.left_stick_x), smoothY(gamepad.left_stick_y));
-        driveEngine.drive(gamepad.left_stick_x, gamepad.left_stick_y);
+        if(gamepad.left_stick_button)
+            driveEngine.drive(gamepad.left_stick_x, gamepad.left_stick_y);
+        else
+            driveEngine.drive(smoothX(gamepad.left_stick_x), smoothY(gamepad.left_stick_y));
     }
+
+    public void incAlpha()
+    {
+        if(alpha + alphaInc<1)
+            alpha += alphaInc;
+    }
+
+    public void decAlpha()
+    {
+        if(alpha - alphaInc>0)
+            alpha -= alphaInc;
+    }
+
+    public double getAlpha()
+    {
+        return alpha;
+    }
+
 
     public void manualRotate()
     {
