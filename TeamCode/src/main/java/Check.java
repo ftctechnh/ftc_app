f6cf1abbd4b2c3f6f7c99db9a7b39414c28f29f0
Computer <mor.tabuch10@gmail.com>
2018-10-20 15:09:53 +0300

    check class

--- TeamCode/src/main/java/Check.java
+++ TeamCode/src/main/java/Check.java
@@ -1,6 +1,13 @@
 public class Check {
-    public void t(){
+    public void t() {
+        int r = 33;
+    }
 
+    public void p() {
+        int t = 343;
+        for(int i=0;i<t;i++){
+            System.out.println("message");
+        }
     }
 }
 
