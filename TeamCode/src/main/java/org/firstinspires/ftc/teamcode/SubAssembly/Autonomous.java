975f8c92450c23e8f5035f3070c43a8bd488dbc5
Braden <39135190+BradenSiegal@users.noreply.github.com>
2018-10-22 17:31:04 -0400

    set up basic autonomous
    we didn't get very far, and it's a little broken

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubAssembly/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubAssembly/Autonomous.java
@@ -1,4 +1,12 @@
 package org.firstinspires.ftc.teamcode.SubAssembly;
 
-public class Autonomous {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Auto", group = "DriveForward")
+public class Auto extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
 }
