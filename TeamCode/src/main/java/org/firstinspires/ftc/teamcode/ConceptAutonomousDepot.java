08472cd8fbd0f0d5fac22a0dca66606f36420768
HarshiniSomisetty <harshini.somisetty@icloud.com>
2018-11-04 15:54:57 -0500

    Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAutonomousDepot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAutonomousDepot.java
@@ -44,7 +44,7 @@ public class ConceptAutonomousDepot extends LinearOpMode
         finalD = walle.getDistFromRight_In();
         thetaDeg = (float) ((Math.asin((finalD - initialD)/distToTravel)) * 180/Math.PI);
 
-        walle.pivot_IMU(-thetaDeg);
+        //walle.pivot(-thetaDeg);
         while (walle.getDistFromFront_In() > 18)
         {
             while (!gamepad1.a){}
@@ -55,34 +55,32 @@ public class ConceptAutonomousDepot extends LinearOpMode
             if (initialD < 4)
             {
                 telemetry.addData("I'm in the <4 Case!", null);
-                walle.pivot_IMU(14);
-                walle.driveMotorsAuto(.16f,.16f);
-                while (walle.getDistFromRight_In() > 5) //flipped the signs since the anything less than 4 is also less than 5
+                walle.pivot(14);
+                while (walle.getDistFromRight_In() < 5) //doing this drive forward until the robot is not too close so the statement is false
                 {
-
+                    walle.driveMotorsAuto(.16f,.16f);
                 }
                 walle.stopAllMotors();
-                walle.pivot_IMU(-14);
+                walle.pivot(-14);
             }
             else if (initialD > 7)
             {
                 telemetry.addData("I'm in the >7 Case!", null);
-                walle.pivot_IMU(-14);
+                walle.pivot(-14);
                 //walle.driveStraight_In(4);
-
-                walle.driveMotorsAuto(.16f,.16f);
-                while (walle.getDistFromRight_In() < 7) //flipped signs since anything greater than 7 is going to be greater than 5
+                while (walle.getDistFromRight_In() > 7) //doing this drive forward until the robot is not too far so the statement is false
                 {
-
+                    walle.driveMotorsAuto(.16f,.16f);
                 }
                 walle.stopAllMotors();
-
-                walle.pivot_IMU(14);
+                walle.pivot(14);
             }
             else
             {
                 telemetry.addData("I'm in the default Case!", null);
                 walle.driveStraight_In(distToTravel);
+                walle.getDistFromFront_In();
+                telemetry.addData("", walle.getDistFromFront_In());
             }
             telemetry.update();
         }
@@ -91,7 +89,7 @@ public class ConceptAutonomousDepot extends LinearOpMode
         telemetry.update();
         while (!isStopRequested())
         {
-
+            walle.stopAllMotors();
         }
     }
 }
