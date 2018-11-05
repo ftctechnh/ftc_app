35bb8b89cdce1c8a50e660dcc97163c497851a08
Andrew <32078009+Andrewsmoak1@users.noreply.github.com>
2018-11-05 16:39:23 -0500

    renamed teleop to Teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.teamcode.SubAssembly.Lift.LiftControl;
 import org.firstinspires.ftc.teamcode.Utilities.GamepadWrapper;
 
 @TeleOp(name = "TeleOp", group = "Drive")
-    public class teleop extends LinearOpMode {
+    public class Teleop extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
