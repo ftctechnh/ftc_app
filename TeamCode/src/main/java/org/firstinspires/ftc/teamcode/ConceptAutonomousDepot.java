0866634467e0fa34e69fadc2e06c72cc93554c27
sahithithumuluri <sahithi.thumuluri@gmail.com>
2018-11-06 18:41:18 -0500

    added new logic in comments of wall following

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAutonomousDepot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptAutonomousDepot.java
@@ -47,7 +47,7 @@ public class ConceptAutonomousDepot extends LinearOpMode
         walle.pivot_IMU(-thetaDeg);
         while (walle.getDistFromFront_In() > 18)
         {
-            while (!gamepad1.a){}
+            while (!gamepad1.a){} //edit this to be while the forward dist is less than 12
             sleep(336);
             initialD = walle.getDistFromRight_In();
 
@@ -56,33 +56,33 @@ public class ConceptAutonomousDepot extends LinearOpMode
             {
                 telemetry.addData("I'm in the <4 Case!", null);
                 walle.pivot_IMU(14);
-                walle.driveMotorsAuto(.16f,.16f);
                 while (walle.getDistFromRight_In() > 5) //flipped the signs since the anything less than 4 is also less than 5
                 {
-
+                    walle.driveMotorsAuto(.16f,.16f);
                 }
                 walle.stopAllMotors();
                 walle.pivot_IMU(-14);
+                //check for dist forward, if less than 12 then stop if greater than 12 keep going
             }
             else if (initialD > 7)
             {
                 telemetry.addData("I'm in the >7 Case!", null);
                 walle.pivot_IMU(-14);
                 //walle.driveStraight_In(4);
-
-                walle.driveMotorsAuto(.16f,.16f);
                 while (walle.getDistFromRight_In() < 7) //flipped signs since anything greater than 7 is going to be greater than 5
                 {
-
+                    walle.driveMotorsAuto(.16f,.16f);
                 }
                 walle.stopAllMotors();
 
                 walle.pivot_IMU(14);
+                //check for dist forward, if less than 12 then stop if greater than 12 keep going
             }
             else
             {
                 telemetry.addData("I'm in the default Case!", null);
                 walle.driveStraight_In(distToTravel);
+                //check for dist forward, if less than 12 then stop if greater than 12 keep going
             }
             telemetry.update();
         }
@@ -91,7 +91,7 @@ public class ConceptAutonomousDepot extends LinearOpMode
         telemetry.update();
         while (!isStopRequested())
         {
-
+            walle.stopAllMotors();
         }
     }
 }
