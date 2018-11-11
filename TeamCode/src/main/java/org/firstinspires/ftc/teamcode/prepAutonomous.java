fc4754e721c6971828b694596a3249034a8ecb46
prestonraab <prestonraab@gmail.com>
2018-11-10 20:24:19 -0700

    Use in competition to prepare the robot
    There is no need to hold up the hook.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/prepAutonomous.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous(name="autonomousDrive_DropPlacePark", group = "Testing")
+public class prepAutonomous extends LinearOpMode
+{
+    Bogg robot;
+    Mode action;
+    ElapsedTime timer;
+
+    private enum Mode
+    {
+        Stop,
+        Drop
+    }
+
+    @Override
+    public void runOpMode()
+    {
+        robot = new Bogg(hardwareMap, gamepad1, telemetry);
+        action = Mode.Stop;
+        robot.sensors.rotateMobile(0);
+        waitForStart();
+        action = Mode.Drop;
+        timer = new ElapsedTime();
+        timer.startTime();
+
+        while (opModeIsActive())
+        {
+            double t = timer.seconds();
+            switch(action)
+            {
+                case Drop:
+                    if(t < 5) //for the two seconds
+                    {
+                        robot.lift(-.3); //pull up
+                        //robot.sensors.dServoX.setPosition(0);
+                    }
+                    else if (t < 7)
+                    {
+                        robot.lift(-.5);
+                    }
+                    else if(t < 9)
+                    {
+                        robot.setBrake(true);
+                        //robot.sensors.dServoZ.setPosition(-1);
+                    }
+                    else if(t < 11) //wait for brake to engage
+                    {
+                        robot.lift(0); //relax, which drops the robot
+                    }
+                    else //if t > 9
+                        action = Mode.Stop;
+                    break;
+
+
+                default: //if action !Drop e.g. Stop
+                    robot.driveEngine.drive(0,0); //Stop driving
+                    robot.lift(0); //Stop Lifting
+
+            }
+
+            // Display the current values
+            telemetry.addData("time: ", t);
+            telemetry.addData("brake position: ", robot.brake.getPosition());
+            telemetry.addData("touch ", robot.sensors.touchBottom.isPressed());
+            telemetry.addData("mode", action);
+            telemetry.update();
+            idle();
+        }
+    }
+
+
+}
+
