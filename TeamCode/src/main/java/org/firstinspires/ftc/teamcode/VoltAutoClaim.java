555b0500a6a6de0a142e73d4ca281ac11a4deba0
Robin McDonald <robinmeilu@aol.com>
2018-11-11 20:05:04 -0700

    Made voltage autonomous classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoltAutoClaim.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+/**
+ * Created by Robin on 11/11/2018.
+ **/
+
+@Autonomous(group = "Voltage", name = "Claiming Side")
+public class VoltAutoClaim extends VoltageBaseAutonomous{
+    @Override
+    public void DriveTheRobot()
+    {
+        //position-specific instructions here
+    }
+
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VoltAutoCrater.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+/**
+ * Created by Robin on 11/11/2018.
+ **/
+
+@Autonomous(group = "Voltage", name = "Crater Side")
+public class VoltAutoCrater extends VoltageBaseAutonomous{
+    @Override
+    public void DriveTheRobot()
+    {
+        //position-specific instructions here
+    }
+}
