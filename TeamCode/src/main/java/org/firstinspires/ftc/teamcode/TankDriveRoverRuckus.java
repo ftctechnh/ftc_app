c58f80982b722b5aee80523dc19f3f8f88e7634d
Emilyhinds <emilyhinds217@gmail.com>
2018-11-12 16:43:42 -0800

    Update Tank Drive- 11/12
    Fixing the left power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveRoverRuckus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveRoverRuckus.java
@@ -12,15 +12,23 @@ public class TankDriveRoverRuckus extends LinearOpMode {
     // Declare OpMode members.
     private DcMotor Left;
     private DcMotor Right;
+    private DcMotor Shoulder;
+    private DcMotor Elbow;
     @Override
     public void runOpMode() {
         Left = hardwareMap.get(DcMotor.class, "left");
         Right = hardwareMap.get(DcMotor.class, "right");
+        Shoulder = hardwareMap.get(DcMotor.class, "shoulder");
+        Elbow = hardwareMap.get(DcMotor.class, "elbow");
+
+
         waitForStart();
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            Left.setPower(Range.clip(gamepad1.left_stick_y, -1, 1));
+            Left.setPower(-Range.clip(gamepad1.left_stick_y, -1, 1));
             Right.setPower(Range.clip(-gamepad1.right_stick_y, -1, 1));
+            Shoulder.setPower(Range.clip(gamepad2.left_stick_y, -1,1));
+            Elbow.setPower(Range.clip(-gamepad2.right_stick_y, -1,1));
         }
     }
 }
