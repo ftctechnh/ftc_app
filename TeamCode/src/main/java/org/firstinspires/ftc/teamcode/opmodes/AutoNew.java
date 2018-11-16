ace42ae34f7eab49101602348d9fc4a110e9b819
Frank Zheng <ztcfrank123@gmail.com>
2018-11-16 12:42:36 -0800

    not working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoNew.java
@@ -1,12 +1,15 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.Util;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.robotutil.Direction;
 import org.firstinspires.ftc.teamcode.robotutil.DriveTrainNew;
 import org.firstinspires.ftc.teamcode.robotutil.Utils;
 import org.firstinspires.ftc.teamcode.robotutil.Vision;
+
+import java.lang.reflect.Method;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Autonomous", group="FinalShit")
 
@@ -16,7 +19,7 @@ public class AutoNew extends LinearOpMode {
     private Vision vision;
 
     private Telemetry.Item goldXTelem;
-    private Telemetry.Item direction;
+    private Telemetry.Item telemDirection;
 
     // Tune these
     private final double DIST_TO_GOLD = 12;
@@ -24,37 +27,59 @@ public class AutoNew extends LinearOpMode {
     private final double DIST_TO_CRATER = 36;
     private final int GOLD_ALIGN_LOC = 100;
 
+    // Options
+    // Method: 0 = moveP, 1 = rotateIMU
+    // Direction: 0 = FW, 1 = BW, 2 = R, 3 = L, 4 = CW, 5 = CCW
+    private int method;
+    private int direction;
+    private double power;
+    private double value;
+
+    private Telemetry.Item opMethod;
+    private Telemetry.Item opDirection;
+    private Telemetry.Item opPower;
+    private Telemetry.Item opValue;
+
     @Override
     public void runOpMode() throws InterruptedException {
         initialize();
+        options();
         waitForStart();
 
         if (opModeIsActive()) {
-//            vision.startVision();
-//            waitForButton("Next: CCW 90 0.7");
-//
-//            dt.rotateIMU(Direction.CCW, 90, .7, 10);
-//            waitForButton("Next: CCW 180 0.7");
-//
-//            dt.rotateIMU(Direction.CCW, 180, .7, 10);
-//            waitForButton("Next: CW 180 1");
-//
-//            dt.rotateIMU(Direction.CW, 180, 1, 10);
-            waitForButton("Next: FW prop encoder drive 18 0.3");
+            waitForButton(String.format("Next: %d %d %f %f",
+                    method, direction, power, value));
 
-            dt.moveP(Direction.FORWARD, 0.3, 18,10);
+            Direction dir;
+            switch (direction) {
+                case 0:
+                    dir = Direction.FORWARD;
+                    break;
+                case 1:
+                    dir = Direction.BACK;
+                    break;
+                case 2:
+                    dir = Direction.RIGHT;
+                    break;
+                case 3:
+                    dir = Direction.LEFT;
+                    break;
+                case 4:
+                    dir = Direction.CW;
+                    break;
+                case 5:
+                    dir = Direction.CCW;
+                    break;
+                default:
+                    dir = Direction.FORWARD;
+            }
 
-            waitForButton("Next: BW prop encoder drive 18 0.3");
-
-            dt.moveP(Direction.BACK, 0.3, 18, 10);
-
-            waitForButton("Next: CCW 90 0.5");
-
-            dt.rotateIMU(Direction.CCW, 90, 0.5, 10);
-
-            waitForButton("Next: CW 90 0.5");
-
-            dt.rotateIMU(Direction.CW, 90, 0.5, 10);
+            switch (method) {
+                case 0:
+                    dt.moveP(dir, power, value, 10);
+                case 1:
+                    dt.rotateIMU(dir, power, value, 10);
+            }
 
             // Land
 
@@ -87,6 +112,70 @@ public class AutoNew extends LinearOpMode {
         }
 
 //        vision.shutDown();
+    }
+
+    private void updateOptions() {
+        opMethod = telemetry.addData("Method", method);
+        opDirection = telemetry.addData("Direction", direction);
+        opPower = telemetry.addData("Power", power);
+        opValue = telemetry.addData("Value", value);
+    }
+
+    private void options() {
+        boolean confirmed = false;
+
+        while(!confirmed) {
+
+            if (gamepad1.a) {
+                if (gamepad1.dpad_up) {
+                    if (method < 1) {
+                        method++;
+                    }
+                } else if (gamepad1.dpad_down) {
+                    if (method > 0) {
+                        method--;
+                    }
+                }
+            } else if (gamepad1.b) {
+                if (gamepad1.dpad_up) {
+                    if (direction < 5) {
+                        direction++;
+                    }
+                } else if (gamepad1.dpad_down) {
+                    if (direction > 0) {
+                        direction--;
+                    }
+                }
+            } else if (gamepad1.x) {
+                if (gamepad1.dpad_up) {
+                    if (power < 1.0) {
+                        power += 0.05;
+                    }
+                } else if (gamepad1.dpad_down) {
+                    if (power > 0) {
+                        power -= 0.05;
+                    }
+                }
+            } else if (gamepad1.y) {
+                if (gamepad1.dpad_up) {
+                    if (value < 180) {
+                        method++;
+                    }
+                } else if (gamepad1.dpad_down) {
+                    if (method > 0) {
+                        method--;
+                    }
+                }
+            } else if (gamepad1.left_stick_button && gamepad1.right_stick_button) {
+                telemetry.addLine("Confirmed!");
+                confirmed = true;
+            }
+
+            updateOptions();
+            telemetry.update();
+
+            Utils.waitFor(300);
+        }
     }
 
     private void waitForButton(String message){
@@ -132,8 +221,16 @@ public class AutoNew extends LinearOpMode {
         dt = new DriveTrainNew(this);
         vision = new Vision(this);
 
-        goldXTelem = telemetry.addData("Gold mineral position", -1);
-        direction = telemetry.addData("Direction", "NONE");
+//        goldXTelem = telemetry.addData("Gold mineral position", -1);
+//        telemDirection = telemetry.addData("Direction", "NONE");
+
+        method = 0;
+        direction = 0;
+        power = 0.5;
+        value = 18;
+
+        updateOptions();
+        telemetry.update();
     }
 
     private void haltUntilPressStart() {
