2272c30df98d74336e8704b6f38e694c0d970ee9
xDeimen <i_huszar@yahoo.com>
2018-11-21 10:21:59 +0200

    Aowolo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankMode.java
@@ -52,7 +52,7 @@ import com.qualcomm.robotcore.util.Range;
  */
 
 @TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
-@Disabled
+
 public class TankMode extends LinearOpMode {
 
     // Declare OpMode members.
