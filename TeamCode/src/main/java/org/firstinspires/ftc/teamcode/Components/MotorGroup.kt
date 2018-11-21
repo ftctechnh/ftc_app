1d98f4290eb4a020f6d7cbe21b85aff04460283c
Anton's Mac <antonjlin@gmail.com>
2018-11-20 23:11:23 -0800

    added motor group

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/MotorGroup.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/MotorGroup.kt
@@ -3,12 +3,34 @@ package org.firstinspires.ftc.teamcode.Components
 import org.firstinspires.ftc.teamcode.Components.Motor
 
 public class MotorGroup(){
-    var motors:List<Motor> = listOf()
+    var motors:ArrayList<Motor> = arrayListOf()
 
 
-//    fun addMotor(motor: Motor){
-//        motors.ad
-//    }
+    fun addMotor(motor: Motor){
+        motors.add(motor)
+    }
 
+    fun removeMotor(motor: Motor){
+        motors.remove(motor)
+    }
 
+    fun useEncoders(){
+        motors.forEach { useEncoders() }
+    }
+
+    fun setPower(power:Double){
+        motors.forEach { setPower(power) }
+    }
+
+    fun logInfo(){
+        motors.forEach { logInfo() }
+    }
+
+    fun prepareEncoderDrive(){
+        motors.forEach { prepareEncoderDrive() }
+    }
+
+    fun stop(coast:Boolean=false){
+        motors.forEach { stop(coast=coast) }
+    }
 }
