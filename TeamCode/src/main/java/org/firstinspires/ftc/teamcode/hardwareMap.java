d14367ecef6fac17cd53c151edd7bebdf18795fe
Joshua Yun <joshuayun61@gmail.com>
2018-11-22 14:26:18 -0500

    Added All De Comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareMap.java
@@ -7,13 +7,16 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class hardwareMap
 {
+    //Initializes Motor variables in hardware class
     public DcMotor leftFront = null;
     public DcMotor rightFront = null;
     public DcMotor leftBack = null;
     public DcMotor rightBack = null;
 
+    //Initializes RunMode variable for Dc motors so that they can be changed in OpMode class
     private  DcMotor.RunMode initialMode = null;
 
+    //Initializes hardware map variable
     HardwareMap map = null;
 
     public hardwareMap(DcMotor.RunMode enteredMode)
@@ -25,32 +28,37 @@ public class hardwareMap
     {
         map = aMap;
 
+        //Maps DC motor variables to configuration
         leftFront  = map.dcMotor.get("leftFront");
         rightFront = map.dcMotor.get("rightFront");
         leftBack  = map.dcMotor.get("leftBack");
         rightBack = map.dcMotor.get("rightBack");
 
-        //Encoders
+        //Encoder mode set
         leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        //All
+        //Sets DC mode to what is referenced in OpMode Code if you want to not use encoders, etc.
         leftFront.setMode(initialMode);
         rightFront.setMode(initialMode);
         leftBack.setMode(initialMode);
         rightBack.setMode(initialMode);
 
+        //Sets DC directions
         leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
         rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
         rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        //Makes Sure Robot is not moving at all when class called
         stopRobot();
+
     }
     public void stopRobot()
     {
+        //Sets all DC power to 0
         leftFront.setPower(0);
         rightFront.setPower(0);
         leftBack.setPower(0);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sparks.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sparks.java
@@ -9,14 +9,19 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp
 public class sparks extends OpMode
 {
+    //Creates hardware map from the hardwareMap class
     hardwareMap robot = new hardwareMap(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    //Initializes Time for Robot
     private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void init()
     {
+        //Tells driver that hardware mapping is initializing
         telemetry.addData("Status ", "Initializing");
 
+        //Initializes hardware mapping from hardwareMap class
         robot.init(hardwareMap);
 
         // Tell the driver that initialization is complete.
@@ -26,26 +31,32 @@ public class sparks extends OpMode
     @Override
     public void start()
     {
+        //Sets runtime back to zero as soon as loop starts
         runtime.reset();
     }
 
     @Override
     public void loop()
     {
+        //Initializes variables that hold value of motor powers
         double leftPower;
         double rightPower;
 
+        //Initializes variables which holds input from controller
         double drive = gamepad1.left_stick_y;
         double turn  = gamepad1.right_stick_x;
 
+        //Modifies input of controller variables and feeds them into power variables
         leftPower   = Range.clip(drive + turn, -1.0, 1.0) ;
         rightPower  = Range.clip(drive - turn, -1.0, 1.0) ;
 
+        //Sets DC motor powers to the power variables
         robot.leftFront.setPower(leftPower);
         robot.rightFront.setPower(rightPower);
         robot.leftBack.setPower(leftPower);
         robot.rightBack.setPower(rightPower);
 
+        //Gives information on power and time for troubleshooting
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
     }
