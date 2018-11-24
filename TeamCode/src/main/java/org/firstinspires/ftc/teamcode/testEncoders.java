5b19dc7ebc46d593c1b74b63269fd430f38d311c
lenakkees <31745707+lenakkees@users.noreply.github.com>
2018-11-24 17:25:22 -0500

    encoders test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testEncoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testEncoders.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-//Lena is testing
+//Lena is testing!
 @TeleOp(name = "Test Encoders", group = "Linear Opmode")
 // @Autonomous(...) is the other common choice
 //@Disabled
