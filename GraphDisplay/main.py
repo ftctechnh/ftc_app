31d10881e6d622edb2cbe734206a02e3b2bddedf
Anton's Mac <antonjlin@gmail.com>
2018-11-24 00:45:03 -0800

    add multiple plot

--- GraphDisplay/main.py
+++ GraphDisplay/main.py
@@ -10,7 +10,11 @@ from matplotlib import style
 style.use("fivethirtyeight")
 
 fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+
+
+# ax2 = fig.add_subplot(1,1,1)
 
 SOCKET_URL="ws://127.0.0.1:8080"
 FILE = "pidReadout.txt"
@@ -31,27 +35,45 @@ def animate(i):
     times = []
     errors = []
     outputs = []
+    ps = []
+    ds = []
+    iss = []
 
     for line in lines:
-        j = json.loads(line)
-        ts = j.get("ts")
-        error = j.get("error")
-        de = j.get("de")
-        dt = j.get("dt")
-        p = j.get("p")
-        i = j.get("i")
-        d = j.get("d")
-        output = j.get("output")
+        try:
+            j = json.loads(line)
+            ts = j.get("ts")
+            error = -j.get("error")
+            de = j.get("de")
+            dt = j.get("dt")
+            p = j.get("p")
+            i = j.get("i")
+            d = j.get("d")
+            output = j.get("output")
 
-        times.append(ts)
-        outputs.append(output)
-        errors.append(error)
+            times.append(ts)
+            outputs.append(output)
+            errors.append(error)
+            iss.append(i)
+            ps.append(p)
+            ds.append(d)
+
+
+
+        except Exception as e:
+            print(e)
     ax1.clear()
+    ax2.clear()
     # print(errors)
     # print(outputs)
     # print(times)
-    ax1.plot(times, errors,lineWidth=1,color='b',label='error')
+    ax2.plot(times, errors,lineWidth=1,color='b',label='error')
     ax1.plot(times, outputs,lineWidth=1,color='r',label='output')
+    ax1.plot(times,iss ,lineWidth=1,color='g',label='I')
+    ax1.plot(times, ps,lineWidth=1,color='y',label='P')
+    ax1.plot(times, ds,lineWidth=1,color='m',label='D')
+
+
 
 def connectWS():
     while(True):
@@ -61,6 +83,7 @@ def connectWS():
             break
         except Exception as e:
             print(e)
+            time.sleep(1)
 
     print(f"connected to websocket: {SOCKET_URL}")
     return ws
@@ -87,7 +110,7 @@ if __name__ == "__main__":
         ws = connectWS()
         threading.Thread(target=recieveFromWS,args=[ws]).start()
 
-    ani = animation.FuncAnimation(fig, animate, interval=100)
+    ani = animation.FuncAnimation(fig, animate, interval=1000)
     plt.show()
 
 
