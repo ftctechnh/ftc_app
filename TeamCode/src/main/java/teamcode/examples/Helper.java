27856cb0a56fc2b48bfb3548b3d1de43826dbb4d
PrimeTactic <30270951+PrimeTactic@users.noreply.github.com>
2018-11-30 00:24:52 -0800

    kk auto

--- TeamCode/src/main/java/teamcode/examples/Helper.java
+++ TeamCode/src/main/java/teamcode/examples/Helper.java
@@ -14,4 +14,11 @@ public class Helper {
 
     // http://www.revrobotics.com/content/docs/Encoder-Guide.pdf
     public static final int REV_HD_HEX_60_MOTOR_TICKS_PER_ROTATION = 1680;
+
+    // distance from 537.6494 x 930.5470
+    public static final double CAMERA_DISTANCE = 930.55;
+
+    public static double getCentimetersFromPixels(double pixels) {
+        return ((pixels - 300) / -100) * 30.48; // centimeters
+    }
 }
