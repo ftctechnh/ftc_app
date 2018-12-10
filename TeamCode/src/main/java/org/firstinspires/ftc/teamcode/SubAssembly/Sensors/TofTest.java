02cca6228b48b03f76b2642461f5ecc8a5aa52b0
Andrew <32078009+Andrewsmoak1@users.noreply.github.com>
2018-12-10 15:53:46 -0500

    time of flight sensor added
    check and compare distances

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubAssembly/Sensors/TofTest.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.SubAssembly.Sensors;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.Utilities.GamepadWrapper;
+
+/* Sub Assembly Test OpMode
+ * This TeleOp OpMode is used to test the functionality of the specific sub assembly
+ */
+// Assign OpMode type (TeleOp or Autonomous), name, and grouping
+@TeleOp(name = "TofTest Test", group = "Test")
+public class TofTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        telemetry.addLine("Tof Test: ");
+
+        /* initialize sub-assemblies
+         */
+        TofControl Tof = new TofControl();
+
+        GamepadWrapper egamepad1 = new GamepadWrapper(gamepad1);
+        GamepadWrapper egamepad2 = new GamepadWrapper(gamepad2);
+
+        Tof.init(this);
+
+        telemetry.update();
+
+        //waits for that giant PLAY button to be pressed on RC
+        waitForStart();
+
+
+        //telling the code to run until you press that giant STOP button on RC
+        while (opModeIsActive()) {
+
+            egamepad1.updateEdge();
+            egamepad2.updateEdge();
+
+
+           Tof.GetDistance();
+
+
+            //SubAssembly.test();
+            telemetry.update();
+
+            //let the robot have a little rest, sleep is healthy
+            sleep(40);
+        }
+
+    }
+}
