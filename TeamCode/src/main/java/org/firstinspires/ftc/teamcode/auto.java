928857f1ca36d492a0b1c4541456fc1d830d73f5
jminer <jelly1667@gmail.com>
2018-12-09 20:19:30 -0500

    Robot paths and coordinate plane.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -14,17 +14,31 @@ public class auto extends superAuto {
         configVuforiaRoverRuckus();
         getQuadrant();
 
+        final double mmPerInch       = 25.4f;
+        final double halfFieldWidth  = (12*6) * mmPerInch;       // the width of the FTC field (from the center point to the outer panels)
+        final double squareSize = 23.5*mmPerInch;
+        final double robotSize = 18*mmPerInch;
+        final double landerDiagonalSize = 69*mmPerInch;
+        final double dropPtDist = 0.5*landerDiagonalSize*Math.cos(45);//from origin
+        final double centerMineralLocation = Math.sqrt(Math.pow(1.5*squareSize,2)*2);
+        final double diagonalHalfField = Math.sqrt(Math.pow(halfFieldWidth,2)*2);
+        final double stopInFrontOfMineral= diagonalHalfField-centerMineralLocation-(robotSize/2);
+        final double xStopinFrontofMineral = stopInFrontOfMineral* Math.cos(45);
+        final double yStopinFrontofMineral =xStopinFrontofMineral;
 
         //ALL AUTOS DO:
         //Drop down
         //Go straight
+        goToPoint(714,714);
         //Knock correct mineral
         //Take left
-        //else Stuff
+        goToPoint(0,1562);
+
 
         if(quadrantOdd) {
             //Turn 270 degrees
             // move close, not a long time
+            goToPoint(-1193,1562);
         }
 
 
@@ -35,6 +49,8 @@ public class auto extends superAuto {
         //ALL AUTOS DO:
         //Deposit Claimer
         //Move backwards full speed into crater
+        goToPoint(1193,1562);
+
 
 
 
