3579d19bc1e54ab4117487b80286c248727aa517
Gavin Uberti <gavin.uberti@gmail.com>
2018-12-12 17:18:17 -0800

    Add op mode for debugging deployment

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/Auto/JustDeploy.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode.RoverRuckus.Auto;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.DriveSystems.Mecanum.RoadRunner.SampleMecanumDriveREV;
+import org.firstinspires.ftc.teamcode.Mechanisms.SparkyTheRobot;
+import org.firstinspires.ftc.teamcode.RoverRuckus.Deployers.Auto.EndGoal;
+import org.firstinspires.ftc.teamcode.RoverRuckus.Deployers.Auto.StartingPosition;
+import org.firstinspires.ftc.teamcode.Utilities.RoadRunner.AssetsTrajectoryLoader;
+
+import java.io.IOException;
+
+@Autonomous
+public class JustDeploy extends AutoUtils {
+
+    public EndGoal goal;
+    public static int position = -1;
+
+    public void switchAppendagePositions() {
+        robot.winch.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.winch.setPower(1);
+        robot.winch.setTargetPosition(-1500);
+
+        robot.intake.collect();
+
+        robot.sleep(2500);
+
+        robot.winch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.winch.setPower(0);
+        refoldMechanisms();
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        goal = EndGoal.BLUE_DOUBLE_SAMPLE;
+        startingPosition = StartingPosition.CRATER;
+
+        // Set up road runner
+        SampleMecanumDriveREV drive = new SampleMecanumDriveREV(hardwareMap);
+        robot = new SparkyTheRobot(this);
+        robot.calibrate(true);
+        robot.markerDeployer.setPosition(MARKER_DEPLOYER_RETRACTED);
+        initVuforia();
+        setWinchHoldPosition();
+
+        // Display telemetry feedback
+        telemetry.log().add("Running sophisticated sampling op-mode");
+        telemetry.log().add("Starting from: [[" + startingPosition.name() + "]]");
+        telemetry.log().add("Final actions: [[" + goal.name() + "]]");
+        telemetry.update();
+
+        GoldPosition goldLoc = waitAndWatchMinerals();
+        if (isStopRequested()) return;
+
+        if (position == 0) {
+            goldLoc = GoldPosition.LEFT;
+        } else if (position == 1) {
+            goldLoc = GoldPosition.CENTER;
+        } else if (position == 2) {
+            goldLoc = GoldPosition.RIGHT;
+        }
+
+        // Use appropriate method for dehooking
+        if (startingPosition == StartingPosition.DEPOT) {
+            double[] possibleHeadings = {Math.PI / 2, Math.PI * 3 / 4, Math.PI};
+            unhookFromLander(drive, robot, possibleHeadings[goldLoc.index]);
+        } else {
+            unhookFromLander(drive, robot, Math.PI * 1.75);
+        }
+    }
+}
