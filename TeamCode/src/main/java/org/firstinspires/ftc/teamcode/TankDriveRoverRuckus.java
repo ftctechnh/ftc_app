81e530ee3736fb912615741d79c3c1ffffe877c6
tazule <jxfiorito@comcast.net>
2018-12-14 21:17:00 -0800

    last min grind

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveRoverRuckus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveRoverRuckus.java
@@ -17,22 +17,22 @@ public class TankDriveRoverRuckus extends LinearOpMode {
     private DcMotor Right;
     private DcMotor Shoulder;
     private DcMotor Elbow;
-    private DcMotor Wrist;
+    //private DcMotor Wrist;
     private Servo LGrabber;
     private Servo RGrabber;
-    private DcMotor Winch;
-    private DcMotor Sideways;
+    //private DcMotor Winch;
+    //private DcMotor Sideways;
     @Override
     public void runOpMode() {
         Left = hardwareMap.get(DcMotor.class, "left");
         Right = hardwareMap.get(DcMotor.class, "right");
         Shoulder = hardwareMap.get(DcMotor.class, "shoulder");
         Elbow = hardwareMap.get(DcMotor.class, "elbow");
-        Wrist = hardwareMap.get(DcMotor.class,"wrist");
+        //Wrist = hardwareMap.get(DcMotor.class,"wrist");
         LGrabber = hardwareMap.get(Servo.class, "lgrab");
         RGrabber = hardwareMap.get(Servo.class, "rgrab");
-        Winch = hardwareMap.get(DcMotor.class,"winch");
-        Sideways = hardwareMap.get(DcMotor.class,"sideways");
+        //Winch = hardwareMap.get(DcMotor.class,"winch");
+        //Sideways = hardwareMap.get(DcMotor.class,"sideways");
         boolean bumper = false;
         int wristdir = 1;
         waitForStart();
@@ -58,7 +58,7 @@ public class TankDriveRoverRuckus extends LinearOpMode {
                 wristdir = wristdir*-1;
             }
             bumper = gamepad2.right_bumper;
-            Wrist.setPower(Range.clip(.5*gamepad2.right_trigger*wristdir,-1,1));
+            //Wrist.setPower(Range.clip(.5*gamepad2.right_trigger*wristdir,-1,1));
             /*if(gamepad2.b&&!b){
                 roffset = roffset*-1;
             }*/
@@ -83,10 +83,10 @@ public class TankDriveRoverRuckus extends LinearOpMode {
             elbowPID.iteratePID(elbow-Elbow.getCurrentPosition(), currentTime-prevTime);
             shoulderPID.iteratePID(shoulder-Shoulder.getCurrentPosition(), currentTime-prevTime);
             //Winch code- up: right trigger, down: left trigger
-            Winch.setPower(Range.clip(gamepad1.right_trigger-gamepad1.left_trigger,-1,1));
+            //Winch.setPower(Range.clip(gamepad1.right_trigger-gamepad1.left_trigger,-1,1));
             //Sideways- front wheel, push both sticks to control
             // both gamepads are inversed, so need to minus
-            Sideways.setPower(Range.clip(-gamepad1.right_stick_y+gamepad1.left_stick_y,-1,1));
+            //Sideways.setPower(Range.clip(-gamepad1.right_stick_y+gamepad1.left_stick_y,-1,1));
             //telemetry.addData("bools",liftmode+" "+a);
             //telemetry.addData("lift pos", Lift.getCurrentPosition());
             telemetry.addData("loffset",loffset);
