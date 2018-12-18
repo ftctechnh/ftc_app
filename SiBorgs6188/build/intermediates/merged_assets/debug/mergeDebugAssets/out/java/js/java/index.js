ce069cfffaa6e4c392c00d0eae02f5ae30ae926e
Calvin Sprouse <sprousecalvint@gmail.com>
2018-12-17 21:54:29 -0800

    Removeed gitignore files

--- SiBorgs6188/build/intermediates/merged_assets/debug/mergeDebugAssets/out/java/js/java/index.js
+++
@@ -1,4 +0,0 @@
-exports.antlr4 = require('../antlr4/index');
-exports.JavaLexer = require('./JavaLexer').JavaLexer;
-exports.JavaListener = require('./JavaListener').JavaListener;
-exports.JavaParser = require('./JavaParser').JavaParser;
