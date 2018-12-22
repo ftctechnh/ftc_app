6e27bd7eb841b94c693056e4578c9e613dc6e1ba
d0g <d0g@stepone.gg>
2018-12-22 10:27:09 -0800

    Added: - Constructors added in Functions & Hardware classes - Fixed opModeIsActive() in Functions class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Functions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Functions.java
@@ -1,62 +1,75 @@
 package org.firstinspires.ftc.teamcode.core;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 
-public class Functions {
+public class Functions extends Hardware {
     Orientation lastAngles = new Orientation();
     double globalAngle, power = .30, correction;
-    Hardware Hw = new Hardware();
+    private LinearOpMode opmode;
+
+
+    public Functions(Telemetry telemetry, HardwareMap hardwareMap, LinearOpMode linearOpMode)
+    {
+        super(telemetry, hardwareMap, linearOpMode);
+
+        opmode = linearOpMode;
+    }
+
+
 
     //Move function using INCHES and POWER
     public void move(int distance, double power) {
 
         int target = distance * (int) (288 / (4 * Math.PI));
-        Hw.backLeftDrive.setTargetPosition(target);
-        Hw.backRightDrive.setTargetPosition(target);
-        Hw.frontLeftDrive.setTargetPosition(target);
-        Hw.frontRightDrive.setTargetPosition(target);
+        backLeftDrive.setTargetPosition(target);
+        backRightDrive.setTargetPosition(target);
+        frontLeftDrive.setTargetPosition(target);
+        frontRightDrive.setTargetPosition(target);
 
-        Hw.backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        Hw.backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        Hw.frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        Hw.frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        Hw.backLeftDrive.setPower(power);
-        Hw.backRightDrive.setPower(power);
-        Hw.frontLeftDrive.setPower(power);
-        Hw.frontRightDrive.setPower(power);
+        backLeftDrive.setPower(power);
+        backRightDrive.setPower(power);
+        frontLeftDrive.setPower(power);
+        frontRightDrive.setPower(power);
 
-        while (Hw.backLeftDrive.isBusy() && Hw.backRightDrive.isBusy()) {
-            Hw.telemetry.addData("BackLeft", Hw.backLeftDrive.isBusy());
+        while (backLeftDrive.isBusy() && backRightDrive.isBusy()) {
+            telemetry.addData("BackLeft", backLeftDrive.isBusy());
         }
-        Hw.backLeftDrive.setPower(0);
-        Hw.backRightDrive.setPower(0);
-        Hw.frontLeftDrive.setPower(0);
-        Hw.frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
     }
 
     public float getZAxis(){
-        return Hw.gyro.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+        return gyro.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
     }
 
 
     private void resetAngle()
     {
-        lastAngles = Hw.gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        lastAngles = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         globalAngle = 0;
     }
 
     private double getAngle()
     {
 
-        Orientation angles = Hw.gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        Orientation angles = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
 
         double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
 
@@ -79,14 +92,15 @@ public class Functions {
         angle = getAngle();
 
         if (angle == 0)
-            correction = 0;             // no adjustment.
+            correction = 0; // no adjustment.
         else
-            correction = -angle;        // reverse sign of angle for correction.
+            correction = -angle; // reverse sign of angle for correction.
 
         correction = correction * gain;
 
         return correction;
     }
+
     //Turn function using DEGREES and POWER
     private void turn(int degrees, double power)
     {
@@ -106,27 +120,27 @@ public class Functions {
         }
         else return;
 
-        Hw.backLeftDrive.setPower(leftPower);
-        Hw.backRightDrive.setPower(rightPower);
-        Hw.frontLeftDrive.setPower(leftPower);
-        Hw.frontRightDrive.setPower(rightPower);
+        backLeftDrive.setPower(leftPower);
+        backRightDrive.setPower(rightPower);
+        frontLeftDrive.setPower(leftPower);
+        frontRightDrive.setPower(rightPower);
 
         // rotate until turn is completed.
         if (degrees < 0)
         {
             // On right turn we have to get off zero first.
-            while (opModeIsActive() && getAngle() == 0) {}
+            while (opmode.opModeIsActive() && getAngle() == 0) {}
 
-            while (opModeIsActive() && getAngle() > degrees) {}
+            while (opmode.opModeIsActive() && getAngle() > degrees) {}
         }
         else    // left turn.
-            while (opModeIsActive() && getAngle() < degrees) {}
+            while (opmode.opModeIsActive() && getAngle() < degrees) {}
 
         // turn the motors off.
-        Hw.backLeftDrive.setPower(0);
-        Hw.backRightDrive.setPower(0);
-        Hw.frontLeftDrive.setPower(0);
-        Hw.frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
 
         // wait for rotation to stop.
         sleep(1000);
@@ -145,8 +159,8 @@ public class Functions {
 
     //PlaceMarker function that places the marker
     public void PlaceMarker() {
-        Hw.markerServo.setPosition(-1);
-        Hw.markerServo.setPosition(1);
+        markerServo.setPosition(-1);
+        markerServo.setPosition(1);
     }
 
 }
