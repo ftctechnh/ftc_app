f76978938c192b1ccbf1247ac66f4b934843e663
prestonraab <prestonraab@gmail.com>
2019-01-02 17:08:25 -0800

    Floats the motors and gives us ticks

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FloatMotors.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="  encoderTest", group="Testing")
+public class FloatMotors extends LinearOpMode
+{
+    Bogg robot;
+
+    @Override
+    public void runOpMode()
+    {
+        robot = new Bogg(hardwareMap, gamepad1, telemetry);
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            robot.driveEngine.back.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            robot.driveEngine.right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            robot.driveEngine.left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            robot.lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+            robot.driveEngine.stop();
+
+            telemetry.addData("back", robot.driveEngine.back.getCurrentPosition());
+            telemetry.addData("right", robot.driveEngine.right.getCurrentPosition());
+            telemetry.addData("left", robot.driveEngine.left.getCurrentPosition());
+            telemetry.addData("lift", robot.lift.getCurrentPosition());
+            telemetry.update();
+            idle();
+        }
+    }
+}
+
