454338ee99c036e4c835dd83d805f424b4a31f46
prestonraab <prestonraab@gmail.com>
2019-01-02 17:08:49 -0800

    Makes the orbit method more useful

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/holonomicDrive_0_4.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp(name="holonomicDrive 1G varOrbit", group="Testing")
+public class holonomicDrive_0_4 extends LinearOpMode
+{
+    Bogg robot;
+
+    private boolean orbit;
+
+    @Override
+    public void runOpMode()
+    {
+        robot = new Bogg(hardwareMap, gamepad1, telemetry);
+        robot.driveEngine.driveAtAngle(Math.PI);
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            if(gamepad1.x)
+                orbit = true;
+            else if(gamepad1.y)
+                orbit = false;
+
+            robot.manualDriveVarOrbit(gamepad1, gamepad1, orbit);
+
+            if(gamepad1.dpad_up)
+            {
+                robot.setBrake(true);
+            }
+            else if(gamepad1.dpad_down)
+            {
+                robot.setBrake(false);
+            }
+
+            if(gamepad1.start)
+            {
+                robot.driveEngine.resetDistances();
+            }
+
+
+
+            if(gamepad1.left_bumper)
+            {
+                robot.dropMarker(Bogg.Direction.Left);
+            }
+            else if(gamepad1.right_bumper)
+            {
+                robot.dropMarker(Bogg.Direction.Right);
+            }
+
+
+            robot.manualLift();
+
+            // Display the current value
+            // Display the current value
+            telemetry.addLine("'Pressing A must move the arm down/robot up.'");
+            telemetry.addLine("Set brake: d-down. Remove brake: d-up.");
+            telemetry.addData("back encoder inches", robot.driveEngine.back.getCurrentPosition() * DriveEngine.inPerTicks);
+            telemetry.addData("touchBottom", robot.sensors.touchBottom.isPressed());
+            telemetry.addData("touchTop", robot.sensors.touchTop.isPressed());
+            telemetry.addData("fixed distance", robot.sensors.dFixed.getDistance(DistanceUnit.INCH));
+            telemetry.addData("mobile distance", robot.sensors.dMobile.getDistance(DistanceUnit.INCH));
+
+            telemetry.update();
+            idle();
+        }
+    }
+
+}
+
