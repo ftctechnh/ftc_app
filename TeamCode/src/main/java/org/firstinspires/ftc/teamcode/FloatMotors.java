eac60a4c9d1bcded40a31fb0d87697e51e52d7f2
prestonraab <prestonraab@gmail.com>
2019-01-07 21:55:17 -0800

    name change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FloatMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FloatMotors.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="  encoderTest", group="Testing")
+@TeleOp(name="  float motors", group="Testing")
 public class floatMotors extends LinearOpMode
 {
     Bogg robot;
