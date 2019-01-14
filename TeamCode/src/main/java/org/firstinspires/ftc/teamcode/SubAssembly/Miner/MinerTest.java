b077a16ad9394273cb8033f20872a5d76d1eb9e6
Andrew <32078009+Andrewsmoak1@users.noreply.github.com>
2019-01-14 14:00:09 -0500

    Update MinerTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubAssembly/Miner/MinerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubAssembly/Miner/MinerTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.SubAssembly.Lift;
+package org.firstinspires.ftc.teamcode.SubAssembly.Miner;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
