3f7024fd99196b5be0203be762d0c30cf3b824cc
prestonraab <prestonraab@gmail.com>
2019-01-17 16:12:54 -0800

    Uses angular correction

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderTestCorrect.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="  encoderTestCorrect", group="Testing")
+public class encoderTestCorrect extends LinearOpMode
+{
+    Bogg robot;
+
+    @Override
+    public void runOpMode()
+    {
+        robot = new Bogg(hardwareMap, telemetry);
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            if (robot.driveEngine.moveOnPath(false, true,
+                    new double[]{-6, 0},
+                    new double[]{0, 4},
+                    new double[]{6, 0},
+                    new double[]{0, 24},
+//                    new double[]{17, 0},
+//                    new double[]{0, 12},
+//                    new double[]{0, -12},
+//                    new double[]{-17, 0},
+//                    new double[]{0, 12},
+//                    new double[]{0, -12},
+//                    new double[]{-17, 0},
+//                    new double[]{0, 12},
+//                    new double[]{0, -12},
+//                    new double[]{-17, 0},
+                    new double[]{-34, 0},
+                    new double[]{Math.PI / 4}))
+            {
+                robot.driveEngine.drive(0,0);
+            }
+            telemetry.update();
+            robot.update();
+            idle();
+        }
+    }
+}
+
