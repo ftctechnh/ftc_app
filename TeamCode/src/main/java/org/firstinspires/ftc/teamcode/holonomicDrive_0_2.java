e0a5ba97e2223312bd2be0531b745b916abbfac9
prestonraab <prestonraab@gmail.com>
2019-01-19 11:32:44 -0800

    Adjust for new arm methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/holonomicDrive_0_2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/holonomicDrive_0_2.java
@@ -3,11 +3,17 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="holonomicDrive Curvy", group="Testing")
 public class holonomicDrive_0_2 extends LinearOpMode
 {
     Bogg robot;
+
+    private ElapsedTime timer;
+
+    private boolean leftButtonPressed = false;
+    private boolean rightButtonPressed = false;
 
     @Override
     public void runOpMode()
@@ -16,12 +22,15 @@ public class holonomicDrive_0_2 extends LinearOpMode
         robot.driveEngine.driveAtAngle(Math.PI);
         waitForStart();
 
+        timer = new ElapsedTime();
         Gamepad g1 = gamepad1;
         Gamepad g2 = gamepad2;
 
         while (opModeIsActive())
         {
-            robot.manualCurvy(g1, g1);
+            if(!robot.manualRotate(true, g2.right_stick_x / 3))
+                if(!robot.manualRotate(true, -g2.left_stick_x /3))
+                    robot.manualCurvy(g1, g1);
 
             if(g1.dpad_down)
                 robot.setBrake(Bogg.Direction.On);
@@ -34,10 +43,32 @@ public class holonomicDrive_0_2 extends LinearOpMode
             else if(g1.right_bumper)
                 robot.dropMarker(Bogg.Direction.Right);
 
-
             robot.manualLift(g1.y, g1.a);
 
-            robot.manualEffect(g2); //we can always change the arm manually
+            //When down
+            if(g2.left_stick_button) {
+                if(!leftButtonPressed)
+                    timer.reset();
+                robot.endEffector.flipUp(timer.seconds());
+            }
+            else
+                leftButtonPressed = false;
+
+            robot.endEffector.extend(-g2.left_stick_y * 4);
+
+
+            //When up
+            if(g2.right_stick_button) {
+                if(!rightButtonPressed)
+                    timer.reset();
+                robot.endEffector.flipDown(timer.seconds());
+            }
+            else
+                rightButtonPressed = false;
+
+            robot.endEffector.extend(g2.right_stick_y * 3);
+
+
 
             // Display the current value
             telemetry.addLine("'Pressing A must move the arm down/robot up.'");
