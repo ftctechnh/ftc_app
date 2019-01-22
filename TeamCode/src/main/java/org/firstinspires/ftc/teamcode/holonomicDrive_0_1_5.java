99409ef3c13edf75ec1c212803b2c70b0a968356
prestonraab <prestonraab@gmail.com>
2019-01-21 18:01:02 -0800

    Delay  correction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/holonomicDrive_0_1_5.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/holonomicDrive_0_1_5.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="holonomicDrive AutoCorrect", group="Testing")
 public class holonomicDrive_0_1_5 extends LinearOpMode
@@ -10,12 +11,14 @@ public class holonomicDrive_0_1_5 extends LinearOpMode
     Bogg robot;
 
     Gamepad g1;
+    private ElapsedTime timer;
 
     @Override
     public void runOpMode()
     {
         robot = new Bogg(hardwareMap, telemetry);
         robot.driveEngine.driveAtAngle(Math.PI);
+        timer = new ElapsedTime();
         g1 = gamepad1;
         waitForStart();
 
@@ -23,10 +26,16 @@ public class holonomicDrive_0_1_5 extends LinearOpMode
         {
             if(!robot.manualRotate(g1.right_stick_button, g1.right_stick_x)) //if we're not rotating
             {
-                robot.manualDriveAutoCorrect(g1.left_stick_button, g1.left_stick_x, g1.left_stick_y);
+                //We don't correct until a second has passed.
+                if(timer.seconds() < 1){
+                    robot.driveEngine.resetForward();
+                    robot.manualDrive(g1.left_stick_button, g1.left_stick_x, -g1.left_stick_y);
+                }
+                else
+                    robot.manualDriveAutoCorrect(g1.left_stick_button, g1.left_stick_x, g1.left_stick_y);
             }
             else //if we are rotating
-                robot.driveEngine.resetForward();
+                timer.reset();
 
 
             if(g1.dpad_down)
