ccf5f6a3fd40d4a2c3a3db9d598bf9a335a9ccfe
GoldenPigFlynn <flynn@flynnconnolly.com>
2019-01-22 23:56:34 -0800

    2 new opmodes from the FTC district competition

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PhatSwipeDropAndRetreat.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Phat; DropAndRetreat(depot)", group="BPhatSwipe")
+public class PhatSwipeDropAndRetreat extends DropAndRetreat {
+    public PhatSwipeDropAndRetreat() {
+        super(ChassisConfig.forPhatSwipe());
+    }
+}
+
+
