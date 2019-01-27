8b62463bb717db6918dfd2193469fda9babfcdc8
VasudevTheGreat <vasumenon@gmail.com>
2019-01-27 17:34:42 -0500

    Create Autonomous1.java
    Autonomous FTC Code, brings the robot down from the lander.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous1.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.io.InterruptedIOException;
+
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous
+public class Autonomous1 extends LinearOpMode {
+    DcMotor lift;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        lift = hardwareMap.dcMotor.get("lift");
+
+        while(opModeIsActive()){
+            lift.setPower(1);
+        }
+
+    }
+
+}
