a83e1b80a7931446adc7babce4846779c2bd5f30
CouriersRyan <zhangryan09@gmail.com>
2019-01-30 18:27:38 -0800

    TeleOp work on 1/30

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JahsehDwayneRicardoOnfroy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JahsehDwayneRicardoOnfroy.java
@@ -42,38 +42,29 @@ public class JahsehDwayneRicardoOnfroy extends LinearOpMode
         {
             double leftPower;
             double rightPower;
+            double clawMovement;
             boolean openClaw;
             boolean closeClaw;
-            double clawMovement;
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
             leftPower = -gamepad1.right_trigger;
             rightPower = gamepad1.left_trigger;
             openClaw = gamepad1.a;
             closeClaw = gamepad1.b;
             clawMovement = gamepad1.left_stick_y;
-            if (gamepad1.right_bumper)
-            {
-                leftPower *= -1;
-                rightPower *= -1;
-            }
 
             leftPower = Range.clip(leftPower, -1.0, 1.0);
             rightPower = Range.clip(rightPower, -1.0, 1.0);
             clawMovement = Range.clip(clawMovement, -1.0, 1.0);
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower = -gamepad1.left_stick_y;
-            //rightPower = -gamepad1.right_stick_y;
 
             // Send calculated power to wheels
             leftWheel.setPower(leftPower);
             rightWheel.setPower(rightPower);
             claw.setPower(clawMovement);
+            if (gamepad1.right_bumper)
+            {
+                leftPower *= -1;
+                rightPower *= -1;
+            }
             if (openClaw)
             {
                 leftClaw.setPosition(1.0);
