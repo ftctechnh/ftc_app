acd06221fbd5f91ad40622f4735460b0d9bb8ec9
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2019-02-02 16:07:01 +0100

    first OPmode added

---
+++ FtcRobotController/src/main/java/BasicOPmode.java
@@ -0,0 +1,8 @@
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class BasicOPmode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
