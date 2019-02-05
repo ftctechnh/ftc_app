9aa94379c889d2bdcb67484b2c0ac8cfa94be1a3
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2019-02-05 08:26:03 +0100

    Revert "first OPmode added"
    This reverts commit acd06221fbd5f91ad40622f4735460b0d9bb8ec9.

--- FtcRobotController/src/main/java/BasicOPmode.java
+++
@@ -1,8 +0,0 @@
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-public class BasicOPmode extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
-
-    }
-}
