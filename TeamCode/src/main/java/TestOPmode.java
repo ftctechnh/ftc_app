4ada1ce5672da45c9f63437e1881718b8e7c7555
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2019-02-08 16:49:33 +0100

    very simple control working

--- TeamCode/src/main/java/TestOPmode.java
+++ TeamCode/src/main/java/TestOPmode.java
@@ -1,9 +1,25 @@
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+@TeleOp
 public class TestOPmode extends LinearOpMode {
 
+    private DcMotor LeftMotor;
+    private  DcMotor RightMotor;
+
     @Override
-    public void runOpMode() throws InterruptedException {
-        
+    public void runOpMode() {
+         LeftMotor = hardwareMap.get(DcMotor.class, "LeftMotor");
+        RightMotor = hardwareMap.get(DcMotor.class, "RightMotor");
+
+        telemetry.addData("sycka blyat", "urmom");
+        telemetry.update();
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            LeftMotor.setPower(-this.gamepad1.left_stick_y);
+        }
     }
 }
