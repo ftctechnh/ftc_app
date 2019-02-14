784fe520cf84cff0aea7f93969ad7dd5162fa31f
prestonraab <prestonraab@gmail.com>
2019-02-13 20:17:51 -0800

    Incorporation of flotation into other classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FloatMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FloatMotors.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="  float motors", group="Testing")
 public class floatMotors extends LinearOpMode
@@ -13,25 +12,13 @@ public class floatMotors extends LinearOpMode
     public void runOpMode()
     {
         robot = Bogg.determineRobot(hardwareMap, telemetry);
-        robot.driveEngine.floatMotors();
-        robot.driveEngine.stop();
 
         waitForStart();
 
         while (opModeIsActive())
         {
-            robot.driveEngine.floatMotors();
-            robot.driveEngine.reportPositionsToScreen();
+            robot.floatMotors();
 
-            if(robot.name == Bogg.Name.Bogg) {
-                robot.lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-                robot.endEffector.pivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-                robot.endEffector.contract.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
-                telemetry.addData("lift", robot.lift.getCurrentPosition());
-                telemetry.addData("pivot", robot.endEffector.pivot.getCurrentPosition());
-                telemetry.addData("contract", robot.endEffector.contract.getCurrentPosition());
-            }
             telemetry.update();
             robot.update();
             idle();
