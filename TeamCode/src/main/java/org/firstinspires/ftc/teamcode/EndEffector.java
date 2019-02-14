2e38ef7943af94ccb58584a608aa2a1929741537
prestonraab <prestonraab@gmail.com>
2019-02-13 20:29:17 -0800

    Pivot by a set number of ticks, two wheels
    And some reorganization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EndEffector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EndEffector.java
@@ -36,7 +36,7 @@ class EndEffector {
     Sensors sensors;
     Telemetry telemetry;
 
-    double initialStringLength = 5;
+    double initialStringLength = 7;
     double initialArmLength = 14;
     double armLength = initialArmLength;
 
@@ -49,15 +49,14 @@ class EndEffector {
 
         pivot = hardwareMap.dcMotor.get("pivot");
         pivot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        pivot.setDirection(DcMotorSimple.Direction.FORWARD);
+        pivot.setDirection(DcMotorSimple.Direction.REVERSE);
         pivot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        pivot.setPower(1);
+        pivot.setPower(.4);
 
         contract = hardwareMap.dcMotor.get("contract");
         contract.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        contract.setDirection(DcMotorSimple.Direction.REVERSE);
-        contract.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        contract.setPower(1);
+        contract.setDirection(DcMotorSimple.Direction.FORWARD);
+        contract.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     void open() {
@@ -68,16 +67,74 @@ class EndEffector {
         pinch.setPosition(.4);
     }
 
-    boolean extend(double inchesPerSecond) {
-        armLength += inchesPerSecond * Bogg.averageClockTime;
-        moveToLength(armLength);
-        return inchesPerSecond == 0;
+    void pickleUp() {
+        swing.setPosition(0);
+    }
+
+    void pickleDown() {
+        swing.setPosition(.5);
+    }
+
+    boolean extend(double power) {
+        if(power == 0)
+            return false;
+        contract.setPower(power);
+        return true;
     }
 
     void moveToLength(double targetLength) {
         armLength = Math.min(targetLength, 28);
-        contract.setTargetPosition((int) Math.round((targetLength - initialArmLength) * smallerInPerTicks));
+        telemetry.addData("arm string length", armLength);
+        contract.setTargetPosition((int) Math.round((targetLength - initialArmLength) * smallerTicksPerIn));
     }
+
+
+    private int initialPivotTicks = 0;
+    void flipUp(double t) {
+        close();
+        if(t < 1){
+            initialPivotTicks = pivot.getCurrentPosition();
+        }
+        if (t > 1) {
+            pivot.setTargetPosition(initialPivotTicks - 1000);
+            pickleUp();
+        }
+    }
+
+    void flipDown(double t) {
+        open();
+        if(t < .5){
+            initialPivotTicks = pivot.getCurrentPosition();
+        }
+        if (t > .5) {
+            pivot.setTargetPosition(initialPivotTicks + 1000);
+        }
+        if (t > 1)
+            pickleDown();
+    }
+
+
+
+
+
+    double getRadius()
+    {
+        return Math.cos(getAngle()) * armLength;
+    }
+
+    double getAngle()
+    {
+        double stringLength = (initialStringLength + pivot.getCurrentPosition() * largerInPerTicks);
+
+        double s = Math.pow(stringLength, 2);
+        double f = Math.pow(stringLength, 4);
+
+        double w = Math.sqrt(-4 * (f - 455 * s + 4675.25));
+        double numerator = .625 * (w - 3.2 *(s - 227.5));
+        double denominator = w + 1.25 * (s - 227.5);
+        return Math.atan2(numerator, denominator);
+    }
+
 
     void moveToPosition(double x, double z) {
         double angle = Math.atan2(z, x);
@@ -95,52 +152,16 @@ class EndEffector {
 
     void pivot(double angle) {
         double stringLength = .707107 * Math.sqrt(-368 * (-.625 * Math.cos(angle) + Math.sin(angle) - 1.23641));
-        pivot.setTargetPosition((int) Math.round((stringLength - initialStringLength) * largerTicksPerIn));
+        telemetry.addData("pivot string length", stringLength);
+        pivot.setTargetPosition((int)( Math.round((stringLength - initialStringLength) * largerTicksPerIn)));
     }
 
-    void flipUp(double t) {
-        close();
-        if (t > 1) {
-            double x = MyMath.closestToZero(sensors.getHighDistance() + 3, 30);
-            double z = 33;
-            moveToPosition(x, z);
-            pickleUp();
-        }
-    }
-
-    void flipDown(double t) {
-        open();
-        if (t > .5) {
-            moveToPosition(armLength, 0);
-        }
-        if (t > 1)
-            pickleDown();
-    }
-
-    void pickleUp() {
-        swing.setPosition(.25);
-    }
-
-    void pickleDown() {
-        swing.setPosition(.65);
-    }
-
-    
-    double getRadius()
+    void floatMotors()
     {
-        return Math.cos(getAngle()) * armLength;
-    }
+        pivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        contract.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
-    double getAngle()
-    {
-        double stringLength = (initialStringLength + pivot.getCurrentPosition()) * largerInPerTicks;
-
-        double s = Math.pow(stringLength, 2);
-        double f = Math.pow(stringLength, 4);
-
-        double w = Math.sqrt(-4 * (f - 455 * s + 4675.25));
-        double numerator = .625 * (w - 3.2 *(s - 227.5));
-        double denominator = w + 1.25 * (s - 227.5);
-        return Math.atan2(numerator, denominator);
+        telemetry.addData("pivot", pivot.getCurrentPosition());
+        telemetry.addData("contract", contract.getCurrentPosition());
     }
 }
