5510c9afa1c8acf2971766f4218cb8f9d95a9c7a
prestonraab <prestonraab@gmail.com>
2019-03-11 19:28:30 -0700

    Smarter way to calibrate motors.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderFineTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderFineTuning.java
@@ -12,8 +12,6 @@ public class encoderFineTuning extends LinearOpMode
     public void runOpMode()
     {
         robot = Bogg.determineRobot(hardwareMap, telemetry);
-        Button a = new Button(gamepad1, Button.Location.A, Button.Type.Increment);
-        Button y = new Button(gamepad1, Button.Location.Y, Button.Type.Increment);
 
         waitForStart();
         double deltaX = 0;
@@ -33,36 +31,15 @@ public class encoderFineTuning extends LinearOpMode
                 deltaY = 36 * Math.signum(gamepad1.left_stick_y);
                 deltaS = 0;
             }
-            if(gamepad1.right_stick_x > .5){
-                deltaX = 0;
-                deltaY = 0;
+            if(gamepad1.right_stick_x > .7){
                 deltaS = Math.PI;
             }
-
-            if(a.count() > 0)
-            {
-                double distance = (double)a.count() - (double)y.count() / 4;
-                double ticks = 0;
-                if(Math.abs(deltaX) > 0) {
-                    double backTicks = robot.driveEngine.motors.get(0).getCurrentPosition();
-                    double rightTicks = robot.driveEngine.motors.get(1).getCurrentPosition();
-                    double leftTicks = robot.driveEngine.motors.get(2).getCurrentPosition();
-                    ticks = backTicks - 2 * rightTicks - 2 * leftTicks;
-                }
-                else if(Math.abs(deltaY) > 0)
-                {
-                    double rightTicks = robot.driveEngine.motors.get(1).getCurrentPosition();
-                    double leftTicks = robot.driveEngine.motors.get(2).getCurrentPosition();
-                    ticks = 2 / Math.sqrt(3)* (rightTicks - leftTicks);
-                }
-                telemetry.addData("Distance Ticks", ticks);
-                telemetry.addData("gamepad Distance", distance);
-
-                if(gamepad1.start)
-                {
-                    DriveEngine.setEffectiveWheelDiameter(distance, ticks);
-                }
+            if(gamepad1.right_stick_x < .7){
+                deltaS = 0;
             }
+
+            DriveEngine.effectiveWheelDiameter += gamepad1.right_stick_y / 3 * Bogg.averageClockTime;
+            telemetry.addData("wheelDiameter", DriveEngine.effectiveWheelDiameter);
 
 
             telemetry.addData("deltaX", deltaX);
@@ -78,11 +55,11 @@ public class encoderFineTuning extends LinearOpMode
 
             if(gamepad1.dpad_right)
             {
-                robot.driveEngine.mP += .005 * Bogg.averageClockTime;
+                robot.driveEngine.mP += .01 * Bogg.averageClockTime;
             }
             else if(gamepad1.dpad_left)
             {
-                robot.driveEngine.mP -= .005 * Bogg.averageClockTime;
+                robot.driveEngine.mP -= .01 * Bogg.averageClockTime;
             }
 
             if(gamepad1.b)
