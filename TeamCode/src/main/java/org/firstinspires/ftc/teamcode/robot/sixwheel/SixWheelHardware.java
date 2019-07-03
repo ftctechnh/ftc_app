afa8e02244743fcd7e716bcdf5d4849baa8ec69d
Gavin Uberti <gavin.uberti@gmail.com>
2019-07-02 17:25:07 -0700

    Add bulk read functionality and logging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/sixwheel/SixWheelHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/sixwheel/SixWheelHardware.java
@@ -1,18 +1,45 @@
 package org.firstinspires.ftc.teamcode.robot.sixwheel;
 
 
+import android.os.Build;
+
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.I2cDevice;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Device;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.common.LoadTimer;
+import org.openftc.revextensions2.ExpansionHubEx;
+import org.openftc.revextensions2.RevBulkData;
+import org.openftc.revextensions2.RevExtensions2;
+
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.*;
 
 public class SixWheelHardware {
+
+    public Telemetry telemetry;
+
+    private Telemetry.Item[] telEncoders;
+    private Telemetry.Item[] telAnalog;
+    private Telemetry.Item telDigital;
+
+    public LynxModule chassisLynxHub;
+    public ExpansionHubEx chassisHub;
+
     public DcMotorEx driveLeft;
     public DcMotorEx driveRight;
-    public DcMotorEx PTOleft;
-    public DcMotorEx PTOright;
+    public DcMotorEx PTOLeft;
+    public DcMotorEx PTORight;
 
     public List<DcMotorEx> chassisMotors;
     public List<DcMotorEx> driveMotors;
@@ -21,22 +48,113 @@ public class SixWheelHardware {
     public List<DcMotorEx> rightChassisMotors;
 
 
-
     public SixWheelHardware(LinearOpMode opMode) {
-        driveLeft = (DcMotorEx) opMode.hardwareMap.dcMotor.get("frontLeft");
-        driveRight = (DcMotorEx) opMode.hardwareMap.dcMotor.get("frontRight");
-        PTOleft = (DcMotorEx) opMode.hardwareMap.dcMotor.get("backLeft");
-        PTOright = (DcMotorEx) opMode.hardwareMap.dcMotor.get("backRight");
+        LoadTimer loadTime = new LoadTimer();
+        RevExtensions2.init();
+
+        driveLeft = opMode.hardwareMap.get(DcMotorEx.class, "frontLeft");
+        driveRight = opMode.hardwareMap.get(DcMotorEx.class, "frontRight");
+        PTOLeft = opMode.hardwareMap.get(DcMotorEx.class, "backLeft");
+        PTORight = opMode.hardwareMap.get(DcMotorEx.class, "backRight");
+
+        chassisLynxHub = opMode.hardwareMap.get(LynxModule.class, "frontHub");
+        chassisHub = opMode.hardwareMap.get(ExpansionHubEx.class, "frontHub");
 
         // Reverse right hand motors
         driveRight.setDirection(DcMotor.Direction.REVERSE);
-        PTOright.setDirection(DcMotor.Direction.REVERSE);
+        PTORight.setDirection(DcMotor.Direction.REVERSE);
 
         // Set up fast access linked lists
-        chassisMotors = Arrays.asList(driveLeft, driveRight, PTOleft, PTOright);
+        chassisMotors = Arrays.asList(driveLeft, driveRight, PTOLeft, PTORight);
         driveMotors = Arrays.asList(driveLeft, driveRight);
-        PTOMotors = Arrays.asList(PTOleft, PTOright);
-        leftChassisMotors = Arrays.asList(driveLeft, PTOleft);
-        rightChassisMotors = Arrays.asList(driveRight, PTOright);
+        PTOMotors = Arrays.asList(PTOLeft, PTORight);
+        leftChassisMotors = Arrays.asList(driveLeft, PTOLeft);
+        rightChassisMotors = Arrays.asList(driveRight, PTORight);
+
+        // Perform calibration
+        LoadTimer calTime = new LoadTimer();
+        calTime.stop();
+
+        // Set up telemetry
+        this.telemetry = opMode.telemetry;
+        initTelemetry();
+        logBootTelemetry(opMode.hardwareMap, loadTime, calTime);
+    }
+
+    private void initTelemetry() {
+        telemetry.setMsTransmissionInterval(50); // Update at 20 Hz
+        telemetry.setAutoClear(false); // Force not to autoclear
+        telemetry.setItemSeparator("; ");
+        telemetry.setCaptionValueSeparator(" ");
+    }
+
+    private void logBootTelemetry(HardwareMap hardwareMap, LoadTimer lT, LoadTimer cT) {
+        Telemetry.Log log = telemetry.log();
+        log.clear();
+        log.setCapacity(6);
+
+        log.add("-- 8802 RC by Gavin Uberti --");
+
+        // Build information
+        Date buildDate = new Date(Build.TIME);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd at HH:mm:ss");
+        String javaVersion = System.getProperty("java.version");
+        log.add("Built " + dateFormat.format(buildDate) + " with Java " + javaVersion);
+
+        // Device information
+        log.add(Build.MANUFACTURER + " " + Build.MODEL + " running Android " + Build.VERSION.SDK_INT);
+
+        // Chassis information
+        String firmware = chassisHub.getFirmwareVersion();
+        int rev = chassisHub.getHardwareRevision();
+        log.add(this.getClass().getSimpleName() + " with hub " + rev + " " + firmware);
+
+        // Robot information
+        List<LynxModule> revHubs = hardwareMap.getAll(LynxModule.class);
+        List<DcMotor> motors = hardwareMap.getAll(DcMotor.class);
+        List<Servo> servos = hardwareMap.getAll(Servo.class);
+        List<DigitalChannel> digital = hardwareMap.getAll(DigitalChannel.class);
+        List<AnalogInput> analog = hardwareMap.getAll(AnalogInput.class);
+        List<I2cDevice> i2c = hardwareMap.getAll(I2cDevice.class);
+        log.add(revHubs.size() + " Hubs; " + motors.size() + " Motors; " + servos.size() +
+                " Servos; " + digital.size() + analog.size() + i2c.size() + " Sensors");
+
+        lT.stop();
+
+        // Load information
+        log.add("Total time " + lT.millis() + " ms; Calibrate time " + cT.millis() + " ms");
+    }
+
+        Telemetry.Line encoderLine = telemetry.addLine();
+        telEncoders = new Telemetry.Item[4];
+        for (int i = 0; i < 4; i++) {
+            telEncoders[i] = encoderLine.addData("E" + i, -1);
+        }
+
+        Telemetry.Line analogLine = telemetry.addLine();
+        telAnalog = new Telemetry.Item[4];
+        for (int i = 0; i < 4; i++) {
+            telAnalog[i] = encoderLine.addData("A" + i, -1);
+        }
+
+        telDigital = telemetry.addLine().addData("DIGITALS", "0 0 0 0 0 0 0 0");
+    }
+
+    public RevBulkData performBulkRead() {
+        RevBulkData data = chassisHub.getBulkInputData();
+
+        for (int i = 0; i < 4; i++) {
+            telEncoders[i].setValue(data.getMotorCurrentPosition(i));
+            telAnalog[i].setValue(data.getAnalogInputValue(i));
+        }
+
+        String digitals = "";
+        for (int i = 0; i < 8; i++) {
+            digitals += (data.getDigitalInputState(i) ? 1 : 0) + " ";
+        }
+        telDigital.setValue(digitals);
+
+        telemetry.update();
+        return data;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/sixwheel/SixWheelTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/sixwheel/SixWheelTeleop.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.robot.sixwheel;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.openftc.revextensions2.RevBulkData;
+
 public class SixWheelTeleop extends LinearOpMode {
 
     SixWheelHardware robot;
@@ -11,13 +13,17 @@ public class SixWheelTeleop extends LinearOpMode {
         robot = new SixWheelHardware(this);
         waitForStart();
 
+        robot.initBulkReadTelemetry();
+
         while (opModeIsActive()) {
+            RevBulkData data = robot.performBulkRead();
+            
             double left = clamp(gamepad1.left_stick_y + gamepad1.right_stick_x);
             double right = clamp(gamepad1.left_stick_y - gamepad1.right_stick_x);
             robot.driveLeft.setPower(left);
             robot.driveRight.setPower(right);
-            robot.PTOleft.setPower(left);
-            robot.PTOright.setPower(right);
+            robot.PTOLeft.setPower(left);
+            robot.PTORight.setPower(right);
         }
     }
 
