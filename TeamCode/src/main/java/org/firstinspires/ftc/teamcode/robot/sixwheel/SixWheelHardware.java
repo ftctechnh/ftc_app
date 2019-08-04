4ac66da906ee91d16bc128e3c15cfa0081327a4d
Gavin Uberti <gavin.uberti@gmail.com>
2019-08-04 16:40:09 -0700

    Add IMU and odometry wheel support to SixWheelHardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/sixwheel/SixWheelHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/sixwheel/SixWheelHardware.java
@@ -3,8 +3,10 @@ package org.firstinspires.ftc.teamcode.robot.sixwheel;
 
 import android.os.Build;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -12,9 +14,14 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.I2cDevice;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Hardware;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.teamcode.BuildConfig;
+import org.firstinspires.ftc.teamcode.autonomous.StandardTrackingWheelLocalizer;
+import org.firstinspires.ftc.teamcode.common.AxesSigns;
+import org.firstinspires.ftc.teamcode.common.BNO055IMUUtil;
 import org.firstinspires.ftc.teamcode.common.LoadTimer;
 import org.openftc.revextensions2.ExpansionHubEx;
 import org.openftc.revextensions2.RevBulkData;
@@ -29,6 +36,7 @@ public class SixWheelHardware {
 
     public Telemetry telemetry;
 
+    private Telemetry.Item[] telOdometry;
     private Telemetry.Item[] telEncoders;
     private Telemetry.Item[] telAnalog;
     private Telemetry.Item telDigital;
@@ -38,6 +46,9 @@ public class SixWheelHardware {
 
     public LynxModule chassisLynxHub;
     public ExpansionHubEx chassisHub;
+    private BNO055IMU imu;
+
+    public StandardTrackingWheelLocalizer localizer;
 
     public DcMotorEx driveLeft;
     public DcMotorEx driveRight;
@@ -51,7 +62,7 @@ public class SixWheelHardware {
     public List<DcMotorEx> rightChassisMotors;
 
 
-    public SixWheelHardware(LinearOpMode opMode) {
+    public SixWheelHardware(OpMode opMode) {
         LoadTimer loadTime = new LoadTimer();
         RevExtensions2.init();
 
@@ -63,7 +74,7 @@ public class SixWheelHardware {
         chassisLynxHub = opMode.hardwareMap.get(LynxModule.class, "chassisHub");
         chassisHub = opMode.hardwareMap.get(ExpansionHubEx.class, "chassisHub");
 
-        // Reverse right hand motors
+        // Reverse left hand motors
         driveLeft.setDirection(DcMotor.Direction.REVERSE);
         PTOLeft.setDirection(DcMotor.Direction.REVERSE);
 
@@ -74,10 +85,16 @@ public class SixWheelHardware {
         leftChassisMotors = Arrays.asList(driveLeft, PTOLeft);
         rightChassisMotors = Arrays.asList(driveRight, PTORight);
 
-
         // Perform calibration
         LoadTimer calTime = new LoadTimer();
+        for (DcMotorEx m : chassisMotors) {
+            m.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            m.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
         calTime.stop();
+
+        // Set up localization with motor names the wheels are connected to
+        localizer = new StandardTrackingWheelLocalizer(0, 1, 2);
 
         // Set up telemetry
         this.telemetry = opMode.telemetry;
@@ -131,7 +148,21 @@ public class SixWheelHardware {
         lastTelemetryUpdate = System.nanoTime();
     }
 
+    public void initBNO055IMU(HardwareMap hardwareMap) {
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        imu.initialize(parameters);
+        BNO055IMUUtil.remapAxes(imu, AxesOrder.XYZ, AxesSigns.NPN);
+    }
+
     public void initBulkReadTelemetry() {
+        Telemetry.Line odometryLine = telemetry.addLine();
+        telOdometry = new Telemetry.Item[4];
+        telOdometry[0] = odometryLine.addData("X", "%.1f", "-1");
+        telOdometry[1] = odometryLine.addData("Y", "%.1f", "-1");
+        telOdometry[2] = odometryLine.addData("θ", "%.3f", "-1");
+
         Telemetry.Line encoderLine = telemetry.addLine();
         telEncoders = new Telemetry.Item[4];
         for (int i = 0; i < 4; i++) {
@@ -155,6 +186,14 @@ public class SixWheelHardware {
     public RevBulkData performBulkRead() {
         RevBulkData data = chassisHub.getBulkInputData();
 
+        // Update localizer
+        localizer.update(data);
+
+        // Adjust telemetry localizer info
+        telOdometry[0].setValue(String.format("%.1f", localizer.x()));
+        telOdometry[1].setValue(String.format("%.1f", localizer.y()));
+        telOdometry[2].setValue(String.format("%.3f", localizer.h()));
+
         // Adjust encoders and analog inputs
         for (int i = 0; i < 4; i++) {
             telEncoders[i].setValue(data.getMotorCurrentPosition(i));
@@ -177,5 +216,12 @@ public class SixWheelHardware {
         telemetry.update();
         lastTelemetryUpdate = System.nanoTime();
         return data;
+    }
+
+    public void setWheelPowers(SixWheelPowers powers) {
+        this.PTOLeft.setPower(powers.left);
+        this.driveLeft.setPower(powers.left);
+        this.driveRight.setPower(powers.right);
+        this.PTORight.setPower(powers.right);
     }
 }
