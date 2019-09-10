e5421abe9bebd28c7048c906d9b1c76e63f073f4
Gavin Uberti <guberti@eastsideprep.org>
2019-09-10 09:30:23 -0700

    Unit test for virtual mecanum hardware

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/simulator/VirtualMecanumHardwareTest.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.simulator;
+
+import org.firstinspires.ftc.teamcode.common.math.MathUtil;
+import org.firstinspires.ftc.teamcode.common.math.Pose;
+import org.firstinspires.ftc.teamcode.robot.mecanum.MecanumUtil;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VirtualMecanumHardwareTest {
+
+    @Test
+    void testVirtualMecanumSquare() {
+        VirtualMecanumHardware virtualRobot = new VirtualMecanumHardware(new Pose(0, 0, 0));
+
+        // Drive top left (+x +y)
+        virtualRobot.setPowers(MecanumUtil.powersFromAngle(-Math.PI/4, 1, 0));
+        virtualRobot.elapse(2000);
+        // Ensure we're in Q1
+        assertTrue(virtualRobot.pose().x > 0);
+        assertTrue(virtualRobot.pose().y > 0);
+        assertTrue(MathUtil.approxEquals(virtualRobot.pose().heading, 0));
+
+        // Drive backwards and ensure we're in Q2
+        virtualRobot.setPowers(MecanumUtil.BACKWARD);
+        virtualRobot.elapse(2000);
+        assertTrue(virtualRobot.pose().x < 0);
+        assertTrue(virtualRobot.pose().y > 0);
+        assertTrue(MathUtil.approxEquals(virtualRobot.pose().heading, 0));
+
+        // Drive right and ensure we're in Q3
+        virtualRobot.setPowers(MecanumUtil.RIGHT);
+        virtualRobot.elapse(2000);
+        assertTrue(virtualRobot.pose().x < 0);
+        assertTrue(virtualRobot.pose().y < 0);
+        assertTrue(MathUtil.approxEquals(virtualRobot.pose().heading, 0));
+
+        // Drive right and ensure we're in Q4
+        virtualRobot.setPowers(MecanumUtil.FORWARD);
+        virtualRobot.elapse(2000);
+        assertTrue(virtualRobot.pose().x > 0);
+        assertTrue(virtualRobot.pose().y < 0);
+        assertTrue(MathUtil.approxEquals(virtualRobot.pose().heading, 0));
+
+        // Drive left and ensure we're in Q1
+        virtualRobot.setPowers(MecanumUtil.LEFT);
+        virtualRobot.elapse(2000);
+        assertTrue(virtualRobot.pose().x > 0);
+        assertTrue(virtualRobot.pose().y > 0);
+        assertTrue(MathUtil.approxEquals(virtualRobot.pose().heading, 0));
+
+        // Drive back right and ensure we're back where we started
+        virtualRobot.setPowers(MecanumUtil.powersFromAngle(-5*Math.PI/4, 1, 0));
+        virtualRobot.elapse(2000);
+        // Ensure we're back at 0, 0 where we started
+        assertTrue(MathUtil.approxEquals(virtualRobot.pose().x, 0));
+        assertTrue(MathUtil.approxEquals(virtualRobot.pose().y, 0));
+        assertTrue(MathUtil.approxEquals(virtualRobot.pose().heading, 0));
+    }
+}
