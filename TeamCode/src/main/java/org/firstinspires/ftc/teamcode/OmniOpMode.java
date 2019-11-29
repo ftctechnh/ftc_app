30c2efc932cfbfb854c6873a1ec70ffde7a682f4
DolphinGui <57329636+DolphinGui@users.noreply.github.com>
2019-11-29 15:17:36 -0700

    Add files via upload
    this is omnidrive mode.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniOpMode.java
@@ -0,0 +1,126 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * This file is used for an omnidrive.
+ */
+
+@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+@Disabled
+
+public class OmniOpMode extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor driveSW = null;
+    private DcMotor driveNE = null;
+    private DcMotor driveSE = null;
+    private DcMotor driveNW = null;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        driveNE  = hardwareMap.get(DcMotor.class, "left_drive");
+        driveSW = hardwareMap.get(DcMotor.class, "right_drive");
+        driveNW = hardwareMap.get(DcMotor.class, "front_drive");
+        driveSE = hardwareMap.get(DcMotor.class, "back_drive");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        driveSW.setDirection(DcMotor.Direction.FORWARD);
+        driveNE.setDirection(DcMotor.Direction.FORWARD);
+        driveNW.setDirection(DcMotor.Direction.FORWARD);
+        driveSE.setDirection(DcMotor.Direction.FORWARD);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+            //double leftPower;
+            //double rightPower;
+
+            // Choose to drive using either Tank Mode, or POV Mode
+            // Comment out the method that's not used.  The default below is POV.
+
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            double driveRht = - ( gamepad1.left_stick_x + gamepad1.right_stick_x ) /2;
+            double driveFwd = - ( gamepad1.left_stick_y + gamepad1.right_stick_y ) / 2;
+            double driveClockwise = - gamepad1.left_stick_x;
+
+            if(gamepad1.left_bumper||gamepad1.right_bumper){                driveNW.setPower(driveClockwise);
+                driveNW.setPower(driveClockwise);
+                driveNE.setPower(driveClockwise);
+                driveSW.setPower(driveClockwise);
+                driveSE.setPower(driveClockwise);
+            }else{
+                driveNW.setPower(driveRht + driveFwd);
+                driveNE.setPower(driveRht - driveFwd);
+                driveSW.setPower(- driveRht + driveFwd);
+                driveSE.setPower(- driveRht - driveFwd);
+            }
+
+
+            // Tank Mode uses one stick to control each wheel.
+            // - This requires no math, but it is hard to drive forward slowly and keep straight.
+            // leftPower  = -gamepad1.left_stick_y ;
+            // rightPower = -gamepad1.right_stick_y ;
+
+            // Send calculated power to wheels
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", driveNW, driveSE);
+            telemetry.update();
+
+        }
+
+
+    }
+
+}
