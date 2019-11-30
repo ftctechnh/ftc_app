f5c1c85a3b80bb4a20e55fe5b5dbeb1158a1a4da
LXing <31298876+FlyOrBoom@users.noreply.github.com>
2019-11-30 11:56:54 -0800

    sleep3 seconds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniOpModeA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniOpModeA.java
@@ -100,6 +100,9 @@ public class OmniOpModeA extends LinearOpMode {
 
     sleep(500);
     
+    
+    sleep(3000);
+    
     driveNW.setPower(0);
     driveNE.setPower(0);
     driveSE.setPower(0);
