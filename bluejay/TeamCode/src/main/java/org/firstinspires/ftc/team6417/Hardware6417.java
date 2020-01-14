aa1eeaccd40542b55bb4939086c32ead614c2da4
cyborg48 <cyborg414781@gmail.com>
2020-01-13 20:25:04 -0500

    Added OpenCV stuff

---
+++ bluejay/TeamCode/src/main/java/org/firstinspires/ftc/team6417/Hardware6417.java
@@ -0,0 +1,270 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.team6417;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.lynx.LynxEmbeddedIMU;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.ServoConfigurationType;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left  drive motor:        "left_drive"
+ * Motor channel:  Right drive motor:        "right_drive"
+ * Motor channel:  Manipulator drive motor:  "left_arm"
+ * Servo channel:  Servo to open left claw:  "left_hand"
+ * Servo channel:  Servo to open right claw: "right_hand"
+ */
+// Based on HardwarePushbot
+public class Hardware6417
+{
+    /* Public OpMode members. */
+    public DcMotor  leftFront = null, rightFront = null, leftBack = null, rightBack = null,
+            armMotor = null, extendMotor = null;
+
+    public Servo grabServo = null, alignServo = null, leftDragServo = null, rightDragServo = null;
+
+    ColorSensor colorSensor;
+
+    public static final double TURN_POWER_LIFT =  0.5 ;
+    public static final int CPR = 1440;
+    public static final double DIAMETER = 3.93701; //inches
+    //public static final int BLOCK_HEIGHT = 127; //millimeters
+
+    public LynxEmbeddedIMU imu;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public Hardware6417(){
+    }
+
+    private void checkOrientation() {
+        // read the orientation of the robot
+        Orientation angles = this.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        this.imu.getPosition();
+        // and save the heading
+        double curHeading = angles.firstAngle;
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and initialize motors
+        leftFront = hwMap.get(DcMotor.class, "FrontLeft");
+        leftBack = hwMap.get(DcMotor.class, "BackLeft");
+        rightFront = hwMap.get(DcMotor.class, "FrontRight");
+        rightBack = hwMap.get(DcMotor.class, "BackRight");
+        armMotor = hwMap.get(DcMotor.class, "ArmMotor");
+        extendMotor = hwMap.get(DcMotor.class, "ExtendMotor");
+
+        // Define and initialize servos
+        alignServo = hwMap.get(Servo.class, "AlignServo");
+        leftDragServo = hwMap.get(Servo.class, "leftDragServo");
+        rightDragServo = hwMap.get(Servo.class, "rightDragServo");
+        grabServo = hwMap.get(Servo.class, "GrabServo");
+
+        colorSensor = hwMap.colorSensor.get("color");
+
+        // Set motor and servo directions based on orientation of motors on robot
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+        armMotor.setDirection(DcMotor.Direction.FORWARD);
+        extendMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        alignServo.setDirection(Servo.Direction.REVERSE);
+        leftDragServo.setDirection(Servo.Direction.FORWARD);
+        rightDragServo.setDirection(Servo.Direction.FORWARD);
+
+        // Set initial servo positions
+        //alignServo.setPosition(1.0);
+        //rightDragServo.setPosition(0);
+        //leftDragServo.setPosition(0);
+
+        // Set all motors to zero power
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+
+        // Set all motors to run with encoders
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        LynxEmbeddedIMU imu = hwMap.get(LynxEmbeddedIMU.class, "imu");
+        BNO055IMU.Parameters params = new BNO055IMU.Parameters();
+        imu.initialize(params);
+
+    }
+
+    public void drivetoPosition(int d, double power){
+
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        int distance = (int)(CPR / (DIAMETER * Math.PI) * d);
+
+        leftFront.setTargetPosition(distance);
+        rightFront.setTargetPosition(distance);
+        leftBack.setTargetPosition(distance);
+        rightBack.setTargetPosition(distance);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftFront.setPower(power);
+        rightFront.setPower(power);
+        leftBack.setPower(power);
+        rightBack.setPower(power);
+
+        while(leftFront.isBusy() || rightFront.isBusy() || leftBack.isBusy() || rightBack.isBusy()){ }
+
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+
+    public void strafeToPosition(int d, double power){
+
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        int distance = (int)(CPR / (DIAMETER * Math.PI) * d);
+
+        leftFront.setTargetPosition(distance);
+        rightFront.setTargetPosition(-distance);
+        leftBack.setTargetPosition(-distance);
+        rightBack.setTargetPosition(distance);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftFront.setPower(power);
+        rightFront.setPower(power);
+        leftBack.setPower(power);
+        rightBack.setPower(power);
+
+        while(leftFront.isBusy() || rightFront.isBusy() || leftBack.isBusy() || rightBack.isBusy()){ }
+
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void strafe(double power){
+
+        // Set all motors to the same power
+        // rightFront and leftBack go in the opposite directions to strafe
+        leftFront.setPower(power);
+        rightFront.setPower(-power);
+        leftBack.setPower(-power);
+        rightBack.setPower(power);
+
+    }
+
+    public void drive(double power){
+
+        // Set all motors to the same power to drive forward
+        leftFront.setPower(power);
+        rightFront.setPower(power);
+        leftBack.setPower(power);
+        rightBack.setPower(power);
+
+    }
+
+    public void stop(){
+
+        // Set all motors to 0 power
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftBack.setPower(0);
+        rightBack.setPower(0);
+
+    }
+
+    public void turnWithEncoder(double input){
+        leftFront.setPower(input);
+        leftBack.setPower(input);
+        rightFront.setPower(-input);
+        rightBack.setPower(-input);
+    }
+
+}
---
+++ bluejay/TeamCode/src/main/java/org/firstinspires/ftc/team6417/irrelevent/AlignFind.java
@@ -0,0 +1,222 @@
+/* Copyright (c) 2019 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.team6417.irrelevent;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.internal.vuforia.VuforiaException;
+import org.firstinspires.ftc.team6417.Hardware6417;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.RobotLog;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+
+
+@Autonomous(name="VueBoi", group ="Linear Opmode")
+
+public class AlignFind extends LinearOpMode {
+
+    private ElapsedTime searchTime = new ElapsedTime(0);
+
+    /* vuforia key */
+    private static final String VUFORIA_KEY =
+            "AarvkvP/////AAABmUGlrY1/5k8ZlPGkwMHuu6wNmXRu7oo06n2KRf3TtubqLyTqBB8UaH9dAba0CjYVCdgFOih0PbY3y5tEmjUIE9HWvfLjG9HeweLBQYq9OBX2KF9PXPJbu64ZNMTOjp2/B3rJEmZd7IBkml4EfSyzaG/J5ESPSKqZMYwRVHNqMR8PhPY26KmGOWFzOsAe474/YAWJEnBgBApocG6jQ9mBjbFp5boU+ZfML3LFa7KhL3liocD8AXO9x6pG32yZ9zmVjjnCRhvVJ4vSfsKbhdhHXl5EoWMra/p+By6mElbeqwdxdfl3AgUKxG1zXyAKVFVHUMJ1pYuooNgvkliHad68CTUW1DbMIcI1frUNqlQxIlkj";
+
+    private static final int skystoneMid = -100; //X positions of skystone positions
+    private static final int skystoneCenter = 100;
+
+    // Class Members
+    private OpenGLMatrix skystonePositionCoords = null;
+    public VuforiaLocalizer vuforia = null;
+
+    private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
+    private static final boolean PHONE_IS_PORTRAIT = true;
+
+    /**
+     * This is the webcam we are to use. As with other hardware devices such as motors and
+     * servos, this device is identified using the robot configuration tool in the FTC application.
+     */
+
+    private boolean targetVisible = false;
+
+    private int skystonePosition = 4;
+
+    Hardware6417 robot = new Hardware6417();
+    BNO055IMU imu;
+    Orientation angles;
+    Acceleration gravity;
+
+    //private WebcamName webcamName = null;
+
+
+    /**
+     * This method initiates vuforia. Make sure to pass a webcam object
+     * <p>
+     * // * @param awebcamName The webcam object
+     */
+
+    public void runOpMode() {
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        telemetry.addLine("initialized camera");
+        telemetry.update();
+
+        //robot.drivetoPosition(20, 0.5);
+
+        blueInit();
+        telemetry.addLine("blueInit");
+        telemetry.update();
+
+        try {
+            skystonePosition = visionTest();
+            telemetry.addData("SkyStone position:", skystonePosition);
+            telemetry.update();
+
+        } catch (Exception e) {
+            telemetry.addLine(e.getMessage());
+            telemetry.update();
+        }
+
+
+        /***
+
+         if(skystonePosition == 0){
+         robot.strafeToPosition(-8, 0.5);
+
+         } else if(skystonePosition == 2){
+         robot.strafeToPosition(8, 0.5);
+         }
+         ***/
+
+
+    }
+
+    public void blueInit() {
+
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         * We can pass Vuforia the handle to a camera preview resource (on the RC phone);
+         * If no camera monitor is desired, use the parameter-less constructor instead (commented out below).
+         */
+
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = CAMERA_CHOICE;
+
+        /*
+         * We also indicate which camera on the RC we wish to use.
+         */
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        float mmPerInch = 25.4f;
+        float mmBotWidth = 18 * mmPerInch;
+        float mmFTCFieldWidth = (12 * 12 - 2) * mmPerInch;
+
+        OpenGLMatrix phoneLocationOnRobot = OpenGLMatrix
+                .translation(mmBotWidth / 2, 0, 0)
+                .multiplied(Orientation.getRotationMatrix(
+                        AxesReference.EXTRINSIC, AxesOrder.YZY,
+                        AngleUnit.DEGREES, -90, 0, 0));
+
+    }
+
+    public int visionTest() throws VuforiaException {
+
+        VuforiaTrackables targetsSkyStone = this.vuforia.loadTrackablesFromAsset("Skystone");
+        VuforiaTrackable stoneTarget = targetsSkyStone.get(0);
+        stoneTarget.setName("Skystone");
+
+        targetsSkyStone.activate();
+
+        boolean noFoundSkystone = true;
+
+        searchTime.reset();
+        while (!isStopRequested()) {
+            //Just  a refresh timer, don't actually need this
+            //if(timer.milliseconds() == 3) continue;
+            boolean stoneVisible = false;
+
+            // check to see if the skystone is visible.
+            if (((VuforiaTrackableDefaultListener) stoneTarget.getListener()).isVisible()) {
+                telemetry.addData("Visible Target", stoneTarget.getName()); //just returns "Stone Target"
+                /*In these coordinates, the X axis goes from the left (negative) to the right (positive).
+                    The Y axis goes up and down on the middle of the screen, and the Z axis goes from the camera outward. */
+
+                //command to get the relative position as provided by vuforia
+                OpenGLMatrix location = ((VuforiaTrackableDefaultListener) stoneTarget.getListener()).getVuforiaCameraFromTarget();
+                if (location != null) {
+                    // Get the positional part of the coordinates
+                    VectorF translation = location.getTranslation();
+                    //clip the actual X to see if it is closer to the left or right
+                    float closestX = Range.clip(translation.get(0), -20f, 20f);
+                    /*"center" because we (my team) only looks at the right two in the farthest set of three in the quarry,
+                    so the leftmost image would be the center of the three stones concerned */
+                    if (closestX == -20) telemetry.addData("Skystone Target:", "Center");
+                    //Right most stone of the two
+                    if (closestX == 20) telemetry.addData("Skystone Target:", "Right");
+                    //Also express the relative pose (for info purposes)
+                    telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
+                            translation.get(0), translation.get(1), translation.get(2));
+                }
+            } else {
+                telemetry.addData( "Visible Target", "none");
+            }
+            telemetry.update();
+            //timer.reset();
+
+            if (skystonePosition != 1 && skystonePosition != 2) skystonePosition = 0;
+
+            // Disable Tracking when we are done;
+            targetsSkyStone.deactivate();
+
+        }
+        return skystonePosition;
+    }
+}
