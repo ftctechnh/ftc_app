0829b12db8cedf2f0947bc62e0ef4a8275da39fa
SarangaR <sarnga.raj@gmail.com>
2020-07-13 11:41:25 -0400

    Deleted Gyro Sample Program and added Encoder Test to Autonomous Program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -7,44 +7,95 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous (name = "AutonomousTest", group = "Tests")
 public class AutonomousTest extends LinearOpMode {
-    //Sets global variables outside of the run opmode method.
-         private static final double CLAW_DOWN_POSITION = 0.5;
+
+        //Sets global variables outside of the run opmode method.
+        static final int MOTOR_TICK_COUNT = 1120;
+        private static final double CLAW_DOWN_POSITION = 0.5;
         private static final double CLAW_UP_POSITION = 0;
+        DcMotor frontLeft = null;
+        DcMotor frontRight = null;
+        DcMotor backLeft = null;
+        DcMotor backRight = null;
+        DcMotor slide = null;
+        Servo claw = null;
 
         @Override
         public void runOpMode() throws InterruptedException {
 
-//Initializes motors/servos and maps them to the hardware device in the DriverStation Config.
-            DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
-            DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
-            DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
-            DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+            //Maps Motors and Servos to the hardware device in the DriverStation Config.
+            frontLeft = hardwareMap.dcMotor.get("frontLeft");
+            frontRight = hardwareMap.dcMotor.get("frontRight");
+            backLeft = hardwareMap.dcMotor.get("backLeft");
+            backRight = hardwareMap.dcMotor.get("backRight");
 
-            DcMotor slide = hardwareMap.dcMotor.get("slide");
+            slide = hardwareMap.dcMotor.get("slide");
 
-            Servo claw = hardwareMap.servo.get("claw");
+            claw = hardwareMap.servo.get("claw");
 
-//Reverses the left motors so the robot can move forward on positive power.
+            //set the motors to RUN_USING_ENCODER.
+            frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            //Reverses the left motors so the robot can move forward on positive power.
             frontLeft.setDirection(DcMotor.Direction.REVERSE);
             backLeft.setDirection(DcMotor.Direction.REVERSE);
 
+            //Wait for the Start Button to be pressed.
             waitForStart();
 
-//Goes forward for 500 milliseconds.
-            frontLeft.setPower(1);
-            frontRight.setPower(1);
-            backLeft.setPower(1);
-            backRight.setPower(1);
+            DriveForwardInches(10,.5);
 
-            sleep(500);
+        }
+        public void DriveForwardInches(int inches, int power) {
+            //Reset Encoder values.
+            frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-//Stops
-            frontRight.setPower(0);
+            //Calculates the circumference of the wheel.
+            double circumference = 3.14*0.00;
+            //Calculates the number rotations the wheels needs to make to drive the specified number inches.
+            double rotationsNeeded = inches/circumference;
+            //Convert the double value into an integer tick value to feed into the motor position.
+            int encoderDrivingTarget = (int)(rotationsNeeded*1120);
+
+            //Set the motor target positions to the tick value just calculated.
+            frontLeft.setTargetPosition(encoderDrivingTarget);
+            frontRight.setTargetPosition(encoderDrivingTarget);
+            backLeft.setTargetPosition(encoderDrivingTarget);
+            backRight.setTargetPosition(encoderDrivingTarget);
+
+            //Set motor powers.
+            frontLeft.setPower(power);
+            frontRight.setPower(power);
+            backLeft.setPower(power);
+            backRight.setPower(power);
+
+            //Set motor modes to Run to the encoderDrivingTarget.
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            //Do nothing until motors are finished driving.
+            while (frontLeft.isBusy() || frontRight.isBusy() || backLeft.isBusy() || backRight.isBusy() ) {
+                //Indicate that motors are driving in telemetry.
+                telemetry.addData("Status", "Driving");
+                telemetry.update();
+            }
+
+            //Stop motors after driving is finished.
             frontLeft.setPower(0);
-            backRight.setPower(0);
+            frontRight.setPower(0);
             backLeft.setPower(0);
+            backRight.setPower(0);
 
-            idle();
+            //Indicate that the motors are stopped and driving is complete in telemetry.
+            telemetry.addData("Status", "Driving Complete");
+            telemetry.update();
 
         }
     }
