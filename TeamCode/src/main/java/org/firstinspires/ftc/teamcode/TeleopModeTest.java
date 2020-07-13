60144414a6704485894646b1b8d80d06f0aee668
Keshav Varadarajan <kvthevk@gmail.com>
2020-07-13 16:00:45 -0400

    Adding Gamepad 2 to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopModeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopModeTest.java
@@ -27,10 +27,10 @@ public class TeleopModeTest extends LinearOpMode {
 
     //Method sets motor powers to make the robot strafe left or right based on sign of input power.
     public void Strafe(int power) {
-        frontLeft.setPower(power);
-        frontRight.setPower(-power);
-        backLeft.setPower(-power);
-        backRight.setPower(power);
+        frontLeft.setPower(-power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(-power);
     }
     //Method sets all motors to 0 (makes robot stop).
     public void Stop() {
@@ -53,43 +53,47 @@ public class TeleopModeTest extends LinearOpMode {
 
         claw = hardwareMap.servo.get("claw");
 //Reverses left side motors to make the robot go forward on positive power.
-        frontLeft.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
 
         while (opModeIsActive()) {
 //Sets the motor powers to the inputs given by the gamepad's analog sticks (gamepad y-values are inverted).
-            frontLeft.setPower(-gamepad1.left_stick_y);
-            backLeft.setPower(-gamepad1.left_stick_y);
-            frontRight.setPower(-gamepad1.right_stick_y);
-            backRight.setPower(-gamepad1.right_stick_y);
+            frontLeft.setPower(gamepad1.left_stick_y);
+            backLeft.setPower(gamepad1.left_stick_y);
+            frontRight.setPower(gamepad1.right_stick_y);
+            backRight.setPower(gamepad1.right_stick_y);
 
             Slide.setPower(gamepad2.right_stick_y);
 //Uses gamepad button/bumper inputs to set servo positions.
-            if (gamepad1.b) {
-                claw.setPosition(CLAW_DOWN_POSITION);
-            }
-            else if (gamepad1.a) {
-                claw.setPosition(CLAW_UP_POSITION);
-            }
-            else if (gamepad1.right_bumper) {
+
+            if (gamepad1.right_bumper) {
                 Strafe(1);
             }
             else if (gamepad1.left_bumper) {
                 Strafe(-1);
             }
+//Stops the motors so that the robot only moves while the buttons are being pressed.
+            else {
+                Stop();
+            }
 //Allows the driver to drive slower for more precise movements when needed (by pressing the right trigger).
-            else if (gamepad1.right_trigger <= 0.5){
+            if (gamepad1.right_trigger <= 0.5){
                 frontLeft.setPower(-gamepad1.left_stick_y / 2);
                 backLeft.setPower(-gamepad1.left_stick_y / 2);
                 frontRight.setPower(-gamepad1.right_stick_y / 2);
                 backRight.setPower(-gamepad1.right_stick_y / 2);
             }
-//Stops the motors so that the robot only moves while the buttons are being pressed.
-            else {
-                Stop();
+
+
+            if (gamepad2.b) {
+                claw.setPosition(CLAW_DOWN_POSITION);
             }
+            else if (gamepad2.a) {
+                claw.setPosition(CLAW_UP_POSITION);
+            }
+
 //Gives the hardware a small amount of time to catch up before looping again.
             idle();
         }
